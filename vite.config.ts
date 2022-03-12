eb610a2c8fb5f987840cfcde5c54cf4b1145daa3
MasamiOgasawara <m-ogasawara@wely.tech>
2022-03-12 17:29:03 +0900

    setting: load a tool to analyze bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-export default defineConfig({
-  base: './', // Setting relative path
-  css: { postcss: { plugins: [require('autoprefixer')] } },
-  plugins: [legacy({ targets: ['defaults', 'not IE 11'] })],
-  server: { port: 1759 },
+export default defineConfig(({ mode }) => {
+  return {
+    base: './', // Setting relative path
+    build: {
+      rollupOptions: {
+        plugins: [
+          mode === 'analyze' &&
+            visualizer({
+              open: true,
+              filename: 'dist/analytics.html',
+              gzipSize: true, // The method to compress files
+              brotliSize: true, // The compression algorithm
+            }),
+        ],
+      },
+    },
+    css: { postcss: { plugins: [require('autoprefixer')] } },
+    plugins: [legacy({ targets: ['defaults', 'not IE 11'] })],
+    server: { port: 1759 },
+  }
 })
