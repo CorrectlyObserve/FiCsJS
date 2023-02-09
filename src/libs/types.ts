db333e6c861636406c5eab1d05acfcf59289c68c
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-02-09 18:30:28 +0700

    debug: fix type, Args

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -4,6 +4,6 @@ export interface Args {
   name: string
   parent: string
   html: string
-  css: string
-  events: { [key: string]: () => void }
+  css?: string
+  events?: { [key: string]: () => void }
 }
