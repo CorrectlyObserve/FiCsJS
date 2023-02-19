69d58a6911d0d8b69d2cca64b0be1a83060f6307
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-02-19 22:22:18 +0700

    refactoring: change type of html to the function return string

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -2,7 +2,7 @@ export type Branch<T> = T | string | (() => void)
 
 export interface Arg {
   name: string
-  html: string
+  html: () => string
   className?: string
   css?: string
   events?: { [key: string]: () => void }
