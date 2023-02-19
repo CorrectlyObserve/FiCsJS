9c6ffd09b963c89e20385620bc3dd35c9dd2c1df
MasamiOgasawara <m-ogasawara@wely.tech>
2023-02-19 22:22:18 +0700

    refactoring: change type of html to the function return string

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -2,7 +2,7 @@ export type Branch<T> = T | string | (() => void)
 
 export interface WelifyArg {
   name: string
-  html: string
+  html: () => string
   className?: string
   css?: string
   events?: { [key: string]: () => void }
