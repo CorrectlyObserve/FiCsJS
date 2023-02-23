fa01866a43ca677e1f70cd8278b1ea5a8017f683
MasamiOgasawara <m-ogasawara@wely.tech>
2023-02-23 22:42:31 +0700

    feature: create new types

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,9 +1,19 @@
 export type Branch<T> = T | string | (() => void)
 
-export interface WelifyArg {
+export interface WelifyArgs {
   name: string
   html: () => string
   className?: string
   css?: string
   events?: { [key: string]: () => void }
+}
+
+export interface welifyIfArgs {}
+
+export interface welifyEachArgs {}
+
+export interface welifySlotArgs {
+  slotId?: string
+  html: () => string
+  css?: string
 }
