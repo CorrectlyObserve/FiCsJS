8f02f59fbdc3a4e0bbed3bace897de3ea69e1d24
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-03-01 12:33:46 +0700

    refactoring: change class arg to array

--- src/libs/wely.ts
+++ src/libs/wely.ts
@@ -7,7 +7,7 @@ export class Element extends HTMLElement {
   private isInitial: boolean = false
   name: string = ''
   html: () => string = () => ''
-  class?: string
+  classes: string[] = []
   css?: string
   events: { [key: string]: () => void } = {}
 
@@ -78,10 +78,14 @@ export class Element extends HTMLElement {
         )
       }
 
-      this.setAttribute(
-        'class',
-        toKebabCase(this.class ? `${this.name} ${this.class}` : this.name)
+      this.classes.forEach((className) =>
+        this.setAttribute('class', toKebabCase(className))
       )
+
+      // this.setAttribute(
+      //   'class',
+      //   toKebabCase(this.class ? `${this.name} ${this.class}` : this.name)
+      // )
 
       this.isInitial = true
     }
