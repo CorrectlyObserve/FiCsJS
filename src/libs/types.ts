ec363b477b68fe63fd1d5e7e2e423c78aaf14f28
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-03-10 00:12:04 +0700

    feature: edit types for

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,14 +1,27 @@
 export type Branch<T> = T | string | (() => void)
 
-export interface Args {
+interface commonArgs {
   name: string
-  html: () => string
   className?: string
   css?: string
   slot?: string
   events?: { [key: string]: () => void }
 }
 
-export interface IfArgs {}
+interface normalArgs extends commonArgs {
+  html: () => string
+  syntax?: undefined
+}
 
-export interface EachArgs {}
+interface ifArgs extends commonArgs {
+  html: () => string
+  syntax: 'if'
+}
+
+interface eachArgs<T> extends commonArgs {
+  html: Array<T>
+  display: (arg: T) => string
+  syntax: 'each'
+}
+
+export type Args<T> = normalArgs | ifArgs | eachArgs<T>
