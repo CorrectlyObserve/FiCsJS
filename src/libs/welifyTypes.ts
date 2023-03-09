a21e12b2212eb9894c1a633c0cec36d79a75f292
MasamiOgasawara <m-ogasawara@wely.tech>
2023-03-10 00:29:37 +0700

    refactoring: rename a file name and simplify type name

--- src/libs/welifyTypes.ts
+++ src/libs/welifyTypes.ts
@@ -1,27 +1,25 @@
-export type Branch<T> = T | string | (() => void)
-
 interface commonArgs {
   name: string
   className?: string
   css?: string
-  slot?: string
   events?: { [key: string]: () => void }
 }
 
 interface normalArgs extends commonArgs {
-  html: () => string
   syntax?: undefined
+  html: () => string
+  slot?: string
 }
 
 interface ifArgs extends commonArgs {
-  html: () => string
   syntax: 'if'
+  html: () => string
 }
 
 interface eachArgs<T> extends commonArgs {
+  syntax: 'each'
   html: Array<T>
   display: (arg: T) => string
-  syntax: 'each'
 }
 
 export type WelifyArgs<T> = normalArgs | ifArgs | eachArgs<T>
