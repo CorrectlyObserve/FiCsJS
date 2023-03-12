722d6a15ed853b9ba6fd95eff2541dbcab0195a1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-03-12 16:39:51 +0700

    refactoring: delete unncessary function

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -6,22 +6,6 @@ export const getChildNodes = (element: string): Array<ChildNode> =>
     new DOMParser().parseFromString(element, 'text/html').body.childNodes
   )
 
-export const cloneNode = (shadowRoot: ShadowRoot, element: string): void => {
-  for (const child of getChildNodes(element)) {
-    shadowRoot.appendChild(child.cloneNode(true))
-  }
-}
-
-export const manageError = (error: Error | string | unknown): void => {
-  if (error instanceof Error) {
-    throw Error(error.message)
-  } else if (typeof error === 'string') {
-    throw Error(error)
-  } else {
-    throw Error('unexpected error...')
-  }
-}
-
 export const toKebabCase = (str: string): string => {
   const newStr = str.slice(1)
   const upperCase = new RegExp(/[A-Z]/g)
