022196fbe21290c160ffb8e8bc3e32c949ca706b
MasamiOgasawara <m-ogasawara@wely.tech>
2023-03-15 23:28:22 +0700

    refactoring: make a function easy to understand

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -9,16 +9,11 @@ export const getChildNodes = (element: string): ChildNode[] =>
   )
 
 export const toKebabCase = (str: string): string => {
-  const head = str[0].toLowerCase()
   const newStr = str.slice(1)
   const upperCase = new RegExp(/[A-Z]/g)
+  const body = upperCase.test(newStr)
+    ? newStr.replace(upperCase, (val) => `-${val.toLowerCase()}`)
+    : newStr
 
-  return (
-    head +
-    `${
-      upperCase.test(newStr)
-        ? newStr.replace(upperCase, (val) => `-${val.toLowerCase()}`)
-        : newStr
-    }`
-  )
+  return str[0].toLowerCase() + body
 }
