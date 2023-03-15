00ab407a3226eb3dfb853d8dce6db83582dee438
MasamiOgasawara <m-ogasawara@wely.tech>
2023-03-15 23:29:04 +0700

    refactoring: simplify types of Welify arg

--- src/libs/welifyTypes.ts
+++ src/libs/welifyTypes.ts
@@ -1,16 +1,35 @@
-export type Type<T> = T | (() => T)
-
-export interface IfHtml {
-  judge: Type<boolean> | unknown
-  render: Type<string>
+export interface Each<T> {
+  contents: Type<T[]>
+  render: (arg: T) => string | undefined
 }
+
+export interface EachIf<T> {
+  contents: Type<T[]>
+  branches: Type<
+    {
+      judge: (arg: T) => boolean
+      render: (arg: T) => string
+    }[]
+  >
+  fallback?: (arg: T) => string
+}
+
+export interface If {
+  branches: Type<
+    {
+      judge: Type<boolean> | unknown
+      render: Type<string>
+    }[]
+  >
+  fallback?: Type<string>
+}
+
+export type Type<T> = T | (() => T)
 
 export interface Welify<T> {
   name: string
   className?: string
-  html: Type<string> | Type<T[]> | Type<IfHtml[]>
-  render?: Type<IfHtml[]> | ((arg: T) => string | undefined)
-  fallback?: Type<string>
+  html: Type<string> | Type<Each<T>> | Type<EachIf<T>> | Type<If>
   css?: string
   slot?: string
   events?: { [key: string]: () => void }
