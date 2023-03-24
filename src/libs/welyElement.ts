38a0a0d0949e80778e8f53cca56d668ac202221f
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-03-24 19:45:27 +0700

    feature: add a delegatedEvents property

--- src/libs/welyElement.ts
+++ src/libs/welyElement.ts
@@ -1,20 +1,21 @@
 import { createUniqueId } from './generator'
 import { getChildNodes } from './utils'
-import { Data } from './Types'
+import { Data, DelegatedEvents, EventListener, Events } from './Types'
 
-export class Element<U> extends HTMLElement {
-  Id: string = ''
+export class Element<T> extends HTMLElement {
   readonly shadowRoot!: ShadowRoot
+  private Id: string = ''
   private isInitial: boolean = false
+  private templateId: string
+  private template: HTMLTemplateElement = document.createElement('template')
   name: string = ''
-  data: Data<U> = {}
+  data: Data<T> = {}
   html: string = ''
   classes: string[] = []
   css?: string
   slotContent?: string
-  events: {
-    [key: string]: (data: Data<U>) => void
-  } = {}
+  events: Events<T> = {}
+  delegatedEvents: DelegatedEvents<T> = []
 
   constructor() {
     super()
@@ -22,9 +23,17 @@ export class Element<U> extends HTMLElement {
 
     this.Id = createUniqueId()
     this.setAttribute('id', this.Id)
+
+    this.templateId = `${this.Id}-template`
+    this.template.setAttribute('id', this.templateId)
+    this.template.setAttribute('style', 'display:block')
   }
 
   connectedCallback(): void {
+    if (this.html !== '')
+      for (const child of getChildNodes(this.html))
+        this.template.appendChild(child.cloneNode(true))
+
     if (!this.isInitial) {
       this.setAttribute('class', this.classes.join(' '))
 
@@ -38,18 +47,41 @@ export class Element<U> extends HTMLElement {
         this.insertAdjacentHTML('beforeend', this.slotContent)
 
       const  = document.getElementById(this.Id)
-      const keys = Object.keys(this.events)
 
-      if ( && keys.length > 0)
-        keys.forEach((handler: string) =>
-          .addEventListener(handler, () => this.events[handler](this.data))
-        )
+      if () {
+        if (Object.keys(this.events).length > 0)
+          Object.keys(this.events).forEach((listener: string) =>
+            .addEventListener(listener, () =>
+              this.events[listener](this.data)
+            )
+          )
+
+        if (this.delegatedEvents.length > 0)
+          for (const event of this.delegatedEvents) {
+            if (event.selector === '') break
+
+            const keys = new Set(Object.keys(event))
+            keys.delete('selector')
+            const key = Array.from(keys)[0]
+
+            const targets = Array.from(
+              this.template.querySelectorAll(
+                `#${this.templateId} > ${event.selector}`
+              )
+            )
+
+            const listener = <EventListener<T>>event[key]
+
+            if (targets.length > 0)
+              targets.forEach((target) =>
+                target.addEventListener(key, () => listener(this.data))
+              )
+          }
+      }
 
       this.isInitial = true
     }
 
-    if (this.html !== '')
-      for (const child of getChildNodes(this.html))
-        this.shadowRoot.appendChild(child.cloneNode(true))
+    this.shadowRoot.appendChild(this.template)
   }
 }
