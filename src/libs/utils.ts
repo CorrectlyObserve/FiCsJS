4c563435fa694ebc3384bcf0524e510086f6bf2b
MasamiOgasawara <m-ogasawara@wely.tech>
2023-04-09 13:08:51 +0700

    refactoring: integrate to a new function

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,3 +1,17 @@
+export const appendChild = (
+  parent: HTMLElement | ShadowRoot,
+  children: string
+): void => {
+  const fragment = document.createDocumentFragment()
+  const childNodes: ChildNode[] = Array.from(
+    new DOMParser().parseFromString(children, 'text/html').body.childNodes
+  )
+
+  for (const child of childNodes) fragment.appendChild(child.cloneNode(true))
+
+  parent?.appendChild(fragment)
+}
+
 export const convertType = <T, U>(html: any, data: { [key: string]: T }) =>
   typeof html === 'function' ? <U>html(data) : <U>html
 
@@ -5,20 +19,15 @@ export const delay = async (
   time: number,
   callback: () => void
 ): Promise<void> => {
-  await new Promise<void>((resolve) => setTimeout(resolve, time))
+  await new Promise<void>(resolve => setTimeout(resolve, time))
   callback()
 }
-
-export const getChildNodes = (element: string): ChildNode[] =>
-  Array.from(
-    new DOMParser().parseFromString(element, 'text/html').body.childNodes
-  )
 
 export const toKebabCase = (str: string): string => {
   const newStr = str.slice(1)
   const upperCase = new RegExp(/[A-Z]/g)
   const body = upperCase.test(newStr)
-    ? newStr.replace(upperCase, (val) => `-${val.toLowerCase()}`)
+    ? newStr.replace(upperCase, val => `-${val.toLowerCase()}`)
     : newStr
 
   return str[0].toLowerCase() + body
