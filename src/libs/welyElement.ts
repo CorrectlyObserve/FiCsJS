042c3d7a71a738e9c5540d1703d867d5eb2df04d
MasamiOgasawara <m-ogasawara@wely.tech>
2023-04-20 12:01:14 +0700

    feature: reflect change of wely type

--- src/libs/welyElement.ts
+++ src/libs/welyElement.ts
@@ -1,16 +1,15 @@
 import { createUniqueId } from './generator'
 import { appendChild, toKebabCase } from './utils'
-import { Css, DelegatedEvents, Events, PropsStack } from './welifyTypes'
-
-let propsStack: PropsStack<unknown> = []
+import { Css, DelegatedEvents, Events, Values } from './welifyTypes'
 
 export class WelyElement<T> extends HTMLElement {
   readonly shadowRoot!: ShadowRoot
-  private _id: string = ''
+  readonly welyId: string = ''
   private _isInitialized: boolean = false
   name: string = ''
-  parents: string[] = []
+  descendants: WelyElement<T>[] = []
   data: T = <T>{}
+  props: T = <T>{}
   html: string = ''
   classes: string[] = []
   css?: string | Css<T>
@@ -23,36 +22,12 @@ export class WelyElement<T> extends HTMLElement {
     super()
     this.shadowRoot = this.attachShadow({ mode: 'open' })
 
-    this._id = createUniqueId()
-    this.setAttribute('id', this._id)
+    this.welyId = createUniqueId()
+    this.setAttribute('id', this.welyId)
   }
 
   connectedCallback(): void {
     if (this.html !== '') appendChild(this.shadowRoot, this.html)
-
-    for (let i = propsStack.length - 1; i >= 0; i--) {
-      if (
-        !this.parents.includes(propsStack[i].name) ||
-        this.closest(`#${propsStack[i].id}`) === null
-      )
-        break
-
-      console.log({ id: this._id, name: this.name, props: {} })
-    }
-
-    propsStack.push({ id: this._id, name: this.name, props: {} })
-
-    // 名前で
-    // const localParents = this.parents.filter(parent => this.closest()
-
-    // for (let i = propsStack.length - 1; i >= 0; i--) {
-    //   if (this.closest(`#${propsStack[i].id}`)) break
-
-    //   propsStack.pop()
-    // }
-
-    // console.log(this._id, propsStack)
-    // propsStack[this.name] = { id: this._id, data: this.data }
 
     if (this._isInitialized) return
 
@@ -66,7 +41,7 @@ export class WelyElement<T> extends HTMLElement {
         const styles = this.css.map(obj => {
           if (obj.selector === '') return ''
 
-          const style = Object.entries(obj.style(this.data))
+          const style = Object.entries(obj.style({ data: { ...this.data } }))
             .map(([key, value]) => `${toKebabCase(key)}: ${value};`)
             .join('\n')
 
@@ -77,42 +52,19 @@ export class WelyElement<T> extends HTMLElement {
       }
 
       this.shadowRoot.appendChild(css)
-
-      let startTime, endTime
-
-      startTime = performance.now()
-      if (typeof this.css !== 'string') {
-        for (let i = 0; i < 10000; i++) {
-          const styles = this.css.map(obj => {
-            if (obj.selector === '') return ''
-
-            const style = Object.entries(obj.style(this.data))
-              .map(([key, value]) => `${toKebabCase(key)}: ${value};`)
-              .join('\n')
-
-            return `${obj.selector} {${style}}`
-          })
-
-          css.textContent = styles.join('')
-        }
-      }
-
-      endTime = performance.now()
-      console.log('css-in-jsの処理時間:', endTime - startTime)
     }
 
     if (this.slotContent) appendChild(this, this.slotContent)
 
-    const wely = document.getElementById(this._id)
-    if (wely) {
+    if (this) {
       const keys = Object.keys(this.events)
 
       if (keys.length > 0)
         for (const listener of keys) {
           const eventListener = (event: Event) =>
-            this.events[listener]({ ...this.data }, event)
+            this.events[listener]({ data: { ...this.data } }, event)
 
-          wely.addEventListener(listener, eventListener)
+          this.addEventListener(listener, eventListener)
         }
 
       if (this.delegatedEvents.length > 0)
@@ -121,21 +73,11 @@ export class WelyElement<T> extends HTMLElement {
 
           const { selector, ...event } = delegatedEvent
           const key = Object.keys(event)[0]
-
-          let startTime, endTime
-
-          startTime = performance.now()
-          for (let i = 0; i < 10000; i++) {
-            Array.from(this.shadowRoot.querySelectorAll(`:host > ${selector}`))
-          }
-          endTime = performance.now()
-          console.log('delegatedEventsの処理時間:', endTime - startTime)
-
           const targets = Array.from(
             this.shadowRoot.querySelectorAll(`:host > ${selector}`)
           )
           const listener = event[key] as (
-            data: T,
+            values: Values<T>,
             event: Event,
             index?: number
           ) => void
@@ -144,7 +86,7 @@ export class WelyElement<T> extends HTMLElement {
             for (let i = 0; i < targets.length; i++)
               targets[i].addEventListener(key, (localEvent: Event) =>
                 listener(
-                  { ...this.data },
+                  { data: { ...this.data } },
                   localEvent,
                   this.isEach ? i : undefined
                 )
