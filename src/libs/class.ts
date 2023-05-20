ce83b9971734b8c072168f4006f80fcea665007a
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-05-20 16:57:35 +0700

    refactoring: reflect integrating a file and fix inheritances

--- src/libs/class.ts
+++ src/libs/class.ts
@@ -1,5 +1,3 @@
-import { createUniqueId } from '@/libs/generator'
-import { appendChild, toKebabCase } from '@/libs/utils'
 import {
   Args,
   Css,
@@ -8,10 +6,21 @@ import {
   Html,
   Inheritances
 } from '@/libs/types'
+import { appendChild, toKebabCase } from '@/libs/utils'
+
+const generate = function* (): Generator<number> {
+  let i = 1
+
+  while (true) {
+    yield i
+    i++
+  }
+}
+
+const generated: Generator<number> = generate()
 
 export class <D, P> extends HTMLElement {
   readonly shadowRoot!: ShadowRoot
-  private _Id: string = ''
   private _isInitialized: boolean = false
   private _inheritedSet: Set<string> = new Set()
 
@@ -30,7 +39,7 @@ export class <D, P> extends HTMLElement {
   constructor() {
     super()
     this.shadowRoot = this.attachShadow({ mode: 'open' })
-    this._Id = createUniqueId()
+    this.setAttribute('id', `welified-id${generated.next().value}`)
   }
 
   connectedCallback(): void {
@@ -42,9 +51,7 @@ export class <D, P> extends HTMLElement {
       this.inheritances.forEach(inheritance => {
         const { elements, props } = inheritance
 
-        for (let element of elements as <D, P>[]) {
-          element.setAttribute('id', this._Id)
-
+        for (let element of elements as <D, P>[])
           if (
             this._inheritedSet.has(element.id) ||
             this.shadowRoot.querySelector(`#${element.id}`)
@@ -58,9 +65,6 @@ export class <D, P> extends HTMLElement {
             if (!this._inheritedSet.has(element.id))
               this._inheritedSet.add(element.id)
           } else this._inheritedSet.delete(element.id)
-
-          element.removeAttribute('id')
-        }
       })
 
     this.setAttribute('class', this.classes.join(' '))
