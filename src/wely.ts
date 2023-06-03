a2c3cf72bd457ffdb0ef2a7fddfdda603ac71333
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-06-03 21:55:16 +0700

    refactoring: edit define function

--- src/wely.ts
+++ src/wely.ts
@@ -1,9 +1,9 @@
 import {  } from '@/libs/class'
 import { Each, EachIf, Html, If,  } from '@/libs/types'
 import { appendChild, convertToArray, toKebabCase } from '@/libs/utils'
-// import cssUrl from '@/style.css?url'
+import cssUrl from './style.css?url'
 
-export const define = <T, D, P>({
+const define = <T, D, P>({
   name,
   data,
   props,
@@ -14,76 +14,72 @@ export const define = <T, D, P>({
   slot,
   events
 }: <T, D, P>): HTMLElement => {
-  if (name === '' || name === undefined)
-    throw new Error('The name argument is not defined...')
-  else {
-    const kebabName = toKebabCase(name)
-    const Name = `w-${kebabName}`
+  const kebabName = toKebabCase(name)
+  const Name = `w-${kebabName}`
+  const instantiate = (name: string): void =>
+    customElements.define(name, <D, P>)
 
-    customElements.define(
-      Name,
-      class extends <D, P> {
-        constructor() {
-          super()
-          this.name = kebabName
+  if (!customElements.get(Name)) instantiate(Name)
 
-          if (data) this.data = { ...data }
-          if (props) this.props = { ...props }
-          if (inheritances) this.inheritances = [...inheritances]
+  const  = <<D, P>>document.createElement(Name)
 
-          this.classes.push(kebabName)
+  if (data) .data = <D>{ ...data }
+  if (props) .props = <P>{ ...props }
+  if (inheritances) .inheritances = [...inheritances]
 
-          if (className)
-            for (const localName of className.split(' '))
-              this.classes.push(toKebabCase(localName))
+  .classes.push(kebabName)
+  if (className)
+    for (const localName of className.split(' '))
+      .classes.push(toKebabCase(localName))
 
-          let converter =
-            typeof html === 'function'
-              ? html({ data: { ...this.data }, props: { ...this.props } })
-              : html
+  let converter =
+    typeof html === 'function'
+      ? html({ data: { ....data }, props: { ....props } })
+      : html
 
-          if ('contents' in <Each<T> | EachIf<T>>converter) {
-            this.isEach = true
+  if ('contents' in <Each<T> | EachIf<T>>converter) {
+    .isEach = true
 
-            if ('branches' in <EachIf<T>>converter)
-              (<EachIf<T>>converter).contents.forEach((content, index) => {
-                for (const branch of (<EachIf<T>>converter).branches)
-                  if (branch.judge(content))
-                    this.html.push(branch.render(content, index))
+    if ('branches' in <EachIf<T>>converter)
+      (<EachIf<T>>converter).contents.forEach((content, index) => {
+        for (const branch of (<EachIf<T>>converter).branches)
+          if (branch.judge(content))
+            .html.push(branch.render(content, index))
 
-                const fallback = (<EachIf<T>>converter)?.fallback
+        const fallback = (<EachIf<T>>converter)?.fallback
 
-                if (fallback !== undefined)
-                  this.html.push(fallback(content, index))
-              })
-            else
-              (<Each<T>>converter).contents.forEach((content, index) =>
-                this.html.push(
-                  (<Each<T>>converter).render(content, index) ?? ''
-                )
-              )
-          } else if ('branches' in <If>converter) {
-            converter = <If>converter
+        if (fallback !== undefined) .html.push(fallback(content, index))
+      })
+    else
+      (<Each<T>>converter).contents.forEach((content, index) =>
+        .html.push((<Each<T>>converter).render(content, index) ?? '')
+      )
+  } else if ('branches' in <If>converter) {
+    converter = <If>converter
 
-            for (const branch of converter.branches)
-              if (branch.judge) {
-                this.html.push(branch.render)
-                break
-              }
-
-            if (this.html.length === 0 && converter.fallback)
-              this.html.push(converter.fallback)
-          } else this.html = convertToArray(<Html | Html[]>converter)
-
-          this.css = [...(css ?? [])]
-          if (slot) this.slotContent = slot
-          this.events = [...(events ?? [])]
-        }
+    for (const branch of converter.branches)
+      if (branch.judge) {
+        .html.push(branch.render)
+        break
       }
-    )
 
-    return new (customElements.get(Name) as { new (): <D, P> })()
-  }
+    if (.html.length === 0 && converter.fallback)
+      .html.push(converter.fallback)
+  } else .html = convertToArray(<Html | Html[]>converter)
+
+  if (css) .css = [...css]
+  if (slot) .slotContent = slot
+  if (events) .events = [...events]
+
+  return 
+}
+
+interface Data {
+  count: number
+  message: string
+  color: string
+  back: string
+  childMessage: string
 }
 
 interface Props {
@@ -104,14 +100,14 @@ const child = define({
     data: { childMessage },
     props: { color }
   }: {
-    data: { childMessage: string }
+    data: Data
     props: Props
   }) => [
     `<div><p class="hello" style="display: inline">${childMessage}</p></div>`,
     `<p>${color}</p>`
   ],
   css: [
-    // cssUrl,
+    cssUrl,
     {
       selector: 'p',
       style: () => ({ cursor: 'pointer' })
@@ -159,26 +155,14 @@ const parent = define({
   css: [`p {color: green;}`]
 })
 
-// const 2 = define({
-//   name: '2',
-//   data: {
-//     numbers: [1, 2, 3],
-//     color: 'green'
-//   },
-//   html: ({ numbers }) => ({
-//     contents: numbers as number[],
-//     render: (arg: number, index) => `<p class="class-${index}">${arg * 2}</p>`
-//   }),
-//   events: {
-//     click: ({ data: { numbers } }) => console.log(numbers)
-//   },
-//   delegatedEvents: [
-//     {
-//       selector: 'p',
-//       click: ({ data: { numbers } }, _, index) => console.log(numbers[index])
-//     }
-//   ]
-// })
+const parent2 = define({
+  name: 'parent2',
+  data: {
+    numbers: [1, 2, 3],
+    color: 'green'
+  },
+  html: child
+})
 
 // const 3 = define({
 //   name: '3',
@@ -245,4 +229,4 @@ const parent = define({
 export const mount = (parent: string, children: Html | Html[]) =>
   appendChild(parent, children)
 
-mount('app', parent)
+mount('app', [parent, parent2])
