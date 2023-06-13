9dd88c252ea4847786a60e66569163dd367d9e97
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-06-13 23:58:30 +0700

    refactoring: edit types to remove slot from args

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -52,7 +52,7 @@ export type Inheritances<D, P> = {
 
 export interface Constructor<D, P> {
   new (...params: any[]): HTMLElement
-  create: ({ data, props, slot }: { data?: D; props?: P; slot?: Html }) => HTMLElement
+  create: ({ data, props }: { data?: D; props?: P }) => HTMLElement
 }
 
 export interface <T, D, P> {
@@ -63,6 +63,6 @@ export interface <T, D, P> {
   className?: string
   html: Convert<Html | Html[] | Each<T> | EachIf<T> | If, D, P>
   css?: Css<D, P>
-  slot?: string
+  slot?: Convert<string, D, P>
   events?: Events<D, P>
 }
