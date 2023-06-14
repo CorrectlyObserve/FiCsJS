f2858d0d0cd4f0f16871a8368427fb6d19ebd957
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-06-14 11:53:48 +0700

    feature: reflect change of Constructor

--- src/wely.ts
+++ src/wely.ts
@@ -21,18 +21,14 @@ const define = <T, D, P>({
     customElements.define(
       Name(name),
       class extends <D, P> {
-        static create(
-          { data: individualData, props: individualProps }: { data?: D; props?: P } = {
-            data: undefined,
-            props: undefined
-          }
-        ): <D, P> {
+        static create({
+          data: partialData,
+          props: partialProps
+        }: { data?: Partial<D>; props?: Partial<P> } = {}): <D, P> {
           const  = <<D, P>>document.createElement(Name(name))
 
-          if (data) .data = <D>individualData ? { ...data, ...individualData } : { ...data }
-          if (props)
-            .props = <P>individualProps ? { ...props, ...individualProps } : { ...props }
-
+          if (data) .data = <D>partialData ? { ...data, ...partialData } : { ...data }
+          if (props) .props = <P>partialProps ? { ...props, ...partialProps } : { ...props }
           if (inheritances) .inheritances = [...inheritances]
 
           .classes.push(kebabName(name))
@@ -103,6 +99,13 @@ interface Props {
 
 const childClass = define({
   name: 'child',
+  data: {
+    count: 1,
+    message: 'Hello',
+    color: 'red',
+    back: 'black',
+    childMessage: 'Child hello'
+  },
   html: ({ data: { childMessage }, props: { color } }: { data: Data; props: Props }) => [
     `<div><p class="hello" style="display: inline">${childMessage}</p></div>`,
     `<p>${color}</p>`
@@ -155,8 +158,7 @@ const parent = define({
       props: ({ color, click }) => ({ color, click })
     }
   ],
-  html: child,
-  css: [`p {color: green;}`]
+  html: child
 }).create({ data: { color: 'red' } })
 
 const parent2 = define({
