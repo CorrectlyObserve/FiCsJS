f2ae650aae216d3b7807cb9e52179f2bb688a363
MasamiOgasawara <m-ogasawara@wely.tech>
2023-06-24 17:26:21 +0700

    refactoring: reflect change of the order of keys

--- src/wely.ts
+++ src/wely.ts
@@ -116,8 +116,8 @@ const childClass = define({
       method: ({ data: { count } }) => console.log(count++)
     },
     {
-      handler: 'click',
       selector: 'div',
+      handler: 'click',
       method: ({ data: { message }, props: { click } }) => click(message)
     }
   ]
@@ -154,7 +154,7 @@ const parent2 = define({
 
 // const wely3 = define({
 //   name: 'wely3',
-//   data: {
+//   data: () => ({
 //     number: 100,
 //     text: 'AA',
 //     count: 1,
@@ -162,8 +162,8 @@ const parent2 = define({
 //     color: 'red',
 //     back: 'black',
 //     _childMessage: 'Child hello'
-//   },
-//   html: ({ number }) => ({
+//   }),
+//   html: ({ data: { number } }) => ({
 //     branches: [
 //       {
 //         judge: number > 100,
@@ -177,42 +177,42 @@ const parent2 = define({
 //     fallback: `<slot />`
 //   }),
 //   slot: `<p>AAA</p>`,
-//   delegatedEvents: [
+//   events: [
 //     {
+//       handler: 'click',
 //       selector: 'slot',
-//       click: ({ data: { number, text } }, e, index) =>
-//         console.log(number, text, e, index)
+//       method: ({ data: { number, text } }, e, index) => console.log(number, text, e, index)
 //     }
 //   ]
-// })
+// }).create()
 
 // const wely4 = define({
 //   name: 'Wely4',
-//   data: {
+//   data: () => ({
 //     numbers: [1, 2, 3]
-//   },
-//   html: ({ numbers }) => ({
-//     contents: numbers as number[],
+//   }),
+//   html: ({ data: { numbers } }: { data: { numbers: number[] } }) => ({
+//     contents: numbers,
 //     branches: [
 //       {
-//         judge: (arg: number) => arg === 100,
-//         render: (arg: number, index) =>
-//           `<p class="class-${index}">${arg * 2}</p>`
+//         judge: arg => arg === 100,
+//         render: (arg: number, index) => `<p class="class-${index}">${arg * 2}</p>`
 //       },
 //       {
-//         judge: (arg: number) => typeof arg !== 'number',
-//         render: (arg: number, index) => `<p class="class-${index}">${arg}</p>`
+//         judge: arg => typeof arg !== 'number',
+//         render: (arg, index) => `<p class="class-${index}">${arg}</p>`
 //       }
 //     ],
 //     fallback: (arg: number) => `<p class="class-z">${arg * 10}</p>`
 //   }),
-//   delegatedEvents: [
+//   events: [
 //     {
 //       selector: '.class-z',
-//       click: ({ data: { numbers } }, e, index) => console.log(numbers[index], e)
+//       handler: 'click',
+//       method: ({ data: { numbers } }, e, index) => console.log(numbers[index ?? 0], e)
 //     }
 //   ]
-// })
+// }).create()
 
 export const mount = (parent: string, children: Html | Html[]): void => {
   const parentElement = document.getElementById(<string>parent)
