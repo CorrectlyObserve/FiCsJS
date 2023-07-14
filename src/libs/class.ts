5af0ce9e4e7ee31c886c9489055d54dfd43d0699
MasamiOgasawara <m-ogasawara@wely.tech>
2023-07-14 09:41:00 +0700

    feature: make propaties of Wely class private in JavaScript

--- src/libs/class.ts
+++ src/libs/class.ts
@@ -15,18 +15,18 @@ export class Wely<T, D, P> extends HTMLElement {
   readonly shadowRoot!: ShadowRoot
   readonly welyId: string = ''
 
-  private data: D = <D>{}
-  private props: P = <P>{}
-  private inheritances: Inheritances<D, P> = []
-  private classes: string[] = []
-  private html: Html[] = []
-  private css?: Css<D, P>
-  private slotContent?: Html
-  private events: Events<D, P> = []
+  #data: D = <D>{}
+  #props: P = <P>{}
+  #inheritances: Inheritances<D, P> = []
+  #classes: string[] = []
+  #html: Html[] = []
+  #css?: Css<D, P>
+  #slotContent?: Html
+  #events: Events<D, P> = []
 
-  private isEach: boolean = false
-  private isRendered: boolean = false
-  private inheritedSet: Set<HTMLElement> = new Set()
+  #isEach: boolean = false
+  #isRendered: boolean = false
+  #inheritedSet: Set<HTMLElement> = new Set()
 
   constructor() {
     super()
@@ -44,58 +44,58 @@ export class Wely<T, D, P> extends HTMLElement {
     slot,
     events
   }: Initialize<T, D, P>) {
-    if (dataObj) this.data = { ...dataObj }
-    if (inheritances) this.inheritances = [...inheritances]
+    if (dataObj) this.#data = { ...dataObj }
+    if (inheritances) this.#inheritances = [...inheritances]
 
-    this.classes.push(toKebabCase(name))
+    this.#classes.push(toKebabCase(name))
     if (className)
-      for (const localName of className.split(' ')) this.classes.push(toKebabCase(localName).trim())
+      for (const localName of className.split(' ')) this.#classes.push(toKebabCase(localName).trim())
 
     let converter =
-      typeof html === 'function' ? html({ data: { ...this.data }, props: { ...this.props } }) : html
+      typeof html === 'function' ? html({ data: { ...this.#data }, props: { ...this.#props } }) : html
 
-    if (typeof converter === 'string') this.html = convertToArray(<Html | Html[]>converter)
+    if (typeof converter === 'string') this.#html = convertToArray(<Html | Html[]>converter)
     else if ('contents' in <Each<T> | EachIf<T>>converter) {
-      this.isEach = true
+      this.#isEach = true
 
       if ('branches' in <EachIf<T>>converter)
         (<EachIf<T>>converter).contents.forEach((content, index) => {
           for (const branch of (<EachIf<T>>converter).branches)
-            if (branch.judge(content)) this.html.push(branch.render(content, index))
+            if (branch.judge(content)) this.#html.push(branch.render(content, index))
 
           const fallback = (<EachIf<T>>converter)?.fallback
-          if (fallback !== undefined) this.html.push(fallback(content, index))
+          if (fallback !== undefined) this.#html.push(fallback(content, index))
         })
       else
         (<Each<T>>converter).contents.forEach((content, index) =>
-          this.html.push((<Each<T>>converter).render(content, index) ?? '')
+          this.#html.push((<Each<T>>converter).render(content, index) ?? '')
         )
     } else if ('branches' in <If>converter) {
       for (const branch of (<If>converter).branches)
         if (branch.judge) {
-          this.html.push(branch.render)
+          this.#html.push(branch.render)
           break
         }
 
       const fallback = (<If>converter)?.fallback
-      if (this.html.length === 0 && fallback) this.html.push(fallback)
-    } else this.html = convertToArray(<Html | Html[]>converter)
+      if (this.#html.length === 0 && fallback) this.#html.push(fallback)
+    } else this.#html = convertToArray(<Html | Html[]>converter)
 
-    if (css) this.css = [...css]
+    if (css) this.#css = [...css]
     if (slot)
-      this.slotContent =
+      this.#slotContent =
         typeof slot === 'function'
-          ? slot({ data: { ...this.data }, props: { ...this.props } })
+          ? slot({ data: { ...this.#data }, props: { ...this.#props } })
           : slot
 
-    if (events) this.events = [...events]
+    if (events) this.#events = [...events]
   }
 
   connectedCallback(): void {
-    if (this.isRendered) return
+    if (this.#isRendered) return
 
-    if (this.html.length > 0)
-      for (let child of convertToArray(this.html)) {
+    if (this.#html.length > 0)
+      for (let child of convertToArray(this.#html)) {
         if (typeof child === 'string')
           child = <HTMLElement>(
             Array.from(
@@ -106,13 +106,13 @@ export class Wely<T, D, P> extends HTMLElement {
         this.shadowRoot.appendChild(child)
       }
 
-    if (this.inheritances.length > 0)
-      this.inheritances.forEach(inheritance => {
+    if (this.#inheritances.length > 0)
+      this.#inheritances.forEach(inheritance => {
         const { descendants } = inheritance
 
         for (const descendant of <Wely<T, D, P>[]>convertToArray(descendants)) {
-          if (this.html.includes(descendant) || this.inheritedSet.has(descendant))
-            descendant.props = { ...inheritance.props(this.data) }
+          if (this.#html.includes(descendant) || this.#inheritedSet.has(descendant))
+            descendant.#props = { ...inheritance.props(this.#data) }
           else {
             const { welyId } = descendant
             descendant.id = welyId
@@ -142,27 +142,27 @@ export class Wely<T, D, P> extends HTMLElement {
             }
 
             if (element) {
-              element.props = { ...inheritance.props(this.data) }
-              this.inheritedSet.add(element)
+              element.#props = { ...inheritance.props(this.#data) }
+              this.#inheritedSet.add(element)
             } else {
-              if (this.inheritedSet.has(descendant)) this.inheritedSet.delete(descendant)
+              if (this.#inheritedSet.has(descendant)) this.#inheritedSet.delete(descendant)
               throw Error(`This component is not a descendant...`)
             }
           }
         }
       })
 
-    this.classList.add(this.classes.join(' '))
+    this.classList.add(this.#classes.join(' '))
 
-    if (this.css) {
+    if (this.#css) {
       const css = document.createElement('style')
 
-      if (this.css.length > 0)
-        this.css.forEach(localCss => {
+      if (this.#css.length > 0)
+        this.#css.forEach(localCss => {
           if (typeof localCss === 'string') css.textContent += localCss
           else if (localCss.selector && 'style' in localCss) {
             const style = Object.entries(
-              localCss.style({ data: { ...this.data }, props: { ...this.props } })
+              localCss.style({ data: { ...this.#data }, props: { ...this.#props } })
             )
               .map(([key, value]) => `${toKebabCase(key)}: ${value};`)
               .join('\n')
@@ -174,13 +174,13 @@ export class Wely<T, D, P> extends HTMLElement {
       this.shadowRoot.appendChild(css)
     }
 
-    if (this.slotContent)
-      typeof this.slotContent === 'string'
-        ? this.insertAdjacentHTML('beforeend', this.slotContent)
-        : this.insertAdjacentElement('beforeend', this.slotContent)
+    if (this.#slotContent)
+      typeof this.#slotContent === 'string'
+        ? this.insertAdjacentHTML('beforeend', this.#slotContent)
+        : this.insertAdjacentElement('beforeend', this.#slotContent)
 
-    if (this && this.events.length > 0) {
-      for (const obj of this.events) {
+    if (this && this.#events.length > 0) {
+      for (const obj of this.#events) {
         const { selector, handler, method } = obj
 
         if (selector) {
@@ -206,18 +206,18 @@ export class Wely<T, D, P> extends HTMLElement {
             for (let i = 0; i < targets.length; i++)
               targets[i].addEventListener(handler, (event: Event) =>
                 method(
-                  { data: { ...this.data }, props: { ...this.props } },
+                  { data: { ...this.#data }, props: { ...this.#props } },
                   event,
-                  this.isEach ? i : undefined
+                  this.#isEach ? i : undefined
                 )
               )
         } else
           this.addEventListener(handler, (event: Event) =>
-            method({ data: { ...this.data }, props: { ...this.props } }, event)
+            method({ data: { ...this.#data }, props: { ...this.#props } }, event)
           )
       }
     }
 
-    this.isRendered = true
+    this.#isRendered = true
   }
 }
