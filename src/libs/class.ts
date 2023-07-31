76b034b6254d7424316de54bbae810eaf32fa647
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-07-31 19:25:32 +0700

    refactoring: make code understand easily

--- src/libs/class.ts
+++ src/libs/class.ts
@@ -160,8 +160,8 @@ export class <T, D, P> extends HTMLElement {
 
     // Event handlers
     if (events) {
-      for (const obj of events) {
-        const { selector, handler, method } = obj
+      for (const eventObj of events) {
+        const { selector, handler, method } = eventObj
 
         if (selector) {
           const targets: Element[] = (() => {
