996bb1a99e2aa59d38c09272ebc6d612eb55a632
MasamiOgasawara <m-ogasawara@wely.tech>
2023-08-20 15:09:23 +0700

    feature: re-create types in line with re-creating define function

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -26,13 +26,34 @@ interface Arg<T, D, P> {
 
 type Convert<T, D, P> = T | (({ data, props }: DataProps<D, P>) => T)
 
+export type Css<D, P> = (
+  | string
+  | {
+      selector: string
+      style: ({ data, props }: DataProps<D, P>) => {
+        [key: string]: string | number
+      }
+    }
+)[]
+
 interface DataProps<D, P> {
   data: D
   props: P
 }
 
-export interface Define<T, D, P> extends Arg<T, D, P> {
+export interface Define<T, D, P> {
+  name: string
+  className?: string
+  dependencies?: Wely<D> | Wely<D>[]
+  inheritances?: {
+    descendants: HTMLElement | HTMLElement[]
+    props: (data: D) => P
+  }[]
   data?: () => D
+  html: Html2<T, D, P>
+  css?: Css<D, P>[]
+  slot?: Convert<string | HTMLElement, D, P>
+  events?: Events<D, P>
 }
 
 export interface Each<T> {
@@ -49,7 +70,15 @@ export interface EachIf<T> {
   fallback?: (arg: T, index: number) => Html
 }
 
+export type Events<D, P> = {
+  handler: string
+  selector?: string
+  method: ({ data, props }: DataProps<D, P>, event: Event, index?: number) => void
+}[]
+
 export type Html = string | HTMLElement | DocumentFragment
+
+export type Html2<T, D, P> = Convert<Html | Each<T> | EachIf<T> | If, D, P>
 
 export interface If {
   branches: {
@@ -62,6 +91,8 @@ export interface If {
 export interface Initialize<T, D, P> extends Arg<T, D, P> {
   integratedData?: D
 }
+
+export type Slot<D, P> = Convert<string | HTMLElement, D, P>
 
 export interface Wely<D> {
   new (...params: any[]): HTMLElement
