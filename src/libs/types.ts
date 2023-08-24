42d168f14e0cf15a599edd5ca8c4bafdae4bab4a
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-08-24 10:10:10 +0700

    debug: fix  type

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -94,8 +94,7 @@ export interface Initialize<T, D, P> extends Arg<T, D, P> {
 
 export type Slot<D, P> = Convert<string | HTMLElement, D, P>
 
-export interface <D> {
-  new (...params: unknown[]): HTMLElement
+export interface <D> extends CustomElementConstructor {
   overwrite: (data: () => Partial<D>) => <D>
-  create: ({ data }: { data?: () => Partial<D> }) => HTMLElement
+  instantiate: () => HTMLElement
 }
