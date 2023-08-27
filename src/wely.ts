cd301101f6f70644dce97695a1fcb7b7ae55d048
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-08-27 15:12:39 +0700

    feature: create create function

--- src/wely.ts
+++ src/wely.ts
@@ -1,17 +1,7 @@
-import { Define, DefineArgs,  } from '@/libs/types'
+import { Css, Define, Events, Html2, Inheritances, Slot } from '@/libs/types'
 import { generator, toKebabCase } from '@/libs/utils'
 
-// const aaa = () =>
-//   class extends HTMLElement {
-//     static hello() {
-//       console.log('hello')
-//     }
-//   }
-
-// const AAA = aaa()
-// AAA.hello()
-
-export const define = <T, D, P>({
+export const create = <T, D, P>({
   name,
   className,
   dependencies,
@@ -21,53 +11,134 @@ export const define = <T, D, P>({
   css,
   slot,
   events
-}: Define<T, D, P>): <D> => {
-  const Name = `w-${toKebabCase(name)}`
-  const get = () => <<D>>customElements.get(Name)
+}: Define<T, D, P>) =>
+  class {
+    readonly #name: string = ''
+    readonly #className: string = ''
+    readonly #dependencies: CustomElementConstructor[] = []
+    readonly #inheritances: Inheritances<D, P> = []
+    readonly #data: D = <D>{}
+    readonly #html: Html2<T, D, P>[] = []
+    readonly #css: Css<D, P> = []
+    readonly #slot: Slot<D, P>[] = []
+    readonly #events: Events<D, P> = []
+    readonly #partialData: Partial<D> = {}
 
-  const args: DefineArgs<T, D, P> = {
-    dependencies: dependencies ? (Array.isArray(dependencies) ? dependencies : [dependencies]) : [],
-    inheritances: inheritances ? [...inheritances] : [],
-    data: <D>{ ...(data ? data() : {}) },
-    props: <P>{},
-    html: [html],
-    css: css && css.length > 0 ? [...css] : [],
-    inheritedSet: new Set(),
-    slot: slot ? [slot] : [],
-    events: events && events.length > 0 ? [...events] : []
+    constructor(partialData?: () => Partial<D>) {
+      this.#name = `w-${toKebabCase(name)}`
+
+      if (className) this.#className = className
+
+      if (dependencies)
+        this.#dependencies = Array.isArray(dependencies) ? [...dependencies] : [dependencies]
+
+      if (inheritances) this.#inheritances = [...inheritances]
+
+      if (partialData) this.#partialData = { ...partialData() }
+      if (data) this.#data = { ...data(), ...this.#partialData }
+
+      this.#html = [html]
+
+      if (css) this.#css = [...css]
+      if (slot) this.#slot = [slot]
+      if (events) this.#events = [...events]
+    }
+
+    define() {
+      const Class = this
+      const get = () => customElements.get(Class.#name)
+
+      if (!get())
+        customElements.define(
+          Class.#name,
+          class extends HTMLElement {
+            readonly shadowRoot!: ShadowRoot
+            readonly Id: string = ''
+            #inheritedSet: Set<CustomElementConstructor> = new Set()
+            #props: P = <P>{}
+
+            constructor() {
+              super()
+              this.shadowRoot = this.attachShadow({ mode: 'open' })
+              this.Id = `-id${generator.next().value}`
+
+              if (Class.#className)
+                this.setAttribute(
+                  'class',
+                  Class.#className
+                    .split(' ')
+                    .reduce((prev, current) => `${prev} ${current}`, Class.#name)
+                )
+              else this.classList.add(Class.#name)
+            }
+
+            connectedCallback() {
+              console.log(Class.#data)
+            }
+          }
+        )
+
+      return <CustomElementConstructor>get()
+    }
   }
 
-  if (!get())
-    customElements.define(
-      Name,
-      class extends HTMLElement {
-        readonly shadowRoot!: ShadowRoot
-        readonly Id: string = ''
+// export const define = <T, D, P>({
+//   name,
+//   className,
+//   dependencies,
+//   inheritances,
+//   data,
+//   html,
+//   css,
+//   slot,
+//   events
+// }: Define<T, D, P>): Element<D> => {
+//   const Name = `w-${toKebabCase(name)}`
+//   const get = () => <Element<D>>customElements.get(Name)
 
-        constructor() {
-          super()
-          this.shadowRoot = this.attachShadow({ mode: 'open' })
-          this.Id = `-id${generator.next().value}`
+//   const args: DefineArgs<T, D, P> = {
+//     dependencies: dependencies ? (Array.isArray(dependencies) ? dependencies : [dependencies]) : [],
+//     inheritances: inheritances ? [...inheritances] : [],
+//     data: <D>{ ...(data ? data() : {}) },
+//     props: <P>{},
+//     html: [html],
+//     css: css && css.length > 0 ? [...css] : [],
+//     inheritedSet: new Set(),
+//     slot: slot ? [slot] : [],
+//     events: events && events.length > 0 ? [...events] : []
+//   }
 
-          if (className)
-            this.setAttribute(
-              'class',
-              className
-                .split(' ')
-                .reduce((prev, current) => `${prev} ${current}`, toKebabCase(name))
-            )
-          else this.classList.add(toKebabCase(name))
-        }
+//   if (!get())
+//     customElements.define(
+//       Name,
+//       class extends HTMLElement {
+//         readonly shadowRoot!: ShadowRoot
+//         readonly Id: string = ''
 
-        static overwrite(data: () => Partial<D>) {
-          args.data = <D>{ ...args.data, ...data() }
-          return get()
-        }
-      }
-    )
+//         constructor() {
+//           super()
+//           this.shadowRoot = this.attachShadow({ mode: 'open' })
+//           this.Id = `-id${generator.next().value}`
 
-  return get()
-}
+//           if (className)
+//             this.setAttribute(
+//               'class',
+//               className
+//                 .split(' ')
+//                 .reduce((prev, current) => `${prev} ${current}`, toKebabCase(name))
+//             )
+//           else this.classList.add(toKebabCase(name))
+//         }
+
+//         static overwrite(data: () => Partial<D>) {
+//           args.data = <D>{ ...args.data, ...data() }
+//           return get()
+//         }
+//       }
+//     )
+
+//   return get()
+// }
 
 export const html = (
   templates: TemplateStringsArray,
