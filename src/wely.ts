4a846c69faa64de6cf1d2fe964e4707451737339
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-08-27 15:31:29 +0700

    refactoring: make mount use easily

--- src/wely.ts
+++ src/wely.ts
@@ -133,5 +133,5 @@ export const html = (
   return fragment
 }
 
-export const mount = (parent: string, child: HTMLElement) =>
-  document.getElementById(parent)?.appendChild(child)
+export const mount = (parent: HTMLElement | string, child: HTMLElement) =>
+  (typeof parent === 'string' ? document.getElementById(parent) : parent)?.appendChild(child)
