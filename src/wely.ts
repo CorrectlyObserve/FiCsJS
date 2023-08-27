001d38f0d3bc7ec45f7d33a4acc396173674c201
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-08-27 15:56:06 +0700

    refactoring: configure an arg of define method to overwrite data property

--- src/wely.ts
+++ src/wely.ts
@@ -37,14 +37,13 @@ export const create = <T, D, P>({
     readonly #className: string = ''
     readonly #dependencies: CustomElementConstructor[] = []
     readonly #inheritances: Inheritances<D, P> = []
-    readonly #data: D = <D>{}
+    readonly #dataArg: D = <D>{}
     readonly #html: Html2<T, D, P>[] = []
     readonly #css: Css<D, P> = []
     readonly #slot: Slot<D, P>[] = []
     readonly #events: Events<D, P> = []
-    readonly #partialData: Partial<D> = {}
 
-    constructor(partialData?: () => Partial<D>) {
+    constructor() {
       this.#name = `w-${toKebabCase(name)}`
 
       if (className) this.#className = className
@@ -54,8 +53,7 @@ export const create = <T, D, P>({
 
       if (inheritances) this.#inheritances = [...inheritances]
 
-      if (partialData) this.#partialData = { ...partialData() }
-      if (data) this.#data = { ...data(), ...this.#partialData }
+      if (data) this.#dataArg = { ...data() }
 
       this.#html = [html]
 
@@ -64,7 +62,7 @@ export const create = <T, D, P>({
       if (events) this.#events = [...events]
     }
 
-    define() {
+    define(partialData: () => Partial<D> = () => <Partial<D>>{}) {
       const Class = this
       const get = () => customElements.get(Class.#name)
 
@@ -74,6 +72,7 @@ export const create = <T, D, P>({
           class extends HTMLElement {
             readonly shadowRoot!: ShadowRoot
             #inheritedSet: Set<CustomElementConstructor> = new Set()
+            #data: D = <D>{}
             #props: P = <P>{}
 
             constructor() {
@@ -88,10 +87,12 @@ export const create = <T, D, P>({
                     .reduce((prev, current) => `${prev} ${current}`, Class.#name)
                 )
               else this.classList.add(Class.#name)
+
+              this.#data = { ...Class.#dataArg, ...(partialData ? partialData() : {}) }
             }
 
             connectedCallback() {
-              console.log(Class.#data)
+              console.log(this.#data)
             }
           }
         )
