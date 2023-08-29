fea6a756d1bd9cd00e360a1f4ee70e8dcff355e6
MasamiOgasawara <m-ogasawara@wely.tech>
2023-08-30 02:04:20 +0700

    refactoring: delete an unnecessary function

--- src/utils.ts
+++ src/utils.ts
@@ -40,13 +40,3 @@ export const convertHtml = (
 
   return fragment
 }
-
-export const toKebabCase = (str: string): string => {
-  const newStr = str.slice(1)
-  let body = newStr
-  const upperCase = new RegExp(/[A-Z]/g)
-
-  if (upperCase.test(newStr)) body = newStr.replace(upperCase, val => `-${val.toLowerCase()}`)
-
-  return str[0].toLowerCase() + body
-}
