801a851a940733e2e4773f72423f6c5e3e0c9cc9
MasamiOgasawara <m-ogasawara@wely.tech>
2023-09-01 17:39:23 +0700

    feature: edit types for tagged template literal

--- src/types.ts
+++ src/types.ts
@@ -15,16 +15,16 @@ interface DataProps<D, P> {
 
 export interface Each<T, D, P> {
   contents: T[]
-  render: (arg: T, index: number) => ReturnValue<T, D, P> | undefined
+  render: (arg: T, index: number) => WelyOrString<T, D, P> | undefined
 }
 
 export interface EachIf<T, D, P> {
   contents: T[]
   branches: {
     judge: (arg: T) => boolean
-    render: (arg: T, index: number) => ReturnValue<T, D, P>
+    render: (arg: T, index: number) => WelyOrString<T, D, P>
   }[]
-  fallback?: (arg: T, index: number) => ReturnValue<T, D, P>
+  fallback?: (arg: T, index: number) => WelyOrString<T, D, P>
 }
 
 export type Events<D, P> = {
@@ -43,14 +43,14 @@ export type Html<T, D, P> =
       dependencies?: WelyClass<T, D, P>[]
     }) => HtmlArg<T, D, P>)
 
-type HtmlArg<T, D, P> = ReturnValue<T, D, P> | Each<T, D, P> | EachIf<T, D, P> | If<T, D, P>
+type HtmlArg<T, D, P> = WelyOrString<T, D, P> | Each<T, D, P> | EachIf<T, D, P> | If<T, D, P>
 
 export interface If<T, D, P> {
   branches: {
     judge: boolean | unknown
-    render: ReturnValue<T, D, P>
+    render: WelyOrString<T, D, P>
   }[]
-  fallback?: ReturnValue<T, D, P>
+  fallback?: WelyOrString<T, D, P>
 }
 
 export type Inheritances<T, D, P> = {
@@ -58,11 +58,9 @@ export type Inheritances<T, D, P> = {
   props: (data: D) => P
 }[]
 
-type ReturnValue<T, D, P> = string | WelyClass<T, D | any, P> | DocumentFragment
-
 export type Slot<T, D, P> =
-  | ReturnValue<T, D, P>
-  | (({ data, props }: DataProps<D, P>) => ReturnValue<T, D, P>)
+  | WelyOrString<T, D, P>
+  | (({ data, props }: DataProps<D, P>) => WelyOrString<T, D, P>)
 
 export interface Wely<T, D, P> {
   name: string
@@ -75,3 +73,8 @@ export interface Wely<T, D, P> {
   slot?: Slot<T, D, P>
   events?: Events<D, P>
 }
+
+type WelyOrString<T, D, P> =
+  | WelyClass<T, D | any, P>
+  | string
+  | (WelyClass<T, D | any, P> | string)[]
