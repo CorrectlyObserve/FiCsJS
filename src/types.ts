d8757a804c2dbdca542e60bb0ebd26cd3fddacf8
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-09-03 14:23:43 +0700

    debug: fix a type error and make types understand easily

--- src/types.ts
+++ src/types.ts
@@ -4,33 +4,28 @@ export type Css<D, P> = (
   | string
   | {
       selector: string
-      style: ({ data, props }: DataProps<D, P>) => Record<string, string | number>
+      style: ({ data, props }: { data: D; props: P }) => Record<string, string | number>
     }
 )[]
 
-interface DataProps<D, P> {
-  data: D
-  props: P
-}
-
 export interface Each<T, D, P> {
   contents: T[]
-  render: (arg: T, index: number) => OrString<T, D, P> | undefined
+  render: (arg: T, index: number) => Result<T, D, P> | undefined
 }
 
 export interface EachIf<T, D, P> {
   contents: T[]
   branches: {
     judge: (arg: T) => boolean
-    render: (arg: T, index: number) => OrString<T, D, P>
+    render: (arg: T, index: number) => Result<T, D, P>
   }[]
-  fallback?: (arg: T, index: number) => OrString<T, D, P>
+  fallback?: (arg: T, index: number) => Result<T, D, P>
 }
 
 export type Events<D, P> = {
   handler: string
   selector?: string
-  method: ({ data, props }: DataProps<D, P>, event: Event, index?: number) => void
+  method: ({ data, props }: { data: D; props: P }, event: Event, index?: number) => void
 }[]
 
 export type Html<T, D, P> =
@@ -39,33 +34,39 @@ export type Html<T, D, P> =
       data,
       props,
       dependencies
-    }: DataProps<D, P> & {
+    }: {
+      data: D | any
+      props: P
       dependencies?: Class<T, D, P>[]
     }) => HtmlArg<T, D, P>)
 
-type HtmlArg<T, D, P> = OrString<T, D, P> | Each<T, D, P> | EachIf<T, D, P> | If<T, D, P>
+type HtmlArg<T, D, P> = Result<T, D, P> | Each<T, D, P> | EachIf<T, D, P> | If<T, D, P>
 
 export interface If<T, D, P> {
   branches: {
     judge: boolean | unknown
-    render: OrString<T, D, P>
+    render: Result<T, D, P>
   }[]
-  fallback?: OrString<T, D, P>
+  fallback?: Result<T, D, P>
 }
 
 export type Inheritances<T, D, P> = {
-  descendants: Class<T, D | any, P> | Class<T, D | any, P>[]
+  descendants: SingleOrArray<Class<T, D | any, P>>
   props: (data: D) => P
 }[]
 
+type Result<T, D, P> = SingleOrArray<Class<T, D | any, P> | string>
+
+export type SingleOrArray<T> = T | T[]
+
 export type Slot<T, D, P> =
-  | OrString<T, D, P>
-  | (({ data, props }: DataProps<D, P>) => OrString<T, D, P>)
+  | Result<T, D, P>
+  | (({ data, props }: { data: D; props: P }) => Result<T, D, P>)
 
 export interface <T, D, P> {
   name: string
   className?: string
-  dependencies?: Class<T, D | any, P> | Class<T, D | any, P>[]
+  dependencies?: SingleOrArray<Class<T, D | any, P>>
   inheritances?: Inheritances<T, D, P>
   data?: () => D
   html: Html<T, D, P>
@@ -73,8 +74,3 @@ export interface <T, D, P> {
   slot?: Slot<T, D, P>
   events?: Events<D, P>
 }
-
-type OrString<T, D, P> =
-  | Class<T, D | any, P | any>
-  | string
-  | (Class<T, D | any, P | any> | string)[]
