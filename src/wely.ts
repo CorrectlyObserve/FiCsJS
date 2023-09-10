f18a4b502d743ada2380f9eddc50063b09c907f1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-09-11 01:57:12 +0700

    feature: reflect change of  type

--- src/wely.ts
+++ src/wely.ts
@@ -13,15 +13,16 @@ export const  = <T, D, P>({
   events
 }: <T, D, P>) =>
   new Class({
-    name,
-    className,
-    dependencies,
-    inheritances,
-    data,
-    html,
-    css,
-    slot,
-    events
+    Id: undefined,
+    name: name,
+    className: className,
+    dependencies: dependencies,
+    inheritances: inheritances,
+    data: data,
+    html: html,
+    css: css,
+    slot: slot,
+    events: events
   })
 
 export const html = <T, D, P>(
