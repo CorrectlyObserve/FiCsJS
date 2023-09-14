974a21bfa415b8e4ab21b4039da3e256029337f6
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-09-14 14:38:49 +0700

    feature: reflect change of Class

--- src/main.ts
+++ src/main.ts
@@ -45,13 +45,13 @@ const child = ({
   ]
 })
 
-// const child2 = child.overwrite(() => ({ message: 'Good bye!' }))
+const child2 = child.overwrite(() => ({ message: 'Good bye!' }))
 
 const parent = ({
   name: 'parent',
   className: 'test',
   dependencies: child,
-  slot: html`${child}
+  slot: html`${child2}
     <p>aaa</p>`,
   html: `<slot />`
 })
@@ -69,7 +69,7 @@ const parent = ({
       <p>人数: ${number}</p>`,
   inheritances: [
     {
-      descendants: child,
+      descendants: [child, child2],
       props: ({ color, click }) => ({ color, click })
     }
   ]
