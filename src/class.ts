8eedcfa0f76b07def3ba314bd95df4d92220a75b
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-10-15 17:10:06 +0700

    feature: reflect change of returned value of html function to addSlot method

--- src/class.ts
+++ src/class.ts
@@ -226,7 +226,9 @@ export class Class<T, D, P> {
     if (this.#slot.length > 0)
       for (const slot of this.#toArray(this.#slot))
         this.#appendChild(
-          <Class<T, D, P> | string>this.#convertHtml(<Slot<T, D, P>>slot),
+          (<Record<symbol, (Class<T, D, P> | string)[]>>this.#convertHtml(<Slot<T, D, P>>slot))[
+            htmlSymbol
+          ],
           ,
           propsChain
         )
