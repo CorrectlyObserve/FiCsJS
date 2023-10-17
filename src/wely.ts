2029eff78a01a98b6f9318fd53b17ea3b05f80f1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-10-17 09:30:56 +0700

    refactoring: reflect integrating generator and symbol

--- src/wely.ts
+++ src/wely.ts
@@ -1,6 +1,6 @@
 import { Class } from './class'
-import { htmlSymbol } from './symbol'
 import { HtmlSymbol,  } from './types'
+import { symbol } from './utils'
 
 export const html = <T, D, P>(
   templates: TemplateStringsArray,
@@ -27,7 +27,7 @@ export const html = <T, D, P>(
     }
   }
 
-  return { [htmlSymbol]: result }
+  return { [symbol]: result }
 }
 
 export const  = <T, D, P>({
