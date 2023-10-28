0152ebd638c36b5e0b8e2b3f1a7bd6d3c77dc4c9
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-10-28 15:49:03 +0700

    refactoring: make insertTemplate function in renderOnServer method simple

--- src/class.ts
+++ src/class.ts
@@ -322,14 +322,12 @@ export class Element<T, D, P> {
     const insertTemplate = (
       arg: SanitizedHtml<T, D, P> | Element<T, D, P> | string,
       propsChain: PropsChain<P>
-    ): string => {
-      let html: string = ''
-
-      for (const element of this.#toArray(arg))
-        html += element instanceof Element ? element.#renderOnServer(propsChain) : element
-
-      return html
-    }
+    ): string =>
+      this.#toArray(arg).reduce(
+        (prev, curr) =>
+          prev + curr instanceof Element ? curr.#renderOnServer(propsChain) : curr,
+        ''
+      )
 
     const addHtml = (instance: Element<T, D, P>) => {
       const html: Html<T, D, P> = instance.#convertHtml(instance.#html[0])
