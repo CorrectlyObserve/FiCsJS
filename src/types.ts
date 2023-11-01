9c3820cde648e4655a4ef70c2df315bf71a7c915
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-11-02 01:02:11 +0700

    refactoring: edit  type by renaming inheritances to props to make it understand easily

--- src/types.ts
+++ src/types.ts
@@ -36,8 +36,8 @@ export interface <D, P> {
   Id?: string
   name: string
   className?: string
-  inheritances?: Inheritances<D>
   data?: () => D
+  props?: Inheritances<D>
   isOnlyCsr?: boolean
   html: Html<D, P>
   css?: Css<D, P>
