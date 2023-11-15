cdc7268aa7471be849a7e6817e16a4872c9bd274
MasamiOgasawara <m-ogasawara@wely.tech>
2023-11-15 10:45:42 +0700

    test: reflect the newest wely function

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { html, wely } from './wely'
+import { html, slot, wely } from './wely'
 import cssUrl from './style.css?inline'
 
 const child = wely({
@@ -13,6 +13,7 @@ const child = wely({
     number: () => 3
   }),
   isOnlyCsr: true,
+  className: ({ data: { back } }) => back,
   html: ({
     data: { message },
     props: { color }
@@ -30,7 +31,13 @@ const child = wely({
     }
   ],
   events: [
-    { handler: 'click', method: ({ data: { count } }) => console.log(count++) },
+    {
+      handler: 'click',
+      method: ({ data }) => {
+        data.count++
+        console.log(data.count)
+      }
+    },
     {
       selector: 'div',
       handler: 'click',
@@ -43,9 +50,17 @@ const child2 = child.overwrite(() => ({ message: 'Good bye!' }))
 
 const parent = wely({
   name: 'parent',
+  data: () => ({
+    color: 'blue',
+    click: (message: string) => console.log(message)
+  }),
+  props: [
+    {
+      descendants: child2,
+      values: ({ color, click }) => ({ color, click })
+    }
+  ],
   className: 'test',
-  data: () => ({ color: 'blue', click: (message: string) => console.log(message) }),
-  props: [{ descendants: child2, props: ({ color, click }) => ({ color, click }) }],
   html: ({ props: { propsColor } }: { props: { propsColor: string } }) =>
     html`${child2}
       <p>propsColor: ${propsColor}</p>`
@@ -55,13 +70,33 @@ const grandParent = wely({
   name: 'grandParent',
   data: () => ({ color: 'green', fontSize: 24, number: 12 }),
   props: [
-    { descendants: [child, child2], props: ({ color }) => ({ color }) },
-    { descendants: parent, props: ({ color }) => ({ propsColor: color }) }
+    {
+      descendants: [child, child2],
+      values: ({ color }) => ({ color })
+    },
+    {
+      descendants: parent,
+      values: ({ color }) => ({ propsColor: color })
+    }
   ],
-  html: ({ data: { number } }) =>
-    html`${parent}
-      <p>人数: ${number}</p>`
+  html: html` <p>Content is...</p>
+    ${slot()}`,
+  // slot: ({ data: { fontSize, number } }) =>
+  //   html`${parent}
+  //     <p>人数: ${number}</p>
+  //     <input value="${fontSize}" />`
+  slot: [
+    ({ data: { fontSize, number } }) =>
+      html`${parent}
+        <p>人数: ${number}</p>
+        <input value="${fontSize}" />`,
+    {
+      name: 'test',
+      contents: html`${parent}`
+    }
+  ]
 })
 
 console.log(grandParent.ssr())
+
 grandParent.define()
