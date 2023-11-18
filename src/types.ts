d75ce447747c2352a71c7b5e37daf459b8fa9844
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-11-18 11:11:38 +0700

    refactoring: remove ssrCss in  type because it has already been unnecessary

--- src/types.ts
+++ src/types.ts
@@ -51,6 +51,5 @@ export interface <D, P> {
   html: Html<D, P>
   slot?: Html<D, P> | (Html<D, P> | { name: string; contents: Html<D, P> })[]
   css?: Css<D, P>
-  ssrCss?: Css<D, P>
   events?: Events<D, P>
 }
