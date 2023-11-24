123917885657c34309825db9505c808330363abc
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-11-24 17:56:51 +0700

    refactoring: rename effects to reflections to understand easily

--- src/types.ts
+++ src/types.ts
@@ -44,7 +44,6 @@ export interface <D, P> {
   Id?: string
   name: string
   data?: () => D
-  computed?: (data: D) => D
   props?: Props<D>
   isOnlyCsr?: boolean
   className?: Class<D, P>
@@ -52,5 +51,5 @@ export interface <D, P> {
   slot?: Html<D, P> | Slot<D, P>
   css?: Css<D, P>
   events?: Events<D, P>
-  effects?: () => Effects<D>
+  reflections?: () => Effects<D>
 }
