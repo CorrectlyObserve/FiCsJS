5e78e7534c7239a5b3980e29d484855813766cc5
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2023-12-16 12:50:37 +0700

    feature: delete an unnecessary type

--- src/packages/core/types.ts
+++ src/packages/core/types.ts
@@ -39,12 +39,6 @@ export type Sanitized<D extends object, P extends object> = (Element<D, P> | str
 
 export type Slot<D, P> = Html<D, P> | (Html<D, P> | { name: string; contents: Html<D, P> })[]
 
-export type UpdatePropsTrees<P> = {
-  descendantId: string
-  propsKey: keyof P
-  updateSetProps: (setProps: (value: P[keyof P]) => void) => void
-}[]
-
 export interface <D, P> {
   Id?: string
   name: string
