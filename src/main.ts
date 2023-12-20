6d5ed1247b83ed8a83090b867b9d4d7690998c30
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-20 10:41:27 +0700

    test: load the latest components

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import GrandParent from './grandParent'
+import { Parent } from './parent'
+import { Child } from './child'
 
-const grandParent = GrandParent('green')
-
-console.log(grandParent.ssr())
-
-grandParent.define()
+const child = Child('Good bye!')
+const parent = Parent(child)
+const grandParent = GrandParent('green', child, parent)
 
 fetch('https://jsonplaceholder.typicode.com/comments/1')
   .then(response => response.json())
   .then(json => grandParent.setData('email', json.email))
+
+console.log(grandParent.ssr())
+
+grandParent.define()
