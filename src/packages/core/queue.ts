bc9cbd128d388be6f8fe8679e6c1a60b574a53d9
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-22 09:46:05 +0700

    refactoring: improve queue for re-rendering

--- src/packages/core/queue.ts
+++ src/packages/core/queue.ts
@@ -1,23 +1,23 @@
-import WelyElement from './class'
-
-const queue: WelyElement<object, object>[] = []
+const queue: (() => void)[] = []
 const ids: string[] = []
 let hasQueue: boolean = false
 
 const processQueue = async (): Promise<void> => {
   while (queue.length > 0) {
-    const wely = queue[0]
+    const func = queue[0]
 
     queue.shift()
     ids.shift()
+
+    func()
   }
 
   queue.length > 0 ? await processQueue() : (hasQueue = false)
 }
 
-const setQueue = async (wely: WelyElement<any, any>, welyId: string): Promise<void> => {
+const setQueue = async (func: () => void, welyId: string): Promise<void> => {
   if (!ids.includes(welyId)) {
-    queue.push(wely)
+    queue.push(func)
     ids.push(welyId)
 
     if (!hasQueue) {
