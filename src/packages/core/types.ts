1a9fc2c7755b3363898ba6051320ed8fd5731076
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-26 10:58:38 +0700

    refactoring: make types understand easily

--- src/packages/core/types.ts
+++ src/packages/core/types.ts
@@ -1,12 +1,10 @@
 import WelyElement from './class'
 
-type ArrowFuncOrValue<V, D, P> = V | (({ data, props }: { data: D; props: P }) => V)
-
-export type Class<D, P> = ArrowFuncOrValue<string, D, P>
+export type ClassName<D, P> = Value<string, D, P>
 
 export type Css<D, P> = (
   | string
-  | { selector: string; style: ArrowFuncOrValue<Record<string, string | number>, D, P> }
+  | { selector: string; style: Value<Record<string, string | number>, D, P> }
 )[]
 
 type Descendant = WelyElement<any, any>
@@ -17,7 +15,7 @@ export type Events<D, P> = {
   method: Method<D, P>
 }[]
 
-export type Html<D, P> = ArrowFuncOrValue<Record<symbol, (Descendant | string)[]>, D, P>
+export type Html<D, P> = Value<Record<symbol, (Descendant | string)[]>, D, P>
 
 export type Method<D, P> = (
   {
@@ -43,13 +41,15 @@ export type Reflections<D> = { [K in keyof Partial<D>]: (data: D[K]) => void }
 
 export type Sanitized<D extends object, P extends object> = (WelyElement<D, P> | string)[]
 
+export type Value<V, D, P> = V | (({ data, props }: { data: D; props: P }) => V)
+
 export interface Wely<D, P> {
   name: string
   data?: () => D
   reflections?: Reflections<D>
   props?: Props<D>
   isOnlyCsr?: boolean
-  className?: Class<D, P>
+  className?: ClassName<D, P>
   html: Html<D, P>
   css?: Css<D, P>
   events?: Events<D, P>
