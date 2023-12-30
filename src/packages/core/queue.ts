dbff4f8037542e2ff488667b71f4c4f9031931c0
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-30 08:14:08 +0700

    refactoring: load Queue type and use object instead of arrary to improve performance

--- src/packages/core/queue.ts
+++ src/packages/core/queue.ts
@@ -1,22 +1,24 @@
-const queue: (() => void)[] = []
-const ids: string[] = []
+import { Queue } from './types'
+
+const queue: Queue[] = []
+const ids: Record<string, boolean> = {}
 let hasQueue: boolean = false
 
 const processQueue = async (): Promise<void> => {
   while (queue.length > 0) {
-    const func = queue.shift()!
-    ids.shift()
+    const queueEl: Queue = queue.shift()!
 
-    func()
+    delete ids[queueEl.welyId]
+    queueEl.reRender()
   }
 
   hasQueue = false
 }
 
-const addQueue = (func: () => void, welyId: string): void => {
-  if (!ids.includes(welyId)) {
-    queue.push(func)
-    ids.push(welyId)
+const addQueue = (queueEl: Queue): void => {
+  if (!ids[queueEl.welyId]) {
+    queue.push(queueEl)
+    ids[queueEl.welyId] = true
 
     if (!hasQueue) {
       hasQueue = true
