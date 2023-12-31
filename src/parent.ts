e273a3266735838b3fd4c685689c8d2cc265d72d
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-31 12:03:48 +0700

    refactoring: reflect renaming a file

--- src/parent.ts
+++ src/parent.ts
@@ -1,4 +1,4 @@
-import { fics, html } from './packages/core/wely'
+import { fics, html } from './packages/core/fics'
 import { ChildType } from './child'
 
 export const Parent = (child: ChildType) => {
