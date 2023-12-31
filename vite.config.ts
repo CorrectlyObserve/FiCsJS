23214926ad2bffc497409050e445ce3ae87e210f
MasamiOgasawara <m-ogasawara@wely.tech>
2023-12-31 14:40:18 +0700

    feature: create a pages directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
-export default defineConfig({})
+const root = 'src'
+
+export default defineConfig({
+  root,
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, root, 'index.html'),
+        privacy: resolve(__dirname, root, 'todo/index.html')
+      }
+    }
+  }
+})
