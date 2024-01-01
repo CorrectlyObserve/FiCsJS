02615887e483e948c4e9c6f773ed21a6c7b3048a
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-01-01 17:02:18 +0700

    debug: reflect fixing getStyle method to header

--- src/components/header.ts
+++ src/components/header.ts
@@ -10,7 +10,6 @@ const Header = () =>
     }),
     html: ({ data: { title } }) => html`<header><h1 align="center">${title}</h1></header>`,
     css: [
-      `h1 { -webkit-text-fill-color: transparent; }`,
       {
         selector: 'header',
         style: ({ data: { md } }) => ({ padding: `${md} 0`, marginBottom: md })
@@ -20,6 +19,7 @@ const Header = () =>
         style: ({ data: { pink } }) => ({
           background: `linear-gradient(30deg, var(--red) 20%, ${pink})`,
           backgroundClip: 'text',
+          webkitTextFillColor: 'transparent',
           margin: 0
         })
       }
