69afefc068aaf6f304b6d4809c927beec291af0b
MasamiOgasawara <m-ogasawara@wely.tech>
2024-01-04 13:04:41 +0700

    feature: load components

---
+++ src/todo/main.ts
@@ -0,0 +1,9 @@
+import Header from '../components/header'
+import { TodoInput } from '../components/todoInput'
+import { TodoList } from '../components/todoList'
+import Footer from '../components/footer'
+
+Header().define()
+TodoInput().define()
+TodoList().define()
+Footer().define()
