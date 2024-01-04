00311aa20372fc0a71b53c80fd0ceab30114e316
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-01-04 23:09:27 +0700

    feature: reflect change of svg component

--- src/todo/main.ts
+++ src/todo/main.ts
@@ -1,9 +1,10 @@
 import Header from '../components/header'
 import { TodoInput } from '../components/todoInput'
+import { Svg } from '../components/svg'
 import { TodoList } from '../components/todoList'
 import Footer from '../components/footer'
 
 Header().define()
-TodoInput().define()
+TodoInput(Svg()).define()
 TodoList().define()
 Footer().define()
