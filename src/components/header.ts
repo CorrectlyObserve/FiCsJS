800f718ddd9ee23c9b305ab0d3073b80df13acdf
MasamiOgasawara <m-ogasawara@wely.tech>
2024-01-04 12:48:40 +0700

    refactoring: improve css to understand easily

--- src/components/header.ts
+++ src/components/header.ts
@@ -3,19 +3,12 @@ import { fics, html } from '../packages/core/fics'
 const Header = () =>
   fics({
     name: 'header',
-    data: () => ({
-      title: 'To Do App by FiCsJS',
-      md: 'var(--md)',
-      pink: '#e198b4'
-    }),
+    data: () => ({ title: 'To Do App by FiCsJS', pink: '#e198b4' }),
     html: ({ data: { title } }) => html`<header><h1 align="center">${title}</h1></header>`,
     css: [
+      { selector: 'header', style: () => ({ padding: 'var(--md) 0' }) },
       {
-        selector: 'header',
-        style: ({ data: { md } }) => ({ padding: `${md} 0`, marginBottom: md })
-      },
-      {
-        selector: 'h1',
+        selector: 'header > h1',
         style: ({ data: { pink } }) => ({
           background: `linear-gradient(30deg, var(--red) 20%, ${pink})`,
           backgroundClip: 'text',
