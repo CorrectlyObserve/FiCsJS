806f630ff76bb4f7e6b511f0353bfff2089baded
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-01-07 22:47:06 +0700

    refactoring: edit header to improve maintainability

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,19 +1,20 @@
 import { fics, html } from '../packages/core/fics'
 
-const Header = () =>
+const Header = (css: string) =>
   fics({
     name: 'header',
     data: () => ({ title: 'To Do App by FiCsJS', pink: '#e198b4' }),
-    html: ({ data: { title } }) => html`<header><h1 align="center">${title}</h1></header>`,
+    html: ({ data: { title } }) => html`<header><h1>${title}</h1></header>`,
     css: [
+      css,
       { selector: 'header', style: () => ({ padding: 'var(--md) 0' }) },
       {
-        selector: 'header > h1',
+        selector: 'header h1',
         style: ({ data: { pink } }) => ({
           background: `linear-gradient(30deg, var(--red) 20%, ${pink})`,
           backgroundClip: 'text',
           webkitTextFillColor: 'transparent',
-          margin: 0
+          textAlign: 'center'
         })
       }
     ]
