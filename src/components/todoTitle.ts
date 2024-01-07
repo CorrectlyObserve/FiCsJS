caaaee34f5690fc6d5836593ef67323c22447d49
MasamiOgasawara <m-ogasawara@wely.tech>
2024-01-07 15:31:47 +0700

    feature: edit todoTitle component

--- src/components/todoTitle.ts
+++ src/components/todoTitle.ts
@@ -1,9 +1,14 @@
 import { fics, html } from '../packages/core/fics'
+import css from '../styles/todo.css?inline'
 
 export const TodoTitle = () =>
   fics({
     name: 'todo-title',
     html: ({ props: { numberOfTasks } }: { props: { numberOfTasks: number } }) =>
       html`<p>Remaining tasks: ${numberOfTasks}</p>`,
-    css: [{ selector: 'p', style: () => ({ color: '#fff' }) }]
+    css: [
+      css,
+      { style: () => ({ display: 'block', marginBottom: 'calc(var(--md) * 2)' }) },
+      { selector: 'p', style: () => ({ color: '#fff', textAlign: 'center' }) }
+    ]
   })
