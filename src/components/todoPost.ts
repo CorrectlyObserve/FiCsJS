15c98f3ed8ee82318a0441806b8039877810fe7d
MasamiOgasawara <m-ogasawara@wely.tech>
2024-01-07 23:41:20 +0700

    refactoring: reflect change of FiCsElement

--- src/components/todoPost.ts
+++ src/components/todoPost.ts
@@ -5,20 +5,19 @@ import css from '../styles/todoPost.css?inline'
 export const TodoPost = (svg: SvgType) =>
   fics({
     name: 'todo-post',
-    data: () => ({ binding: 'post', value: '', placeholder: 'Please enter a new task.' }),
+    data: () => ({ bind: 'post', value: '', placeholder: 'Please enter a new task.' }),
     props: [{ descendants: svg, values: () => ({ path: 'add', color: '#fff' }) }],
-    html: ({ data: { binding, value, placeholder } }) =>
+    html: ({ data: { bind, value, placeholder } }) =>
       html`<div class="container">
-        <input binding="${binding}" type="text" value="${value}" placeholder="${placeholder}" />
-        ${svg}
+        <input bind="${bind}" type="text" value="${value}" placeholder="${placeholder}" />${svg}
       </div>`,
     css: [css],
     actions: [
       {
         handler: 'input',
         selector: 'input',
-        method: ({ data: { binding }, setData }, event) =>
-          setData('value', (<HTMLInputElement>event.target).value, binding)
+        method: ({ data: { bind }, setData }, event) =>
+          setData('value', (<HTMLInputElement>event.target).value, bind)
       }
     ]
   })
