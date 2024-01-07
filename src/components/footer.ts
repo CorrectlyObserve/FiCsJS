8c95dab02410901516170a8e6273abf26824e34d
MasamiOgasawara <m-ogasawara@wely.tech>
2024-01-07 22:47:32 +0700

    refactoring: edit footer to improve maintainability

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -1,13 +1,17 @@
 import { fics, html } from '../packages/core/fics'
 
-const Footer = () =>
+const Footer = (css: string) =>
   fics({
     name: 'footer',
     data: () => ({ copyright: '&copy; 2023 Masami Ogasawara' }),
-    html: ({ data: { copyright } }) => html`<footer><p align="center">${copyright}</p></footer>`,
+    html: ({ data: { copyright } }) => html`<footer><p>${copyright}</p></footer>`,
     css: [
+      css,
       { selector: 'footer', style: () => ({ padding: 'var(--md) 0' }) },
-      { selector: 'footer > p', style: () => ({ fontSize: 'var(--md)', color: '#fff', margin: 0 }) }
+      {
+        selector: 'footer p',
+        style: () => ({ fontSize: 'var(--md)', color: '#fff', textAlign: 'center' })
+      }
     ]
   })
 
