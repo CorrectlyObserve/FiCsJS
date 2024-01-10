dd183242576e70b69ba79dea8d370afdca2df4f4
MasamiOgasawara <m-ogasawara@wely.tech>
2024-01-10 18:21:25 +0700

    refactoring: reflect change of FiCsElement

--- src/components/todoTitle.ts
+++ src/components/todoTitle.ts
@@ -5,14 +5,13 @@ export const TodoTitle = () =>
   fics({
     name: 'todo-title',
     data: () => ({ md: 'var(--md)' }),
-    html: ({ props: { length } }: { props: { length: number } }) =>
-      html`<p>Remaining tasks: ${length}</p>`,
+    html: (_, { length }: { length: number }) => html`<p>Remaining tasks: ${length}</p>`,
     css: [
       css,
-      { style: ({ data: { md } }) => ({ display: 'block', marginBottom: `calc(${md} * 2)` }) },
+      { style: ({ md }) => ({ display: 'block', marginBottom: `calc(${md} * 2)` }) },
       {
         selector: 'p',
-        style: ({ data: { md } }) => ({ fontSize: md, color: '#fff', textAlign: 'center' })
+        style: ({ md }) => ({ fontSize: md, color: '#fff', textAlign: 'center' })
       }
     ]
   })
