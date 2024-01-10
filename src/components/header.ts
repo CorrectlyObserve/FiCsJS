a47a37d2331024465d50989cad15d9efd4dd7e5e
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-01-10 18:21:08 +0700

    refactoring: reflect change of FiCsElement

--- src/components/header.ts
+++ src/components/header.ts
@@ -4,13 +4,13 @@ const Header = (css: string) =>
   fics({
     name: 'header',
     data: () => ({ title: 'To Do App by FiCsJS', pink: '#e198b4' }),
-    html: ({ data: { title } }) => html`<header><h1>${title}</h1></header>`,
+    html: ({ title }) => html`<header><h1>${title}</h1></header>`,
     css: [
       css,
-      { selector: 'header', style: () => ({ padding: 'var(--md) 0' }) },
+      { selector: 'header', style: { padding: 'var(--md) 0' } },
       {
         selector: 'header h1',
-        style: ({ data: { pink } }) => ({
+        style: ({ pink }) => ({
           background: `linear-gradient(30deg, var(--red) 20%, ${pink})`,
           backgroundClip: 'text',
           webkitTextFillColor: 'transparent',
