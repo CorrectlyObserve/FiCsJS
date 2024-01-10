3288867007d6aef8f8ae1983e5cac5f583aa1ab8
MasamiOgasawara <m-ogasawara@wely.tech>
2024-01-10 18:20:59 +0700

    refactoring: reflect change of FiCsElement

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -4,13 +4,13 @@ const Footer = (css: string) =>
   fics({
     name: 'footer',
     data: () => ({ copyright: '&copy; 2023 Masami Ogasawara', md: 'var(--md)' }),
-    html: ({ data: { copyright } }) => html`<footer><p>${copyright}</p></footer>`,
+    html: ({ copyright }) => html`<footer><p>${copyright}</p></footer>`,
     css: [
       css,
-      { selector: 'footer', style: ({ data: { md } }) => ({ padding: `${md} 0` }) },
+      { selector: 'footer', style: ({ md }) => ({ padding: `${md} 0` }) },
       {
         selector: 'footer p',
-        style: ({ data: { md } }) => ({ fontSize: md, color: '#fff', textAlign: 'center' })
+        style: ({ md }) => ({ fontSize: md, color: '#fff', textAlign: 'center' })
       }
     ]
   })
