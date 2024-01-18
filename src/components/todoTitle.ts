6f1fcdd2e1bc38d9771fef153d44faf3f8d3fdf0
MasamiOgasawara <m-ogasawara@wely.tech>
2024-01-18 15:08:14 +0700

    feature: reflect change of integrate html function to FiCsElement

--- src/components/todoTitle.ts
+++ src/components/todoTitle.ts
@@ -1,11 +1,11 @@
-import { fics, html } from '../packages/core/fics'
+import { fics } from '../packages/core/fics'
 import css from '../styles/todo.css?inline'
 
 export const TodoTitle = () =>
   fics({
     name: 'todo-title',
     data: () => ({ md: 'var(--md)' }),
-    html: (_, { length }: { length: number }) => html`<p>Remaining tasks: ${length}</p>`,
+    html: ({ html }, { length }: { length: number }) => html`<p>Remaining tasks: ${length}</p>`,
     css: [
       css,
       { style: ({ md }) => ({ display: 'block', marginBottom: `calc(${md} * 2)` }) },
