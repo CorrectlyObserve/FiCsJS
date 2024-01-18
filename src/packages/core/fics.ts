6a32a10372404409424613483f6f9b45500c3834
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-01-18 15:04:37 +0700

    feature: delete html function

--- src/packages/core/fics.ts
+++ src/packages/core/fics.ts
@@ -1,38 +1,5 @@
 import FiCsElement from './class'
-import symbol from './symbol'
-import { FiCs, Sanitized } from './types'
-
-export const html = <D extends object, P extends object>(
-  templates: TemplateStringsArray,
-  ...variables: unknown[]
-): Record<symbol, Sanitized<D, P>> => {
-  const result: (Sanitized<D, P> | unknown)[] = new Array()
-
-  for (const [index, template] of templates.entries()) {
-    const trimmed: string = template.trim()
-    const sanitize = (arg: unknown): Sanitized<D, P> | unknown =>
-      typeof arg === 'string' && arg !== ''
-        ? arg.replaceAll(/[<>]/g, tag => (tag === '<' ? '&lt;' : '&gt;'))
-        : arg ?? ''
-
-    const variable: Sanitized<D, P> | unknown = sanitize(variables[index])
-
-    if (index === 0 && template === '') result.push(variable)
-    else {
-      const last: Sanitized<D, P> | unknown = result[result.length - 1] ?? ''
-      const isFiCsElement: boolean = variable instanceof FiCsElement
-
-      if (last instanceof FiCsElement)
-        isFiCsElement ? result.push(trimmed, variable) : result.push(`${trimmed}${variable}`)
-      else {
-        result.splice(result.length - 1, 1, `${last}${trimmed}${isFiCsElement ? '' : variable}`)
-        if (isFiCsElement) result.push(variable)
-      }
-    }
-  }
-
-  return { [symbol]: result as Sanitized<D, P> }
-}
+import { FiCs } from './types'
 
 export const fics = <D extends object, P extends object>({
   name,
