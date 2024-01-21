e2673bf539e14fc1898fc54d31d9652323f7ce12
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-01-21 10:25:33 +0700

    debug: fix the error of props type

--- src/components/svg.ts
+++ src/components/svg.ts
@@ -1,21 +1,20 @@
 import fics from '../packages/core/fics'
 
-interface Props {
-  path: string
-  color: string
-  click: () => void
-}
-
 export const Svg = () =>
   fics({
     name: 'svg',
     data: () => ({ size: 'var(--lg)', dir: '../icons' }),
-    html: ({ html }, props: Props) => html`<button />`,
+    props: {} as {
+      path: string
+      color: string
+      click: () => void
+    },
+    html: ({ html }) => html`<button />`,
     css: [
       { style: { display: 'flex' } },
       {
         selector: 'button',
-        style: ({ size, dir }, { path, color }: Props) => ({
+        style: ({ size, dir }, { path, color }) => ({
           width: size,
           height: size,
           maskImage: `url("${dir}/${path}.svg")`,
@@ -25,7 +24,7 @@ export const Svg = () =>
         })
       }
     ],
-    actions: [{ handler: 'click', method: (_, { click }: Props) => click() }]
+    actions: [{ handler: 'click', method: (_, { click }) => click() }]
   })
 
 export type SvgType = ReturnType<typeof Svg>
