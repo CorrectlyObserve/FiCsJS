7fcb4ea7ca7076df0c6d8b154116ef4f9be0cf3f
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-02-02 10:13:58 +0700

    feature: reflect change of getHtml method

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -4,7 +4,7 @@ const Footer = (css: string) =>
   fics({
     name: 'footer',
     data: () => ({ copyright: '&copy; 2023 Masami Ogasawara', md: 'var(--md)' }),
-    html: ({ data: { copyright }, html }) => html`<footer><p>${copyright}</p></footer>`,
+    html: ({ data: { copyright }, template }) => template`<footer><p>${copyright}</p></footer>`,
     css: [
       css,
       { selector: 'footer', style: ({ data: { md } }) => ({ padding: `${md} 0` }) },
