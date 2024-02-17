147984ae97b5d9da1e95ed04416736269a07bd8e
MasamiOgasawara <m-ogasawara@wely.tech>
2024-02-17 22:31:32 +0700

    debug: reflect fixing render method

--- src/iframe/main.ts
+++ src/iframe/main.ts
@@ -18,12 +18,24 @@ const Iframe = () =>
         />`
   })
 
-const Content = () =>
+type IframeType = ReturnType<typeof Iframe>
+
+const Video = () =>
+  fics({
+    name: 'video',
+    data: () => ({ value: '' }),
+    html: ({ template, bind }) =>
+      template`<video ${bind()} width="500" src="https://b-risk.jp/wp/wp-content/uploads/2021/08/mp4_embedding.mp4" controls loop autoplay muted playsinline></video>`
+  })
+
+type VideoType = ReturnType<typeof Video>
+
+const Content = (iframe: IframeType, video: VideoType) =>
   fics({
     name: 'iframe-sample',
     data: () => ({ value: '' }),
     html: ({ data: { value }, template, bind }) =>
-      template`<div><input ${bind()} value="${value}" type="text" /><p>value: ${value}</p></div>${Iframe()}`,
+      template`<div><input ${bind()} value="${value}" type="text" /><p>value: ${value}</p></div>${iframe}${video}`,
     css: [
       { selector: 'div', style: { padding: '32px', textAlign: 'center' } },
       { selector: 'p', style: { color: '#fff' } }
@@ -37,11 +49,28 @@ const Content = () =>
     ]
   })
 
-fics({
-  name: 'route',
-  data: () => ({ value: '' }),
-  html: ({ template, bind }) =>
-    template`<audio ${bind()} controls loop>
+type ContentType = ReturnType<typeof Content>
+
+const Route = (content: ContentType) =>
+  fics({
+    name: 'route',
+    data: () => ({ value: '' }),
+    html: ({ data: { value }, template, bind }) =>
+      template`<div><input ${bind()} value="${value}" type="text" /><p>value: ${value}</p></div>
+    <audio ${bind()} controls loop>
           <source src="https://qumeru.com/preview/JavaScript/376/sample1/musics/music.mp3" type="audio/mp3">
-        </audio>${Content()}`
-}).define()
+        </audio>${content}`,
+    css: [
+      { selector: 'div', style: { padding: '32px', textAlign: 'center' } },
+      { selector: 'p', style: { color: '#fff' } }
+    ],
+    actions: [
+      {
+        selector: 'input',
+        handler: 'input',
+        method: ({ setData, event }) => setData('value', (event.target as HTMLInputElement).value)
+      }
+    ]
+  })
+
+Route(Content(Iframe(), Video())).define()
