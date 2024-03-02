9f3c0906a95be060150ca6452e7674dbd9087dea
MasamiOgasawara <m-ogasawara@wely.tech>
2024-03-02 15:48:40 +0700

    refactoring: reflect change of deleting a file

--- src/packages/core/fics.ts
+++ src/packages/core/fics.ts
@@ -1,6 +1,5 @@
 import FiCsElement from './class'
 import { FiCs } from './types'
-import { toKebabCase } from './utils'
 
 const fics = <D extends object, P extends object>({
   name,
@@ -13,22 +12,18 @@ const fics = <D extends object, P extends object>({
   html,
   css,
   actions
-}: FiCs<D, P>): FiCsElement<D, P> => {
-  if ({ var: true, iframe: true }[toKebabCase(name)])
-    throw new Error(`${name} is a reserved word in FiCsJS...`)
-  else
-    return new FiCsElement({
-      name,
-      data,
-      reflections,
-      inheritances,
-      props,
-      isOnlyCsr,
-      className,
-      html,
-      css,
-      actions
-    })
-}
+}: FiCs<D, P>): FiCsElement<D, P> =>
+  new FiCsElement({
+    name,
+    data,
+    reflections,
+    inheritances,
+    props,
+    isOnlyCsr,
+    className,
+    html,
+    css,
+    actions
+  })
 
 export default fics
