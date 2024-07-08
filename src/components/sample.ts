794bd8b247313b87e77753c9fba55bcc0b612ff0
MasamiOgasawara <m-ogasawara@wely.tech>
2024-07-09 00:56:32 +0700

    feature: reflect importJson function and FiCsElement

--- src/components/sample.ts
+++ src/components/sample.ts
@@ -1,11 +1,19 @@
 import fics from '../packages/core/fics'
+import importJson from '../packages/core/i18n'
+
+const json = await importJson({ langs: ['ja'], directory: '.' })
 
 export const Sample = () =>
   fics({
     name: 'sample',
     data: () => ({ value: '' }),
-    html: ({ data: { value }, template }) =>
-      template`<div><input key="1" value="${value}" value="1" /><p>value is: ${value}</p></div>`,
+    html: ({ data: { value }, template, i18n }) =>
+      template`
+        <p>${i18n({ json, lang: 'ja', keys: ['apple', 'banana'] })}</p>
+        ${value !== '' ? template`<p>${'value' + value}</p>` : ''}
+        <input key="1" value="${value}" />
+        ${value !== '' ? template`<input value="${value}" />` : ''}
+      `,
     css: [{ style: { color: 'white' } }, { selector: 'div', style: { padding: '32px' } }],
     actions: [
       {
