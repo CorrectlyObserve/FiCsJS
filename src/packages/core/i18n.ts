aa8a8dce22c4ab600f228179cca9e03986e25db1
MasamiOgasawara <m-ogasawara@wely.tech>
2024-07-09 00:49:44 +0700

    feature: create importJson function

---
+++ src/packages/core/i18n.ts
@@ -0,0 +1,15 @@
+const importJson = async ({
+  langs,
+  directory
+}: {
+  langs: string[]
+  directory: string
+}): Promise<Record<string, string>> => {
+  const json: Record<string, string> = {}
+
+  for (const lang of langs) json[lang] = await import(`${directory}/${lang}.json`)
+
+  return json
+}
+
+export default importJson
