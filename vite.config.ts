b9bb9b72aa2e89919d0ae7a4e7ba504ea0e449a8
MasamiOgasawara <m-ogasawara@wely.tech>
2024-07-17 10:16:25 +0700

    refactoring: make it simple

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import { glob } from 'glob';
+import { glob } from 'glob'
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
     emptyOutDir: true,
     outDir: '../dist',
-    rollupOptions: {input: glob.sync('src/*.html')},
+    rollupOptions: { input: glob.sync('src/*.html') },
     minify: true
   },
   server: { hmr: true }
-});
+})
