d7aa1c06a3881c8975fde2ad77f917d357bf18fb
MasamiOgasawara <m-ogasawara@wely.tech>
2024-08-19 16:00:16 +0700

    test: add key to input

--- src/components/sample.ts
+++ src/components/sample.ts
@@ -4,19 +4,19 @@ export const Sample = () =>
   fics({
     name: 'sample',
     data: () => ({ value: '' }),
-    html: ({ data: { value }, template }) =>
-      template`
-        ${value !== '' ? template`<p>${value}</p>` : ''}
-        <input id="id" type="text" value="${value}" />
-        ${value !== '' ? template`<input type="text" value="${value}" />` : ''}
+    html: ({ $data: { value }, $template }) =>
+      $template`
+        ${value !== '' ? $template`<p>${value}</p>` : ''}
+        <input key="1" type="text" value="${value}" />
+        ${value !== '' ? $template`<input type="text" value="${value}" />` : ''}
       `,
     css: [{ style: { color: 'white' } }],
     actions: [
       {
         handler: 'input',
         selector: 'input',
-        method: ({ setData, event }) => {
-          setData('value', (event.target as HTMLInputElement).value)
+        method: ({ $setData, $event }) => {
+          $setData('value', ($event.target as HTMLInputElement).value)
         }
       }
     ]
