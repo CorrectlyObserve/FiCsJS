6cbc64c142f54f69e88abb5e70516ec2134ad5d5
MasamiOgasawara <m-ogasawara@wely.tech>
2024-08-20 19:57:28 +0700

    feature: reflect change of original syntax

--- src/components/parent.ts
+++ src/components/parent.ts
@@ -26,8 +26,8 @@ export const Parent = (child: ChildType) => {
     ],
     props: {} as { propsColor: string },
     className: 'test',
-    html: ({ props: { propsColor }, template }) =>
-      template`${child}
+    html: ({ $props: { propsColor }, $template }) =>
+      $template`${child}
         <p>propsColor: ${propsColor}</p>`
   })
 }
