41de78370ff6f6e2fdd475fc8b2176d67f3f4ad6
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-08-31 23:34:28 +0700

    refactoring: use destructuring for readability

--- src/packages/core/queue.ts
+++ src/packages/core/queue.ts
@@ -13,10 +13,10 @@ const addToQueue = (queue: Queue): void => {
       isProcessing = true
 
       while (queues.length > 0) {
-        const queue: Queue = queues.shift()!
+        const { ficsId, reRender }: Queue = queues.shift()!
 
-        delete ficsIds[queue.ficsId]
-        queue.reRender
+        delete ficsIds[ficsId]
+        reRender
       }
 
       isProcessing = false
