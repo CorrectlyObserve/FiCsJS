a554594443dd3b58b7cd8a6a783fd959e9c657ec
MasamiOgasawara <m-ogasawara@wely.tech>
2024-09-05 16:09:00 +0700

    refactoring: make it easier to understand

--- src/packages/router/param.ts
+++ src/packages/router/param.ts
@@ -1,18 +1,17 @@
 export default (pattern: string, path: string): Map<string, string> => {
-  const param: RegExp = /\/:[^\/]+/g
+  const customParam: RegExp = /\/:[^\/]+/g
   const optional: RegExp = /\?$/
   const regExp: string = pattern.replaceAll(
-    param,
-    (str: string) => `${/\/([^/]+?)/}${optional.test(str) ? '?' : ''}`
+    customParam,
+    (str: string) => `\/([^/]+?)${optional.test(str) ? '?' : ''}`
   )
   const params: string[] = new RegExp(`^${regExp}/?$`).exec(path)?.slice(1) ?? []
   const paramMap: Map<string, string> = new Map()
 
   if (params.length > 0) {
-    const names: string[] = new Array()
-
-    for (const key of pattern.match(param) ?? [])
-      names.push(key.replace(optional, '').replace(/^\/:/, ''))
+    const names: string[] = (pattern.match(customParam) ?? []).map(param =>
+      param.replace(optional, '').replace(/^\/:/, '')
+    )
 
     if (names.length > 0)
       for (const [index, value] of params.entries()) paramMap.set(names[index], value ?? '')
