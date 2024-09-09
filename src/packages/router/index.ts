9e59a873ce6262d4a7d590ff4952ae6275a4ce20
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-09-09 16:30:10 +0700

    setting: define export syntax

---
+++ src/packages/router/index.ts
@@ -0,0 +1,6 @@
+import { Link, Router } from './components'
+import getPathParams from './getPathParams'
+import getQueryParams from './getQueryParams'
+import { getRegExp, pathParam } from './pathParam'
+
+export { Link, Router, getPathParams, getQueryParams, getRegExp, pathParam }
