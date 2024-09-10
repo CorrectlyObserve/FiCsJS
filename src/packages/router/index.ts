6c670da58948b2b9ae2e2383fc3a89304f720798
MasamiOgasawara <m-ogasawara@wely.tech>
2024-09-11 03:52:29 +0700

    refactoring: remove unnecessary exports

--- src/packages/router/index.ts
+++ src/packages/router/index.ts
@@ -1,6 +1,5 @@
 import { Link, Router } from './components'
 import getPathParams from './getPathParams'
 import getQueryParams from './getQueryParams'
-import { getRegExp, pathParam } from './pathParam'
 
-export { Link, Router, getPathParams, getQueryParams, getRegExp, pathParam }
+export { getPathParams, getQueryParams, Link, Router }
