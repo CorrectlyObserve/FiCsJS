26393580ba97ab132247bbbcbf61e60be50c2d83
MasamiOgasawara <m-ogasawara@wely.tech>
2024-09-24 00:31:17 +0700

    debug: reflect modification of FiCsLink type

--- packages/router/linkComponent.ts
+++ packages/router/linkComponent.ts
@@ -1,8 +1,8 @@
 import FiCsElement from '../core/class'
-import type { FiCsLink, RouterContent } from './types'
+import type { FiCsLink, RouterContent, RouterData } from './types'
 
 const Link = ({ href, content, router, className, attributes, css, actions, hooks }: FiCsLink) =>
-  new FiCsElement<{}, {}>({
+  new FiCsElement<RouterData, {}>({
     name: 'link',
     isExceptional: true,
     isImmutable: true,
@@ -11,7 +11,6 @@ const Link = ({ href, content, router, className, attributes, css, actions, hook
     attributes,
     html: ({ $template, $html, $show, $i18n }) => {
       const returned: RouterContent = content({ $template, $html, $show, $i18n })
-
       return $template`
         <a href="${href}">${returned instanceof FiCsElement ? $template`${returned}` : returned}</a>
       `
