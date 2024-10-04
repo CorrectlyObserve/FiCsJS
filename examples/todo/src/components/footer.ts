4b5e875d33552c86be934663d52b76ec4106c49f
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-04 09:19:57 +0700

    feature: reflect modification of ficsjs

--- examples/todo/src/components/footer.ts
+++ examples/todo/src/components/footer.ts
@@ -1,18 +1,19 @@
-import fics from '@ficsjs'
+import { fics } from '@ficsjs'
 import resetCss from '@/styles/resetCss'
 
-const Footer = () => fics({
-  name: 'footer',
-  isImmutable: true,
-  html: ({ $template }) => $template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
-  css: [
-    resetCss,
-    { selector: 'footer', style: { padding: 'var(--md) 0' } },
-    {
-      selector: 'footer p',
-      style: { fontSize: 'var(--sm)', color: '#fff', textAlign: 'center', lineHeight: '100%' }
-    }
-  ]
-})
+const Footer = () =>
+  fics({
+    name: 'footer',
+    isImmutable: true,
+    html: ({ $template }) => $template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
+    css: [
+      resetCss,
+      { selector: 'footer', style: { padding: 'var(--md) 0' } },
+      {
+        selector: 'footer p',
+        style: { fontSize: 'var(--sm)', color: '#fff', textAlign: 'center', lineHeight: '100%' }
+      }
+    ]
+  })
 
 export default Footer
