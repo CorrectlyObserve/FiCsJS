b3a810aaf9765c0a5bb3b32a3d82572b15df9f14
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-04 09:14:08 +0700

    feature: implement the queue architecture for automatic definition of web components

--- packages/core/queue.ts
+++ packages/core/queue.ts
@@ -1,25 +1,45 @@
 import type { Queue } from './types'
+import throwWindowError from './utils'
 
 const ficsIds: Record<string, boolean> = {}
 const queues: Queue[] = new Array()
+let hasLoaded: boolean = false
 let isProcessing: boolean = false
 
-export default (queue: Queue): void => {
+export const addToQueue = (queue: Queue): void => {
   if (!ficsIds[queue.ficsId]) {
     queues.push(queue)
     ficsIds[queue.ficsId] = true
 
-    if (!isProcessing) {
+    if (hasLoaded && !isProcessing) {
       isProcessing = true
 
       while (queues.length > 0) {
         const { ficsId, func }: Queue = queues.shift()!
 
         delete ficsIds[ficsId]
-        func
+        func()
       }
 
       isProcessing = false
     }
+  }
+}
+
+export const useClient = () => {
+  throwWindowError()
+  if (typeof document === 'undefined') throw new Error('document is not defined...')
+
+  const completeLoading = (): void => {
+    hasLoaded = true
+  }
+  const controlEventListener = (type: 'add' | 'remove'): void => {
+    window[`${type}EventListener`]('DOMContentLoaded', completeLoading)
+  }
+
+  if (document.readyState === 'loading') controlEventListener('add')
+  else {
+    controlEventListener('remove')
+    completeLoading()
   }
 }
