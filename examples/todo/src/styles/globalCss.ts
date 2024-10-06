1cfbc0e7cc1cfdad0af771d363538e7c989151db
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-06 12:06:53 +0700

    refactoring: rename a css file

---
+++ examples/todo/src/styles/globalCss.ts
@@ -0,0 +1,8 @@
+const globalCss = [
+  {
+    selector: '*',
+    style: { padding: 0, margin: 0, boxSizing: 'border-box' }
+  }
+]
+
+export default globalCss
--- examples/todo/src/styles/resetCss.ts
+++
@@ -1,7 +0,0 @@
-const resetCss = {
-  selector: '*',
-  style: { padding: 0, margin: 0, boxSizing: 'border-box' },
-  ssr: false
-}
-
-export default resetCss
