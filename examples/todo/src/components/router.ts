14e2ae083aca7a4135498cb22339f0c17b64f8db
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-10-06 12:09:40 +0700

    refactoring: reflect modification of ficsRouter

--- examples/todo/src/components/router.ts
+++ examples/todo/src/components/router.ts
@@ -1,12 +1,12 @@
 import { ficsRouter } from '@ficsjs/router'
-import Top from '@/components/top'
+import TopPage from '@/components/topPage'
 import NotFound from '@/components/notFound'
 
 const Router = (lang: string) =>
   ficsRouter({
     pages: [
-      { paths: ['/', `/${lang}`], content: () => Top() },
-      { paths: ['/todo', `/${lang}/todo`], content: ({ $template }) => $template`` }
+      { path: ['/', `/${lang}`], content: () => TopPage() },
+      { path: ['/todo', `/${lang}/todo`], content: ({ $template }) => $template`` }
     ],
     notFound: { content: () => NotFound() }
   })
