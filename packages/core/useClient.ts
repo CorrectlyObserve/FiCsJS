8bfc06f0bda92225568a178d6269b2176d66d8ef
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-10 15:53:31 +0700

    refactoring: separate useClient function from queue

---
+++ packages/core/useClient.ts
@@ -0,0 +1,23 @@
+import throwWindowError from './utils'
+
+let hasLoaded: boolean = false
+
+export const getHasLoaded = (): boolean => hasLoaded
+
+export const useClient = (): void => {
+  throwWindowError()
+  if (typeof document === 'undefined') throw new Error('document is not defined...')
+
+  const completeLoading = (): void => {
+    hasLoaded = true
+  }
+  const controlEventListener = (type: 'add' | 'remove'): void => {
+    window[`${type}EventListener`]('DOMContentLoaded', completeLoading)
+  }
+
+  if (document.readyState === 'loading') controlEventListener('add')
+  else {
+    controlEventListener('remove')
+    completeLoading()
+  }
+}
