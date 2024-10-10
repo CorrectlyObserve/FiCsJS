a5147814a3b9ce52755496a18bc71a254d80cc5c
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-10 16:20:42 +0700

    design: create reset css for all web components

---
+++ examples/todo/src/styles/resetCss.ts
@@ -0,0 +1,14 @@
+export default [
+  { selector: '*', style: { padding: 0, margin: 0, boxSizing: 'border-box' } },
+  { selector: 'button', style: { border: 0 } },
+  { selector: 'span', style: { color: 'inherit', lineHeight: '100%' } },
+  {
+    selector: 'a',
+    style: {
+      width: '100%',
+      display: 'inline-block',
+      color: 'inherit',
+      textDecoration: 'none'
+    }
+  }
+]
