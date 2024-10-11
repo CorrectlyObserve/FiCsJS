49429074d5464251368d93e496fdf3fc81d57672
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-11 23:29:05 +0700

    design: add default css

--- examples/todo/src/styles/resetCss.ts
+++ examples/todo/src/styles/resetCss.ts
@@ -1,6 +1,10 @@
 export default [
   { selector: '*', style: { padding: 0, margin: 0, boxSizing: 'border-box' } },
-  { selector: 'button', style: { border: 0 } },
+  { selector: ['h1', 'h2'], style: { textAlign: 'center' } },
+  { selector: 'h2', style: { marginBottom: 'var(--lg)' } },
+  { selector: ['h2', 'p', 'button'], style: { color: '#fff', textAlign: 'center' } },
+  { selector: ['p', 'button'], style: { fontSize: 'var(--md)', lineHeight: '100%' } },
+  { selector: 'button', style: { border: 0, cursor: 'pointer' } },
   { selector: 'span', style: { color: 'inherit', lineHeight: '100%' } },
   {
     selector: 'a',
@@ -8,6 +12,7 @@ export default [
       width: '100%',
       display: 'inline-block',
       color: 'inherit',
+      cursor: 'pointer',
       textDecoration: 'none'
     }
   }
