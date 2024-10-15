4309aec5f4b24c5eea2a168614ec348b99abff5d
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-15 10:18:42 +0700

    refactoring: organize duplicated css

--- examples/todo/src/styles/resetCss.ts
+++ examples/todo/src/styles/resetCss.ts
@@ -1,23 +1,18 @@
 export default [
   { selector: '*', style: { padding: 0, margin: 0, boxSizing: 'border-box' } },
-  { selector: ['h1', 'h2'], style: { textAlign: 'center' } },
+  { selector: ['h1', 'h2', 'p', 'button'], style: { textAlign: 'center' } },
   { selector: 'h2', style: { marginBottom: 'var(--lg)' } },
-  { selector: ['h2', 'p', 'button'], style: { color: '#fff', textAlign: 'center' } },
-  { selector: ['p', 'button'], style: { fontSize: 'var(--md)', lineHeight: '100%' } },
+  { selector: ['h2', 'p', 'button'], style: { color: '#fff' } },
+  { selector: ['p', 'button'], style: { fontSize: 'var(--md)' } },
+  { selector: ['p', 'button', 'span'], style: { lineHeight: '100%' } },
   {
     selector: 'button',
     style: { border: 0, cursor: 'pointer', transition: '0.2s ease-out' },
     nested: [{ selector: '&:hover', style: { opacity: '0.5' } }]
   },
-  { selector: 'span', style: { color: 'inherit', lineHeight: '100%' } },
+  { selector: ['span', 'a'], style: { color: 'inherit' } },
   {
     selector: 'a',
-    style: {
-      width: '100%',
-      display: 'inline-block',
-      color: 'inherit',
-      cursor: 'pointer',
-      textDecoration: 'none'
-    }
+    style: { width: '100%', display: 'inline-block', cursor: 'pointer', textDecoration: 'none' }
   }
 ]
