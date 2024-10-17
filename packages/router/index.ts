6496d589452a49bcba1fe9d88d268786ccbef2ba
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-17 22:45:48 +0700

    feature: export goto function

--- packages/router/index.ts
+++ packages/router/index.ts
@@ -1,6 +1,7 @@
 import getPathParams from './getPathParams'
 import getQueryParams from './getQueryParams'
+import goto from './goto'
 import ficsLink from './link'
 import ficsRouter from './router'
 
-export { ficsLink, ficsRouter, getPathParams, getQueryParams }
+export { ficsLink, ficsRouter, getPathParams, getQueryParams, goto }
