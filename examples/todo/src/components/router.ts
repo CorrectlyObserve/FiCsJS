7809f067c88c400f3d229ad8fc26868c41348c1c
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-10-17 19:12:41 +0700

    feature: modify router

--- examples/todo/src/components/router.ts
+++ examples/todo/src/components/router.ts
@@ -1,12 +1,26 @@
 import { ficsRouter } from 'ficsjs/router'
 import TopPage from '@/components/topPage'
+import TodoApp from '@/components/todoApp'
+import TodoDetail from '@/components/todoDetail'
 import NotFound from '@/components/notFound'
 
-export default (lang: string) =>
-  ficsRouter({
+export default async (lang: string) => {
+  const notFound = await NotFound(lang)
+
+  return ficsRouter({
     pages: [
       { path: ['/', `/${lang}`], content: () => TopPage() },
-      { path: ['/todo', `/${lang}/todo`], content: ({ $template }) => $template`` }
+      { path: ['/todo', `/${lang}/todo`], content: () => TodoApp() },
+      { path: ['/todo/:id', `/${lang}/todo/:id`], content: () => TodoDetail() }
     ],
-    notFound: { content: () => NotFound() }
+    notFound: { content: () => notFound },
+    inheritances: [
+      {
+        descendant: notFound,
+        props: () => ({
+          back: () => (window.location.pathname = lang === 'en' ? '' : lang)
+        })
+      }
+    ]
   })
+}
