02261eb73880dbe1325968f74f1967db93a45829
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-10-19 01:07:59 +0700

    design: apply nested css

--- examples/todo/src/styles/resetCss.ts
+++ examples/todo/src/styles/resetCss.ts
@@ -1,18 +1,27 @@
 export default [
   { selector: '*', style: { padding: 0, margin: 0, boxSizing: 'border-box' } },
-  { selector: ['h1', 'h2', 'p', 'button'], style: { textAlign: 'center' } },
-  { selector: 'h2', style: { marginBottom: 'var(--lg)' } },
-  { selector: ['h2', 'p', 'button'], style: { color: '#fff' } },
-  { selector: ['p', 'button'], style: { fontSize: 'var(--md)' } },
-  { selector: ['p', 'button', 'span'], style: { lineHeight: '100%' } },
+  { selector: ['h1', 'h2'], style: { textAlign: 'center', lineHeight: 1.5 } },
+  { selector: 'h1', style: { fontSize: 'var(--lg)' } },
+  {
+    selector: 'h2',
+    style: { fontSize: 'calc(var(--md) * 1.5)', color: '#fff', marginBottom: 'var(--lg)' }
+  },
+  {
+    selector: ['p', 'button'],
+    style: { fontSize: 'var(--md)', color: '#fff', textAlign: 'center', lineHeight: 1 }
+  },
+  { selector: ['button', 'a'], style: { cursor: 'pointer' } },
   {
     selector: 'button',
-    style: { border: 0, cursor: 'pointer', transition: '0.2s ease-out' },
-    nested: [{ selector: '&:hover', style: { opacity: '0.5' } }]
+    style: { border: 'none', outline: 'none' },
+    nested: [
+      { selector: ':hover', style: { opacity: '0.5' } },
+      { selector: ':focus', style: { outline: '2px #fff solid' } }
+    ]
   },
-  { selector: ['span', 'a'], style: { color: 'inherit' } },
+  { selector: ['span', 'a'], style: { color: 'inherit', lineHeight: 'inherit' } },
   {
     selector: 'a',
-    style: { width: '100%', display: 'inline-block', cursor: 'pointer', textDecoration: 'none' }
+    style: { width: '100%', display: 'inline-block', textDecoration: 'none' }
   }
 ]
