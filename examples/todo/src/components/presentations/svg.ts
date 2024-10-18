e020eabe410e3913e6fd637e78b504fa632018eb
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-18 12:43:11 +0700

    feature: create a new component

---
+++ examples/todo/src/components/presentations/svg.ts
@@ -0,0 +1,32 @@
+import { fics } from 'ficsjs'
+
+interface Svg {
+  icon: string
+  click: () => void
+  isLarge?: boolean
+  isAlert?: boolean
+}
+
+export default ({ icon, click, isLarge, isAlert }: Svg) => {
+  const size = `var(--${isLarge ? 'lg' : 'md'})`
+  const color = isAlert ? 'var(--red)' : '#fff'
+
+  return fics({
+    name: 'svg',
+    isImmutable: true,
+    html: ({ $template }) => $template`<button />`,
+    css: [
+      { style: { display: 'flex' } },
+      {
+        selector: 'button',
+        style: {
+          width: size,
+          height: size,
+          maskImage: `url("/icons/${icon}.svg")`,
+          background: color
+        }
+      }
+    ],
+    actions: [{ handler: 'click', method: () => click() }]
+  })
+}
