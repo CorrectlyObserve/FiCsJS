b60230cdb85610d85b7da433034d45e768ad764f
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-10-19 01:11:07 +0700

    refactoring: abolish unnecessary files

--- examples/todo/src/components/footer.ts
+++
@@ -1,9 +0,0 @@
-import { fics } from 'ficsjs'
-
-export default () =>
-  fics({
-    name: 'footer',
-    isImmutable: true,
-    html: ({ $template }) => $template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
-    css: [{ selector: 'footer', style: { paddingBlock: 'var(--md)' } }]
-  })
