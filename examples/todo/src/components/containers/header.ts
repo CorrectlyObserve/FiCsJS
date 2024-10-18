bc6e506547464d993e1816950e7a4a60966516e5
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-10-19 01:10:17 +0700

    feature: create a new component

---
+++ examples/todo/src/components/containers/header.ts
@@ -0,0 +1,55 @@
+import { fics, i18n } from 'ficsjs'
+import { setState } from 'ficsjs/state'
+import langs from '@/components/presentations/langs/'
+
+export default async ({ lang, pathname }: { lang: string; pathname: string }) => {
+  const title = await i18n({ directory: '/i18n', lang, key: 'title' })
+
+  return fics({
+    name: 'header',
+    data: () => ({ lang, pathname }),
+    inheritances: [
+      {
+        descendant: langs,
+        props: ({ $getData }) => ({
+          lang: $getData('lang'),
+          pathname: $getData('pathname'),
+          switchLang: (_lang: string) => setState(lang, _lang)
+        })
+      }
+    ],
+    html: ({ $template }) => $template`<header><h1>${title}</h1><div>${langs}</div></header>`,
+    css: [
+      { style: { display: 'block', marginBottom: 'var(--lg)' } },
+      {
+        selector: 'header',
+        style: {
+          position: 'relative',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          paddingBlock: 'var(--md)'
+        },
+        nested: [
+          {
+            selector: 'h1',
+            style: {
+              background: 'var(--gradation)',
+              backgroundClip: 'text',
+              webkitTextFillColor: 'transparent'
+            }
+          },
+          {
+            selector: 'div',
+            style: {
+              position: 'absolute',
+              top: '50%',
+              transform: 'translateY(-50%)',
+              right: 'var(--ex-sm)'
+            }
+          }
+        ]
+      }
+    ]
+  })
+}
