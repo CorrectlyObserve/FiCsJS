02e3b8a6edff7fd2924463ab37d244d9bceff2b8
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-21 01:52:02 +0700

    debug: reflect of fixing props architecture

--- examples/todo/src/components/presentations/notFound.ts
+++ examples/todo/src/components/presentations/notFound.ts
@@ -1,7 +1,10 @@
 import { fics } from 'ficsjs'
 import button from '@/components/presentations/button'
 
-export default fics<{}, { error404: string }>({
+export default fics<{}, { error404: string; btnText: string; click: () => void }>({
   name: 'not-found',
-  html: ({ $props: {error404}, $template }) => $template`<h2>404 ${error404}</h2>${button}`
+  inheritances: [
+    { descendant: button, props: ({ $props: { btnText, click } }) => ({ btnText, click }) }
+  ],
+  html: ({ $props: { error404 }, $template }) => $template`<h2>404 ${error404}</h2>${button}`
 })
