0c4eb7d04730ef8a228cba2a36f7bed12cedea48
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-21 22:30:37 +0700

    refactoring: define Props type

--- examples/todo/src/components/presentations/notFound.ts
+++ examples/todo/src/components/presentations/notFound.ts
@@ -1,7 +1,13 @@
 import { fics } from 'ficsjs'
 import button from '@/components/presentations/button'
 
-export default fics<{}, { error404: string; btnText: string; click: () => void }>({
+interface Props {
+  error404: string
+  btnText: string
+  click: () => void
+}
+
+export default fics<{}, Props>({
   name: 'not-found',
   inheritances: [
     { descendant: button, props: ({ $props: { btnText, click } }) => ({ btnText, click }) }
