3bf577cd95e6d7130530d2cd127a707457520eae
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-22 00:13:21 +0700

    refactoring: abolish export of i18n

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,6 +1,5 @@
 import fics from './fics'
 import { globalCss } from './globalCss'
-import i18n from './i18n'
 import { useClient } from './useClient'
 
-export { fics, globalCss, i18n, useClient }
+export { fics, globalCss, useClient }
