b6e4d148f19ab0f3ad18328ae72e4bb045f0757c
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-24 16:38:43 +0700

    refactoring: reflect modification of types for css

--- packages/core/globalCss.ts
+++ packages/core/globalCss.ts
@@ -1,9 +1,11 @@
-import type { GlobalCss } from './types'
+import type { GlobalCss, GlobalCssContent } from './types'
 
 let _globalCss: GlobalCss = new Array()
 
 export const getGlobalCss = (): GlobalCss => _globalCss
 
-export const ficsCss = (globalCss: GlobalCss): void => {
-  _globalCss = [...globalCss]
+export const ficsCss = (globalCss: GlobalCssContent): void => {
+  _globalCss = Array.isArray(globalCss)
+    ? [...globalCss]
+    : [typeof globalCss === 'string' ? globalCss : { ...globalCss }]
 }
