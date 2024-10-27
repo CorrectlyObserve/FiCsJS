d0d2b0c0bcbb7c3f30424c29668078c2bce8dbf5
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-10-28 00:55:03 +0700

    refactoring: reflect renaming a function

--- packages/router/link.ts
+++ packages/router/link.ts
@@ -1,5 +1,5 @@
 import FiCsElement from '../core/class'
-import { convertContent, convertToArray } from '../core/helpers'
+import { convertToArray, template } from '../core/helpers'
 import goto from './goto'
 import type { FiCsLink } from './types'
 
@@ -22,7 +22,7 @@ export default <D extends object>({
     className,
     attributes,
     html: ({ $template, $html, $show }) =>
-      $template`<a href="${href}">${convertContent(content({ $template, $html, $show }), $template)}</a>`,
+      $template`<a href="${href}">${template(content({ $template, $html, $show }), $template)}</a>`,
     css,
     actions: [
       ...(actions ? convertToArray(actions) : []),
