94f41e35599e58e253702a073a4d1b89abc4bc98
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-28 00:12:10 +0700

    feature: create convertContent function to comply with DRY principle

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -1,4 +1,10 @@
-import type { SingleOrArray } from './types'
+import FiCsElement from './class'
+import type { Descendant, Sanitized, SingleOrArray, Syntaxes } from './types'
+
+export const convertContent = <D extends object>(
+  param: Descendant | Sanitized<D, {}>,
+  $template: Syntaxes<D, {}>['$template']
+) => (param instanceof FiCsElement ? $template`${param}` : param)
 
 export const convertToArray = <T>(params: SingleOrArray<T>): T[] =>
   Array.isArray(params)
