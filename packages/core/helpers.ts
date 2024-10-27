f9f71d66af71693cd7270a8f1bb595378d36c8e9
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-28 00:53:54 +0700

    refactoring: rename a function

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -1,12 +1,12 @@
 import FiCsElement from './class'
 import type { Descendant, Sanitized, SingleOrArray, Syntaxes } from './types'
 
-export const convertContent = <D extends object>(
-  param: Descendant | Sanitized<D, {}>,
-  $template: Syntaxes<D, {}>['$template']
-) => (param instanceof FiCsElement ? $template`${param}` : param)
-
 export const convertToArray = <T>(params: SingleOrArray<T>): T[] =>
   Array.isArray(params)
     ? [...params]
     : [params && typeof params === 'object' ? { ...params } : params]
+
+export const template = <D extends object>(
+  param: Descendant | Sanitized<D, {}>,
+  $template: Syntaxes<D, {}>['$template']
+) => (param instanceof FiCsElement ? $template`${param}` : param)
