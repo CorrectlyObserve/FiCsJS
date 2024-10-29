fb9e456e17d8f00771514eef492d89cfd1f52d49
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-29 15:54:08 +0700

    feature: load a new common function

--- examples/todo/src/components/presentations/notFound.ts
+++ examples/todo/src/components/presentations/notFound.ts
@@ -1,17 +1,18 @@
 import { fics } from 'ficsjs'
 import button from '@/components/presentations/button'
+import goto from '@/utils'
 
 interface Props {
+  lang: string
   title: string
   buttonText: string
-  click: () => void
 }
 
 export default fics<{}, Props>({
   name: 'not-found',
   props: {
     descendants: button,
-    values: ({ $props: { buttonText, click } }) => ({ buttonText, click })
+    values: ({ $props: { buttonText, lang } }) => ({ buttonText, click: () => goto(lang) })
   },
   html: ({ $props: { title }, $template }) => $template`<h2>404 ${title}</h2>${button}`
 })
