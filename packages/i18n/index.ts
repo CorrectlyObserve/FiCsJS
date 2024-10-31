3664ab5fcd745017d8889e10f5469d068196ebe0
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-10-31 11:40:43 +0700

    refactoring: make param name easier to understand

--- packages/i18n/index.ts
+++ packages/i18n/index.ts
@@ -6,20 +6,20 @@ type I18n = SingleOrArray | { [keys: string]: I18n }
 export default async <T>({
   directory,
   lang,
-  keys
+  key
 }: {
   directory: string
   lang: string
-  keys: SingleOrArray
+  key: SingleOrArray
 }): Promise<T> =>
   await fetch(`${directory}/${lang}.json`)
     .then(res => res.json())
     .then(json => {
-      keys = convertToArray(keys)
-      const i18n: I18n | undefined = keys.reduce((acc, key) => acc && acc[key], json)
+      key = convertToArray(key)
+      const i18n: I18n | undefined = key.reduce((acc, _key) => acc && acc[_key], json)
 
       if (i18n === undefined)
-        throw new Error(`${keys.join('.')} does not exist in ${directory}/${lang}.json...`)
+        throw new Error(`${key.join('.')} does not exist in ${directory}/${lang}.json...`)
 
       return i18n as T
     })
