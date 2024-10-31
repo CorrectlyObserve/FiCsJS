610d02b4e3f9d704060ded1ab9081cca63a075f1
MasamiOgasawara <m-ogasawara@wely.tech>
2024-10-31 09:44:18 +0700

    refactoring: integrate helpers and errors

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -10,3 +10,18 @@ export const sanitize = <D extends object>(
   param: Descendant | Sanitized<D, {}>,
   $template: Syntaxes<D, {}>['$template']
 ): Sanitized<D, {}> => (param instanceof FiCsElement ? $template`${param}` : param)
+
+export const throwDataPropsError = <T extends object, P>(
+  object: T,
+  key: keyof T,
+  tagName: string
+): void => {
+  if (!(key in object))
+    throw new Error(
+      `"${key as string}" is not defined in ${(object as object as P) ? 'props' : 'data'} of ${tagName}...`
+    )
+}
+
+export const throwWindowError = (): void => {
+  if (typeof window === 'undefined') throw new Error('window is not defined...')
+}
