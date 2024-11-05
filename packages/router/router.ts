e20ec9a933ad339ccbe0d7608c44653389ff9ade
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-11-06 04:38:58 +0700

    feature: apply props to FiCsRouter

--- packages/router/router.ts
+++ packages/router/router.ts
@@ -5,7 +5,7 @@ import { getRegExp } from './dynamicParam'
 import goto from './goto'
 import type { FiCsRouter, PageContent } from './types'
 
-export default <D extends object>({
+export default <D extends object, P extends object>({
   pages,
   notFound,
   data,
@@ -16,8 +16,8 @@ export default <D extends object>({
   actions,
   hooks,
   options
-}: FiCsRouter<D>): FiCsElement<D, {}> =>
-  new FiCsElement<D, {}>({
+}: FiCsRouter<D, P>): FiCsElement<D, P> =>
+  new FiCsElement<D, P>({
     name: 'router',
     isExceptional: true,
     data: () => {
@@ -28,17 +28,17 @@ export default <D extends object>({
     props,
     className,
     attributes,
-    html: ({ $data, $template, $html, $show }) => {
+    html: ({ $data, $props, $template, $html, $show }) => {
       let { pathname }: { pathname: string } = $data as { pathname: string }
-      const setContent = (): Sanitized<D, {}> => {
-        const resolveContent = ({ content, redirect }: PageContent<D>): Sanitized<D, {}> => {
+      const setContent = (): Sanitized<D, P> => {
+        const resolveContent = ({ content, redirect }: PageContent<D, P>): Sanitized<D, P> => {
           if (redirect && 'pathname' in $data) {
             pathname = redirect
             goto(pathname, { history: false, reload: false })
             return setContent()
           }
 
-          return sanitize(content({ $template, $html, $show }), $template)
+          return sanitize(content({ $props, $template, $html, $show }), $template)
         }
 
         for (const { paths, content, redirect } of pages)
@@ -56,13 +56,13 @@ export default <D extends object>({
     css,
     actions,
     hooks: {
-      created: (dataParams: DataParams<D, {}>) => {
+      created: (dataParams: DataParams<D, P>) => {
         throwWindowError()
 
         dataParams.$setData('pathname' as keyof D, window.location.pathname as D[keyof D])
         hooks?.created?.(dataParams)
       },
-      mounted: (dataParams: DataParams<D, {}>) => {
+      mounted: (dataParams: DataParams<D, P>) => {
         throwWindowError()
 
         window.addEventListener('popstate', () =>
@@ -71,8 +71,8 @@ export default <D extends object>({
         hooks?.mounted?.(dataParams)
       },
       updated: hooks?.updated,
-      destroyed: (dataParams: DataParams<D, {}>) => hooks?.destroyed?.(dataParams),
-      adopted: (dataParams: DataParams<D, {}>) => hooks?.adopted?.(dataParams)
+      destroyed: (dataParams: DataParams<D, P>) => hooks?.destroyed?.(dataParams),
+      adopted: (dataParams: DataParams<D, P>) => hooks?.adopted?.(dataParams)
     },
     options: { ...(options ?? {}), immutable: false }
   })
