ba8fc8de4d55f84545ff3a150917162270c73920
MasamiOgasawara <m-ogasawara@wely.tech>
2024-11-06 04:37:50 +0700

    feature: apply props to types

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -12,33 +12,33 @@ import type {
   Syntaxes
 } from '../core/types'
 
-export interface FiCsLink<D extends object> {
+export interface FiCsLink<D extends object, P extends object> {
   href: string
-  content: (syntaxes: Syntaxes<D, {}>) => ResultContent<D>
-  router: FiCsElement<D, {}>
-  props?: SingleOrArray<Props<D, {}>>
-  className?: ClassName<D, {}>
-  attributes?: Attrs<D, {}>
-  css?: SingleOrArray<string | CssContent<D, {}>>
-  actions?: Action<D, {}>[]
-  hooks?: Hooks<D, {}>
+  content: (syntaxes: Syntaxes<D, P>) => ResultContent<D, P>
+  router: FiCsElement<D, P>
+  props?: SingleOrArray<Props<D, P>>
+  className?: ClassName<D, P>
+  attributes?: Attrs<D, P>
+  css?: SingleOrArray<string | CssContent<D, P>>
+  actions?: Action<D, P>[]
+  hooks?: Hooks<D, P>
   options?: Omit<Options, 'immutable'>
 }
 
-export interface FiCsRouter<D extends object> {
-  pages: (PageContent<D> & { paths: SingleOrArray })[]
-  notFound?: PageContent<D>
+export interface FiCsRouter<D extends object, P extends object> {
+  pages: (PageContent<D, P> & { paths: SingleOrArray })[]
+  notFound?: PageContent<D, P>
   data?: () => D
-  props?: SingleOrArray<Props<D, {}>>
-  className?: ClassName<D, {}>
-  attributes?: Attrs<D, {}>
-  css?: SingleOrArray<string | CssContent<D, {}>>
-  actions?: Action<D, {}>[]
-  hooks?: Hooks<D, {}>
+  props?: SingleOrArray<Props<D, P>>
+  className?: ClassName<D, P>
+  attributes?: Attrs<D, P>
+  css?: SingleOrArray<string | CssContent<D, P>>
+  actions?: Action<D, P>[]
+  hooks?: Hooks<D, P>
   options?: Omit<Options, 'immutable'>
 }
 
-export interface PageContent<D extends object> {
-  content: (syntaxes: Syntaxes<D, {}>) => ResultContent<D>
+export interface PageContent<D extends object, P extends object> {
+  content: (syntaxes: Syntaxes<D, P>) => ResultContent<D, P>
   redirect?: string
 }
