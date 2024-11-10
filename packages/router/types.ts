bbc1bccc871797018597d3ae32dd8b4020116a2c
MasamiOgasawara <m-ogasawara@wely.tech>
2024-11-10 17:10:22 +0700

    refactoring: reflect modification of Options type

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -22,7 +22,7 @@ export interface FiCsLink<D extends object, P extends object> {
   css?: SingleOrArray<string | CssContent<D, P>>
   actions?: Action<D, P>[]
   hooks?: Hooks<D, P>
-  options?: Omit<Partial<Options>, 'immutable'>
+  options?: Omit<Options, 'immutable'>
 }
 
 export interface FiCsRouter<D extends object, P extends object> {
@@ -35,7 +35,7 @@ export interface FiCsRouter<D extends object, P extends object> {
   css?: SingleOrArray<string | CssContent<D, P>>
   actions?: Action<D, P>[]
   hooks?: Hooks<D, P>
-  options?: Omit<Partial<Options>, 'immutable'>
+  options?: Omit<Options, 'immutable'>
 }
 
 export interface PageContent<D extends object, P extends object> {
