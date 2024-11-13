506781564a9d0b03f811f20d6dbda62237f00760
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-11-13 09:23:51 +0700

    feature: restrict features for better architecture

--- packages/core/await.ts
+++ packages/core/await.ts
@@ -1,59 +1,30 @@
 import FiCsElement from './class'
 import { sanitize } from './helpers'
-import type { DataParams, FiCsAwait, Poll } from './types'
+import type { DataParams, FiCsAwait, FicsAwaitData } from './types'
 
-export default <D extends object, P extends object, R>({
-  data,
+export default <R, P extends object>({
   fetch,
-  awaited,
+  html,
   fallback,
   props,
-  className,
-  attributes,
-  css,
-  actions,
-  hooks,
   options
-}: FiCsAwait<D, P, R>): FiCsElement<D, P> =>
-  new FiCsElement<D, P>({
+}: FiCsAwait<R, P>): FiCsElement<FicsAwaitData<R>, P> =>
+  new FiCsElement<FicsAwaitData<R>, P>({
     name: 'await',
     isExceptional: true,
+    data: () => ({ isLoaded: false, res: undefined }) as FicsAwaitData<R>,
     props,
-    className,
-    data: () => {
-      for (const key of ['isLoaded', 'response'])
-        if (data && key in data())
-          throw new Error(`"${key}" is a reserved word in f-await component...`)
-
-      return { ...(data?.() ?? {}), isLoaded: false, response: undefined } as D
-    },
-    attributes,
-    html: ({ $data, $props, $template, $html, $show }) => {
-      const { isLoaded, response }: { isLoaded: boolean; response: R } = $data as {
-        isLoaded: boolean
-        response: R
-      }
-
-      return sanitize(
-        isLoaded
-          ? awaited({ $data, $props, $template, $html, $show, $response: response })
-          : (fallback?.({ $data, $props, $template, $html, $show }) ?? ''),
-        $template
-      )
-    },
-    css,
-    actions,
+    html: ({ $data: { isLoaded, res }, ...args }) =>
+      sanitize(
+        isLoaded ? html({ ...args, $response: res }) : (fallback?.({ ...args }) ?? ''),
+        args.$template
+      ),
     hooks: {
-      created: async (params: DataParams<D, P>) =>
-        await fetch(params).then(response => {
-          params.$setData('isLoaded' as keyof D, true as D[keyof D])
-          params.$setData('response' as keyof D, response as D[keyof D])
-          hooks?.created?.(params)
-        }),
-      mounted: (params: DataParams<D, P> & Poll) => hooks?.mounted?.(params),
-      updated: hooks?.updated,
-      destroyed: (params: DataParams<D, P>) => hooks?.destroyed?.(params),
-      adopted: (params: DataParams<D, P>) => hooks?.adopted?.(params)
+      created: async (params: DataParams<FicsAwaitData<R>, P>) =>
+        await fetch(params).then(res => {
+          params.$setData('isLoaded', true)
+          params.$setData('res', res)
+        })
     },
     options
   })
