8d8d522f4259bddf7b1cd95a6d1d69372b8fd88a
MasamiOgasawara <m-ogasawara@wely.tech>
2024-11-19 15:59:01 +0700

    refactoring: abolish a function since it is integated to class method

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -18,17 +18,6 @@ export const sanitize = <D extends object, P extends object>(
   $template: Syntaxes<D, P>['$template']
 ): Sanitized<D, P> => (param === '' || param instanceof FiCsElement ? $template`${param}` : param)
 
-export const throwDataPropsError = <T extends object, P>(
-  object: T,
-  key: keyof T,
-  tagName: string
-): void => {
-  if (!(key in object))
-    throw new Error(
-      `"${key as string}" is not defined in ${(object as object as P) ? 'props' : 'data'} of ${tagName}...`
-    )
-}
-
 export const throwWindowError = (): void => {
   if (typeof window === 'undefined') throw new Error('window is not defined...')
 }
