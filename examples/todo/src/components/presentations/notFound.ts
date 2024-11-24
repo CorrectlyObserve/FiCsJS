819afb407821bf81ceda1b6a47755582081074df
MasamiOgasawara <m-ogasawara@wely.tech>
2024-11-24 11:07:58 +0700

    feature: reflect implementation of async function like Suspense in React

--- examples/todo/src/components/presentations/notFound.ts
+++ examples/todo/src/components/presentations/notFound.ts
@@ -1,43 +1,47 @@
 import { fics } from 'ficsjs'
+import i18n from 'ficsjs/i18n'
 import button from '@/components/presentations/button'
 import goto from '@/utils'
 
-interface Params {
-  lang: string
+interface Data {
+  seconds: number
   title: string
   descriptions: string[]
   buttonText: string
 }
 
-export default ({ lang, title, descriptions, buttonText }: Params) =>
-  fics({
-    name: 'not-found',
-    data: () => ({ lang, title, descriptions, buttonText, seconds: 10 }),
-    props: {
-      descendant: button,
-      values: ({ $getData }) => ({
-        buttonText: $getData('buttonText'),
-        click: () => goto($getData('lang'))
-      })
+export default fics<Data, { lang: string }>({
+  name: 'not-found',
+  data: () => ({ seconds: 5, title: '', descriptions: [], buttonText: '' }),
+  fetch: ({ $props: { lang } }) => i18n({ directory: '/i18n', lang, key: 'notFound' }),
+  props: {
+    descendant: button,
+    values: ({ $props: { lang }, $getData }) => ({
+      buttonText: $getData('buttonText'),
+      click: () => goto(lang)
+    })
+  },
+  html: ({
+    $data: {
+      seconds,
+      title,
+      descriptions: [start, end]
     },
-    html: ({
-      $data: {
-        title,
-        descriptions: [start, end],
-        seconds
-      },
-      $template
-    }) => $template`<h2>${title}</h2><p>${start}${seconds}${end}</p>${button}`,
-    css: { selector: 'p', style: { marginBottom: 'var(--ex-lg)' } },
-    hooks: {
-      mounted: ({ $data: { seconds }, $setData, $getData, $poll }) =>
-        $poll(
-          ({ $times }) => {
-            if ($times === seconds - 1) goto($getData('lang'))
-            $setData('seconds', seconds - $times - 1)
-          },
-          { interval: 1000, max: seconds }
-        )
-    },
-    options: { lazyLoad: true }
-  })
+    $template,
+    $isLoaded
+  }) =>
+    $isLoaded
+      ? $template`<h2>${title}</h2><p>${start}${seconds}${end}</p>${button}`
+      : $template`<h2>Loading...</h2>`,
+  css: { selector: 'p', style: { marginBottom: 'var(--ex-lg)' } },
+  hooks: {
+    mounted: ({ $data: { seconds }, $props: { lang }, $setData, $poll }) =>
+      $poll(
+        ({ $times }) => {
+          if ($times === seconds - 1) goto(lang)
+          $setData('seconds', seconds - $times - 1)
+        },
+        { interval: 1000, max: seconds }
+      )
+  }
+})
