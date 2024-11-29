ba4104920b89b6db7869f25fe2d2d1abf1cdd570
MasamiOgasawara <m-ogasawara@wely.tech>
2024-11-29 10:37:00 +0700

    debug: modify how to load components due to fixing  props architecture

--- examples/todo/main.ts
+++ examples/todo/main.ts
@@ -1,10 +1,10 @@
 import { ficsCss, ficsInit } from 'ficsjs'
 import { getState, setState, syncState } from 'ficsjs/state'
-import Header from '@/components/containers/header'
-import Router from '@/components/containers/router'
+import header from '@/components/containers/header'
+import router from '@/components/containers/router'
 import footer from '@/components/presentations/footer'
 import resetCss from '@/resetCss'
-import { lang } from '@/store'
+import { $lang } from '@/store'
 
 ficsInit()
 ficsCss(resetCss)
@@ -15,23 +15,15 @@ const {
 } = document
 let pathname = window.location.pathname.substring(1)
 
-setState(lang, _lang || 'en')
+setState($lang, _lang || 'en')
 
-if (pathname.split('/')[0] === getState(lang)) pathname = pathname.slice(3)
+if (pathname.split('/')[0] === getState($lang)) pathname = pathname.slice(3)
 
-const init = async () => {
-  await Header().then(header => {
-    header.setData('pathname', pathname)
-    header.ssr(body, 'before')
-    syncState({ state: lang, data: [{ fics: header, key: 'lang' }] })
-  })
+header.setData('pathname', pathname)
+header.ssr(body, 'before')
+syncState({ state: $lang, data: [{ component: header, key: 'lang' }] })
 
-  await Router().then(router => {
-    const main = document.querySelector('main')
-    if (main) router.ssr(main)
-  })
+const main = document.querySelector('main')
+if (main) router.ssr(main)
 
-  footer.ssr(body)
-}
-
-init()
+footer.ssr(body)
