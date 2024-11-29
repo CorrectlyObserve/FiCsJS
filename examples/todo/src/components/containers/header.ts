c149a7f681719a0f8a704161504c9dc9977773e5
MasamiOgasawara <m-ogasawara@wely.tech>
2024-11-29 10:32:29 +0700

    debug: reflect fixing props architecture

--- examples/todo/src/components/containers/header.ts
+++ examples/todo/src/components/containers/header.ts
@@ -3,57 +3,61 @@ import i18n from 'ficsjs/i18n'
 import { goto } from 'ficsjs/router'
 import { setState, getState } from 'ficsjs/state'
 import langs from '@/components/presentations/langs/'
-import { lang } from '@/store'
+import { $lang } from '@/store'
 
-export default async () => {
-  const _lang = getState<string>(lang)
-  const title = await i18n<string>({ directory: '/i18n', lang: _lang, key: 'title' })
-
-  return fics({
-    name: 'header',
-    data: () => ({ lang: _lang, pathname: '' }),
-    props: {
-      descendant: langs,
-      values: ({ $data: { lang: _lang, pathname } }) => ({
-        lang: _lang,
-        switchLang: (_lang: string) => {
-          setState(lang, _lang)
-          goto(`/${getState(lang) === 'en' ? '' : getState(lang) + '/'}${pathname}`)
-        }
-      })
-    },
-    html: ({ $template }) => $template`<header><h1>${title}</h1><div>${langs}</div></header>`,
-    css: [
-      { style: { display: 'block' } },
+export default fics({
+  name: 'header',
+  data: () => ({ lang: getState<string>($lang), pathname: '', title: '' }),
+  fetch: async ({ $data: { lang } }) => ({
+    title: await i18n<string>({ directory: '/i18n', lang, key: 'title' })
+  }),
+  props: {
+    descendant: langs,
+    values: [
+      { key: 'lang', content: ({ $data: { lang } }) => lang },
       {
-        selector: 'header',
-        style: {
-          position: 'relative',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          paddingBlock: 'var(--md)'
-        },
-        nested: [
-          {
-            selector: '> h1',
-            style: {
-              background: 'var(--gradation)',
-              backgroundClip: 'text',
-              webkitTextFillColor: 'transparent'
-            }
-          },
-          {
-            selector: '> div',
-            style: {
-              position: 'absolute',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              right: 'var(--ex-sm)'
-            }
+        key: 'switchLang',
+        content:
+          ({ $data: { pathname } }) =>
+          (_lang: string) => {
+            setState($lang, _lang)
+            goto(`/${getState($lang) === 'en' ? '' : getState($lang) + '/'}${pathname}`)
           }
-        ]
       }
     ]
-  })
-}
+  },
+  html: ({ $data: { title }, $template }) =>
+    $template`<header><h1>${title}</h1><div>${langs}</div></header>`,
+  css: [
+    { style: { display: 'block' } },
+    {
+      selector: 'header',
+      style: {
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBlock: 'var(--md)'
+      },
+      nested: [
+        {
+          selector: '> h1',
+          style: {
+            background: 'var(--gradation)',
+            backgroundClip: 'text',
+            webkitTextFillColor: 'transparent'
+          }
+        },
+        {
+          selector: '> div',
+          style: {
+            position: 'absolute',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            right: 'var(--ex-sm)'
+          }
+        }
+      ]
+    }
+  ]
+})
