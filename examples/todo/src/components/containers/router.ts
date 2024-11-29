dda8e7e6ad53d10293c5aad9a76aa7353c5da4d2
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-11-29 10:33:00 +0700

    debug: reflect fixing props architecture and modification of ficsRouter

--- examples/todo/src/components/containers/router.ts
+++ examples/todo/src/components/containers/router.ts
@@ -1,26 +1,19 @@
-import i18n from 'ficsjs/i18n'
 import { ficsRouter } from 'ficsjs/router'
 import { getState } from 'ficsjs/state'
 import topPage from '@/components/presentations/topPage'
 import tasks from '@/components/containers/tasks'
 import taskDetail from '@/components/containers/taskDetail'
 import notFound from '@/components/presentations/notFound'
-import { lang } from '@/store'
+import { $lang } from '@/store'
 
-export default async () => {
-  const _lang = getState<string>(lang)
-  const texts = await i18n<JSON>({ directory: '/i18n', lang: _lang, key: 'topPage' })
+const lang = getState<string>($lang)
 
-  return ficsRouter({
-    pages: [
-      { paths: ['/', `/${_lang}`], content: () => topPage },
-      { paths: ['/todo', `/${_lang}/todo`], content: () => tasks },
-      { paths: ['/todo/:id', `/${_lang}/todo/:id`], content: () => taskDetail }
-    ],
-    notFound: { content: () => notFound },
-    props: [
-      { descendant: [topPage, notFound], values: () => ({ lang: _lang }) },
-      { descendant: topPage, values: () => ({ ...texts }) }
-    ]
-  })
-}
+export default ficsRouter({
+  pages: [
+    { path: ['/', `/${lang}`], content: () => topPage },
+    { path: ['/todo', `/${lang}/todo`], content: () => tasks },
+    { path: ['/todo/:id', `/${lang}/todo/:id`], content: () => taskDetail }
+  ],
+  notFound: { content: () => notFound },
+  props: { descendant: [topPage, notFound], values: { key: 'lang', content: () => lang } }
+})
