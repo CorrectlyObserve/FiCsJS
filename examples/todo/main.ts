0bf7ef9ed1960567d988845fab9506307f8099c9
MasamiOgasawara <m-ogasawara@wely.tech>
2024-11-30 12:58:23 +0700

    debug: reflect modification of components

--- examples/todo/main.ts
+++ examples/todo/main.ts
@@ -1,7 +1,7 @@
 import { ficsCss, ficsInit } from 'ficsjs'
 import { getState, setState, syncState } from 'ficsjs/state'
-import header from '@/components/containers/header'
-import router from '@/components/containers/router'
+import Header from '@/components/containers/header'
+import Router from '@/components/containers/router'
 import footer from '@/components/presentations/footer'
 import resetCss from '@/resetCss'
 import { $lang } from '@/store'
@@ -19,11 +19,12 @@ setState($lang, _lang || 'en')
 
 if (pathname.split('/')[0] === getState($lang)) pathname = pathname.slice(3)
 
+const header = Header()
 header.setData('pathname', pathname)
 header.ssr(body, 'before')
 syncState({ state: $lang, data: [{ component: header, key: 'lang' }] })
 
 const main = document.querySelector('main')
-if (main) router.ssr(main)
+if (main) Router().ssr(main)
 
 footer.ssr(body)
