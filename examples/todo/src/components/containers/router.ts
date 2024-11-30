32dc6783e1854a1e38785ea9186bc7c0f536bff7
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-11-30 12:57:12 +0700

    debug: make a instance of class a function to return the instance

--- examples/todo/src/components/containers/router.ts
+++ examples/todo/src/components/containers/router.ts
@@ -6,14 +6,19 @@ import taskDetail from '@/components/containers/taskDetail'
 import notFound from '@/components/presentations/notFound'
 import { $lang } from '@/store'
 
-const lang = getState<string>($lang)
+export default () => {
+  const lang = getState<string>($lang)
 
-export default ficsRouter({
-  pages: [
-    { path: ['/', `/${lang}`], content: () => topPage },
-    { path: ['/todo', `/${lang}/todo`], content: () => tasks },
-    { path: ['/todo/:id', `/${lang}/todo/:id`], content: () => taskDetail }
-  ],
-  notFound: { content: () => notFound },
-  props: { descendant: [topPage, notFound], values: { key: 'lang', content: () => lang } }
-})
+  return ficsRouter({
+    pages: [
+      { path: ['/', `/${lang}`], content: () => topPage },
+      { path: ['/todo', `/${lang}/todo`], content: () => tasks },
+      { path: ['/todo/:id', `/${lang}/todo/:id`], content: () => taskDetail }
+    ],
+    notFound: { content: () => notFound },
+    props: {
+      descendant: [topPage, tasks, taskDetail, notFound],
+      values: { key: 'lang', content: () => lang }
+    }
+  })
+}
