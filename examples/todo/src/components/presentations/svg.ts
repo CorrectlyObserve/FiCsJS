1aff3c3e2923a775c77cf24cda570a14c39e4263
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-12-02 13:50:54 +0700

    design: remove display:flex

--- examples/todo/src/components/presentations/svg.ts
+++ examples/todo/src/components/presentations/svg.ts
@@ -12,25 +12,19 @@ export default () =>
   fics<{}, Props>({
     name: 'svg',
     html: ({ $template }) => $template`<button><div /></button>`,
-    css: [
-      { style: { display: 'flex' } },
-      {
-        selector: 'button',
-        style: ({ $props: { padding } }) => ({
-          background: 'none',
-          padding: padding ?? 0
-        }),
-        nested: {
-          selector: 'div',
-          style: ({ $props: { icon, size, color } }) => ({
-            width: size ?? 'calc(var(--lg) * 1.5)',
-            height: size ?? 'calc(var(--lg) * 1.5)',
-            maskImage: `url("/icons/${icon}.svg")`,
-            background: color ?? '#fff'
-          })
-        }
+    css: {
+      selector: 'button',
+      style: ({ $props: { padding } }) => ({ background: 'none', padding: padding ?? 0 }),
+      nested: {
+        selector: 'div',
+        style: ({ $props: { icon, size, color } }) => ({
+          width: size ?? 'calc(var(--lg) * 1.5)',
+          height: size ?? 'calc(var(--lg) * 1.5)',
+          maskImage: `url("/icons/${icon}.svg")`,
+          background: color ?? '#fff'
+        })
       }
-    ],
+    },
     actions: [
       {
         handler: 'click',
