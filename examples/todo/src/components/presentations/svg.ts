e1dfab254582967129b9622b62abdb7b1a5a4f87
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-02 13:49:54 +0700

    design: add padding

--- examples/todo/src/components/presentations/svg.ts
+++ examples/todo/src/components/presentations/svg.ts
@@ -4,27 +4,38 @@ interface Props {
   icon: string
   size?: string
   color?: string
+  padding?: string
   click: () => void
 }
 
-export default () => fics<{}, Props>({
-  name: 'svg',
-  html: ({ $template }) => $template`<button />`,
-  css: [
-    { style: { display: 'flex' } },
-    {
-      selector: 'button',
-      style: ({ $props: { icon, size, color } }) => ({
-        width: size ?? 'calc(var(--lg) * 1.5)',
-        height: size ?? 'calc(var(--lg) * 1.5)',
-        maskImage: `url("/icons/${icon}.svg")`,
-        background: color ?? '#fff'
-      })
-    }
-  ],
-  actions: [{
-    handler: 'click',
-    method: ({ $props: { click } }) => click(),
-    options: { throttle: 500, blur: true }
-  }]
-})
+export default () =>
+  fics<{}, Props>({
+    name: 'svg',
+    html: ({ $template }) => $template`<button><div /></button>`,
+    css: [
+      { style: { display: 'flex' } },
+      {
+        selector: 'button',
+        style: ({ $props: { padding } }) => ({
+          background: 'none',
+          padding: padding ?? 0
+        }),
+        nested: {
+          selector: 'div',
+          style: ({ $props: { icon, size, color } }) => ({
+            width: size ?? 'calc(var(--lg) * 1.5)',
+            height: size ?? 'calc(var(--lg) * 1.5)',
+            maskImage: `url("/icons/${icon}.svg")`,
+            background: color ?? '#fff'
+          })
+        }
+      }
+    ],
+    actions: [
+      {
+        handler: 'click',
+        method: ({ $props: { click } }) => click(),
+        options: { throttle: 500, blur: true }
+      }
+    ]
+  })
