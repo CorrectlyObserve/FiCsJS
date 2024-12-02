78b6dd0207f860e43a7129da448b06220a9cb6bf
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-12-02 13:34:16 +0700

    feature: create input component

---
+++ examples/todo/src/components/presentations/input.ts
@@ -0,0 +1,24 @@
+import { fics } from 'ficsjs'
+
+export default fics<{ value: string }, { placeholder: string; enter: () => void }>({
+  name: 'input',
+  data: () => ({ value: '' }),
+  html: ({ $data: { value }, $props: { placeholder }, $template }) =>
+    $template`<input value="${value}" placeholder="${placeholder}" />`,
+  actions: [
+    {
+      handler: 'input',
+      selector: 'input',
+      method: ({ $setData, $value }) => $setData('value', $value!),
+      options: { debounce: 200 }
+    },
+    {
+      handler: 'keydown',
+      selector: 'input',
+      method: ({ $data: { value }, $props: { enter }, $event }) => {
+        if ((value !== '' && ($event as KeyboardEvent).key) === 'Enter') enter()
+      },
+      options: { throttle: 500 }
+    }
+  ]
+})
