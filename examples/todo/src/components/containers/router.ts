bc068004e876e73ce2c44f7ec15d645b8ba1ee60
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-05 19:07:15 +0700

    feature: modify router component

--- examples/todo/src/components/containers/router.ts
+++ examples/todo/src/components/containers/router.ts
@@ -1,15 +1,11 @@
 import { ficsRouter } from 'ficsjs/router'
-import { getState } from 'ficsjs/state'
 import topPage from '@/components/presentations/topPage'
 import tasks from '@/components/containers/tasks'
 import taskDetail from '@/components/containers/taskDetail'
 import notFound from '@/components/presentations/notFound'
-import { $lang } from '@/store'
 
-export default () => {
-  const lang = getState<string>($lang)
-
-  return ficsRouter({
+export default (lang: string) =>
+  ficsRouter({
     pages: [
       { path: ['/', `/${lang}`], content: () => topPage },
       { path: ['/todo', `/${lang}/todo`], content: () => tasks },
@@ -21,4 +17,3 @@ export default () => {
       values: { key: 'lang', content: () => lang }
     }
   })
-}
