2525c864e507c87db1dd3dd09bfb48f2a2c45b48
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-06 17:28:42 +0700

    feature: modify types for improvement of spa routing

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -19,19 +19,18 @@ export interface FiCsLink<P extends object> {
 }
 
 export interface FiCsRouter<P extends object> {
-  pages: (PageContent<P> & { path: SingleOrArray })[]
+  pages: (PageContent<P> & { path: string })[]
   notFound?: PageContent<P>
   props?: SingleOrArray<Props<RouterData, P>>
   options?: Omit<Options, 'immutable'>
 }
 
 export interface PageContent<P extends object> {
-  content: (
-    syntaxes: Syntaxes<RouterData, P>
-  ) => Descendant | Sanitized<RouterData, P>
+  content: (syntaxes: Syntaxes<RouterData, P>) => Descendant | Sanitized<RouterData, P>
   redirect?: string
 }
 
 export interface RouterData {
   pathname: string
+  lang: string
 }
