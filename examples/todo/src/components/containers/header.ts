7b4a580a1367f9af586086123fa61bcdbba6869d
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-06 16:14:47 +0700

    refactoring: remove spaces

--- examples/todo/src/components/containers/header.ts
+++ examples/todo/src/components/containers/header.ts
@@ -6,58 +6,58 @@ import langs from '@/components/presentations/langs/'
 import { $lang } from '@/store'
 
 export default fics({
-    name: 'header',
-    data: () => ({ lang: getState<string>($lang), pathname: '', title: '' }),
-    fetch: async ({ $data: { lang } }) => ({
-      title: await i18n<string>({ directory: '/i18n', lang, key: 'title' })
-    }),
-    props: {
-      descendant: langs,
-      values: [
-        { key: 'lang', content: ({ $data: { lang } }) => lang },
-        {
-          key: 'switchLang',
-          content:
-            ({ $data: { pathname } }) =>
-            (_lang: string) => {
-              setState($lang, _lang)
-              goto(`/${getState($lang) === 'en' ? '' : getState($lang) + '/'}${pathname}`)
-            }
-        }
-      ]
-    },
-    html: ({ $data: { title }, $template }) =>
-      $template`<header><h1>${title}</h1><div>${langs}</div></header>`,
-    css: [
-      { style: { display: 'block' } },
+  name: 'header',
+  data: () => ({ lang: getState<string>($lang), pathname: '', title: '' }),
+  fetch: async ({ $data: { lang } }) => ({
+    title: await i18n<string>({ directory: '/i18n', lang, key: 'title' })
+  }),
+  props: {
+    descendant: langs,
+    values: [
+      { key: 'lang', content: ({ $data: { lang } }) => lang },
       {
-        selector: 'header',
-        style: {
-          position: 'relative',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          paddingBlock: 'var(--md)'
-        },
-        nested: [
-          {
-            selector: '> h1',
-            style: {
-              background: 'var(--gradation)',
-              backgroundClip: 'text',
-              webkitTextFillColor: 'transparent'
-            }
-          },
-          {
-            selector: '> div',
-            style: {
-              position: 'absolute',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              right: 'var(--ex-sm)'
-            }
+        key: 'switchLang',
+        content:
+          ({ $data: { pathname } }) =>
+          (_lang: string) => {
+            setState($lang, _lang)
+            goto(`/${getState($lang) === 'en' ? '' : getState($lang) + '/'}${pathname}`)
           }
-        ]
       }
     ]
-  })
+  },
+  html: ({ $data: { title }, $template }) =>
+    $template`<header><h1>${title}</h1><div>${langs}</div></header>`,
+  css: [
+    { style: { display: 'block' } },
+    {
+      selector: 'header',
+      style: {
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBlock: 'var(--md)'
+      },
+      nested: [
+        {
+          selector: '> h1',
+          style: {
+            background: 'var(--gradation)',
+            backgroundClip: 'text',
+            webkitTextFillColor: 'transparent'
+          }
+        },
+        {
+          selector: '> div',
+          style: {
+            position: 'absolute',
+            top: '50%',
+            transform: 'translateY(-50%)',
+            right: 'var(--ex-sm)'
+          }
+        }
+      ]
+    }
+  ]
+})
