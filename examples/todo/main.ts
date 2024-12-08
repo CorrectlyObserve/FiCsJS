c7e145191e257c2a0be76d418c9107d5e3a958a5
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-12-08 12:41:14 +0700

    feature: reflect modification of components

--- examples/todo/main.ts
+++ examples/todo/main.ts
@@ -1,7 +1,7 @@
 import { ficsCss, ficsInit } from 'ficsjs'
 import { getState, setState, syncState } from 'ficsjs/state'
 import header from '@/components/containers/header'
-import Router from '@/components/containers/router'
+import { router } from '@/components/containers/router'
 import footer from '@/components/presentations/footer'
 import resetCss from '@/resetCss'
 import { $lang } from '@/store'
@@ -19,11 +19,15 @@ setState($lang, lang || 'en')
 
 if (pathname.split('/')[0] === getState($lang)) pathname = pathname.slice(3)
 
+header.setData('lang', getState($lang))
 header.setData('pathname', pathname)
 header.ssr(body, 'before')
 syncState({ state: $lang, data: [{ component: header, key: 'lang' }] })
 
 const main = document.querySelector('main')
-if (main) Router(getState($lang)).ssr(main)
+if (main) {
+  router.setData('lang', getState($lang))
+  router.ssr(main)
+}
 
 footer.ssr(body)
