18cec1126b808dee20a48ea0ed86593ff2fa4968
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-12-08 12:39:45 +0700

    feature: load task component

--- examples/todo/src/components/containers/router.ts
+++ examples/todo/src/components/containers/router.ts
@@ -1,19 +1,18 @@
 import { ficsRouter } from 'ficsjs/router'
 import topPage from '@/components/presentations/topPage'
 import tasks from '@/components/containers/tasks'
-import taskDetail from '@/components/containers/taskDetail'
+import task from '@/components/containers/task'
 import notFound from '@/components/presentations/notFound'
 
-export default (lang: string) =>
-  ficsRouter({
-    pages: [
-      { path: ['/', `/${lang}`], content: () => topPage },
-      { path: ['/todo', `/${lang}/todo`], content: () => tasks },
-      { path: ['/todo/:id', `/${lang}/todo/:id`], content: () => taskDetail }
-    ],
-    notFound: { content: () => notFound },
-    props: {
-      descendant: [topPage, tasks, taskDetail, notFound],
-      values: { key: 'lang', content: () => lang }
-    }
-  })
+export const router = ficsRouter({
+  pages: [
+    { path: '/', content: () => topPage },
+    { path: '/todo', content: () => tasks },
+    { path: '/todo/:id', content: () => task }
+  ],
+  notFound: { content: () => notFound },
+  props: {
+    descendant: [topPage, tasks, task, notFound],
+    values: { key: 'lang', content: ({ $data: { lang } }) => lang }
+  }
+})
