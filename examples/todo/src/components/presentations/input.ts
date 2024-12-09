cc594c3310c1774d606b441cd2111c3998b928ad
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-12-09 10:51:16 +0700

    debug: prevent keydown event in entering values

--- examples/todo/src/components/presentations/input.ts
+++ examples/todo/src/components/presentations/input.ts
@@ -1,8 +1,18 @@
 import { fics } from 'ficsjs'
 
-export default fics<{ value: string }, { placeholder: string; enter: () => void }>({
+interface Data {
+  value: string
+  isComposing: boolean
+}
+
+interface Props {
+  placeholder: string
+  enter: () => void
+}
+
+export default fics<Data, Props>({
   name: 'input',
-  data: () => ({ value: '' }),
+  data: () => ({ value: '', isComposing: false }),
   html: ({ $data: { value }, $props: { placeholder }, $template }) =>
     $template`<input value="${value}" placeholder="${placeholder}" type="text" />`,
   actions: [
@@ -13,10 +23,20 @@ export default fics<{ value: string }, { placeholder: string; enter: () => void 
       options: { debounce: 200 }
     },
     {
+      handler: 'compositionstart',
+      selector: 'input',
+      method: ({ $setData }) => $setData('isComposing', true)
+    },
+    {
+      handler: 'compositionend',
+      selector: 'input',
+      method: ({ $setData }) => $setData('isComposing', false)
+    },
+    {
       handler: 'keydown',
       selector: 'input',
-      method: ({ $data: { value }, $props: { enter }, $event }) => {
-        if ((value !== '' && ($event as KeyboardEvent).key) === 'Enter') enter()
+      method: ({ $data: { value, isComposing }, $props: { enter }, $event }) => {
+        if ((value !== '' && ($event as KeyboardEvent).key) === 'Enter' && !isComposing) enter()
       },
       options: { throttle: 500 }
     }
