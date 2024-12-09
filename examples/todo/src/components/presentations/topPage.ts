c8487203bcb12fea69ad8bd392e8c062b4afacbe
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-09 08:55:07 +0700

    refactoring: reflect modification of getPath function

--- examples/todo/src/components/presentations/topPage.ts
+++ examples/todo/src/components/presentations/topPage.ts
@@ -25,7 +25,7 @@ export default fics<Data, { lang: string }>({
         content:
           ({ $props: { lang } }) =>
           () =>
-            goto(getPath(lang, 'todo'))
+            goto(getPath(lang, '/todo'))
       }
     ]
   },
