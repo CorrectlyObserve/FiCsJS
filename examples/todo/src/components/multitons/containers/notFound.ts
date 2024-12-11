20e068549a54c909e2a509f2dcfe0e0507ec5668
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-11 16:54:39 +0700

    refactoring: move a directory

--- examples/todo/src/components/multitons/containers/notFound.ts
+++ examples/todo/src/components/multitons/containers/notFound.ts
@@ -1,8 +1,8 @@
 import { fics } from 'ficsjs'
 import i18n from 'ficsjs/i18n'
 import { goto } from 'ficsjs/router'
-import button from '@/components/presentations/button'
-import loadingIcon from '@/components/presentations/loadingIcon/'
+import button from '@/components/materials/button'
+import loadingIcon from '@/components/materials/loadingIcon'
 import getPath from '@/utils'
 
 interface Data {
