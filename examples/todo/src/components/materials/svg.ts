4e5bb7a64f93215a218f23b68426b921fb7e81b6
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-12 20:14:09 +0700

    feature: modify svg component

--- examples/todo/src/components/materials/svg.ts
+++ examples/todo/src/components/materials/svg.ts
@@ -1,28 +1,27 @@
 import { fics } from 'ficsjs'
 
-export default (icon: string) =>
-  fics<{}, { color?: string; click: () => void }>({
-    name: 'svg',
-    html: ({ $template }) => $template`<button><span /></button>`,
-    css: {
-      selector: 'button',
-      style: { background: 'none', padding: 'var(--ex-sm)' },
-      nested: {
-        selector: 'span',
-        style: ({ $props: { color } }) => ({
-          width: 'calc(var(--lg) * 1.5)',
-          height: 'calc(var(--lg) * 1.5)',
-          display: 'block',
-          maskImage: `url("/icons/${icon}.svg")`,
-          background: color ?? '#fff'
-        })
-      }
-    },
-    actions: [
-      {
-        handler: 'click',
-        method: ({ $props: { click } }) => click(),
-        options: { throttle: 500, blur: true }
-      }
-    ]
-  })
+export default fics<{ icon: string }, { color?: string; click: () => void }>({
+  name: 'svg',
+  html: ({ $template }) => $template`<button><span /></button>`,
+  css: {
+    selector: 'button',
+    style: { background: 'none', padding: 'var(--ex-sm)' },
+    nested: {
+      selector: 'span',
+      style: ({ $data: { icon }, $props: { color } }) => ({
+        width: 'calc(var(--lg) * 1.5)',
+        height: 'calc(var(--lg) * 1.5)',
+        display: 'block',
+        maskImage: `url("/icons/${icon}.svg")`,
+        background: color ?? '#fff'
+      })
+    }
+  },
+  actions: [
+    {
+      handler: 'click',
+      method: ({ $props: { click } }) => click(),
+      options: { throttle: 500, blur: true }
+    }
+  ]
+})
