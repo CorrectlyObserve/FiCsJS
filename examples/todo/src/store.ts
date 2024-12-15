99a357d617b937d2b51edf63fc9e4683e0921e73
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-15 15:57:08 +0700

    refactoring: remove unnecessary stores

--- examples/todo/src/store.ts
+++ examples/todo/src/store.ts
@@ -1,6 +1,3 @@
 import { createState } from 'ficsjs/state'
-import { Task, TaskQueue } from '@/types'
 
 export const $lang = createState<string>('en')
-export const $tasks = createState<Task[]>([])
-export const $taskQueue = createState<TaskQueue>({} as TaskQueue)
