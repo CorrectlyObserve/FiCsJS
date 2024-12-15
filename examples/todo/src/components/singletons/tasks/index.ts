7ae62b672adfe055994fae63919b03d6e1dcb777
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-12-15 15:59:03 +0700

    feature: move a directory and implement todo list with indexedDB

--- examples/todo/src/components/singletons/tasks/index.ts
+++ examples/todo/src/components/singletons/tasks/index.ts
@@ -1,10 +1,10 @@
 import { fics } from 'ficsjs'
 import i18n from 'ficsjs/i18n'
-import { Task } from '@/types'
-import { getAllTasks, addTask, completeTask, deleteTask } from '@/indexedDB'
 import loadingIcon from '@/components/materials/loadingIcon'
 import input from '@/components/materials/input'
 import svg from '@/components/materials/svg'
+import { addTask, completeTask, deleteTask, getAllTasks } from '@/indexedDB'
+import { Task } from '@/types'
 import getPath from '@/utils'
 import css from './style.css?inline'
 
@@ -16,17 +16,14 @@ interface Data {
   unapplicable: string
 }
 
-const addIcon = svg.clone({ icon: 'add' })
-const squareIcon = svg.clone({ icon: 'square' })
-const checkSquareIcon = svg.clone({ icon: 'check-square' })
+const addIcon = svg.extend({ icon: 'add' })
+const squareIcon = svg.extend({ icon: 'square' })
+const checkSquareIcon = svg.extend({ icon: 'check-square' })
 
 export default fics<Data, { lang: string }>({
   name: 'tasks',
   data: () => ({ placeholder: '', isShown: false, checkbox: '', tasks: [], unapplicable: '' }),
-  fetch: async ({ $props: { lang } }) => ({
-    ...(await i18n({ directory: '/i18n', lang, key: ['tasks'] })),
-    tasks: await getAllTasks()
-  }),
+  fetch: ({ $props: { lang } }) => i18n({ directory: '/i18n', lang, key: ['tasks'] }),
   props: [
     {
       descendant: input,
@@ -37,8 +34,7 @@ export default fics<Data, { lang: string }>({
           content:
             ({ $setData }) =>
             async () => {
-              await addTask(input.getData('value'))
-              $setData('tasks', await getAllTasks())
+              $setData('tasks', await addTask(input.getData('value')))
               input.setData('value', '')
             }
         }
@@ -54,8 +50,7 @@ export default fics<Data, { lang: string }>({
             const value = input.getData('value')
 
             if (value !== '') {
-              await addTask(value)
-              $setData('tasks', await getAllTasks())
+              $setData('tasks', await addTask(value))
               input.setData('value', '')
             }
           }
@@ -75,6 +70,7 @@ export default fics<Data, { lang: string }>({
   html: ({
     $data: { isShown, checkbox, tasks, unapplicable },
     $props: { lang },
+    $setData,
     $template,
     $setProps,
     $isLoaded
@@ -84,7 +80,7 @@ export default fics<Data, { lang: string }>({
     return $template`
       <div class="menu">
         <div>${input}${addIcon}</div>
-        <div>${isShown ? checkSquareIcon : squareIcon}<span style="opacity:0; width: 0">${isShown ? checkSquareIcon : squareIcon}</span><span>${checkbox}</span></div>
+        <div>${isShown ? checkSquareIcon : squareIcon}<span>${checkbox}</span></div>
       </div>
       ${
         tasks.length > 0
@@ -92,19 +88,19 @@ export default fics<Data, { lang: string }>({
               ({ id, title, completed_at }) => $template`
                 <div class="task" key="${id}">
                   <div>
-                    ${$setProps(svg.clone({ icon: completed_at ? 'check' : 'circle' }), {
+                    ${$setProps(svg.extend({ icon: completed_at ? 'check' : 'circle' }), {
                       click: async () => {
-                        await completeTask(id)
-                        console.log(await getAllTasks())
+                        $setData('tasks', await completeTask(id))
+                        $setData('isShown', true)
                       }
                     })}
                     <span class="${completed_at ? 'done' : ''}">
                       <a href="${getPath(lang, `/todo/${id}`)}">${title}</a>
                     </span>
                   </div>
-                  ${$setProps(svg.clone({ icon: 'trash' }), {
+                  ${$setProps(svg.extend({ icon: 'trash' }), {
                     color: 'var(--red)',
-                    click: async () => await deleteTask(id)
+                    click: async () => $setData('tasks', await deleteTask(id))
                   })}
                 </div>
               `
@@ -122,5 +118,6 @@ export default fics<Data, { lang: string }>({
       options: { blur: true }
     }
   ],
+  hooks: { mounted: async ({ $setData }) => $setData('tasks', await getAllTasks()) },
   options: { lazyLoad: true }
 })
