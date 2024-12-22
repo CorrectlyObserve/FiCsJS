4f6a3425951c31a767c1d12bd517a13e83108b62
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-22 09:24:09 +0700

    feature: add isDisabled status

--- examples/todo/src/components/materials/button.ts
+++ examples/todo/src/components/materials/button.ts
@@ -2,24 +2,32 @@ import { fics } from 'ficsjs'
 
 interface Props {
   buttonText: string
+  isDisabled?: boolean
   click: () => void
 }
 
 export default fics<{}, Props>({
   name: 'button',
-  html: ({ $props: { buttonText }, $template }) => $template`<button>${buttonText}</button>`,
+  html: ({ $props: { buttonText, isDisabled }, $template }) =>
+    $template`<button ${isDisabled ? 'disabled' : ''}>${buttonText}</button>`,
   css: [
     { style: { display: 'block', textAlign: 'center' } },
     {
       selector: 'button',
-      style: { background: 'var(--gradation)', padding: 'var(--md)', borderRadius: 'var(--ex-sm)' }
+      style: ({ $props: { isDisabled } }) => ({
+        background: isDisabled ? 'rgba(255, 255, 255, 0.1)' : 'var(--gradation)',
+        padding: 'var(--md)',
+        borderRadius: 'var(--ex-sm)'
+      })
     }
   ],
   actions: [
     {
       handler: 'click',
       selector: 'button',
-      method: ({ $props: { click } }) => click(),
+      method: ({ $props: { isDisabled, click } }) => {
+        if (!isDisabled) click()
+      },
       options: { throttle: 500, blur: true }
     }
   ]
