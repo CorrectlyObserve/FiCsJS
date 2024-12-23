22ebd33a75c729692599268ad348f52db35078cf
MasamiOgasawara <m-ogasawara@wely.tech>
2024-12-23 14:28:15 +0700

    refactoring: integrate loadingIcon component to icon component

--- examples/todo/src/components/materials/loadingIcon/index.ts
+++
@@ -1,9 +0,0 @@
-import { fics } from 'ficsjs'
-import css from './style.css?inline'
-
-export default fics({
-  name: 'loading',
-  html: ({ $template }) => $template`<button class="loading" />`,
-  css,
-  options: { immutable: true }
-})
