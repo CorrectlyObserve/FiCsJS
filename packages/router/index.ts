01e28ecae2da084133f1a986177a406f7276f211
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-12-24 14:19:20 +0700

    refactoring: reflect integration of files and abolishment of ficsLink

--- packages/router/index.ts
+++ packages/router/index.ts
@@ -1,7 +1,4 @@
-import getPathParams from './getPathParams'
-import getQueryParams from './getQueryParams'
 import goto from './goto'
-import ficsLink from './link'
 import ficsRouter from './router'
 
-export { ficsLink, ficsRouter, getPathParams, getQueryParams, goto }
+export { ficsRouter, goto }
