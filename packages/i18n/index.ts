886b76dfe84b4b24d87566d878e3997841c0f8b0
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2024-12-25 12:29:13 +0700

    feature: abolish variable feature in i18n

--- packages/i18n/index.ts
+++ packages/i18n/index.ts
@@ -1,47 +1,23 @@
 import { convertToArray } from '../core/helpers'
 import type { SingleOrArray } from '../core/types'
 
-type I18n = SingleOrArray | { [keys: string]: I18n }
-
 export default async <T>({
   directory,
   lang,
-  key,
-  variables
+  key
 }: {
   directory: string
   lang: string
   key: SingleOrArray
-  variables?: Record<string, string | number>
 }): Promise<T> =>
   await fetch(`${directory}/${lang}.json`)
     .then(res => res.json())
     .then(json => {
       key = convertToArray(key)
-      let i18n: I18n | undefined = key.reduce((acc, _key) => acc && acc[_key], json)
+      let i18n: T | undefined = key.reduce((acc, _key) => acc && acc[_key], json)
 
-      if (!i18n)
-        throw new Error(`${key.join('.')} does not exist in ${`${directory}/${lang}.json`}...`)
-
-      const replaceText = (str: string, key: string, value: string | number): string =>
-        str.replaceAll(`$var(${key})`, `${value}`)
-
-      const replaceI18n = (i18n: I18n, variables: Record<string, string | number>): I18n => {
-        for (const [key, value] of Object.entries(variables))
-          if (typeof i18n === 'string') i18n = replaceText(i18n, key, value)
-          else if (Array.isArray(i18n))
-            for (let i = 0; i < i18n.length; i++)
-              if (typeof i18n[i] === 'string') i18n[i] = replaceText(i18n[i], key, value)
-              else replaceI18n(i18n[i], variables)
-          else
-            for (const _key of Object.keys(i18n))
-              if (typeof i18n[_key] === 'string') i18n[_key] = replaceText(i18n[_key], key, value)
-              else replaceI18n(i18n[_key], variables)
-
-        return i18n
-      }
-
-      return (variables ? replaceI18n(i18n, variables) : i18n) as T
+      if (i18n) return i18n
+      throw new Error(`${key.join('.')} does not exist in ${`${directory}/${lang}.json`}...`)
     })
     .catch(error => {
       throw new Error(error)
