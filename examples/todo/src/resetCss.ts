d0b744dd1e115723ce2e8c2d302d750513731647
MasamiOgasawara <m-ogasawara@wely.tech>
2025-01-03 08:35:09 +0700

    feature: apply css functions

--- examples/todo/src/resetCss.ts
+++ examples/todo/src/resetCss.ts
@@ -1,3 +1,5 @@
+import { color, calc, scale, variable } from 'ficsjs/css'
+
 export default [
   {
     selector: '*',
@@ -5,18 +7,22 @@ export default [
     nested: {
       selector: '[tabindex]',
       style: { cursor: 'pointer' },
-      nested: { selector: ':focus', style: { color: 'var(--red)', outline: 'none' } }
+      nested: { selector: ':focus', style: { color: variable('red'), outline: 'none' } }
     }
   },
   { selector: ['h1', 'h2'], style: { textAlign: 'center', lineHeight: 1.5 } },
-  { selector: 'h1', style: { fontSize: 'var(--ex-lg)' } },
+  { selector: 'h1', style: { fontSize: variable('ex-lg') } },
   {
     selector: 'h2',
-    style: { fontSize: 'calc(var(--md) * 1.5)', color: '#fff', marginBottom: 'var(--ex-lg)' }
+    style: {
+      fontSize: calc([variable('md'), 1.5], '*'),
+      color: '#fff',
+      marginBottom: variable('ex-lg')
+    }
   },
   {
     selector: ['p', 'button', 'li', 'label', 'span'],
-    style: { fontSize: 'var(--lg)', color: '#fff' },
+    style: { fontSize: variable('lg'), color: '#fff' },
     nested: [
       { selector: ':is(li)', style: { lineHeight: 1.8 } },
       { selector: ':not(li, label)', style: { textAlign: 'center', lineHeight: 1 } },
@@ -25,14 +31,14 @@ export default [
   },
   {
     selector: 'button',
-    style: { transition: 'var(--transition)', border: 'none', outline: 'none' },
+    style: { transition: variable('transition'), border: 'none', outline: 'none' },
     nested: [
       {
         selector: ':not([disabled])',
         style: { cursor: 'pointer' },
         nested: [
           { selector: ':hover', style: { opacity: 0.5 } },
-          { selector: ':focus', style: { transform: 'scale(0.8)' } }
+          { selector: ':focus', style: { transform: scale(0.8) } }
         ]
       },
       { selector: '[disabled]', style: { cursor: 'not-allowed' } }
@@ -41,12 +47,12 @@ export default [
   {
     selector: ['input', 'textarea'],
     style: {
-      width: 'calc(var(--md) * 20)',
-      background: 'rgba(255, 255, 255, 0.1)',
-      fontSize: 'var(--lg)',
+      width: calc([variable('md'), 20], '*'),
+      background: color('#fff', 0.1),
+      fontSize: variable('lg'),
       color: '#fff',
-      padding: 'var(--ex-sm) var(--md)',
-      borderRadius: 'var(--ex-sm)',
+      padding: `${variable('ex-sm')} ${variable('md')}`,
+      borderRadius: variable('ex-sm'),
       border: 'none',
       outline: 'none'
     },
@@ -54,14 +60,18 @@ export default [
       { selector: ':hover', style: { cursor: 'pointer' } },
       {
         selector: ':focus',
-        style: { background: 'rgba(255, 255, 255, 0.8)', color: 'var(--black)', cursor: 'auto' }
+        style: {
+          background: color('#fff', 0.8),
+          color: variable('black'),
+          cursor: 'auto'
+        }
       }
     ]
   },
   {
     selector: 'textarea',
     style: {
-      height: 'calc(var(--ex-sm) * 2 + var(--lg) * 1.2 * 6)',
+      height: calc([calc([variable('ex-sm'), 2], '*'), calc([variable('lg'), 1.2, 6], '*')], '+'),
       lineHeight: 1.2,
       resize: 'none'
     }
