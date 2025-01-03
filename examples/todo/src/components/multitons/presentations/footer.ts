0e6f9645475d128098efd88edea4bc107ad41be1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-01-03 08:33:25 +0700

    feature: apply variable function

--- examples/todo/src/components/multitons/presentations/footer.ts
+++ examples/todo/src/components/multitons/presentations/footer.ts
@@ -1,11 +1,12 @@
 import { fics } from 'ficsjs'
+import { variable } from 'ficsjs/css'
 
 export default fics({
   name: 'footer',
   html: ({ template }) => template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
   css: {
     selector: 'footer',
-    style: { paddingBlock: 'var(--md)' },
-    nested: { selector: 'p', style: { fontSize: 'var(--sm)' } }
+    style: { paddingBlock: variable('md') },
+    nested: { selector: 'p', style: { fontSize: variable('sm') } }
   }
 })
