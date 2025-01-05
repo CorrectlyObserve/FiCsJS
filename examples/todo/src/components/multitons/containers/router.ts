cdcfe339be6ba5a8e76b9dfbe2bfa1d308cdbaf4
MasamiOgasawara <m-ogasawara@wely.tech>
2025-01-06 00:52:05 +0700

    feature: reflect abolishment of topPage

--- examples/todo/src/components/multitons/containers/router.ts
+++ examples/todo/src/components/multitons/containers/router.ts
@@ -1,20 +1,16 @@
 import { ficsRouter } from 'ficsjs/router'
-import topPage from '@/components/singletons/topPage'
 import tasks from '@/components/singletons/tasks'
 import task from '@/components/singletons/task'
 import notFound from '@/components/multitons/containers/notFound'
 
 export default ficsRouter({
   pages: [
-    { path: '/', content: () => topPage },
-    { path: '/todo', content: () => tasks },
-    { path: '/todo/:id', content: () => task }
+    { path: '/', content: () => tasks },
+    { path: '/:id', content: () => task }
   ],
   notFound: { content: () => notFound },
-  props: [
-    {
-      descendant: [topPage, tasks, task, notFound],
-      values: () => ({ lang: ({ getData }) => getData('lang') })
-    }
-  ]
+  props: {
+    descendant: [tasks, task, notFound],
+    values: ({}) => ({ lang: ({ getData }) => getData('lang') })
+  }
 })
