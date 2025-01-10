a1837dbea47a082a220e9409f0de33d2f4453afd
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-01-10 08:58:47 +0700

    refactoring: make state management simple

--- packages/state/index.ts
+++ packages/state/index.ts
@@ -2,26 +2,28 @@ import { convertToArray, generateUid } from '../core/helpers'
 import { Descendant, SingleOrArray } from '../core/types'
 
 const generator: Generator<number> = generateUid()
-const uneditableStates: Map<string, unknown> = new Map()
 const states: Map<string, unknown> = new Map()
+const writableStates: Set<string> = new Set()
 const syncs: Map<string, Map<Descendant, Set<string>>> = new Map()
 const observers: Map<string, () => void> = new Map()
 
 export const createState = <S>(value: S, isReadonly: boolean = false): string => {
   const key: string = `state-${generator.next().value}`
 
-  isReadonly ? uneditableStates.set(key, value) : states.set(key, value)
+  states.set(key, value)
+  if (!isReadonly) writableStates.add(key)
+
   return key
 }
 
-export const getState = <S>(key: string, isReadonly: boolean = false): S => {
-  const stateMap: Map<string, unknown> = isReadonly ? uneditableStates : states
-
-  if (stateMap.has(key)) return stateMap.get(key) as S
-  throw new Error(`"${key}" is not defined in states...`)
+export const getState = <S>(key: string): S => {
+  if (states.has(key)) return states.get(key) as S
+  throw new Error(`The "${key}" is not defined in states...`)
 }
 
 export const setState = <S>(key: string, value: S): void => {
+  if (!writableStates.has(key)) throw new Error(`The "${key}" is readonly...`)
+
   states.set(key, value)
 
   if (syncs.has(key))
