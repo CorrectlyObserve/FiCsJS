0bd9e8bce64c0f765f2ba8ce7b69f4cbc51ebe41
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-01-19 15:17:43 +0700

    refactoring: rename functions

--- packages/core/globalCss.ts
+++ packages/core/globalCss.ts
@@ -1,10 +1,11 @@
 import { convertToArray } from './helpers'
 import type { GlobalCssContent, SingleOrArray } from './types'
 
-let globalCss: (GlobalCssContent | string)[] = new Array()
+let _globalCss: (GlobalCssContent | string)[] = new Array()
 
-export const getGlobalCss = (): (GlobalCssContent | string)[] => globalCss
-
-export const ficsGlobalCss = (css: SingleOrArray<GlobalCssContent | string>): void => {
-  globalCss = convertToArray(css)
+export const ficsCss = (
+  css: SingleOrArray<GlobalCssContent | string>): void => {
+  _globalCss = convertToArray(css)
 }
+
+export const globalCss = (): (GlobalCssContent | string)[] => _globalCss
