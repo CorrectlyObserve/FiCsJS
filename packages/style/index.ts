a261e6d3ff26e07ddf0838337c380696aa8a86eb
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-01-19 11:14:57 +0700

    refactoring: integate style functions and add a new style function

--- packages/style/index.ts
+++ packages/style/index.ts
@@ -1,6 +1,13 @@
-import calc from './calc'
 import color from './color'
-import scale from './scale'
-import variable from './var'
 
-export { color, calc, scale, variable }
+export const calc = (values: (string | number)[], operator: '+' | '-' | '*' | '/'): string =>
+  `calc(${values.join(` ${operator} `)})`
+
+export const rotate = (degree: number): string => (isNaN(degree) ? '' : `rotate(${degree}deg)`)
+
+export const scale = (decimal: number): string => `scale(${decimal})`
+
+export const variable = (variable: string): string =>
+  `var(--${variable.startsWith('--') ? variable.slice(2) : variable})`
+
+export { color }
