06b89ebb4fcea0b78ab21dc42fdc64f6e16958c5
MasamiOgasawara <m-ogasawara@wely.tech>
2025-01-19 15:20:56 +0700

    design: apply media query

--- examples/todo/src/components/multitons/presentations/footer.ts
+++ examples/todo/src/components/multitons/presentations/footer.ts
@@ -1,10 +1,15 @@
 import { fics } from 'ficsjs'
 import { variable } from 'ficsjs/style'
+import breakpoints from '@/breakpoints'
 
 export default fics({
   name: 'footer',
   html: ({ template }) => template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
   css: {
-    footer: { paddingBlock: variable('md'), p: { fontSize: variable('sm') } }
+    footer: {
+      paddingBlock: variable('md'),
+      p: { fontSize: variable('sm') },
+      [`@media (max-width: ${breakpoints.sm})`]: { paddingBlock: variable('sm') }
+    }
   }
 })
