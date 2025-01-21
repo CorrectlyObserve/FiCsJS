537e05544fb3fc9a0a6f4aa9b6b7547df6cf31b1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-01-21 10:07:44 +0700

    refactoring: extract global css

--- examples/todo/src/globalCss.ts
+++ examples/todo/src/globalCss.ts
@@ -1,4 +1,4 @@
-import { color, calc, scale, variable } from 'ficsjs/style'
+import { scale, variable } from 'ficsjs/style'
 import breakpoints from '@/breakpoints'
 
 export default {
@@ -6,22 +6,17 @@ export default {
   '*[tabindex], button, label, a': { transition: variable('transition') },
   '*[tabindex]:hover, a:hover': { cursor: 'pointer', opacity: 0.5 },
   '*[tabindex]:focus, a:focus': { color: variable('red'), outline: 'none' },
-  'h1, h2': { textAlign: 'center', lineHeight: 1.5 },
+  'h2, p, button, label, span': { color: '#fff', textAlign: 'center' },
   h2: {
     fontSize: variable('xl'),
-    color: '#fff',
     marginBottom: variable('2xl'),
+    lineHeight: 1.5,
     [`@media (max-width: ${breakpoints.sm})`]: { marginBottom: variable('xl') }
   },
-  'p, button, label, span': {
-    fontSize: variable('lg'),
-    color: '#fff',
-    '&:not(label)': { textAlign: 'center', lineHeight: 1 }
-  },
+  'p, button, label, span': { fontSize: variable('md'), lineHeight: 1.2 },
   button: {
     border: 'none',
     outline: 'none',
-    '&[aria-disabled="true"]': { cursor: 'not-allowed' },
     '&:not([aria-disabled="true"])': {
       '&:hover': { cursor: 'pointer', opacity: 0.5 },
       '&:focus': { transform: scale(0.8) }
@@ -32,25 +27,5 @@ export default {
     textAlign: 'left',
     '&:hover': { cursor: 'pointer', opacity: 0.5 }
   },
-  'input, textarea': {
-    width: calc([variable('md'), 20], '*'),
-    background: color('#fff', 0.1),
-    fontSize: variable('lg'),
-    color: '#fff',
-    padding: `${variable('xs')} ${variable('md')}`,
-    borderRadius: variable('xs'),
-    border: 'none',
-    outline: 'none',
-    '&:hover': { cursor: 'pointer' },
-    '&:focus': { background: color('#fff', 0.8), color: variable('black'), cursor: 'auto' }
-  },
-  span: { width: 'fit-content' },
-  a: {
-    width: '100%',
-    display: 'inline-block',
-    color: 'inherit',
-    lineHeight: 'inherit',
-    textDecoration: 'none',
-    outline: 'none'
-  }
+  span: { width: 'fit-content' }
 }
