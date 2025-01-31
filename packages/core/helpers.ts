cf4ec31f6a50367e319f1a2b04dc74cd0c6fb3f9
MasamiOgasawara <m-ogasawara@wely.tech>
2025-01-31 18:21:05 +0700

    refactoring: add a type annotation

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -4,7 +4,7 @@ export const convertToArray = <T>(param: SingleOrArray<T>): T[] =>
   Array.isArray(param) ? [...param] : [param && typeof param === 'object' ? { ...param } : param]
 
 export function* generateUid(): Generator<number> {
-  let n = 1
+  let n: number = 1
 
   while (true) {
     yield n
