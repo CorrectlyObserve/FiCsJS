84afce42debeda7ddde9af6c12b7634a4e68031c
MasamiOgasawara <m-ogasawara@wely.tech>
2025-02-02 14:21:16 +0700

    feature: modify FiCsRouter type

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -1,9 +1,10 @@
-import type { Descendant, OptionArgs, Props, Sanitized, Syntaxes } from '../core/types'
+import type { CssArgs, Descendant, OptionArgs, Props, Sanitized, Syntaxes } from '../core/types'
 
 export interface FiCsRouter<P extends object> {
   pages: (PageContent<P> & { path: string })[]
   notFound?: PageContent<P>
   props?: Props<RouterData, P>[]
+  css?: CssArgs<RouterData, P>
   options?: OptionArgs
 }
 
