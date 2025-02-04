f29d868fc36ebcf8562a31c8eba6e1076b00bd45
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-02-04 16:34:46 +0700

    feature: apply a new consistant

--- examples/todo/src/components/multitons/router.ts
+++ examples/todo/src/components/multitons/router.ts
@@ -1,12 +1,23 @@
 import { ficsRouter } from 'ficsjs/router'
+import breakpoints from '@/breakpoints'
 import tasks from '@/components/singletons/tasks'
 import task from '@/components/singletons/task'
 import notFound from '@/components/multitons/notFound'
+import { calc, color, variable } from 'ficsjs/style'
+
+const xs = calc([variable('xs'), -1], '*')
 
 export default ficsRouter({
   pages: [
-    { path: '/', content: () => tasks },
-    { path: '/:id', content: ({ template }) => template`${tasks}${task}` }
+    {
+      path: '/',
+      content: ({ template }) => {
+        const { id }: { id: number | undefined } = task.getData('task')
+        console.log(id)
+        return template`<div class="container">${tasks}${!id ? task : ''}</div>`
+      }
+    },
+    { path: '/:id', content: () => task }
   ],
   notFound: { content: () => notFound },
   props: [
@@ -14,5 +25,28 @@ export default ficsRouter({
       descendant: [tasks, task, notFound],
       values: () => ({ lang: ({ getData }) => getData('lang') })
     }
-  ]
+  ],
+  css: {
+    '.container > f-task': { display: 'none' },
+    [`@media (min-width: ${breakpoints.lg})`]: {
+      ':host': {
+        position: 'relative',
+        display: 'block',
+        minHeight: variable('min-height'),
+        '.container': {
+          position: 'absolute',
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100%',
+          height: '100%',
+          gap: variable('xl'),
+          'f-task': {
+            display: 'block',
+            paddingLeft: variable('xl'),
+            boxShadow: `${xs} 0px ${variable('xs')} ${xs} ${color({ hex: '--black', rate: 0.5, isOpacity: false })}`
+          }
+        }
+      }
+    }
+  }
 })
