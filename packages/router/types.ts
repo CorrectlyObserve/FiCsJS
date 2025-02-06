d5b2983d2cdfc5df8b915161f42fc7eef220faca
MasamiOgasawara <m-ogasawara@wely.tech>
2025-02-06 23:56:15 +0700

    feature: modify RouterData type and reflect it

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -1,15 +1,16 @@
 import type { CssArgs, Descendant, OptionArgs, Props, Sanitized, Syntaxes } from '../core/types'
 
-export interface FiCsRouter<P extends object> {
-  pages: (PageContent<P> & { path: string })[]
-  notFound?: PageContent<P>
-  props?: Props<RouterData, P>[]
-  css?: CssArgs<RouterData, P>
+export interface FiCsRouter<D extends RouterData, P extends object> {
+  data?: () => Omit<D, keyof RouterData>
+  props?: Props<D, P>[]
+  pages: (PageContent<D, P> & { path: string })[]
+  notFound?: PageContent<D, P>
+  css?: CssArgs<D, P>
   options?: OptionArgs
 }
 
-export interface PageContent<P extends object> {
-  content: (syntaxes: Syntaxes<RouterData, P>) => Descendant | Sanitized<RouterData, P>
+export interface PageContent<D extends object, P extends object> {
+  content: (syntaxes: Syntaxes<D, P> & { data: D }) => Descendant | Sanitized<D, P>
   redirect?: string
 }
 
@@ -18,4 +19,6 @@ export type Param = 'path' | 'query'
 export interface RouterData {
   pathname: string
   lang: string
+  pathParams: Record<string, string>
+  queryParams: Record<string, string>
 }
