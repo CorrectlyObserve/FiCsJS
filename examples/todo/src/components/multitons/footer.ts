e3699c0069ea704ee56a3c930ae16e66dcaa1f86
MasamiOgasawara <m-ogasawara@wely.tech>
2025-02-09 10:35:26 +0700

    feature: make it functional type

--- examples/todo/src/components/multitons/footer.ts
+++ examples/todo/src/components/multitons/footer.ts
@@ -1,10 +1,11 @@
 import { fics } from 'ficsjs'
 import { variable } from 'ficsjs/style'
 
-export default fics({
-  name: 'footer',
-  html: ({ template }) => template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
-  css: {
-    footer: { paddingBottom: variable('md'), p: { fontSize: variable('sm') } }
-  }
-})
+export default () =>
+  fics({
+    name: 'footer',
+    html: ({ template }) => template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
+    css: {
+      footer: { paddingBottom: variable('md'), p: { fontSize: variable('sm') } }
+    }
+  })
