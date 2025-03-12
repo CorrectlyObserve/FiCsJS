9e808dcddb15e9a60f22dedfc3573c96c3b1143c
MasamiOgasawara <m-ogasawara@wely.tech>
2025-03-12 10:44:20 +0700

    feature: apply OptionParams type

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -2,7 +2,7 @@ import type {
   Css,
   Descendant,
   GlobalCssContent,
-  Options,
+  OptionParams,
   Props,
   Sanitized,
   SingleOrArray,
@@ -14,7 +14,7 @@ export interface FiCsRouter<D extends RouterData, P extends object> {
   pages: (PageContent<D, P> & { path: string })[]
   notFound?: PageContent<D, P>
   css?: SingleOrArray<Exclude<Css<D, P>, GlobalCssContent>>
-  options?: Options & { ssr: false }
+  options?: OptionParams
 }
 
 export interface PageContent<D extends object, P extends object> {
