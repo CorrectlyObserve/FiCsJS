82a267fb376b7f13153c09a72de3b0364798c5f5
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-03-18 16:59:14 +0700

    refactoring: make configuration simple

--- examples/fake-api/tailwind.config.js
+++ examples/fake-api/tailwind.config.js
@@ -1,7 +1,5 @@
 export default {
-  content: ['./src/**/*.{html,js,ts,jsx,tsx}', './dist/**/*.{html,js,ts,jsx,tsx}', './app.ts'],
-  theme: {
-    extend: {}
-  },
+  content: ['./src/**/*.ts', './dist/**/*.js'],
+  theme: { extend: {} },
   plugins: []
 }
