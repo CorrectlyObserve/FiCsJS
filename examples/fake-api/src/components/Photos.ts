0e078bf2141bd908fce7a71466207fa3e9c53cc6
MasamiOgasawara <m-ogasawara@wely.tech>
2025-03-20 01:47:22 +0700

    feature: configure Tailwind CSS and mounted hook

--- examples/fake-api/src/components/Photos.ts
+++ examples/fake-api/src/components/Photos.ts
@@ -1,45 +1,40 @@
 import { fics } from 'ficsjs'
-
-interface Data {
-  count: number
-  photos: { id: number; author: string }[]
-}
+import css from '@/.tailwindcss.txt'
 
 const root = 'https://picsum.photos'
 const getPhotos = (page: number) => `${root}/v2/list?page=${page}&limit=10`
-const css = await Bun.file('./src/global.css').text()
 
 export default () =>
-  fics<Data, {}>({
+  fics<{ count: number; photos: { id: string; author: string }[] }, {}>({
     name: 'photos',
-    data: () => ({ count: 0 }),
+    data: () => ({ count: 0, photos: [] }),
     fetch: async ({ data: { count }, crud }) => {
       count++
-      const photos = await crud<Record<'id' | 'author' | string, string>[]>(getPhotos(count))
-
-      return {
-        count,
-        photos: photos.map(({ id, author }) => ({ id: parseInt(id), author }))
-      }
+      return { count, photos: await crud<Array<{ id: string; author: string }>>(getPhotos(count)) }
     },
     html: ({ data: { photos }, template }) => template`
-      <div>
-        ${photos.map(({ id, author }) => {
-          const popoverId = `popover-${id}`
+      ${photos.map(({ id, author }) => {
+        const popoverId = `popover-${id}`
 
-          return template`
-            <img src="${root}/id/${id}/300/300?blur" popovertarget="${popoverId}" />
+        return template`
+          <div class="flex bg-blue-500" key="${id}">
+            <button popovertarget="${popoverId}"><img src="${root}/id/${id}/300/300?blur" /></button>
             <div id="${popoverId}" popover>
               <button popovertarget="${popoverId}" popovertargetaction="hide" aria-hidden="true">X</button>
               <p>Created by ${author}</p>
             </div>
-          `
-        })}
-      </div>
+          </div>
+        `
+      })}
     `,
-    css,
-    actions: {
-      img: { click: ({ event: { target } }) => (target as HTMLImageElement).showPopover() }
-    },
-    hooks: {}
+    css: typeof window !== 'undefined' ? css : undefined,
+    hooks: {
+      mounted: async ({ data: { count }, setData, getData, crud }) => {
+        setData('count', count++)
+        setData('photos', [
+          ...getData('photos'),
+          ...(await crud<Array<{ id: string; author: string }>>(getPhotos(getData('count'))))
+        ])
+      }
+    }
   })
