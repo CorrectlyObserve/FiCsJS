fe48ec6f8f19486d02b727e691fd2e62767336d1
MasamiOgasawara <m-ogasawara@wely.tech>
2025-03-21 08:59:14 +0700

    refactoring: make it easier to read

--- examples/fake-api/src/components/Photos.ts
+++ examples/fake-api/src/components/Photos.ts
@@ -1,31 +1,34 @@
 import { fics } from 'ficsjs'
 import css from '@/.tailwindcss.txt'
 
+interface Photo {
+  id: string
+  author: string
+}
+
 const root = 'https://picsum.photos'
 const getPhotos = (page: number) => `${root}/v2/list?page=${page}&limit=10`
 
 export default () =>
-  fics<{ count: number; photos: { id: string; author: string }[] }, {}>({
+  fics<{ count: number; photos: Photo[] }, {}>({
     name: 'photos',
     data: () => ({ count: 0, photos: [] }),
     fetch: async ({ data: { count }, crud }) => {
       count++
-      return { count, photos: await crud<Array<{ id: string; author: string }>>(getPhotos(count)) }
+      return { count, photos: await crud<Array<Photo>>(getPhotos(count)) }
     },
     html: ({ data: { photos }, template }) => template`
-      ${photos.map(({ id, author }) => {
-        const popoverId = `popover-${id}`
-
-        return template`
-          <div class="flex bg-blue-500" key="${id}">
-            <button popovertarget="${popoverId}"><img src="${root}/id/${id}/300/300?blur" /></button>
-            <div id="${popoverId}" popover>
-              <button popovertarget="${popoverId}" popovertargetaction="hide" aria-hidden="true">X</button>
+      ${photos.map(
+        ({ id, author }) => template`
+          <div key="${id}">
+            <button popovertarget="${id}"><img src="${root}/id/${id}/300/300?blur" /></button>
+            <div id="${id}" popover>
+              <button popovertarget="${id}" popovertargetaction="hide" aria-hidden="true">X</button>
               <p>Created by ${author}</p>
             </div>
           </div>
         `
-      })}
+      )}
     `,
     css: typeof window !== 'undefined' ? css : undefined,
     hooks: {
@@ -33,7 +36,7 @@ export default () =>
         setData('count', count++)
         setData('photos', [
           ...getData('photos'),
-          ...(await crud<Array<{ id: string; author: string }>>(getPhotos(getData('count'))))
+          ...(await crud<Array<Photo>>(getPhotos(getData('count'))))
         ])
       }
     }
