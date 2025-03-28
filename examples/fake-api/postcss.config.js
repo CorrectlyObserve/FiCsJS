da2bb3f277b333fc52dae1bd33dd69f3bc7c7d98
MasamiOgasawara <m-ogasawara@wely.tech>
2025-03-28 13:57:20 +0700

    setting: configure cssnano

--- examples/fake-api/postcss.config.js
+++ examples/fake-api/postcss.config.js
@@ -1,6 +1,12 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {}
+    autoprefixer: {},
+    cssnano: {
+      preset: [
+        'default',
+        { discardComments: { removeAll: true }, minifyFontValues: true, normalizeWhitespace: true }
+      ]
+    }
   }
 }
