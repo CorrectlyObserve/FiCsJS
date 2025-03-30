cbfdd9b90f85392f95b5c5b2e8974a2a8632cf59
MasamiOgasawara <m-ogasawara@wely.tech>
2025-03-31 01:01:26 +0700

    refactoring: enforce the restriction of deferredData to only CSR

--- examples/fake-api/app.ts
+++ examples/fake-api/app.ts
@@ -36,14 +36,14 @@ const template = ({
   `
 
 const users = Users()
-app.get('/', async c =>
+app.get('/', c =>
   c.html(
     template({
       title: 'FiCsJS with Hono',
       description: 'This is a simple example of FiCsJS with Hono in SSR.',
       content: `
         <a href="/scroll">Go to the scroll page</a>
-        ${await users.ssr()}
+        ${users.ssr()}
       `,
       path: '/index'
     })
@@ -51,7 +51,7 @@ app.get('/', async c =>
 )
 
 const photos = Photos()
-app.get('/scroll', async c =>
+app.get('/scroll', c =>
   c.html(
     template({
       title: 'Infinite and virtual scroll',
@@ -59,7 +59,7 @@ app.get('/scroll', async c =>
         'This is a simple example of an infinite scroll and a virtual scroll with FiCsJS.',
       content: `
         <a href="/">Back to the top page</a>
-        ${await photos.ssr()}
+        ${photos.ssr()}
       `,
       path: '/scroll'
     })
