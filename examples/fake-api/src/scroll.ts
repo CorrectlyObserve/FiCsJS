b8e95ad282a3b0eaf54451f86470eebc6878f9aa
MasamiOgasawara <m-ogasawara@wely.tech>
2025-04-01 09:23:05 +0700

    feature: load Footer component on the client

--- examples/fake-api/src/scroll.ts
+++ examples/fake-api/src/scroll.ts
@@ -1,6 +1,8 @@
 import { ficsCss } from 'ficsjs'
 import Photos from '@/components/Photos'
+import Footer from '@/components/Footer'
 import css from '@/.tailwindcss.txt'
 
 ficsCss(css)
 Photos().describe()
+Footer().describe()
