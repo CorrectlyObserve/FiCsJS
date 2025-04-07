16e7bd478953c8589c1a5e03037b614ca21bed8f
MasamiOgasawara <m-ogasawara@wely.tech>
2025-04-08 02:14:32 +0700

    feature: define props and implment crud methods

--- examples/fake-api/src/components/Users.ts
+++ examples/fake-api/src/components/Users.ts
@@ -1,22 +1,57 @@
 import { fics } from 'ficsjs'
-import Button from '@/components/Button'
+import Button from '@/components/CrudButton'
 
 interface User {
-  id: string
+  id: number
   name: string
   email: string
 }
 
 const root = 'https://jsonplaceholder.typicode.com/users'
 const users: User[] = await fetch(root).then(res => res.json())
+const button = Button()
+const headers: HeadersInit = { 'Content-type': 'application/json; charset=UTF-8' }
 
 export default () =>
-  fics<{ users: User[]; selected: string }, {}>({
+  fics<{ users: User[]; userId: number }, {}>({
     name: 'users',
-    data: () => ({ users, selected: '' }),
-    html: ({ data: { users, selected }, template }) => template`
+    data: () => ({ users, userId: NaN }),
+    props: [
+      {
+        descendant: button,
+        values: ({ setData, crud }) => ({
+          id: ({ getData }) => getData('userId'),
+          deleteMethod:
+            ({ getData }) =>
+            async (id: number) => {
+              await crud<User>(`${root}/${id}`, { method: 'DELETE', headers })
+              setData(
+                'users',
+                getData('users').filter(user => user.id !== id)
+              )
+              setData('userId', NaN)
+            },
+          updateMethod:
+            ({ getData }) =>
+            async ({ id, name }: { id: number; name: string }, method: 'put' | 'patch') => {
+              await crud<User>(`${root}/${id}`, {
+                method: method.toUpperCase(),
+                body: JSON.stringify({ id, name }),
+                headers
+              })
+              setData(
+                'users',
+                getData('users').map(user => (user.id === id ? { ...user, name } : user))
+              )
+              setData('userId', NaN)
+            }
+        })
+      }
+    ],
+    html: ({ data: { users, userId }, template, setProps }) => template`
+      ${['put', 'patch', 'delete'].map(method => setProps(button, { method }))}
       ${users.map(user => {
-        const textColor = `${selected === user.id.toString() ? 'text-red-700' : 'text-gray-900'}`
+        const textColor = `${userId === user.id ? 'text-red-700' : 'text-gray-900'}`
         const items = { id: 'Id', name: 'Name', email: 'Email' } as const
         const keys = Object.keys(items) as (keyof typeof items)[]
 
@@ -27,5 +62,19 @@ export default () =>
         `
       })}
     `,
-    actions: { div: { click: ({ setData, attributes }) => setData('selected', attributes.key) } }
+    hooks: {
+      mounted: async ({ setData, getData, crud }) => {
+        setData('users', [
+          ...getData('users'),
+          await crud<User>(root, {
+            method: 'POST',
+            body: JSON.stringify(users[Math.floor(Math.random() * 10) + 1]),
+            headers
+          })
+        ])
+      }
+    },
+    actions: {
+      div: { click: ({ setData, attributes }) => setData('userId', parseInt(attributes.key)) }
+    }
   })
