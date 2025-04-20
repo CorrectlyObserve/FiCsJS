c267a546abe9ec7470019263bfe6d416b8efc0ba
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-04-20 11:13:03 +0700

    refactoring: rename a function

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -3,15 +3,6 @@ import type { SingleOrArray } from './types'
 export const convertToArray = <T>(param: SingleOrArray<T>): T[] =>
   Array.isArray(param) ? [...param] : [param && typeof param === 'object' ? { ...param } : param]
 
-export function* generateUid(): Generator<number> {
-  let n: number = 1
-
-  while (true) {
-    yield n
-    n++
-  }
-}
-
 export const isBrowser = (): boolean =>
   typeof window !== 'undefined' && typeof document !== 'undefined'
 
@@ -21,4 +12,13 @@ export const isString = (param: string | unknown): param is string => typeof par
 
 export const throwBrowserError = (): void => {
   if (!isBrowser()) throw new Error('Window and document are not available...')
+}
+
+export function* uid(): Generator<number> {
+  let n: number = 1
+
+  while (true) {
+    yield n
+    n++
+  }
 }
