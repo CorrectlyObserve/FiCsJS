52bf6a3d3922e6680d7e0ea3eda3087460915783
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-04-20 11:22:36 +0700

    refactoring: rename a function

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -1,15 +1,15 @@
 import type { SingleOrArray } from './types'
 
+export const browserError = (): void => {
+  if (!isBrowser()) throw new Error('Window and document are not available...')
+}
+
 export const isBrowser = (): boolean =>
   typeof window !== 'undefined' && typeof document !== 'undefined'
 
 export const isNumber = (param: number | unknown): param is number => typeof param === 'number'
 
 export const isString = (param: string | unknown): param is string => typeof param === 'string'
-
-export const throwBrowserError = (): void => {
-  if (!isBrowser()) throw new Error('Window and document are not available...')
-}
 
 export const toArray = <T>(param: SingleOrArray<T>): T[] =>
   Array.isArray(param) ? [...param] : [param && typeof param === 'object' ? { ...param } : param]
