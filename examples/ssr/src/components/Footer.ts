d358f30eb2535786da35985bfc0c3eb5f920aa0e
MasamiOgasawara <m-ogasawara@wely.tech>
2025-04-23 16:13:29 +0700

    design: apply to Taiiwind CSS

--- examples/ssr/src/components/Footer.ts
+++ examples/ssr/src/components/Footer.ts
@@ -3,5 +3,6 @@ import { fics } from 'ficsjs'
 export default () =>
   fics({
     name: 'footer',
-    html: ({ template }) => template`<footer><p>&copy; 2025 Masami Ogasawara</p></footer>`
+    html: ({ template }) =>
+      template`<footer class="text-sm text-white text-center"><p>&copy; 2025 Masami Ogasawara</p></footer>`
   })
