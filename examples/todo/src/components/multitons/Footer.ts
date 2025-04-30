1787471327868e32478c83f356b7dc3d2c3a00ba
MasamiOgasawara <m-ogasawara@wely.tech>
2025-04-30 23:04:06 +0700

    refactoring: remove space

--- examples/todo/src/components/multitons/Footer.ts
+++ examples/todo/src/components/multitons/Footer.ts
@@ -5,7 +5,5 @@ export default () =>
   fics({
     name: 'footer',
     html: ({ template }) => template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
-    css: {
-      footer: { paddingBottom: variable('md'), p: { fontSize: variable('sm') } }
-    }
+    css: { footer: { paddingBottom: variable('md'), p: { fontSize: variable('sm') } } }
   })
