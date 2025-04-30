b82827ac799177f5a6db29bf676eb4f9879f5595
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-01 02:25:26 +0700

    refactoring: make it simple

--- packages/style/absoluteCenter.ts
+++ packages/style/absoluteCenter.ts
@@ -1,15 +1,16 @@
 interface Absolute {
   position: 'absolute'
   top: '50%'
+  transform: 'translate(-50%, -50%)' | 'translateY(-50%)'
 }
 
-export function absoluteCenter(axis?: 'y'): Absolute & { transform: 'translateY(-50%)' }
-export function absoluteCenter(
-  axis?: 'xy'
-): Absolute & { left: '50%'; transform: 'translate(-50%, -50%)' }
-export function absoluteCenter(axis: 'xy' | 'y' = 'xy') {
-  const absolute: Absolute = { position: 'absolute', top: '50%' }
-
-  if (axis === 'y') return { ...absolute, transform: 'translateY(-50%)' }
-  return { ...absolute, left: '50%', transform: 'translate(-50%, -50%)' }
+export function absoluteCenter(axis?: 'y'): Absolute
+export function absoluteCenter(axis?: 'xy'): Absolute & { left: '50%' }
+export function absoluteCenter(axis: 'xy' | 'y' = 'xy'): Absolute {
+  return {
+    position: 'absolute',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    ...(axis === 'xy' ? { left: '50%' } : { transform: 'translateY(-50%)' })
+  }
 }
