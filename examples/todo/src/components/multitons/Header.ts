6718ba1af524a8175c1175c65fbe0b9c1d673d69
MasamiOgasawara <m-ogasawara@wely.tech>
2025-04-30 23:21:34 +0700

    feature: apply flexCenter

--- examples/todo/src/components/multitons/Header.ts
+++ examples/todo/src/components/multitons/Header.ts
@@ -1,7 +1,7 @@
 import { fics } from 'ficsjs'
 import { goto } from 'ficsjs/router'
 import { getState, setState } from 'ficsjs/state'
-import { variable } from 'ficsjs/style'
+import { flexCenter, variable } from 'ficsjs/style'
 import Langs from '@/components/multitons/Langs'
 import { $lang } from '@/store'
 import { breakpoints, getPath } from '@/utils'
@@ -34,10 +34,8 @@ export default () =>
         background: variable('black'),
         zIndex: 10,
         header: {
+          ...flexCenter('xy'),
           position: 'relative',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
           paddingBlock: variable('md'),
           [`@media (max-width: ${breakpoints.sm})`]: { paddingBlock: variable('xs') },
           h1: {
@@ -70,7 +68,5 @@ export default () =>
         setData('pathname', `/${_pathname}`)
       }
     },
-    actions: {
-      h1: { click: ({ data: { lang } }) => goto(getPath(lang, '/')) }
-    }
+    actions: { h1: { click: ({ data: { lang } }) => goto(getPath(lang, '/')) } }
   })
