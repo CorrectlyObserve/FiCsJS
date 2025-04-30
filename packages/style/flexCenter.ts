f846083b3002a394cd25a49270610f2ebf071e5e
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-04-30 23:05:22 +0700

    feature: define flexCenter as one of default styles

--- packages/style/flexCenter.ts
+++ packages/style/flexCenter.ts
@@ -1,7 +1,30 @@
-const flex = { display: 'flex' } as const
+type Direction = 'row' | 'column'
+
+interface Flex {
+  display: 'flex'
+  flexDirection: Direction
+}
+
 const justifyCenter = { justifyContent: 'center' } as const
 const alignCenter = { alignItems: 'center' } as const
 
-export const flexCenterXY = { ...flex, ...justifyCenter, ...alignCenter } as const
-export const flexCenterX = { ...flex, ...justifyCenter } as const
-export const flexCenterY = { ...flex, ...alignCenter } as const
+export function flexCenter(axis: 'x', direction?: Direction): Flex & typeof justifyCenter
+export function flexCenter(axis: 'y', direction?: Direction): Flex & typeof alignCenter
+export function flexCenter(
+  axis: 'xy',
+  direction?: Direction
+): Flex & typeof justifyCenter & typeof alignCenter
+export function flexCenter(axis: 'xy' | 'x' | 'y', direction: Direction = 'row'): Flex {
+  const flex: Flex = { display: 'flex', flexDirection: direction }
+
+  switch (axis) {
+    case 'x':
+      return { ...flex, ...justifyCenter }
+
+    case 'y':
+      return { ...flex, ...alignCenter }
+
+    case 'xy':
+      return { ...flex, ...justifyCenter, ...alignCenter }
+  }
+}
