f1dd94366e7d560324f1976926c12a6070eebea4
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-02 15:38:21 +0700

    design: modify css

--- examples/ssr/src/components/Photos.ts
+++ examples/ssr/src/components/Photos.ts
@@ -1,5 +1,5 @@
 import { fics } from 'ficsjs'
-import { absoluteCenter, flexCenter } from 'ficsjs/style'
+import { absoluteCenter, color, flexCenter } from 'ficsjs/style'
 import { api, getPhotos, photos } from '@/data/photos'
 import type { Photo } from '@/types'
 
@@ -16,14 +16,22 @@ export default () =>
               <img src="${api}/id/${id}/200/200.webp?blur" />
             </button>
             <div id="${id}" class="rounded-xl" popover>
-              <button class="clickable block p-3 ml-auto" popovertarget="${id}" popovertargetaction="hide" aria-hidden="true">X</button>
-              <p class="text-dark mx-4 mb-4 whitespace-nowrap">Created by ${author}</p>
+              <button class="clickable block text-white p-3 ml-auto" popovertarget="${id}" popovertargetaction="hide" aria-hidden="true">X</button>
+              <p class="text-base text-white mx-4 mb-4 whitespace-nowrap">Created by ${author}</p>
             </div>
           </div>
         `
       )}
     `,
-    css: { div: { '&[key]': { ...flexCenter('x') }, '&[popover]': { ...absoluteCenter() } } },
+    css: {
+      div: {
+        '&[key]': { ...flexCenter('x') },
+        '&[popover]': {
+          ...absoluteCenter(),
+          background: `${color({ hex: '#282828', rate: 0.5 })}`
+        }
+      }
+    },
     hooks: {
       mounted: async ({ data: { photos, count }, setData, crud }) =>
         await crud<Photo[]>(getPhotos(++count)).then(newPhotos => {
