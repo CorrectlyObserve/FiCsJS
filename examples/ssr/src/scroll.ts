13e9d9f6297b55040b3c95c6a6c281435dc3c45a
MasamiOgasawara <m-ogasawara@wely.tech>
2025-05-06 11:17:17 +0700

    feature: render the Link component in the client

--- examples/ssr/src/scroll.ts
+++ examples/ssr/src/scroll.ts
@@ -1,8 +1,10 @@
 import { ficsCss } from 'ficsjs'
+import Link from '@/components/Link'
 import Photos from '@/components/Photos'
 import Footer from '@/components/Footer'
 import css from '@/.tailwindcss.txt'
 
 ficsCss(css)
+Link().describe()
 Photos().describe()
 Footer().describe()
