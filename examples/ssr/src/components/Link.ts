45ade4ac9f0dc33f511f8cdeceb2bb09f31710d1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-06 11:11:59 +0700

    refactoring: use data instead of params

--- examples/ssr/src/components/Link.ts
+++ examples/ssr/src/components/Link.ts
@@ -1,14 +1,12 @@
 import { fics } from 'ficsjs'
 
-export default ({ href, text }: { href: string; text: string }) =>
+export default () =>
   fics({
     name: 'link',
-    html: ({ template }) =>
-      template`
-        <p class="text-base text-white text-center">
-          <a class="inline-block px-4 py-3 duration-200 ease-out hover:opacity-50 focus:scale-90" href="${href}">
-            <span class="border-b">${text}</span>
-          </a>
-        </p>
-      `
+    data: () => ({ href: '', text: '' }),
+    html: ({ data: { href, text }, template }) => template`
+      <p class="text-base text-white text-center">
+        <a class="clickable inline-block px-4 py-3" href="${href}"><span class="border-b">${text}</span></a>
+      </p>
+    `
   })
