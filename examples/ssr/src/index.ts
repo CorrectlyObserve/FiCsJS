ae4da2259e0524afa6f238334a1dec24a3d45f35
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-06 11:17:08 +0700

    feature: render the Link component in the client

--- examples/ssr/src/index.ts
+++ examples/ssr/src/index.ts
@@ -1,8 +1,10 @@
 import { ficsCss } from 'ficsjs'
+import Link from '@/components/Link'
 import Users from '@/components/Users'
 import Footer from '@/components/Footer'
 import css from '@/.tailwindcss.txt'
 
 ficsCss(css)
+Link().describe()
 Users().describe()
 Footer().describe()
