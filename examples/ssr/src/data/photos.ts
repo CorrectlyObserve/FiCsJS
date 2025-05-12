400b5464a9224fd423270eff1e5e08cfed3e6240
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-12 08:52:07 +0700

    refactoring: delete the photos

--- examples/ssr/src/data/photos.ts
+++ examples/ssr/src/data/photos.ts
@@ -1,7 +1,2 @@
-import type { Photo } from '@/types'
-
-const api = 'https://picsum.photos'
-const getPhotos = (page: number) => `${api}/v2/list?page=${page}&limit=5`
-const photos: Photo[] = await fetch(getPhotos(1)).then(res => res.json())
-
-export { api, getPhotos, photos }
+export const api = 'https://picsum.photos'
+export const getPhotos = (page: number) => `${api}/v2/list?page=${page}&limit=5`
