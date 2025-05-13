ee46bb6bbf1f3ba8f252cdd78098a026ec13d90f
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-13 16:53:48 +0700

    design: modify Tailwind CSS

--- examples/ssr/src/components/Link.ts
+++ examples/ssr/src/components/Link.ts
@@ -5,8 +5,8 @@ export default () =>
     name: 'link',
     data: () => ({ href: '', text: '' }),
     html: ({ data: { href, text }, template }) => template`
-      <p class="text-base text-white text-center">
-        <a class="clickable inline-block px-4 py-3" href="${href}"><span class="border-b">${text}</span></a>
+      <p class="text-base text-white text-center leading-none">
+        <a class="clickable inline-block px-4 py-3.5" href="${href}"><span class="border-b">${text}</span></a>
       </p>
     `
   })
