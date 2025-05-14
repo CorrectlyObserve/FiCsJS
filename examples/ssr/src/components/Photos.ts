c6c21785867c639b6e629b9e1697bce748d1ad00
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-15 01:51:01 +0700

    feature: apply the Icon component

--- examples/ssr/src/components/Photos.ts
+++ examples/ssr/src/components/Photos.ts
@@ -1,15 +1,19 @@
 import { fics } from 'ficsjs'
 import { absoluteCenter, color } from 'ficsjs/style'
+import Icon from '@/components/Icon'
 import Skelton from '@/components/Skelton'
 import { api, getPhotos } from '@/data/photos'
 import type { Photo } from '@/types'
+import { X } from 'lucide-static'
 
+const icon = Icon()
 const skelton = Skelton()
 
 export default () =>
   fics({
     name: 'photos',
     data: () => ({ count: 0, photos: [] as Photo[], photo: {} as Photo }),
+    props: [{ descendant: icon, values: () => ({ icon: X }) }],
     className: 'min-h-200 mt-4',
     html: ({
       data: {
@@ -26,7 +30,7 @@ export default () =>
               template`<img class="clickable mx-auto" src="${api}/id/${id}/200/200.webp?blur" key="${id}" tabindex="0" />`
           )}
           <dialog class="rounded-xl" open ${show(!!id)}>
-            <button class="clickable block text-white p-3 ml-auto">X</button>
+            <button class="clickable block text-white ml-auto">${icon}</button>
             <p class="text-base text-white mx-4 mb-4 whitespace-nowrap">Created by ${author}</p>
           </dialog>
         `
@@ -61,7 +65,7 @@ export default () =>
               'photo',
               !!id && (key === id || key === undefined)
                 ? ({} as Photo)
-                : { id: key, author: photos[parseInt(key)].author }
+                : { id: key, author: photos[parseInt(key)]?.author }
             )
           },
           { throttle: 500, blur: true }
