bca7abf43f7b2f5a26b831ea01753086a00c7aa5
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-15 14:27:19 +0700

    refactoring: apply delete of the Footer component

--- examples/ssr/src/index.ts
+++ examples/ssr/src/index.ts
@@ -1,10 +1,8 @@
 import { ficsCss } from 'ficsjs'
 import Link from '@/components/Link'
 import Users from '@/components/Users'
-import Footer from '@/components/Footer'
 import css from '@/.tailwindcss.txt'
 
 ficsCss(css)
 Link().describe()
 Users().describe()
-Footer().describe()
