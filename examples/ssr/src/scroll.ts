123c69dcc657ed88b4622735eeb3aea8a734fe28
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-15 14:27:31 +0700

    refactoring: apply delete of the Footer component

--- examples/ssr/src/scroll.ts
+++ examples/ssr/src/scroll.ts
@@ -1,10 +1,8 @@
 import { ficsCss } from 'ficsjs'
 import Link from '@/components/Link'
 import Photos from '@/components/Photos'
-import Footer from '@/components/Footer'
 import css from '@/.tailwindcss.txt'
 
 ficsCss(css)
 Link().describe()
 Photos().describe()
-Footer().describe()
