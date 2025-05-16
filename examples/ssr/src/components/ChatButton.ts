0ffcd3f8c162ca4afe912fe5299482c6ed4e8747
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-16 22:35:27 +0700

    feature: add the aria-label and modify Tailwind CSS

--- examples/ssr/src/components/ChatButton.ts
+++ examples/ssr/src/components/ChatButton.ts
@@ -8,9 +8,11 @@ const icon = Icon()
 export default () =>
   fics({
     name: 'chat-button',
-    className: 'fixed bottom-8 right-8',
+    className: 'fixed bottom-8 right-4',
     props: [{ descendant: icon, values: () => ({ icon: MessageCircleMore, isLarge: true }) }],
-    html: ({ template }) => template`<button class="clickable text-white">${icon}</button>`,
+    html: ({ template }) => template`
+      <button class="clickable text-white" aria-label="to the chat page">${icon}</button>
+    `,
     css: { ':host > button.clickable:focus': { scale: 0.8 } },
     actions: { button: { click: [() => goto('/chat'), { throttle: 500, blur: true }] } }
   })
