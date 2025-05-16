0cd3db92f5d8c960e08db8ef9c5cf363330b6750
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-16 12:03:05 +0700

    feature: hydrate the ChatButton component

--- examples/ssr/src/scroll.ts
+++ examples/ssr/src/scroll.ts
@@ -1,8 +1,10 @@
 import { ficsCss } from 'ficsjs'
 import Link from '@/components/Link'
 import Photos from '@/components/Photos'
+import ChatButton from '@/components/ChatButton'
 import css from '@/.tailwindcss.txt'
 
 ficsCss(css)
 Link().describe()
 Photos().describe()
+ChatButton().describe()
