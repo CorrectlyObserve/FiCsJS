8bc0c28efca2a354f39329040b482426bd6a83e0
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-16 12:08:02 +0700

    feature: create ChatButton component

---
+++ examples/ssr/src/components/ChatButton.ts
@@ -0,0 +1,16 @@
+import { fics } from 'ficsjs'
+import { goto } from 'ficsjs/router'
+import Icon from '@/components/Icon'
+import { MessageCircleMore } from 'lucide-static'
+
+const icon = Icon()
+
+export default () =>
+  fics({
+    name: 'chat-button',
+    className: 'fixed bottom-8 right-8',
+    props: [{ descendant: icon, values: () => ({ icon: MessageCircleMore, isLarge: true }) }],
+    html: ({ template }) => template`<button class="clickable text-white">${icon}</button>`,
+    css: { ':host > button.clickable:focus': { scale: 0.8 } },
+    actions: { button: { click: [() => goto('/chat'), { throttle: 500, blur: true }] } }
+  })
