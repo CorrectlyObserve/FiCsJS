55f0aa5519266718573560b5c6292fc57ba70d6a
MasamiOgasawara <m-ogasawara@wely.tech>
2025-05-28 14:31:23 +0700

    design: modify padding and margin

--- examples/ssr/src/components/Link.ts
+++ examples/ssr/src/components/Link.ts
@@ -6,7 +6,7 @@ export default () =>
     data: () => ({ href: '', text: '' }),
     html: ({ data: { href, text }, template }) => template`
       <p class="text-base text-white text-center leading-none">
-        <a class="clickable inline-block px-4 py-3.5" href="${href}"><span class="border-b">${text}</span></a>
+        <a class="clickable inline-block p-4 mb-3" href="${href}"><span class="border-b">${text}</span></a>
       </p>
     `
   })
