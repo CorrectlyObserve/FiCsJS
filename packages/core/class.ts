49d150fb1022f7d5e52de457f6408a3c26eedca1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-05-28 23:55:28 +0700

    debug: modify the infiniteScroll method

--- packages/core/class.ts
+++ packages/core/class.ts
@@ -854,14 +854,7 @@ export default class FiCsElement<D extends object, P extends object> {
         let { lastElementChild: lastChild }: { lastElementChild: Element | null } = root
 
         if (lastChild) {
-          const intersectionObserver: IntersectionObserver = new IntersectionObserver(
-            async ([{ isIntersecting }]) => {
-              if (isIntersecting) method({ ...this.#getDataPropsMethods(true) })
-            },
-            { rootMargin }
-          )
-
-          const mutationObserver = new MutationObserver(() => {
+          const reObserve = (): void => {
             const { lastElementChild }: { lastElementChild: Element | null } = root
 
             if (lastElementChild && lastElementChild !== lastChild) {
@@ -869,9 +862,20 @@ export default class FiCsElement<D extends object, P extends object> {
               intersectionObserver.observe(lastElementChild)
               lastChild = lastElementChild
             }
-          })
+          }
+          const intersectionObserver: IntersectionObserver = new IntersectionObserver(
+            async ([{ isIntersecting }]) => {
+              if (isIntersecting) {
+                method({ ...this.#getDataPropsMethods(true) })
+                reObserve()
+              }
+            },
+            { rootMargin }
+          )
 
           intersectionObserver.observe(lastChild)
+
+          const mutationObserver = new MutationObserver(() => reObserve())
           mutationObserver.observe(root, { childList: true })
           this.#scroll.isEnabled = true
         }
