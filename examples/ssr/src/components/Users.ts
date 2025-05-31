94dfc42d43e2e8d80be639e24c9952d4b6529349
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-06-01 00:16:23 +0700

    feature: apply the CrudButton component and make html contents easier to understand

--- examples/ssr/src/components/Users.ts
+++ examples/ssr/src/components/Users.ts
@@ -1,10 +1,10 @@
 import { fics } from 'ficsjs'
 import { flexCenter } from 'ficsjs/style'
-import Button from '@/components/CrudButton'
+import CrudButton from '@/components/CrudButton'
 import { api, users } from '@/data/users'
 import type { User } from '@/types'
 
-const button = Button()
+const crudButton = CrudButton()
 const headers: HeadersInit = { 'Content-type': 'application/json; charset=UTF-8' }
 
 export default () =>
@@ -13,7 +13,7 @@ export default () =>
     data: () => ({ users, userId: NaN, methods: ['put', 'patch', 'delete'] }),
     props: [
       {
-        descendant: button,
+        descendant: crudButton,
         values: ({ setData, crud }) => ({
           id: ({ getData }) => getData('userId'),
           deleteMethod:
@@ -44,19 +44,22 @@ export default () =>
       }
     ],
     html: ({ data: { users, userId, methods }, template, setProps }) => template`
-      <div class="buttons mb-7 gap-4">${methods.map(method => setProps(button, { method }))}</div>
+      <div class="buttons mb-7 gap-4">${methods.map(method => setProps(crudButton, { method }))}</div>
       <div class="space-y-4">
         ${users.map(user => {
-          const textColor = `${userId === user.id ? 'text-red' : 'text-white'}`
-          const items = { id: 'Id', name: 'Name', email: 'Email' } as const
-          const keys = Object.keys(items) as (keyof typeof items)[]
+          const { id } = user
+          const keys: (keyof User)[] = ['id', 'name', 'email']
 
           return template`
-            <div class="clickable w-3xs space-y-2 mx-auto" key="${user.id}" tabindex="0">
-              ${keys.map(
-                key => template`
-                <p class="text-base ${textColor}" key="${user.id}-${key}">${items[key]}: ${user[key]}</p>`
-              )}
+            <div class="clickable w-3xs space-y-2 mx-auto" key="${id}" tabindex="0">
+              ${keys.map((key, index) => {
+                const _key = keys[index]
+                return template`
+                    <p class="text-base ${userId === id ? 'text-red' : 'text-white'}" key="${id}-${key}">
+                      ${_key.charAt(0).toUpperCase() + _key.slice(1)}: ${user[key]}
+                    </p>
+                  `
+              })}
             </div>
           `
         })}
@@ -81,8 +84,8 @@ export default () =>
     actions: {
       'div > div': {
         click: [
-          ({ setData, getData, attributes }) => {
-            const userId = parseInt(attributes.key)
+          ({ setData, getData, attributes: { key } }) => {
+            const userId = parseInt(key)
             setData('userId', getData('userId') === userId ? NaN : userId)
           },
           { throttle: 500, blur: true }
