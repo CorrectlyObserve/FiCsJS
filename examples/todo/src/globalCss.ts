073994e01c915479d07f9629d6468c568ed2b1a4
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-06-13 23:09:39 +0700

    refactoring: make it easier to read

--- examples/todo/src/globalCss.ts
+++ examples/todo/src/globalCss.ts
@@ -1,14 +1,13 @@
 import { variable } from 'ficsjs/style'
 import { breakpoints, white } from '@/utils'
 
+const hover = { cursor: 'pointer', opacity: 0.5 }
+
 export default {
   '*': { padding: 0, margin: 0, boxSizing: 'border-box' },
   '*[tabindex], button, label, a': {
     transition: variable('transition'),
-    '&:not(button)': {
-      '&:hover': { cursor: 'pointer', opacity: 0.5 },
-      '&:focus': { color: variable('red'), outline: 'none' }
-    }
+    '&:not(button)': { '&:hover': hover, '&:focus': { color: variable('red'), outline: 'none' } }
   },
   'h2, p, button, label, span': { color: white, textAlign: 'center' },
   h2: {
@@ -21,15 +20,8 @@ export default {
   button: {
     border: 'none',
     outline: 'none',
-    '&:not([aria-disabled="true"])': {
-      '&:hover': { cursor: 'pointer', opacity: 0.5 },
-      '&:focus': { scale: 0.8 }
-    }
+    '&:not([aria-disabled="true"])': { '&:hover': hover, '&:focus': { scale: 0.8 } }
   },
-  label: {
-    display: 'inline-block',
-    textAlign: 'left',
-    '&:hover': { cursor: 'pointer', opacity: 0.5 }
-  },
+  label: { display: 'inline-block', textAlign: 'left', '&:hover': hover },
   span: { width: 'fit-content' }
 }
