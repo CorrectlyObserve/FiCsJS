b14aefcc772d26ca6d37816a3ce61280e89e940a
MasamiOgasawara <m-ogasawara@wely.tech>
2025-06-18 03:20:22 +0700

    refactoring: apply the cssVar function

--- examples/todo/src/components/multitons/Router.ts
+++ examples/todo/src/components/multitons/Router.ts
@@ -1,11 +1,11 @@
 import { ficsRouter, getParams } from 'ficsjs/router'
-import { calc, color, flexCenter, remToPx, variable } from 'ficsjs/style'
+import { calc, color, cssVar, flexCenter, remToPx } from 'ficsjs/style'
 import Tasks from '@/components/singletons/Tasks'
 import Task from '@/components/singletons/Task'
 import NotFound from '@/components/multitons/NotFound'
 import { breakpoints } from '@/utils'
 
-const xs = calc([variable('xs'), -1], '*')
+const xs = calc([cssVar('xs'), -1], '*')
 
 export default () =>
   ficsRouter({
@@ -33,17 +33,17 @@ export default () =>
     css: {
       ':host': {
         position: 'relative',
-        minHeight: variable('min-height'),
+        minHeight: cssVar('min-height'),
         'div.container': {
           ...flexCenter('x'),
           position: 'absolute',
           containerType: 'inline-size',
-          gap: variable('xl'),
+          gap: cssVar('xl'),
           width: '100%',
           [`@container (width >= ${breakpoints.lg})`]: {
             'f-task': {
-              paddingLeft: variable('xl'),
-              boxShadow: `${xs} 0px ${variable('xs')} ${xs} ${color({ hex: '--black', rate: 0.5, isOpacity: false })}`
+              paddingLeft: cssVar('xl'),
+              boxShadow: `${xs} 0px ${cssVar('xs')} ${xs} ${color({ hex: '--black', rate: 0.5, isOpacity: false })}`
             }
           }
         }
