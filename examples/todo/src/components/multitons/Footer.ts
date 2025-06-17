7e21d7b8ccc6c03154bee1ce3a04101fd7012880
MasamiOgasawara <m-ogasawara@wely.tech>
2025-06-18 03:19:04 +0700

    refactoring: apply the cssVar function

--- examples/todo/src/components/multitons/Footer.ts
+++ examples/todo/src/components/multitons/Footer.ts
@@ -1,9 +1,9 @@
 import { fics } from 'ficsjs'
-import { variable } from 'ficsjs/style'
+import { cssVar } from 'ficsjs/style'
 
 export default () =>
   fics({
     name: 'footer',
     html: ({ template }) => template`<footer><p>&copy; 2024 Masami Ogasawara</p></footer>`,
-    css: { footer: { paddingBottom: variable('md'), p: { fontSize: variable('sm') } } }
+    css: { footer: { paddingBottom: cssVar('md'), p: { fontSize: cssVar('sm') } } }
   })
