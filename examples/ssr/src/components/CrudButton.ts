bc83de16b1fd91b6b44f79a405120f9adfa395cd
takahashi-jo <jo.takahashi.0120@gmail.com>
2025-06-19 17:03:43 +0900

    debug: fix setProps

--- examples/ssr/src/components/CrudButton.ts
+++ examples/ssr/src/components/CrudButton.ts
@@ -2,13 +2,13 @@ import { fics } from 'ficsjs'
 import Button from '@/components/materials/Button'
 import type { Method } from '@/types'
 
-export default () =>
-  fics<{}, { method: Method; click: (method: Method) => void }>({
-    name: 'crud-button',
+export default (suffix = '') =>
+  fics<{}, { method: Method; click: (method: Method) => void; isDisabled: boolean }>({
+    name: `crud-button${suffix}`,
     children: [Button()],
     props: {
       descendant: ({ children: { button } }) => button,
-      values: ({ props: { method, click } }) => ({ text: method, click: () => click(method) })
+      values: ({ props: { method, click, isDisabled } }) => ({ text: method, click: () => click(method), isDisabled })
     },
     html: ({ children: { button }, template }) => template`${button}`
   })
--- examples/ssr/src/components/Users.ts
+++ examples/ssr/src/components/Users.ts
@@ -6,75 +6,103 @@ import type { Method, User } from '@/types'
 
 const headers: HeadersInit = { 'Content-type': 'application/json; charset=UTF-8' }
 
+const crudButtonPut = CrudButton('-put')
+const crudButtonPatch = CrudButton('-patch')
+const crudButtonDelete = CrudButton('-delete')
+
 export default () =>
   fics({
     name: 'users',
-    children: [CrudButton()],
+    children: [crudButtonPut, crudButtonPatch, crudButtonDelete],
     data: () => ({ users, userId: NaN, methods: ['PUT', 'PATCH', 'DELETE'] as Method[] }),
     props: [
       {
-        descendant: ({ children: { crudButton }, getChildren }) => getChildren(crudButton).button,
+        descendant: ({ children: { crudButtonPut }, getChildren }) => getChildren(crudButtonPut).button,
         values: () => ({ isDisabled: ({ getData }) => isNaN(getData('userId')) })
       },
       {
-        descendant: ({ children: { crudButton } }) => crudButton,
+        descendant: ({ children: { crudButtonPut } }) => crudButtonPut,
         values: ({ setData, crud }) => ({
-          click:
-            ({ getData }) =>
-            async (method: Method) => {
-              const userId = getData('userId')
-              const options = { method, ...headers }
-
-              if (method === 'DELETE') {
-                await crud<User>(`${api}/${userId}`, options)
-                setData(
-                  'users',
-                  getData('users').filter(({ id }) => id !== userId)
-                )
-              } else {
-                const name = prompt('Please enter a new user name.')
-                if (name) {
-                  await crud<User>(`${api}/${userId}`, {
-                    ...options,
-                    body: JSON.stringify({ id: userId, name })
-                  })
-                  setData(
-                    'users',
-                    getData('users').map(user => (user.id === userId ? { ...user, name } : user))
-                  )
-                }
-              }
-
-              setData('userId', NaN)
+          method: 'PUT',
+          click: ({ getData }) => async () => {
+            const userId = getData('userId')
+            const name = prompt('Please enter a new user name.')
+            if (name) {
+              await crud<User>(`${api}/${userId}`, {
+                method: 'PUT',
+                ...headers,
+                body: JSON.stringify({ id: userId, name })
+              })
+              setData('users', getData('users').map(user => (user.id === userId ? { ...user, name } : user)))
             }
+            setData('userId', NaN)
+          }
+        })
+      },
+      {
+        descendant: ({ children: { crudButtonPatch }, getChildren }) => getChildren(crudButtonPatch).button,
+        values: () => ({ isDisabled: ({ getData }) => isNaN(getData('userId')) })
+      },
+      {
+        descendant: ({ children: { crudButtonPatch } }) => crudButtonPatch,
+        values: ({ setData, crud }) => ({
+          method: 'PATCH',
+          click: ({ getData }) => async () => {
+            const userId = getData('userId')
+            const name = prompt('Please enter a new user name.')
+            if (name) {
+              await crud<User>(`${api}/${userId}`, {
+                method: 'PATCH',
+                ...headers,
+                body: JSON.stringify({ id: userId, name })
+              })
+              setData('users', getData('users').map(user => (user.id === userId ? { ...user, name } : user)))
+            }
+            setData('userId', NaN)
+          }
+        })
+      },
+      {
+        descendant: ({ children: { crudButtonDelete }, getChildren }) => getChildren(crudButtonDelete).button,
+        values: () => ({ isDisabled: ({ getData }) => isNaN(getData('userId')) })
+      },
+      {
+        descendant: ({ children: { crudButtonDelete } }) => crudButtonDelete,
+        values: ({ setData, crud }) => ({
+          method: 'DELETE',
+          click: ({ getData }) => async () => {
+            const userId = getData('userId')
+            await crud<User>(`${api}/${userId}`, { method: 'DELETE', ...headers })
+            setData('users', getData('users').filter(({ id }) => id !== userId))
+            setData('userId', NaN)
+          }
         })
       }
     ],
     html: ({
-      children: { crudButton },
-      data: { users, userId, methods },
-      template,
-      setProps
+      children: { crudButtonPut, crudButtonPatch, crudButtonDelete },
+      data: { users, userId },
+      template
     }) => template`
-      <div class="buttons mb-7 gap-4">${methods.map(method => setProps(crudButton, { method }))}</div>
+      <div class="buttons mb-7 gap-4">${crudButtonPut}${crudButtonPatch}${crudButtonDelete}</div>
       <div class="space-y-4">
         ${users.map(user => {
-          const { id } = user
-          const keys: (keyof User)[] = ['id', 'name', 'email']
+      const { id } = user
+      const keys: (keyof User)[] = ['id', 'name', 'email']
 
-          return template`
+      return template`
             <div class="clickable w-3xs space-y-2 mx-auto" key="${id}" tabindex="0">
               ${keys.map((key, index) => {
-                const _key = keys[index]
-                return template`
+        const _key = keys[index]
+        return template`
                   <p class="text-base ${userId === id ? 'text-red' : 'text-white'}" key="${id}-${key}">
                     ${_key.charAt(0).toUpperCase() + _key.slice(1)}: ${user[key]}
                   </p>
                 `
-              })}
+      })}
             </div>
           `
-        })}
+    })}
       </div>
     `,
     css: {
