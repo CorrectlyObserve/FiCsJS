322144283092cd50dd79797b838e25ab10c9cb08
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-06-20 08:55:09 +0700

    refactoring: convert it from the fuction type to the instance type

--- examples/todo/src/components/multitons/Langs.ts
+++ examples/todo/src/components/multitons/Langs.ts
@@ -9,56 +9,55 @@ interface Props {
   getLang: (lang: string) => string
 }
 
-export default () =>
-  fics<{ langs: string[]; isShown: boolean }, Props>({
-    name: 'langs',
-    data: () => ({ langs: ['en', 'ja'], isShown: false }),
-    html: ({ data: { langs, isShown }, props: { lang }, template, show }) => template`
-      <div class="container">
-        <button class="lang">${lang.toUpperCase()}</button>
-        <div class="${('langs ' + (!isShown ? 'hidden' : '')).trim()}" ${show(isShown)}>
-          ${langs.map(
-            _lang => template`
-              <button class="${lang === _lang ? 'selected' : ''}" key="${_lang}">
-                ${_lang.toUpperCase()}
-              </button>
-            `
-          )}
-        </div>
+export default fics<{ langs: string[]; isShown: boolean }, Props>({
+  name: 'langs',
+  data: () => ({ langs: ['en', 'ja'], isShown: false }),
+  html: ({ data: { langs, isShown }, props: { lang }, template, show }) => template`
+    <div class="container">
+      <button class="lang">${lang.toUpperCase()}</button>
+      <div class="${('langs ' + (!isShown ? 'hidden' : '')).trim()}" ${show(isShown)}>
+        ${langs.map(
+          _lang => template`
+            <button class="${lang === _lang ? 'selected' : ''}" key="${_lang}">
+              ${_lang.toUpperCase()}
+            </button>
+          `
+        )}
       </div>
-    `,
-    css: {
-      'div.container': {
-        button: {
-          width: calc([cssVar('md'), 3], '*'),
-          background: cssVar('black'),
-          paddingBlock: cssVar('xs'),
-          [`@media (max-width: ${breakpoints.sm})`]: { paddingBlock: cssVar('md') },
-          '&.lang:focus': { opacity: 0.5 },
-          '&.selected': { color: cssVar('red') }
-        },
-        div: {
-          position: 'absolute',
-          opacity: 1,
-          transition: `${cssVar('transition')} allow-discrete`,
-          '@starting-style': { opacity: 0 },
-          '&.hidden': { opacity: 0 }
-        }
-      }
-    },
-    actions: {
-      'button.lang': {
-        click: [
-          ({ data: { isShown }, setData }) => setData('isShown', !isShown),
-          { throttle: 500, blur: true }
-        ]
+    </div>
+  `,
+  css: {
+    'div.container': {
+      button: {
+        width: calc([cssVar('md'), 3], '*'),
+        background: cssVar('black'),
+        paddingBlock: cssVar('xs'),
+        [`@media (max-width: ${breakpoints.sm})`]: { paddingBlock: cssVar('md') },
+        '&.lang:focus': { opacity: 0.5 },
+        '&.selected': { color: cssVar('red') }
       },
-      'button[key]': {
-        click: [
-          ({ props: { pathname, getLang }, attributes }) =>
-            goto(getPath(getLang(attributes['key']), pathname)),
-          { throttle: 500, blur: true }
-        ]
+      div: {
+        position: 'absolute',
+        opacity: 1,
+        transition: `${cssVar('transition')} allow-discrete`,
+        '@starting-style': { opacity: 0 },
+        '&.hidden': { opacity: 0 }
       }
     }
-  })
+  },
+  actions: {
+    'button.lang': {
+      click: [
+        ({ data: { isShown }, setData }) => setData('isShown', !isShown),
+        { throttle: 500, blur: true }
+      ]
+    },
+    'button[key]': {
+      click: [
+        ({ props: { pathname, getLang }, attributes }) =>
+          goto(getPath(getLang(attributes['key']), pathname)),
+        { throttle: 500, blur: true }
+      ]
+    }
+  }
+})
