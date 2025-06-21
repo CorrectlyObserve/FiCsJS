c8682fda548241b8b91ca542b1eac314e06e9769
MasamiOgasawara <m-ogasawara@wely.tech>
2025-06-21 16:16:02 +0700

    feature: apply the setIndividualProps method and convert it from the fuction type to the instance type

--- examples/ssr/src/components/Tab.ts
+++ examples/ssr/src/components/Tab.ts
@@ -3,35 +3,34 @@ import { goto } from 'ficsjs/router'
 import { flexCenter } from 'ficsjs/style'
 import Button from '@/components/materials/Button'
 
-export default () =>
-  fics({
-    name: 'tab',
-    children: [Button()],
-    data: () => ({ tabs: [{ text: 'Chat' }, { query: '?tab=logs', text: 'Logs' }], path: '' }),
-    html: ({ children: { button }, data: { tabs, path }, template, setData, setProps }) => template`
-      <div class="buttons mb-7 gap-4">
-        ${tabs.map(({ query, text }) => {
-          const href = `/chat${query ?? ''}`
+export default fics({
+  name: 'tab',
+  children: [Button],
+  data: () => ({ tabs: [{ text: 'Chat' }, { query: '?tab=logs', text: 'Logs' }], path: '' }),
+  html: ({ children: { button }, data: { tabs, path }, template, setData }) => template`
+    <div class="buttons mb-7 gap-4">
+      ${tabs.map(({ query, text }) => {
+        const href = `/chat${query ?? ''}`
 
-          return template`
-            ${setProps(button, {
-              isDisabled: href === path,
-              text,
-              click: () => {
-                goto(href, { reload: true })
-                setData('path', href)
-              }
-            })}
-          `
-        })}
-      </div>
-    `,
-    css: { div: { '&.buttons': { ...flexCenter('x') } } },
-    hooks: {
-      created: ({ setData }) => {
-        const { pathname, search } = window.location
-        setData('path', `${pathname}${search}`)
-      }
-    },
-    options: { ssr: false }
-  })
+        return template`
+          ${button.setIndividualProps(text, {
+            isDisabled: href === path,
+            text,
+            click: () => {
+              goto(href, { reload: true })
+              setData('path', href)
+            }
+          })}
+        `
+      })}
+    </div>
+  `,
+  css: { div: { '&.buttons': { ...flexCenter('x') } } },
+  hooks: {
+    created: ({ setData }) => {
+      const { pathname, search } = window.location
+      setData('path', `${pathname}${search}`)
+    }
+  },
+  options: { ssr: false }
+})
