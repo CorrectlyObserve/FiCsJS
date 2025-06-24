e0a4e609ba0188920a05cc874753b71a15fbf5d7
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-06-24 14:13:59 +0700

    refactoring: convert it from the fuction type to the instance type

--- examples/ssr/src/components/CrudButton.ts
+++ examples/ssr/src/components/CrudButton.ts
@@ -2,13 +2,12 @@ import { fics } from 'ficsjs'
 import Button from '@/components/materials/Button'
 import type { Method } from '@/types'
 
-export default () =>
-  fics<{}, { method: Method; click: (method: Method) => void }>({
-    name: 'crud-button',
-    children: [Button()],
-    props: {
-      descendant: ({ children: { button } }) => button,
-      values: ({ props: { method, click } }) => ({ text: method, click: () => click(method) })
-    },
-    html: ({ children: { button }, template }) => template`${button}`
-  })
+export default fics<{}, { method: Method; click: (method: Method) => void }>({
+  name: 'crud-button',
+  children: [Button()],
+  props: {
+    descendant: ({ children: { button } }) => button,
+    values: ({ props: { method, click } }) => ({ buttonText: method, click: () => click(method) })
+  },
+  html: ({ children: { button }, template }) => template`${button}`
+})
