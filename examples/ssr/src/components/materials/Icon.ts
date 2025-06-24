7dfc20f1d128ef505f8f3e00030c98be6a89ae46
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-06-24 13:04:19 +0700

    refactoring: make the props name easier to understand

--- examples/ssr/src/components/materials/Icon.ts
+++ examples/ssr/src/components/materials/Icon.ts
@@ -2,14 +2,14 @@ import { fics } from 'ficsjs'
 import { color } from 'ficsjs/style'
 
 export default () =>
-  fics<{}, { icon: string; isLarge?: string }>({
+  fics<{}, { svg: string; isLarge?: string }>({
     name: 'icon',
-    html: ({ props: { icon, isLarge }, template, html }) => template`
-      <span class="flex ${isLarge ? 'p-4' : 'p-3'}">${html(icon)}</span>
+    html: ({ props: { svg, isLarge }, template, html }) => template`
+      <span class="flex ${isLarge ? 'p-4' : 'p-3'}">${html(svg)}</span>
     `,
     css: {
       span: ({ props: { isLarge } }) => {
-        const size = isLarge ? '2.5rem' : '1.25rem'
+        const size = `${isLarge ? 2.5 : 1.25}rem`
         return {
           svg: { width: size, height: 'auto', stroke: 'currentColor' },
           span: { width: size, height: size, background: color({ hex: '#fff', rate: 0.1 }) }
