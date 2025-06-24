991316a89be0bcc21e7fe928a9726fa50b1690e5
MasamiOgasawara <m-ogasawara@wely.tech>
2025-06-24 14:16:12 +0700

    refactoring: apply the modification of the Button component and organize css

--- examples/ssr/src/components/Photos.ts
+++ examples/ssr/src/components/Photos.ts
@@ -5,108 +5,107 @@ import Icon from '@/components/materials/Icon'
 import Skeleton from '@/components/materials/Skeleton'
 import { api, getPhotos } from '@/data/photos'
 import type { Photo } from '@/types'
-import { X } from 'lucide-static'
+import { CircleX } from 'lucide-static'
 
-export default () =>
-  fics({
-    name: 'photos',
-    children: [Icon(), Skeleton()],
-    data: () => ({ count: 0, photos: [] as Photo[], photo: {} as Photo, isLoading: false }),
-    deferredData: async ({ data: { count }, crud }) =>
-      await crud<Photo[]>(getPhotos(++count)).then(photos => ({ count, photos })),
-    props: { descendant: ({ children: { icon } }) => icon, values: () => ({ icon: X }) },
-    className: 'min-h-200',
-    html: ({
-      children: { icon, skelton },
-      data: {
-        photos,
-        photo: { id, author },
-        isLoading
-      },
-      template,
-      show,
-      isBrowser,
-      isDeferred
-    }) => {
-      const skeletons = template`${[...Array(5)].map(_ => template`${Skeleton()}`)}`
+export default fics({
+  name: 'photos',
+  children: [Icon(), Skeleton()],
+  data: () => ({ count: 0, photos: [] as Photo[], photo: {} as Photo, isLoading: false }),
+  deferredData: async ({ data: { count }, crud }) =>
+    await crud<Photo[]>(getPhotos(++count)).then(photos => ({ count, photos })),
+  props: { descendant: ({ children: { icon } }) => icon, values: () => ({ svg: CircleX }) },
+  className: 'min-h-200',
+  html: ({
+    children: { icon, skelton },
+    data: {
+      photos,
+      photo: { id, author },
+      isLoading
+    },
+    template,
+    show,
+    isBrowser,
+    isDeferred
+  }) => {
+    const skeletons = template`${[...Array(5)].map(_ => template`${skelton}`)}`
 
-      if (!isBrowser || !isDeferred) return skeletons
+    if (!isBrowser || !isDeferred) return skeletons
 
-      return template`
-        <div class="images">
-          ${photos.map(
-            ({ id, author, isLoaded }) => template`
-              <div class="relative h-50" key="${id}-container">
-                ${skelton}
-                <img
-                  class="clickable mx-auto"
-                  src="${api}/id/${id}/200/200.webp?blur"
-                  alt="the image created by ${author}"
-                  key="${id}"
-                  tabindex="0"
-                  ${show(isLoaded)}
-                />
-              </div>
-            `
-          )}
-          </div>
-          ${isLoading ? skeletons : ''}
-        <dialog class="rounded-xl" open ${show(!!id)}>
-          <button class="clickable block text-white ml-auto">${icon}</button>
-          <p class="text-base text-white mx-4 mb-4 whitespace-nowrap">Created by ${author}</p>
-        </dialog>
-      `
-    },
-    css: {
-      img: { ...absoluteCenter('x'), top: 0 },
-      dialog: {
-        ...absoluteCenter('xy', 'fixed'),
-        background: `${color({ hex: '#282828', rate: 0.5 })}`
-      }
-    },
-    actions: {
-      img: {
-        load: [
-          ({ data: { photos }, setData, attributes: { key } }) =>
-            setData(
-              'photos',
-              photos.map(photo => {
-                if (photo.id === key) photo.isLoaded = true
-                return photo
-              })
-            ),
-          { once: true }
-        ]
-      },
-      '.clickable': {
-        click: [
-          ({
-            data: {
-              photos,
-              photo: { id }
-            },
-            setData,
-            attributes: { key }
-          }) =>
-            setData(
-              'photo',
-              !!id && (key === id || key === undefined)
-                ? ({} as Photo)
-                : { id: key, author: photos[parseInt(key)]?.author, isLoaded: false }
-            ),
-          { throttle: 500, blur: true }
-        ]
-      }
-    },
-    scroll: {
-      area: 'div.images',
-      rootMargin: '100px 0px 0px 0px',
-      trigger: ({ data: { photos } }) => photos.length > 0,
-      method: async ({ data: { photos, count }, setData, crud }) =>
-        await crud<Photo[]>(getPhotos(++count), { key: 'isLoading' }).then(newPhotos => {
-          setData('count', count)
-          setData('photos', [...photos, ...newPhotos])
-          goto(`/scroll?page=${count}`, { reload: false })
-        })
+    return template`
+      <div class="images">
+        ${photos.map(
+          ({ id, author, isLoaded }) => template`
+            <div class="relative h-50" key="${id}-container">
+              ${skelton}
+              <img
+                class="clickable mx-auto"
+                src="${api}/id/${id}/200/200.webp?blur"
+                alt="the image created by ${author}"
+                key="${id}"
+                tabindex="0"
+                ${show(isLoaded)}
+              />
+            </div>
+          `
+        )}
+        </div>
+        ${isLoading ? skeletons : ''}
+      <dialog class="rounded-xl" open ${show(!!id)}>
+        <button class="clickable block text-white ml-auto">${icon}</button>
+        <p class="text-base text-white mx-4 mb-4 whitespace-nowrap">Created by ${author}</p>
+      </dialog>
+    `
+  },
+  css: {
+    img: { ...absoluteCenter('x'), top: 0 },
+    dialog: {
+      ...absoluteCenter('xy', 'fixed'),
+      background: `${color({ hex: '#282828', rate: 0.5 })}`
     }
-  })
+  },
+  actions: {
+    img: {
+      load: [
+        ({ data: { photos }, setData, attributes: { key } }) =>
+          setData(
+            'photos',
+            photos.map(photo => {
+              if (photo.id === key) photo.isLoaded = true
+              return photo
+            })
+          ),
+        { once: true }
+      ]
+    },
+    '.clickable': {
+      click: [
+        ({
+          data: {
+            photos,
+            photo: { id }
+          },
+          setData,
+          attributes: { key }
+        }) =>
+          setData(
+            'photo',
+            !!id && (key === id || key === undefined)
+              ? ({} as Photo)
+              : { id: key, author: photos[parseInt(key)]?.author, isLoaded: false }
+          ),
+        { throttle: 500, blur: true }
+      ]
+    }
+  },
+  scroll: {
+    area: 'div.images',
+    rootMargin: '100px 0px 0px 0px',
+    trigger: ({ data: { photos } }) => photos.length > 0,
+    method: async ({ data: { photos, count }, setData, crud }) =>
+      await crud<Photo[]>(getPhotos(++count), { key: 'isLoading' }).then(newPhotos => {
+        setData('count', count)
+        setData('photos', [...photos, ...newPhotos])
+        goto(`/scroll?page=${count}`, { reload: false })
+      })
+  }
+})
