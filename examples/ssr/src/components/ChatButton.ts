bcd3ff1ec3d0203d255d41365e782c90ec71cf38
MasamiOgasawara <m-ogasawara@wely.tech>
2025-06-24 14:13:47 +0700

    refactoring: convert it from the fuction type to the instance type

--- examples/ssr/src/components/ChatButton.ts
+++ examples/ssr/src/components/ChatButton.ts
@@ -3,18 +3,17 @@ import { goto } from 'ficsjs/router'
 import Icon from '@/components/materials/Icon'
 import { MessageCircleMore } from 'lucide-static'
 
-export default () =>
-  fics({
-    name: 'chat-button',
-    className: 'fixed bottom-8 right-4',
-    children: [Icon()],
-    props: {
-      descendant: ({ children: { icon } }) => icon,
-      values: () => ({ icon: MessageCircleMore, isLarge: true })
-    },
-    html: ({ children: { icon }, template }) => template`
-      <button class="clickable text-white" aria-label="go to the chat page">${icon}</button>
-    `,
-    css: { ':host > button.clickable:focus': { scale: 0.8 } },
-    actions: { button: { click: [() => goto('/chat'), { throttle: 500, blur: true }] } }
-  })
+export default fics({
+  name: 'chat-button',
+  className: 'fixed bottom-8 right-4',
+  children: [Icon()],
+  props: {
+    descendant: ({ children: { icon } }) => icon,
+    values: () => ({ svg: MessageCircleMore, isLarge: true })
+  },
+  html: ({ children: { icon }, template }) => template`
+    <button class="clickable text-white" aria-label="go to the chat page">${icon}</button>
+  `,
+  css: { ':host > button.clickable:focus': { scale: 0.8 } },
+  actions: { button: { click: [() => goto('/chat'), { throttle: 500, blur: true }] } }
+})
