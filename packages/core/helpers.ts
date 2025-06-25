358dc057b94d575c26554cd0dec61b0a94ba3299
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-06-25 09:41:28 +0700

    refactoring: define the convertStr function

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -26,6 +26,11 @@ export const checkType = <
     ? typeof param === 'object' && param !== null && !Array.isArray(param)
     : typeof param === type
 
+export const convertStr = (str: string, type: 'kebab' | 'camel'): string => {
+  if (type === 'kebab') return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
+  return str.toLowerCase().replace(/-([a-z])/g, (_, char) => char.toUpperCase())
+}
+
 export const isBrowser = (): boolean => {
   try {
     return !checkType(window, 'undefined') && !checkType(document, 'undefined')
