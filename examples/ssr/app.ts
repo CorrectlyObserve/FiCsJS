8d37e2af3a34844822edd1931d32bc68e9e06fbb
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-06-26 10:27:43 +0700

    feature: oranize server side rendered contents and prepare for the SSE

--- examples/ssr/app.ts
+++ examples/ssr/app.ts
@@ -1,15 +1,17 @@
 import { Hono } from 'hono'
+import { streamSSE } from 'hono/streaming'
 import { serveStatic } from '@hono/node-server/serve-static'
-import Footer from './src/components/Footer'
-import Link from './src/components/Link'
+import Link from './src/components/materials/Link'
 import Users from './src/components/Users'
+import ChatButton from './src/components/ChatButton'
 import Photos from './src/components/Photos'
+import Tab from './src/components/Tab'
+import Logs from './src/components/Logs'
 
 const app = new Hono()
 
 app.get('/dist/*', serveStatic({ root: './' }))
 
-const footer = Footer()
 const template = ({
   title,
   description,
@@ -32,37 +34,75 @@ const template = ({
         <link rel="stylesheet" type="text/css" href="./dist/global.css" />
       </head>
       <body class="bg-dark px-4 pb-4">
-        <header class="py-2"><h1 class="text-xl text-center font-medium">${title}</h1></header>
+        <header class="py-2"><h1 class="text-xl text-center font-semibold">${title}</h1></header>
         <main class="mb-8">${content}</main>
-        ${footer.toString()}
+        <footer class="text-sm text-white text-center"><p>&copy; 2025 Masami Ogasawara</p></footer>
         <script type="module" src="./dist${path}.js"></script>
       </body>
     </html>
   `
+
 const link = Link()
-const users = Users()
 app.get('/', c =>
   c.html(
     template({
       title: 'FiCsJS with Hono',
       description: 'This is a simple example of FiCsJS with Hono in SSR.',
-      content: `${link.toString({ href: '/scroll', text: 'Go to the scroll page' })}${users.toString()}`,
+      content: `
+        ${link.toString({ href: '/scroll', text: 'Go to the scroll page' })}
+        ${Users.toString()}
+        ${ChatButton.toString()}
+      `,
       path: '/index'
     })
   )
 )
 
-const photos = Photos()
 app.get('/scroll', c =>
   c.html(
     template({
       title: 'Infinite and virtual scroll',
       description:
         'This is a simple example of an infinite scroll and a virtual scroll with FiCsJS.',
-      content: `${link.toString({ href: '/', text: 'Back to the top page' })}${photos.toString()}`,
+      content: `
+        ${link.toString({ href: '/', text: 'Back to the top page' })}
+        ${Photos.toString()}
+        ${ChatButton.toString()}
+      `,
       path: '/scroll'
     })
   )
 )
+
+app.get('/chat', c =>
+  c.html(
+    template({
+      title: 'WebSocket and SSE',
+      description: 'This is a simple example of an WebSocket and a SSE with FiCsJS.',
+      content: `
+        ${link.toString({ href: '/', text: 'Back to the top page' })}
+        ${Tab.toString()}
+        ${Logs.toString()}
+      `,
+      path: '/chat'
+    })
+  )
+)
+
+let id = 0
+
+app.get('/sse', async c => {
+  return streamSSE(c, async stream => {
+    while (true) {
+      const message = `It is ${new Date().toISOString()}`
+      await stream.writeSSE({
+        data: message,
+        event: 'time-update',
+        id: String(id++)
+      })
+      await stream.sleep(1000)
+    }
+  })
+})
 
 export default { port: 5174, fetch: app.fetch }
