850c0f94f03313895b2d975ecb6cdbb15cac127c
MasamiOgasawara <m-ogasawara@wely.tech>
2025-07-03 23:55:08 +0700

    feature: use Lucide icons instead of downloaded svgs

--- examples/todo/src/components/materials/Icon.ts
+++ examples/todo/src/components/materials/Icon.ts
@@ -2,33 +2,41 @@ import { fics } from 'ficsjs'
 import { cssVar, rotate } from 'ficsjs/style'
 import { white } from '@/utils'
 
-export default (icon: string) =>
-  fics<{ icon: string }, { color?: string; click: () => void }>({
+interface Props {
+  svg: string
+  areaLabel: string
+  color?: string
+  isLoadingIcon?: boolean
+  click?: () => void
+}
+
+export default () =>
+  fics<{}, Props>({
     name: 'icon',
-    data: () => ({ icon }),
-    html: ({ data: { icon }, template }) => template`<button class="${icon}"><span /></button>`,
+    html: ({ props: { svg, areaLabel }, template, html }) => template`
+      <button aria-label="${areaLabel}">${html(svg)}</button>
+    `,
     css: {
-      button: ({ data: { icon }, props: { color } }) => ({
+      button: ({ props: { color, isLoadingIcon } }) => ({
         background: 'none',
+        color: color ?? white,
         padding: cssVar('xs'),
-        '&:focus': { scale: 0.8 },
-        '&.loading': {
-          display: 'block',
-          marginInline: 'auto',
-          span: {
-            width: cssVar('2xl'),
-            height: cssVar('2xl'),
-            animation: 'loading 1.5s infinite linear'
-          }
+        svg: {
+          width: cssVar(isLoadingIcon ? '2xl' : 'xl'),
+          height: 'auto',
+          stroke: 'currentColor'
         },
-        span: {
-          width: cssVar('xl'),
-          height: cssVar('xl'),
-          display: 'block',
-          maskImage: `url("/icons/${icon}.svg")`,
-          background: color ?? white
-        },
-        '@keyframes loading': { from: { transform: rotate(0) }, to: { transform: rotate(360) } }
+        ...(isLoadingIcon
+          ? {
+              display: 'block',
+              marginInline: 'auto',
+              svg: { animation: 'loading 1.5s infinite linear' },
+              '@keyframes loading': {
+                from: { transform: rotate(0) },
+                to: { transform: rotate(360) }
+              }
+            }
+          : {})
       })
     },
     actions: {
