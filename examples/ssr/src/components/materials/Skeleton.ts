108344bc30eef19382429b8ea9e7b27dd1636962
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-07-10 07:33:45 +0700

    refactoring: make it easier to read

--- examples/ssr/src/components/materials/Skeleton.ts
+++ examples/ssr/src/components/materials/Skeleton.ts
@@ -4,8 +4,7 @@ import { color } from 'ficsjs/style'
 export default () =>
   fics({
     name: 'skeleton',
-    html: ({ template }) => template`
-      <div class="skeleton size-50 block mx-auto animate-pulse"></div>
-    `,
+    html: ({ template }) =>
+      template`<div class="skeleton size-50 block mx-auto animate-pulse"></div>`,
     css: { 'div.skeleton': { background: color({ hex: '#fff', rate: 0.05 }) } }
   })
