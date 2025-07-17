da82b92402fc40c2fe7b9f1fa0e6a959e84e9391
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-07-17 17:01:56 +0700

    refactoring: split to 2 files and make a consistent name and function names easier to understand

--- packages/router/params.ts
+++ packages/router/params.ts
@@ -1,29 +1,5 @@
-import { browserError, isBrowser } from '../core/helpers'
+import { isBrowser } from '../core/helpers'
 import type { Param } from './types'
-
-const pathParam: RegExp = /\/:[^\/]+/g
-
-export const getRegExp = (path: string): RegExp =>
-  new RegExp(`^${path.replaceAll(pathParam, `\/([^/]+?)`)}\/?$`)
-
-export const getPathParams = (path: string): Record<string, string> => {
-  browserError()
-
-  const regExps: string[] | null = getRegExp(path).exec(window.location.pathname),
-    pathParams: Record<string, string> = {}
-
-  if (regExps && regExps.length > 0) {
-    const names: string[] = (path.match(pathParam) ?? []).map(param => param.replace(/^\/:/, ''))
-
-    if (names.length > 0)
-      for (const [index, value] of regExps.slice(1).entries())
-        pathParams[names[index]] = value ?? ''
-  }
-
-  return pathParams
-}
-
-export const isPathParam = (path: string): boolean => pathParam.test(path)
 
 class Params {
   params: Record<Param, Record<string, string>> = { path: {}, query: {} }
