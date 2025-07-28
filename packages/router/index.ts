fd916daf9e8b7e735c1ee75053528c913708f104
MasamiOgasawara <m-ogasawara@wely.tech>
2025-07-28 13:47:11 +0700

    feature: export the ficsLink function

--- packages/router/index.ts
+++ packages/router/index.ts
@@ -1,5 +1,6 @@
 import goto from './goto'
+import ficsLink from './link'
 import { dynamicPathParams, queryParams } from './params'
 import ficsRouter from './router'
 
-export { dynamicPathParams, ficsRouter, goto, queryParams }
+export { dynamicPathParams, ficsLink, ficsRouter, goto, queryParams }
