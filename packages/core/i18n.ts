14757f2023738d2d31b1d63c25e1ba6307e0f909
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-07-29 13:30:15 +0700

    feature: integrate the i18n directory to the core directory

--- packages/core/i18n.ts
+++ packages/core/i18n.ts
@@ -1,33 +1,29 @@
 import { toArray } from '../core/helpers'
 import type { SingleOrArray } from '../core/types'
-import createState from '../state/'
-import State from '../state/state'
 
-let _directory: State<string>
+let _directory: string = ''
 
-export const ficsI18n = (directory: string) => {
-  _directory = createState(directory, { readonly: true })
+export const ficsI18n = (directory: string): void => {
+  _directory = directory
 }
 
 export const i18n = async <T>({
-  directory,
   lang,
   key
 }: {
-  directory?: string
   lang: string
   key: SingleOrArray<string>
 }): Promise<T> => {
-  if (!directory) directory = _directory.get()
+  const json: string = `${_directory}/${lang}.json`
 
-  return await fetch(`${directory}/${lang}.json`)
+  return await fetch(json)
     .then(res => res.json())
     .then(json => {
       key = toArray(key)
       let i18n: T | undefined = key.reduce((prev, curr) => prev && prev[curr], json)
 
       if (i18n) return i18n
-      throw new Error(`The ${key.join('.')} does not exist in ${`${directory}/${lang}.json`}...`)
+      throw new Error(`The ${key.join('.')} does not exist in the ${json}...`)
     })
     .catch(error => {
       throw new Error(error)
