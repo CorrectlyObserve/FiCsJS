39ed6f1d227be1c4fd3b27c0bfe67cb4b42c4e63
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-08-01 17:20:30 +0700

    feature: modify the reconnect param in the WS type

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -232,7 +232,7 @@ export interface Syntaxes<D extends object, P extends object> {
 export interface WS<D, P> {
   path: string
   protocols?: SingleOrArray<string>
-  reconnect?: { interval: number; max?: number }
+  reconnect?: { interval: number; max?: number; isExponential?: boolean }
   onopen?: (params: WSParams<D, P> & { event: Event }) => void
   onmessage?: (params: WSParams<D, P> & { event: MessageEvent }) => void
   onerror?: (params: WSParams<D, P> & { event: Event }) => void
