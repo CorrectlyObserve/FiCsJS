ea087759d4034386963b5aec9c7754840bb6bd69
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-08-05 15:44:53 +0700

    feature: define types for the oklch function

---
+++ packages/style/types.ts
@@ -0,0 +1,35 @@
+export interface Lms {
+  l: number
+  m: number
+  s: number
+}
+
+export interface Oklab {
+  l: number
+  a: number
+  b: number
+}
+
+export interface Oklch {
+  l: number
+  c: number
+  h: number
+}
+
+export interface Rgb {
+  r: number
+  g: number
+  b: number
+}
+
+export interface Vector {
+  R: number
+  G: number
+  B: number
+}
+
+export interface Wave {
+  L: number
+  M: number
+  S: number
+}
