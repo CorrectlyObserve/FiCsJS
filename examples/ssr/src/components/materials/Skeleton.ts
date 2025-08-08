0d73b185dd20b14118ff90bc1d05993183471e92
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-08-08 11:18:55 +0700

    design: use oklch instead of hex

--- examples/ssr/src/components/materials/Skeleton.ts
+++ examples/ssr/src/components/materials/Skeleton.ts
@@ -1,10 +1,10 @@
 import { fics } from 'ficsjs'
-import { color } from 'ficsjs/style'
+import { oklch } from 'ficsjs/style'
 
 export default () =>
   fics({
     name: 'skeleton',
     html: ({ template }) =>
       template`<div class="skeleton size-50 block mx-auto animate-pulse"></div>`,
-    css: { 'div.skeleton': { background: color({ hex: '#fff', rate: 0.05 }) } }
+    css: { 'div.skeleton': { background: oklch('#fff', { opacity: 0.05 }) } }
   })
