d6a9aa8a9eb0752099aa192bb063304686e1a06b
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-08-09 15:36:40 +0700

    refactoring: relocate a file

---
+++ examples/todo/src/components/Router.ts
@@ -0,0 +1,51 @@
+import { ficsRouter, queryParams } from 'ficsjs/router'
+import { calc, cssVar, flexCenter, oklch, remToPx } from 'ficsjs/style'
+import Tasks from '@/components/singletons/Tasks'
+import Task from '@/components/singletons/Task'
+import NotFound from '@/components/NotFound'
+import { breakpoints } from '@/utils'
+
+const xs = calc([cssVar('xs'), -1], '*')
+
+export default ficsRouter({
+  children: [Tasks, Task, NotFound],
+  props: {
+    descendant: ({ children: { tasks, task, notFound } }) => [tasks, task, notFound],
+    values: ({}) => ({ lang: ({ getData }) => getData('lang') })
+  },
+  pages: [
+    {
+      path: '/',
+      content: ({ children: { tasks, task }, template }) => {
+        const queryId = parseInt(queryParams().id)
+
+        if (isNaN(queryId)) return tasks
+
+        return document.documentElement.clientWidth < remToPx(breakpoints.lg)
+          ? task
+          : template`<div class="container">${tasks}${task}</div>`
+      }
+    },
+    { path: '/:id', content: ({ children: { task } }) => task }
+  ],
+  notFound: { content: ({ children: { notFound } }) => notFound },
+  css: {
+    ':host': {
+      position: 'relative',
+      minHeight: cssVar('min-height'),
+      'div.container': {
+        ...flexCenter('x'),
+        position: 'absolute',
+        containerType: 'inline-size',
+        gap: cssVar('xl'),
+        width: '100%',
+        [`@container (width >= ${breakpoints.lg})`]: {
+          '.task': {
+            paddingLeft: cssVar('xl'),
+            boxShadow: `${xs} 0px ${cssVar('xs')} ${xs} ${oklch(cssVar('black'), { darker: 0.3 })}`
+          }
+        }
+      }
+    }
+  }
+})
