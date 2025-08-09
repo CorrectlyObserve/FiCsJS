54c276ba978a405b821bc39a1fbbd48dc0e5bb68
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-08-09 15:36:22 +0700

    refactoring: relocate a file

---
+++ examples/todo/src/components/NotFound.ts
@@ -0,0 +1,63 @@
+import { fics } from 'ficsjs'
+import { goto } from 'ficsjs/router'
+import { cssVar } from 'ficsjs/style'
+import Button from '@/components/materials/Button'
+import LoadingIcon from '@/components/LoadingIcon'
+import { breakpoints, getPath } from '@/utils'
+
+interface Data {
+  seconds: number
+  heading: string
+  descriptions: string[]
+  buttonText: string
+}
+
+export default fics<Data, { lang: string }>({
+  name: 'not-found',
+  children: [Button(), LoadingIcon],
+  data: () => ({ seconds: 10, descriptions: [], buttonText: '' }),
+  i18nData: ({ props: { lang }, i18n }) => i18n<Data>({ lang, key: 'notFound' }),
+  props: [
+    {
+      descendant: ({ children: { button } }) => button,
+      values: ({ props: { lang } }) => ({
+        buttonText: ({ getData }) => getData('buttonText'),
+        click: () => goto(getPath(lang, '/'))
+      })
+    },
+    {
+      descendant: ({ children: { loadingIcon } }) => loadingIcon,
+      values: ({ props: { lang } }) => ({ lang })
+    }
+  ],
+  html: ({
+    children: { button, loadingIcon },
+    data: {
+      seconds,
+      heading,
+      descriptions: [start, end]
+    },
+    template,
+    isDeferred
+  }) =>
+    isDeferred
+      ? template`<h2>404 ${heading}</h2><p>${start}${seconds}${end}</p>${button}`
+      : template`${loadingIcon}`,
+  css: {
+    p: {
+      marginBottom: cssVar('xl'),
+      [`@media (max-width: ${breakpoints.sm})`]: { marginBottom: cssVar('lg') }
+    }
+  },
+  hooks: {
+    mounted: ({ data: { seconds }, props: { lang }, setData, poll }) =>
+      poll(
+        ({ times }) => {
+          if (times === seconds - 1) goto(getPath(lang, '/'))
+          setData('seconds', seconds - times - 1)
+        },
+        { interval: 1000, max: seconds }
+      )
+  },
+  options: { lazyLoad: true }
+})
