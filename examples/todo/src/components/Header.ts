1b24b916d71342ac1f84c846f25078f2bc565436
MasamiOgasawara <m-ogasawara@wely.tech>
2025-08-09 15:35:05 +0700

    refactoring: relocate a file

--- examples/todo/src/components/Header.ts
+++ examples/todo/src/components/Header.ts
@@ -1,7 +1,7 @@
 import { fics } from 'ficsjs'
 import { goto } from 'ficsjs/router'
 import { absoluteCenter, cssVar, flexCenter } from 'ficsjs/style'
-import Langs from '@/components/multitons/Langs'
+import Langs from '@/components/Langs'
 import { $lang } from '@/store'
 import { breakpoints, getPath } from '@/utils'
 
