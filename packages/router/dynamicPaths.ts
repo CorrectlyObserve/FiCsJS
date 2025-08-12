03812db0e2fada85715ce9924994810f69590b55
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-08-12 17:03:23 +0700

    refactoring: rename a constant and functions to make them easier to understand

--- packages/router/dynamicPaths.ts
+++ packages/router/dynamicPaths.ts
@@ -1,28 +1,28 @@
 import { browserError } from '../core/helpers'
 
-export const dynamicPath: RegExp = /\/:([^\/?]+)(\?)?/g
-
-export const dynamicPathToRegExp = (path: string): RegExp => {
+export const dynamicPathToRegex = (path: string): RegExp => {
   const common: string = '/([^/]+?)'
-  const pattern: string = path.replace(dynamicPath, (_1, _2, optional) =>
+  const pattern: string = path.replace(dynamicRegex, (_1, _2, optional) =>
     optional ? `(?:${common})?` : common
   )
   return new RegExp(`^${pattern}/?$`)
 }
 
-export const dynamicPaths = (path: string): Record<string, string> => {
+export const dynamicRegex: RegExp = /\/:([^\/?]+)(\?)?/g
+
+export const getDynamicPaths = (path: string): Record<string, string> => {
   browserError()
 
-  const regExps: string[] | null = dynamicPathToRegExp(path).exec(window.location.pathname),
+  const regexes: string[] | null = dynamicPathToRegex(path).exec(window.location.pathname),
     paths: Record<string, string> = {},
     names: string[] = []
   let match: RegExpExecArray | null
 
-  dynamicPath.lastIndex = 0
-  while ((match = dynamicPath.exec(path))) names.push(match[1])
+  dynamicRegex.lastIndex = 0
+  while ((match = dynamicRegex.exec(path))) names.push(match[1])
 
-  if (regExps && regExps.length > 0)
-    for (const [index, value] of regExps.slice(1).entries()) paths[names[index]] = value ?? ''
+  if (regexes && regexes.length > 0)
+    for (const [index, value] of regexes.slice(1).entries()) paths[names[index]] = value ?? ''
   else if (names.length > 0) for (const name of names) paths[name] = ''
 
   return paths
