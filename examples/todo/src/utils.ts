bcd82283433578cdce1bdc350c8c67e4a0529992
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-08-17 16:28:04 +0700

    refactoring: abolish the backToTop function

--- examples/todo/src/utils.ts
+++ examples/todo/src/utils.ts
@@ -1,8 +1,3 @@
-import { goto } from 'ficsjs/router'
-import type { Lang } from '@/types'
-
-export const backToTop = (lang: Lang): void => goto(`/${lang}`)
-
 export const breakpoints = { sm: '30rem', lg: '60rem' } as const
 
 export const convertTimestamp = (timestamp: number): string => {
