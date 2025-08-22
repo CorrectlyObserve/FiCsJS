7dc7a9ce4670ca28a12c3d98721397d39df7869f
MasamiOgasawara <m-ogasawara@wely.tech>
2025-08-22 11:06:33 +0700

    refactoring: apply definition of the chat page

--- examples/ssr/src/components/ChatButton.ts
+++ examples/ssr/src/components/ChatButton.ts
@@ -1,5 +1,6 @@
 import { fics } from 'ficsjs'
 import Icon from '@/components/materials/Icon'
+import { CHAT_PAGE } from '@/utils'
 import { MessageCircleMore } from 'lucide-static'
 
 export default fics({
@@ -10,6 +11,6 @@ export default fics({
     descendant: ({ children: { icon } }) => icon,
     values: () => ({ svg: MessageCircleMore, areaLabel: 'Go to the chat page', isLarge: true })
   },
-  html: ({ children: { icon }, template }) => template`<a href="/chat">${icon}</a>`,
+  html: ({ children: { icon }, template }) => template`<a href="${CHAT_PAGE}">${icon}</a>`,
   css: { ':host': { background: '#282828' } }
 })
