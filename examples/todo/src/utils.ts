6503d8af683abe8f93147199dc06b17ae80b44f5
MasamiOgasawara <m-ogasawara@wely.tech>
2025-08-26 12:00:28 +0700

    design: apply the oklch function

--- examples/todo/src/utils.ts
+++ examples/todo/src/utils.ts
@@ -1,4 +1,4 @@
-import { remToPx } from 'ficsjs/style'
+import { oklch, remToPx } from 'ficsjs/style'
 
 export const breakpoints = { sm: '30rem', lg: '60rem' } as const
 
@@ -20,4 +20,4 @@ export const getTimestamp = (): number => Date.now()
 export const measureOffsetWidth = (): boolean =>
   document.documentElement.offsetWidth >= remToPx(breakpoints.lg)
 
-export const white: string = '#fff'
+export const white: string = oklch('#fff')
