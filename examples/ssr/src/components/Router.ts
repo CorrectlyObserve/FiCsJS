9eae77f59f3b94d76a88287e3dd5709597f04582
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-09-14 17:47:39 +0700

    feature: integrate the Log component to the Router component

--- examples/ssr/src/components/Router.ts
+++ examples/ssr/src/components/Router.ts
@@ -1,14 +1,30 @@
 import { ficsRouter } from 'ficsjs/router'
-import Tab from '@/components/Tab'
 import Chat from '@/components/Chat'
-import Logs from '@/components/Logs'
 import { CHAT_PAGE } from '@/utils'
 
-export default ficsRouter({
+export default ficsRouter<{ logs: string[] }>({
   pathname: CHAT_PAGE,
-  children: [Tab, Chat, Logs],
+  children: [Chat],
+  data: () => ({ logs: [] }),
   pages: [
-    { path: `${CHAT_PAGE}`, content: ({ children: { chat } }) => chat },
-    { path: `${CHAT_PAGE}/logs`, content: ({ children: { logs } }) => logs }
-  ]
+    { path: CHAT_PAGE, content: ({ children: { chat } }) => chat },
+    {
+      path: `${CHAT_PAGE}/logs`,
+      content: ({ data: { logs }, template }) => template`
+        <h2 class="text-lg text-white text-center">Logs</h2>
+        ${logs.map(log => template`<p class="text-white mb-2">${log}</p>`)}
+      `
+    }
+  ],
+  css: { ':host p.mb-2:last-child': { 'margin-bottom': '0' } },
+  options: {
+    sse: {
+      path: '/sse',
+      onopen: ({ setData }) => setData('logs', ['The server is connected.']),
+      actions: {
+        'time-update': ({ data: { logs }, setData, event: { data } }) =>
+          setData('logs', [...logs, data])
+      }
+    }
+  }
 })
