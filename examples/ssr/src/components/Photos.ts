11d2f9befaf7289efbc30a3c41f142595316b5d9
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-09-30 23:40:16 +0700

    design: remove the whitespace-nowrap class

--- examples/ssr/src/components/Photos.ts
+++ examples/ssr/src/components/Photos.ts
@@ -60,7 +60,7 @@ export default fics({
       </div>
       ${isLoading ? skeletons : ''}
       <dialog class="w-3xs rounded-xl" open ${show(photoId !== '')}>
-        ${icon}<p class="text-base text-white text-center mx-4 mb-4 whitespace-nowrap">Created by ${author}</p>
+        ${icon}<p class="text-base text-white text-center mx-4 mb-4">Created by ${author}</p>
       </dialog>
     `
   },
