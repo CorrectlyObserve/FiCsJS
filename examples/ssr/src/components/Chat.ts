f956ace8578266481b79e1516582d93b0e93b7cd
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-09-30 15:52:44 +0700

    feature: define the Chat component

---
+++ examples/ssr/src/components/Chat.ts
@@ -0,0 +1,58 @@
+import { fics } from 'ficsjs'
+import Button from '@/components/materials/Button'
+import { $userName } from '@/store'
+import type { Message } from '@/types'
+
+export default fics<
+  { comment: string },
+  { messages: Message[]; sendMessage: ({ userName, comment }: Message) => void }
+>({
+  name: 'chat',
+  children: [Button()],
+  data: () => ({ comment: '' }),
+  props: {
+    descendant: ({ children: { button } }) => button,
+    values: ({ props: { sendMessage }, setData }) => ({
+      isDisabled: ({ getData }) => getData('comment') === '',
+      buttonText: 'Send',
+      click:
+        ({ getData }) =>
+        () => {
+          const userName = $userName.get()
+          if (userName !== '') {
+            sendMessage({ userName, comment: getData('comment') })
+            setData('comment', '')
+          }
+        }
+    })
+  },
+  html: ({ children: { button }, data: { comment }, props: { messages }, template }) => template`
+    <div>
+      <h2 class="text-lg text-white text-center">Chat</h2>
+      <div>
+        <textarea id="message" class="text-white" placeholder="Please enter your message">${comment}</textarea>
+        ${button}
+      </div>
+      <div>
+        ${messages.map(({ userName, comment }) => {
+          console.log(userName, comment)
+          return template`<p class="text-white mb-2">${userName}: ${comment}</p>`
+        })}
+      </div>
+    </div>
+  `,
+  hooks: {
+    created: () => {
+      if ($userName.get() === '') {
+        const newUserName = prompt('Please enter your name.')
+        newUserName ? $userName.set(newUserName) : (window.location.href = '/')
+      }
+    }
+  },
+  actions: {
+    textarea: {
+      input: ({ setData, event: { currentTarget } }) =>
+        setData('comment', (currentTarget as HTMLTextAreaElement).value)
+    }
+  }
+})
