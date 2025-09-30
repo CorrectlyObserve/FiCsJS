33505aa1c7e49fbda87be669b8eef953d22c6a84
MasamiOgasawara <m-ogasawara@wely.tech>
2025-09-30 13:51:40 +0700

    feature: declare components in the client side

---
+++ examples/ssr/src/websocket-sse.ts
@@ -0,0 +1,10 @@
+import { ficsCss } from 'ficsjs'
+import Link from '@/components/materials/Link'
+import Tab from '@/components/Tab'
+import Router from '@/components/Router'
+import css from '@/.tailwindcss.txt'
+
+ficsCss(css)
+Link().describe()
+Tab.describe()
+Router.describe()
