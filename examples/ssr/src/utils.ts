fadd9c68150134746f03503298991ace43574659
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-10-01 17:13:08 +0700

    feature: define the getDatetime function

--- examples/ssr/src/utils.ts
+++ examples/ssr/src/utils.ts
@@ -1,1 +1,30 @@
 export const CHAT_PAGE = '/websocket-sse' as const
+
+export const getDatetime = (): string => {
+  const FORMAT = '2-digit' as const,
+    options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: FORMAT,
+      day: FORMAT,
+      hour: FORMAT,
+      minute: FORMAT,
+      second: FORMAT,
+      hour12: false,
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    },
+    map = new Map(
+      new Intl.DateTimeFormat(undefined, options)
+        .formatToParts(new Date())
+        .map(({ type, value }) => [type, value])
+    ),
+    createDatetimeStr = (type: 'date' | 'time'): string => {
+      const isDate = type === 'date',
+        keys: readonly (keyof Intl.DateTimeFormatPartTypesRegistry)[] = isDate
+          ? ['year', 'month', 'day']
+          : ['hour', 'minute', 'second']
+
+      return keys.map(key => map.get(key)).join(isDate ? '-' : ':')
+    }
+
+  return `${createDatetimeStr('date')} ${createDatetimeStr('time')}`
+}
