93daa78fb3a3c343a161befbd65f8c01f7e4f140
MasamiOgasawara <m-ogasawara@wely.tech>
2025-10-01 19:48:02 +0700

    refactoring: make the isBrowser function simple

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -34,13 +34,8 @@ export const convertStr = (str: string, type: 'kebab' | 'camel'): string => {
 export const isBlankObject = (param: unknown): boolean =>
   checkType(param, 'object') && Object.keys(param).length === 0
 
-export const isBrowser = (): boolean => {
-  try {
-    return !checkType(window, 'undefined') && !checkType(document, 'undefined')
-  } catch (_) {
-    return false
-  }
-}
+export const isBrowser = (): boolean =>
+  typeof window !== 'undefined' && typeof document !== 'undefined'
 
 export const numberError = (
   number: Record<string, number | undefined>,
