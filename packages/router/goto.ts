1a703af312f9aafb87261cc6eb49c4c8b5d4fcae
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-10-05 18:14:34 +0700

    refactoring: make the goto function robust

--- packages/router/goto.ts
+++ packages/router/goto.ts
@@ -3,6 +3,9 @@ import { browserError } from '../core/helpers'
 export default (href: string, isWithoutHistory?: boolean): void => {
   browserError()
 
+  href = href.trim()
+  if (href === '') return
+
   window.history[isWithoutHistory ? 'replaceState' : 'pushState']({}, '', href)
   window.dispatchEvent(new CustomEvent('fics:navigate', { detail: { href } }))
 }
