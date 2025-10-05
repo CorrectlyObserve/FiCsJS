9fefb2b2a253e3f2564b0f7dd878f49ae4d30b0b
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-10-05 23:51:30 +0700

    refactoring: make the Params class robust

--- packages/router/params.ts
+++ packages/router/params.ts
@@ -7,30 +7,36 @@ class Params {
   #queries: Record<string, string> = {}
 
   constructor() {
-    if (this.#isBrowser) this.#queries = getQueries()
+    if (!this.#isBrowser) return
+
+    this.#saveQueries()
+    window.addEventListener('popstate', this.#saveQueries.bind(this))
+    window.addEventListener('fics:navigate', this.#saveQueries.bind(this))
+  }
+
+  #saveQueries(): void {
+    this.#queries = getQueries()
   }
 
   set(type: ParamType, value: Record<string, string>): void {
     if (!this.#isBrowser)
       throw new Error('Params can only be accessed in the browser environment...')
 
-    if (type === 'dynamicPaths') this.#dynamicPaths = value
-    else this.#queries = value
+    type === 'dynamicPaths' ? (this.#dynamicPaths = { ...value }) : (this.#queries = { ...value })
   }
 
   get(type: ParamType): Record<string, string> {
     if (!this.#isBrowser)
       throw new Error('Params can only be accessed in the browser environment...')
 
-    return type === 'dynamicPaths' ? this.#dynamicPaths : this.#queries
+    return type === 'dynamicPaths' ? { ...this.#dynamicPaths } : { ...this.#queries }
   }
 }
 
 export const getQueries = (): Record<string, string> =>
   isBrowser() ? Object.fromEntries(new URLSearchParams(window.location.search)) : {}
 
-export const dynamicPaths = (): Record<string, string> => params.get('dynamicPaths')
-
 export const params: Params = new Params()
 
+export const dynamicPaths = (): Record<string, string> => params.get('dynamicPaths')
 export const queries = (): Record<string, string> => params.get('queries')
