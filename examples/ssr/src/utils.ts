fcbfaad13848365514b7c03313d471ab9c80d559
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-10-05 14:05:40 +0700

    refactoring: make the getCurrentDatetime function robust

--- examples/ssr/src/utils.ts
+++ examples/ssr/src/utils.ts
@@ -1,30 +1,39 @@
 export const CHAT_PAGE = '/websocket-sse' as const
 
+const datetimeCache: { format?: Intl.DateTimeFormat } = {},
+  FORMAT = '2-digit' as const,
+  dateKeys = ['year', 'month', 'day'] as const,
+  timeKeys = ['hour', 'minute', 'second'] as const,
+  getOptions = (timeZone: string): Intl.DateTimeFormatOptions => ({
+    year: 'numeric',
+    month: FORMAT,
+    day: FORMAT,
+    hour: FORMAT,
+    minute: FORMAT,
+    second: FORMAT,
+    hour12: false,
+    timeZone
+  }),
+  getDatetimeFormat = (): Intl.DateTimeFormat => {
+    if (datetimeCache.format) return datetimeCache.format
+
+    const format: Intl.DateTimeFormat = new Intl.DateTimeFormat(
+      'ja-JP',
+      getOptions(Intl.DateTimeFormat().resolvedOptions().timeZone)
+    )
+
+    datetimeCache.format = format
+    return format
+  }
+
 export const getCurrentDatetime = (): string => {
-  const FORMAT = '2-digit' as const,
-    options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: FORMAT,
-      day: FORMAT,
-      hour: FORMAT,
-      minute: FORMAT,
-      second: FORMAT,
-      hour12: false,
-      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
-    },
-    map = new Map(
-      new Intl.DateTimeFormat(undefined, options)
+  const map = new Map(
+      getDatetimeFormat()
         .formatToParts(new Date())
         .map(({ type, value }) => [type, value])
     ),
-    createDatetimeStr = (type: 'date' | 'time'): string => {
-      const isDate = type === 'date',
-        keys: readonly (keyof Intl.DateTimeFormatPartTypesRegistry)[] = isDate
-          ? ['year', 'month', 'day']
-          : ['hour', 'minute', 'second']
+    date = dateKeys.map(key => map.get(key)).join('-'),
+    time = timeKeys.map(key => map.get(key)).join(':')
 
-      return keys.map(key => map.get(key)).join(isDate ? '-' : ':')
-    }
-
-  return `${createDatetimeStr('date')} ${createDatetimeStr('time')}`
+  return `${date} ${time}`
 }
