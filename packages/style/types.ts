ad1d93b659ece785de10271bb4c1c4fd1dc1eeec
MasamiOgasawara <m-ogasawara@wely.tech>
2025-10-05 23:31:28 +0700

    refactoring: define types

--- packages/style/types.ts
+++ packages/style/types.ts
@@ -1,3 +1,17 @@
+export type Axis = 'x' | 'y' | 'xy'
+
+export interface Center {
+  position: Position
+  transform: 'translate(-50%, -50%)' | 'translateX(-50%)' | 'translateY(-50%)'
+}
+
+export type Direction = 'row' | 'column'
+
+export interface Flex {
+  display: 'flex'
+  flexDirection: Direction
+}
+
 export interface Lms {
   l: number
   m: number
@@ -15,6 +29,10 @@ export interface Oklch {
   c: number
   h: number
 }
+
+export type Operator = '+' | '-' | '*' | '/'
+
+export type Position = 'absolute' | 'fixed'
 
 export interface Rgb {
   r: number
