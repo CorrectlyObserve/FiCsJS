911b192375d1e6ad5cfe88c4c4112d275bfc7f57
MasamiOgasawara <m-ogasawara@wely.tech>
2025-10-09 09:09:33 +0700

    refactoring: move the directory

--- examples/ssr/src/components/ChatButton.ts
+++ examples/ssr/src/components/ChatButton.ts
@@ -1,17 +1,18 @@
 import { fics } from 'ficsjs'
 import { oklch } from 'ficsjs/style'
-import Icon from '@/components/materials/Icon'
+import Icon from '@/components/Icon'
 import { CHAT_PAGE } from '@/utils'
 import { MessageCircleMore } from 'lucide-static'
 
-export default fics({
-  name: 'chat-button',
-  className: 'fixed bottom-8 right-4',
-  children: [Icon()],
-  props: {
-    descendant: ({ children: { icon } }) => icon,
-    values: () => ({ svg: MessageCircleMore, areaLabel: 'Go to the chat page', isLarge: true })
-  },
-  html: ({ children: { icon }, template }) => template`<a href="${CHAT_PAGE}">${icon}</a>`,
-  css: { ':host': { background: oklch('#282828') } }
-})
+export default () =>
+  fics({
+    name: 'chat-button',
+    className: 'fixed bottom-8 right-4',
+    children: [Icon()],
+    props: {
+      descendant: ({ children: { icon } }) => icon,
+      values: () => ({ svg: MessageCircleMore, areaLabel: 'Go to the chat page', isLarge: true })
+    },
+    html: ({ children: { icon }, template }) => template`<a href="${CHAT_PAGE}">${icon}</a>`,
+    css: { ':host': { background: oklch('#282828') } }
+  })
