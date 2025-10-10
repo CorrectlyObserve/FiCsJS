a6efb59e5a56bcb99c3d9e67bb04c0a6b4d14c5c
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-10-10 15:20:09 +0700

    refactoring: move the directory

---
+++ examples/ssr/src/components/websocket-sse/Tab.ts
@@ -0,0 +1,35 @@
+import { fics } from 'ficsjs'
+import { goto } from 'ficsjs/router'
+import { flexCenter } from 'ficsjs/style'
+import Button from '@/components/Button'
+import { CHAT_PAGE } from '@/utils'
+
+export default fics({
+  name: 'tab',
+  children: [Button()],
+  data: () => ({
+    tabs: [
+      { href: CHAT_PAGE, text: 'Chat' },
+      { href: `${CHAT_PAGE}/logs`, text: 'Logs' }
+    ],
+    current: ''
+  }),
+  html: ({ children: { button }, data: { tabs, current }, template, setData }) => template`
+    <div class="container mb-7 mx-auto gap-4">
+      ${tabs.map(
+        ({ href, text }) => template`
+          ${button.setIndividualProps(text, {
+            isDisabled: current === '' || current === href,
+            buttonText: text,
+            click: () => {
+              goto(href)
+              setData('current', href)
+            }
+          })}
+        `
+      )}
+    </div>
+  `,
+  css: { ':host div.container': { ...flexCenter('x') } },
+  hooks: { mounted: ({ setData }) => setData('current', window.location.pathname) }
+})
