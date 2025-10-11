0efc03870fabed8205999a96d4557010940b59be
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-10-11 14:02:13 +0700

    refactoring: apply moving directories

--- examples/ssr/src/index.ts
+++ examples/ssr/src/index.ts
@@ -1,10 +1,10 @@
 import { ficsCss } from 'ficsjs'
-import Link from '@/components/materials/Link'
-import Users from '@/components/Users'
+import Link from '@/components/Link'
+import Users from '@/components/index/Users'
 import ChatButton from '@/components/ChatButton'
 import css from '@/.tailwindcss.txt'
 
 ficsCss(css)
 Link().describe()
 Users.describe()
-ChatButton.describe()
+ChatButton().describe()
