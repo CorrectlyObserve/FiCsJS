e4bdb3c4f7af6e50792ddda39f64db928c7fd307
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-10-11 14:02:24 +0700

    refactoring: apply moving directories

--- examples/ssr/src/scroll.ts
+++ examples/ssr/src/scroll.ts
@@ -1,10 +1,10 @@
 import { ficsCss } from 'ficsjs'
-import Link from '@/components/materials/Link'
-import Photos from '@/components/Photos'
+import Link from '@/components/Link'
+import Photos from '@/components/scroll/Photos'
 import ChatButton from '@/components/ChatButton'
 import css from '@/.tailwindcss.txt'
 
 ficsCss(css)
 Link().describe()
 Photos.describe()
-ChatButton.describe()
+ChatButton().describe()
