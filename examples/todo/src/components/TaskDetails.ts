bd3621d1df7f82b8e4183515d7347e26701c3bf5
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-11-04 13:16:09 +0700

    refactoring: rename the component

--- examples/todo/src/components/TaskDetails.ts
+++ examples/todo/src/components/TaskDetails.ts
@@ -39,7 +39,7 @@ interface Props {
 const { sm } = breakpoints
 
 export default fics<Data, Props>({
-  name: 'task-detail',
+  name: 'task-details',
   children: [LoadingIcon, Icon(), Input(), Textarea(), Button()],
   data: () => ({
     title: '',
