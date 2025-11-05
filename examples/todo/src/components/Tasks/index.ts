8bd8c61964e9928aceda8dd5031bada15f429cd1
MasamiOgasawara <m-ogasawara@wely.tech>
2025-11-05 22:59:56 +0700

    refactoring: move the directory

--- examples/todo/src/components/Tasks/index.ts
+++ examples/todo/src/components/Tasks/index.ts
@@ -1,12 +1,13 @@
 import { fics } from 'ficsjs'
-import { ficsLink, goto } from 'ficsjs/router'
+import { goto } from 'ficsjs/router'
 import { calc, cssVar, flexCenter } from 'ficsjs/style'
 import LoadingIcon from '@/components/materials/LoadingIcon'
 import Icon from '@/components/materials/Icon'
 import Input from '@/components/materials/Input'
+import Link from '@/components/Tasks/Link'
 import { addTask, completeTask, deleteTask, revertTask } from '@/stores'
 import type { Lang, Task } from '@/types'
-import { breakpoints, measureOffsetWidth } from '@/utils/others'
+import { breakpoints } from '@/utils/others'
 import { Circle, CircleCheckBig, Plus, Square, SquareCheck, Trash2 } from 'lucide-static'
 
 interface Data {
@@ -28,29 +29,7 @@ interface Props {
   setTasks: (tasks: Task[]) => void
 }
 
-const { sm } = breakpoints,
-  link = ({ id, title, completedAt }: Partial<Task>) =>
-    ficsLink({
-      href: `/${measureOffsetWidth() ? '?taskId=' : ''}${id}`,
-      content: ({ template }) =>
-        template`<span class="${completedAt ? 'done' : ''}">${title}</span>`,
-      css: {
-        ':host': {
-          width: calc(`100% - ${cssVar('xl')} * 1.5`),
-          a: {
-            display: 'flex',
-            paddingBlock: cssVar('md'),
-            lineHeight: 1,
-            span: {
-              lineHeight: 'inherit',
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis'
-            }
-          }
-        }
-      }
-    })
+const { sm } = breakpoints
 
 export default fics<Data, Props>({
   name: 'tasks',
@@ -136,7 +115,7 @@ export default fics<Data, Props>({
                       click: async () =>
                         setTasks(await (completedAt ? revertTask(id) : completeTask(id)))
                     })}
-                    ${link({ id, title, completedAt })}
+                    ${Link({ id, title, completedAt })}
                   </div>
                   ${icon.setIndividualProps(`${id}-delete`, {
                     svg: Trash2,
