9992966151c996fb2b5160f8eb044bd15668ed1c
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-11-05 22:49:44 +0700

    refactoring: apply change of the directory path

--- examples/todo/src/components/Tasks.ts
+++ examples/todo/src/components/Tasks.ts
@@ -1,7 +1,7 @@
 import { fics } from 'ficsjs'
 import { ficsLink, goto } from 'ficsjs/router'
 import { calc, cssVar, flexCenter } from 'ficsjs/style'
-import LoadingIcon from '@/components/LoadingIcon'
+import LoadingIcon from '@/components/materials/LoadingIcon'
 import Icon from '@/components/materials/Icon'
 import Input from '@/components/materials/Input'
 import { addTask, completeTask, deleteTask, revertTask } from '@/stores'
