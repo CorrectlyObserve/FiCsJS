76f7ebee4b5c241114fa977c2d0956ea40aa1775
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-11-11 22:53:04 +0700

    refactoring: update a reference to the path

--- examples/ssr/src/pages/scroll/index.ts
+++ examples/ssr/src/pages/scroll/index.ts
@@ -1,6 +1,6 @@
 import { ficsCss } from 'ficsjs'
 import Link from '@/components/Link'
-import Photos from '@/components/scroll/Photos'
+import Photos from '@/pages/scroll/_components/Photos'
 import ChatButton from '@/components/ChatButton'
 import css from '@/.tailwindcss.txt'
 
