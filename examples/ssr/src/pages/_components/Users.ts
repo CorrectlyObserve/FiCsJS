c914ae9de5e4baf79ac69cb8a38f46fc35a04583
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-12-07 00:58:27 +0700

    feature: implement drag-and-drop (draft)

--- examples/ssr/src/pages/_components/Users.ts
+++ examples/ssr/src/pages/_components/Users.ts
@@ -10,14 +10,22 @@ const headers: HeadersInit = { 'Content-type': 'application/json; charset=UTF-8'
 export default fics({
   name: 'users',
   children: [Button()],
-  data: () => ({ users, userId: NaN, methods: ['PUT', 'PATCH', 'DELETE'] as Method[] }),
+  data: () => ({
+    methods: ['PUT', 'PATCH', 'DELETE'] as Method[],
+    users,
+    userId: NaN,
+    draggingId: NaN,
+    highlightedZone: null as HTMLElement | null,
+    isHighlighted: (highlightedZone: HTMLElement | null, zoneId: number) =>
+      highlightedZone?.getAttribute('key') === zoneId.toString()
+  }),
   props: {
     descendant: ({ children: { button } }) => button,
     values: ({}) => ({ isDisabled: ({ getData }) => isNaN(getData('userId')) })
   },
   html: ({
     children: { button },
-    data: { users, userId, methods },
+    data: { methods, users, userId, draggingId, highlightedZone, isHighlighted },
     setData,
     crud,
     template,
@@ -55,36 +63,54 @@ export default fics({
         })
       )}
     </div>
-    <div class="w-fit mx-auto space-y-4">
-      ${users.map(user => {
-        const { id } = user,
-          keys = ['id', 'name', 'email'] as const
+    <div class="w-fit mx-auto">
+      <div class="drop-zone h-4 ${isHighlighted(highlightedZone, -1) ? 'dragged-over' : ''}" key="-1"></div>
+      ${users.map((user, index) => {
+        const keys = ['id', 'name', 'email'] as const
 
         return template`
-          <div key="${id}">
+          <div
+            class="${draggingId === user.id ? 'dragging' : ''}"
+            key="${index}"
+            draggable="true"
+          >
             <div
               class="text-white p-3 cursor-grab" tabindex="0"
-              aria-label="Move user id ${id}"
-              key="${id}-grid"
+              aria-label="Move user id: ${user.id}"
+              key="${index}-grid"
             >
               ${html(GripVertical)}
             </div>
-            <div class="clickable space-y-2" key="${id}-info" tabindex="0">
-              ${keys.map((key, index) => {
-                const _key = keys[index]
+            <div class="clickable space-y-2" key="${user.id}" tabindex="0">
+              ${keys.map((key, _index) => {
+                const _key = keys[_index]
                 return template`
-                  <p class="text-base ${userId === id ? 'text-red' : 'text-white'}" key="${id}-${key}">
+                  <p class="text-base ${user.id === userId ? 'text-red' : 'text-white'}" key="${index}-${key}">
                     ${_key.charAt(0).toUpperCase() + _key.slice(1)}: ${user[key]}
                   </p>
                 `
               })}
             </div>
           </div>
+          <div class="drop-zone h-4 ${isHighlighted(highlightedZone, index) ? 'dragged-over' : ''}" key="${index}"></div>
         `
       })}
     </div>
   `,
-  css: { div: { '&.buttons': flexCenter('x'), '&.w-fit > div': flexCenter('y') } },
+  css: {
+    div: {
+      '&.buttons': flexCenter('x'),
+      '&.dragged-over': {
+        height: '3rem',
+        backgroundColor: 'rgba(70, 130, 230, 0.15)',
+        border: '1px dashed royalblue',
+        margin: '5px 0',
+        borderRadius: '4px'
+      },
+      '&.w-fit > div': flexCenter('y'),
+      '&.dragging': { opacity: 0.4, pointerEvents: 'none' }
+    }
+  },
   hooks: {
     mounted: async ({ setData, getData, crud }) => {
       const users = getData('users')
@@ -99,14 +125,112 @@ export default fics({
     }
   },
   actions: {
-    'div.w-fit > div > div:last-child': {
+    'div.drop-zone': {
+      dragover: ({ event }) => {
+        const drag = event as DragEvent
+        drag.preventDefault()
+
+        if (drag.dataTransfer) drag.dataTransfer.dropEffect = drag.altKey ? 'copy' : 'move'
+      },
+      dragleave: ({ setData, getData, attributes: { key } }) => {
+        const highlightedZone = getData('highlightedZone'),
+          zoneId = highlightedZone?.getAttribute('key')
+
+        if (zoneId && zoneId === key) setData('highlightedZone', null)
+      },
+      drop: ({ setData, getData, event, attributes: { key } }) => {
+        const drag = event as DragEvent
+        drag.preventDefault()
+
+        if (drag.dataTransfer) {
+          const isHighlighted = getData('isHighlighted'),
+            highlightedZone = getData('highlightedZone')
+
+          if (!isHighlighted(highlightedZone, parseInt(key))) return
+
+          setData('highlightedZone', null)
+
+          const draggingId = parseInt(drag.dataTransfer.getData('text/plain')),
+            droppedId = parseInt(key),
+            droppedIndex = draggingId < droppedId ? droppedId - 1 : droppedId,
+            users = getData('users'),
+            user = users.find(({ id }) => id === draggingId)
+
+          if (user)
+            if (drag.altKey) setData('users', users.splice(droppedIndex, 0, user))
+            else if (draggingId !== droppedIndex) {
+              const newUsers = users.filter(user => user.id !== draggingId)
+              newUsers.splice(droppedIndex, 0, user)
+              setData('users', newUsers)
+            }
+        }
+      }
+    },
+    'div[draggable="true"] > div:last-child': {
       click: [
         ({ setData, getData, attributes: { key } }) => {
-          const userId = parseInt(key.replace(/-info/, ''))
+          const userId = parseInt(key)
           setData('userId', getData('userId') === userId ? NaN : userId)
         },
         { throttle: 500, blur: true }
-      ]
+      ],
+      dragstart: ({ setData, event, attributes: { key } }) => {
+        const drag = event as DragEvent
+        drag.preventDefault()
+
+        if (drag.dataTransfer) {
+          drag.dataTransfer.setData('text/plain', key)
+          drag.dataTransfer.effectAllowed = 'copyMove'
+          setData('draggingId', parseInt(key))
+        }
+      },
+      dragover: [
+        ({ setData, getData, event, attributes: { key } }) => {
+          const drag = event as DragEvent
+          drag.preventDefault()
+
+          if (drag.dataTransfer) drag.dataTransfer.dropEffect = drag.altKey ? 'copy' : 'move'
+
+          const { target } = event
+
+          if (target) {
+            const draggingId = getData('draggingId'),
+              { top, height } = (target as HTMLElement).getBoundingClientRect(),
+              isAfter = drag.clientY > top + height / 2,
+              index = parseInt(key),
+              highlightedZone = getData('highlightedZone')
+
+            if (
+              !drag.altKey &&
+              ((index === draggingId - 1 && isAfter) ||
+                (index === draggingId + 1 && !isAfter) ||
+                index === draggingId)
+            ) {
+              if (highlightedZone) setData('highlightedZone', null)
+              return
+            }
+
+            const targetElement = target as HTMLElement,
+              getSiblingDropZone = (targetElement: HTMLElement) => {
+                if (targetElement.getAttribute('draggable') === 'true') {
+                  const sibling = targetElement[`${isAfter ? 'next' : 'previous'}ElementSibling`]
+
+                  if (sibling && sibling.classList.contains('drop-zone')) return sibling
+                } else if (targetElement.parentElement)
+                  return getSiblingDropZone(targetElement.parentElement)
+              },
+              targetZone = getSiblingDropZone(targetElement)
+
+            if (targetZone && highlightedZone !== targetZone)
+              setData('highlightedZone', targetZone as HTMLElement)
+          }
+        },
+        { throttle: 500 }
+      ],
+      dragend: ({ setData, getData }) => {
+        setData('draggingId', NaN)
+        if (getData('highlightedZone')) setData('highlightedZone', null)
+      }
     }
   }
 })
