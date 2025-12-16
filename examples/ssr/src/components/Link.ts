4b479998324b633eaf9ad7d75d538cd1cfa018b9
MasamiOgasawara <m-ogasawara@wely.tech>
2025-12-16 16:49:21 +0700

    refactoring: make the html easier to read

--- examples/ssr/src/components/Link.ts
+++ examples/ssr/src/components/Link.ts
@@ -6,7 +6,9 @@ export default () =>
     data: () => ({ href: '', text: '' }),
     html: ({ data: { href, text }, template }) => template`
       <p class="text-base text-white text-center leading-none">
-        <a class="clickable inline-block p-4 mb-3" href="${href}"><span class="border-b">${text}</span></a>
+        <a class="clickable inline-block p-4 mb-3" href="${href}">
+          <span class="border-b">${text}</span>
+        </a>
       </p>
     `
   })
