ad0e19443816b7358f036ebb9538b1b500baadeb
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-12-22 01:55:01 +0700

    refactoring: modify the routerSymbol

--- packages/core/helpers.ts
+++ packages/core/helpers.ts
@@ -152,7 +152,7 @@ export const numberError = (
   }
 }
 
-export const routerSymbol: unique symbol = Symbol('router')
+export const routerSymbol: unique symbol = Symbol.for('__fics_router__')
 
 export const toArray = <T>(param: SingleOrArray<T>): T[] => {
   if (Array.isArray(param)) return [...param]
