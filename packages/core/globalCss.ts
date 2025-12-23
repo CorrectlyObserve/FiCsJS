149d0ce1e1d238cc6b23eb757894d0e94ba959d9
MasamiOgasawara <m-ogasawara@wely.tech>
2025-12-23 22:56:10 +0700

    refactoring: modify and rename the configGlobalCss function

--- packages/core/globalCss.ts
+++ packages/core/globalCss.ts
@@ -1,10 +1,7 @@
+import FiCsElement from './class'
 import { toArray } from './helpers'
 import type { GlobalCss, SingleOrArray } from './types'
 
-let _globalCss: GlobalCss[] = new Array()
-
-export const ficsCss = (css: SingleOrArray<GlobalCss>): void => {
-  _globalCss = toArray(css)
+export default (css: SingleOrArray<GlobalCss>): void => {
+  FiCsElement.globalCss = toArray(css)
 }
-
-export const globalCss = (): GlobalCss[] => _globalCss
