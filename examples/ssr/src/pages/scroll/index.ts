95b6935d106988b197fa6a6515bb0f05fedca304
MasamiOgasawara <m-ogasawara@wely.tech>
2025-12-23 23:40:05 +0700

    refactoring: apply the configGlobalCss function

--- examples/ssr/src/pages/scroll/index.ts
+++ examples/ssr/src/pages/scroll/index.ts
@@ -1,10 +1,10 @@
-import { ficsCss } from 'ficsjs'
+import { configGlobalCss } from 'ficsjs'
 import Link from '@/components/Link'
 import Photos from '@/pages/scroll/_components/Photos'
 import ChatButton from '@/components/ChatButton'
 import css from '@/.tailwindcss.txt'
 
-ficsCss(css)
+configGlobalCss(css)
 Link().describe()
 Photos.describe()
 ChatButton().describe()
