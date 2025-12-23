c12250d0abf82abf4744fb5b7f5dc0c8d0990ea1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-12-23 23:39:45 +0700

    refactoring: apply the configGlobalCss function

--- examples/ssr/src/pages/websocket-sse/index.ts
+++ examples/ssr/src/pages/websocket-sse/index.ts
@@ -1,10 +1,10 @@
-import { ficsCss } from 'ficsjs'
+import { configGlobalCss } from 'ficsjs'
 import Link from '@/components/Link'
 import Tab from '@/pages/websocket-sse/_components/Tab'
 import Router from '@/pages/websocket-sse/_components/Router'
 import css from '@/.tailwindcss.txt'
 
-ficsCss(css)
+configGlobalCss(css)
 Link().describe()
 Tab.describe()
 Router.describe()
