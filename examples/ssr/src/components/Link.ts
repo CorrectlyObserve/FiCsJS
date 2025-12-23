9ee7a8aae6354a596ac8ed091ffca5fc092a8c8e
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-12-23 16:19:33 +0700

    design: improve a11y

--- examples/ssr/src/components/Link.ts
+++ examples/ssr/src/components/Link.ts
@@ -1,4 +1,5 @@
 import { fics } from 'ficsjs'
+import { white } from '@/utils'
 
 export default () =>
   fics({
@@ -6,9 +7,10 @@ export default () =>
     data: () => ({ href: '', text: '' }),
     html: ({ data: { href, text }, template }) => template`
       <p class="text-base text-white text-center leading-none">
-        <a class="clickable inline-block p-4 mb-3" href="${href}">
+        <a class="clickable inline-block p-4 mb-3 rounded-lg" href="${href}">
           <span class="border-b">${text}</span>
         </a>
       </p>
-    `
+    `,
+    css: { 'a:hover': { background: white(0.1) } }
   })
