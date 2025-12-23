c9a43aff4d7d71aed67696dd644b3c6d3533cc8d
MasamiOgasawara <m-ogasawara@wely.tech>
2025-12-23 23:32:10 +0700

    refactoring: modify exported function names

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,5 +1,5 @@
 import fics from './fics'
-import { ficsCss } from './globalCss'
-import { ficsI18n, i18n } from './i18n'
+import configGlobalCss from './globalCss'
+import { configI18n, i18n } from './i18n'
 
-export { fics, ficsCss, ficsI18n, i18n }
+export { configGlobalCss, configI18n, fics, i18n }
