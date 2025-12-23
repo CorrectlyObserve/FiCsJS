8549ae858454680f521a27133204ee41f973d031
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-12-23 09:23:36 +0700

    refactoring: delete the unnecessary constant

--- packages/core/constants.ts
+++ packages/core/constants.ts
@@ -1,6 +1,1 @@
-export default {
-  FICS_ID_ATTR: 'fics-id',
-  VAR_TAG_NAME: 'f-var',
-  HOST_SELECTOR: ':host',
-  ROUTER_SYMBOL: Symbol.for('__fics_router__')
-} as const
+export default { FICS_ID_ATTR: 'fics-id', VAR_TAG_NAME: 'f-var', HOST_SELECTOR: ':host' } as const
