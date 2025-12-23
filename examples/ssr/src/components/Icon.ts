8e7e45e573462a6c02cb72ee35a7b8c767ed79fa
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-12-23 11:50:54 +0700

    design: improve a11y

--- examples/ssr/src/components/Icon.ts
+++ examples/ssr/src/components/Icon.ts
@@ -1,16 +1,21 @@
 import { fics } from 'ficsjs'
+import { white } from '@/utils'
 
 export default () =>
   fics<{}, { svg: string; areaLabel: string; isLarge?: string; click?: () => void }>({
     name: 'icon',
     className: 'icon',
     html: ({ props: { svg, areaLabel, isLarge }, template, html }) => template`
-      <button class="clickable flex text-white ${isLarge ? 'p-4' : 'p-3'}" aria-label="${areaLabel}">
+      <button
+        class="clickable flex text-white ${isLarge ? 'p-4' : 'p-3'} rounded-lg"
+        aria-label="${areaLabel}"
+      >
         ${html(svg)}
       </button>
     `,
     css: {
       button: ({ props: { isLarge } }) => ({
+        '&:hover': { background: white(0.1) },
         svg: { width: `${isLarge ? 2.5 : 1.25}rem`, height: 'auto', stroke: 'currentColor' }
       })
     },
