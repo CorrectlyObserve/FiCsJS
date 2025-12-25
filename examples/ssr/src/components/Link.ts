4f704b71113b4ca26b98be3c2a2cf6698b153de5
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2025-12-26 02:12:11 +0700

    refactoring: improve a11y

--- examples/ssr/src/components/Link.ts
+++ examples/ssr/src/components/Link.ts
@@ -5,12 +5,18 @@ export default () =>
   fics({
     name: 'back-link',
     data: () => ({ href: '', text: '' }),
-    html: ({ data: { href, text }, template }) => template`
-      <p class="text-base text-white text-center leading-none">
-        <a class="clickable inline-block p-4 mb-3 rounded-lg" href="${href}">
-          <span class="border-b">${text}</span>
-        </a>
-      </p>
-    `,
+    html: ({ data: { href, text }, template }) => {
+      href = href.trim()
+
+      if (href === '') throw new Error('The "href" must be a non-empty string...')
+
+      return template`
+        <p class="text-base text-white text-center leading-none">
+          <a class="clickable inline-block p-4 mb-3 rounded-lg" href="${href}">
+            <span class="border-b">${text}</span>
+          </a>
+        </p>
+      `
+    },
     css: { 'a:hover': { background: white(0.1) } }
   })
