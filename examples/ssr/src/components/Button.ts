907a3b91297d04738e4f626e3e5a17c0e9333a9d
MasamiOgasawara <m-ogasawara@wely.tech>
2025-12-27 15:53:19 +0700

    refactoring: improve a11y

--- examples/ssr/src/components/Button.ts
+++ examples/ssr/src/components/Button.ts
@@ -2,17 +2,23 @@ import { fics } from 'ficsjs'
 import { white } from '@/utils'
 
 export default () =>
-  fics<{}, { isDisabled: boolean; buttonText: string; click: () => void }>({
+  fics<{}, { isDisabled: boolean; isCurrent?: boolean; buttonText: string; click: () => void }>({
     name: 'button',
-    html: ({ props: { isDisabled, buttonText }, template, isBrowser }) => template`
+    html: ({ props: { isDisabled, isCurrent, buttonText }, template, isBrowser }) => template`
       <button
         class="clickable w-24 text-white border border-white p-3 rounded-lg"
         ${!isBrowser || isDisabled ? 'disabled' : ''}
         aria-disabled="${isDisabled ? 'true' : 'false'}"
+        ${isCurrent ? 'aria-current="page"' : ''}
         type="button"
       >${buttonText}</button>
     `,
-    css: { 'button:hover:not([disabled])': { background: white(0.1) } },
+    css: {
+      button: {
+        '&:not([disabled]):hover': { background: white(0.1) },
+        '&[aria-current="page"]': { color: white(0.5), borderColor: white(0.5) }
+      }
+    },
     actions: {
       button: { click: [({ props: { click } }) => click(), { throttle: 500, blur: true }] }
     }
