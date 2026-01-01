deb72f9a09f1a951303f6745e605ca93018635e6
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-01-01 15:53:38 +0700

    refactoring: make the Loading component easier to read

--- examples/todo/src/components/materials/Loading.ts
+++ examples/todo/src/components/materials/Loading.ts
@@ -1,22 +1,30 @@
 import { fics } from 'ficsjs'
 import { spin } from 'ficsjs/animation'
 import { cssVar, forScreenReaders } from 'ficsjs/style'
+import type { Lang } from '@/types'
 import { white } from '@/utils/others'
 import { Loader } from 'lucide-static'
 
-export default () => fics<{ texts: { en: string; ja: string } }, { lang: 'en' | 'ja' }>({
-  name: 'loading',
-  data: () => ({ texts: { en: 'Loading...', ja: '読み込み中' } }),
-  html: ({ data: { texts }, props: { lang }, template, html }) => template`
+export default () =>
+  fics<{ texts: Record<Lang, string> }, { lang: Lang }>({
+    name: 'loading',
+    data: () => ({ texts: { en: 'Loading...', ja: '読み込み中' } }),
+    html: ({ data: { texts }, props: { lang }, template, html }) => template`
     <p role="status" aria-live="polite" aria-atomic="true">${texts[lang]}</p>
     <div aria-hidden="true">${html(Loader)}</div>
   `,
-  css: {
-    p: forScreenReaders,
-    div: {
-      padding: cssVar('xs'),
-      marginInline: 'auto',
-      svg: { ...spin(1.5), display: 'flex', width: cssVar('2xl'), height: 'auto', stroke: white() }
+    css: {
+      p: forScreenReaders,
+      div: {
+        padding: cssVar('xs'),
+        marginInline: 'auto',
+        svg: {
+          ...spin(1.5),
+          display: 'flex',
+          width: cssVar('2xl'),
+          height: 'auto',
+          stroke: white()
+        }
+      }
     }
-  }
-})
+  })
