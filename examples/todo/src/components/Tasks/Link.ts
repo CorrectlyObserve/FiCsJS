5b5450994dcf5b4f6958579d15cb58c67342782b
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-01-04 18:05:06 +0700

    design: add new css

--- examples/todo/src/components/Tasks/Link.ts
+++ examples/todo/src/components/Tasks/Link.ts
@@ -1,6 +1,6 @@
 import { ficsLink } from 'ficsjs/router'
 import { calc, cssVar } from 'ficsjs/style'
-import { measureOffsetWidth } from '@/utils/others'
+import { measureOffsetWidth, white } from '@/utils/others'
 
 export default ficsLink<{ id: number; title: string; completedAt?: number; status: string }>({
   attributes: ({ props: { title, status } }) => ({ 'aria-label': `${title} ${status}` }),
@@ -12,8 +12,8 @@ export default ficsLink<{ id: number; title: string; completedAt?: number; statu
       width: calc(`100% - ${cssVar('xl')} * 1.5`),
       a: {
         display: 'flex',
-        paddingBlock: cssVar('md'),
-        paddingLeft: calc(`${cssVar('xs')} / 2`),
+        color: white(),
+        padding: `${cssVar('md')} ${calc(`${cssVar('xs')} / 2`)}`,
         lineHeight: 1,
         span: {
           width: '100%',
