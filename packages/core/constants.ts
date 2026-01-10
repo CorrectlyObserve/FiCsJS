7c36c42ceba09b249f2acd57c253de6b452ce141
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-01-10 16:29:42 +0700

    feature: modify constants

--- packages/core/constants.ts
+++ packages/core/constants.ts
@@ -1,1 +1,8 @@
-export default { FICS_ID_ATTR: 'fics-id', VAR_TAG_NAME: 'f-var', HOST_SELECTOR: ':host' } as const
+export default {
+  a11y: {
+    STATUS_LIVE_REGION: 'role="status" aria-live="polite" aria-atomic="true"'
+  },
+  FICS_ID_ATTR: 'fics-id',
+  VAR_TAG_NAME: 'f-var',
+  HOST_SELECTOR: ':host'
+} as const
