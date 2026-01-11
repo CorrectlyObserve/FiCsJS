a9bbd4794a7133bdefef35608e92a9f58420addf
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-01-11 19:03:07 +0700

    design: integrate css for each button type

--- examples/todo/src/components/materials/Button.ts
+++ examples/todo/src/components/materials/Button.ts
@@ -1,35 +1,61 @@
 import { fics } from 'ficsjs'
 import { cssVar } from 'ficsjs/style'
+import { white } from '@/utils/others'
+
+interface Props {
+  isDisabled?: boolean
+  isPressed?: boolean
+  type: 'normal' | 'gradation' | 'label' | 'delete'
+  buttonText: string
+  click: () => void
+}
 
 export default () =>
-  fics<{}, { isDisabled?: boolean; buttonText: string; click: () => void }>({
+  fics<{}, Props>({
     name: 'button',
-    html: ({ props: { isDisabled, buttonText }, template, attributes: { boolean } }) => template`
+    html: ({
+      props: { isDisabled, isPressed, type, buttonText },
+      template,
+      attributes: { boolean }
+    }) => template`
       <button
         ${isDisabled ? 'disabled' : ''}
         aria-disabled="${boolean(isDisabled)}"
+        ${isPressed === undefined ? '' : `aria-pressed="${boolean(isPressed)}"`}
         type="button"
+        data-type="${type}"
       >${buttonText}</button>
     `,
     css: {
       ':host': {
         textAlign: 'center',
         button: {
-          position: 'relative',
-          background: cssVar('gradation'),
-          paddingInline: cssVar('md'),
-          overflow: 'hidden',
-          zIndex: 0,
-          '&::before': {
-            position: 'absolute',
-            content: "''",
-            inset: 0,
-            background: cssVar('black'),
-            opacity: 0,
-            transition: cssVar('transition'),
-            zIndex: -1
+          padding: cssVar('md'),
+          '&[disabled]': {
+            background: 'none !important',
+            color: white(0.2),
+            cursor: 'not-allowed'
           },
-          '&:not([disabled]):hover::before': { opacity: 0.5 }
+          '&:not([disabled]):active': { scale: 0.95 },
+          '&[data-type="gradation"]': {
+            position: 'relative',
+            background: cssVar('gradation'),
+            paddingInline: cssVar('md'),
+            overflow: 'hidden',
+            zIndex: 0,
+            '&::before': {
+              position: 'absolute',
+              content: "''",
+              inset: 0,
+              background: cssVar('black'),
+              opacity: 0,
+              transition: cssVar('transition'),
+              zIndex: -1
+            },
+            '&:not([disabled]):hover::before': { opacity: 0.5 }
+          },
+          '&[data-type="label"]': { paddingInline: cssVar('outline') },
+          '&[data-type="delete"]': { color: cssVar('red') }
         }
       }
     },
