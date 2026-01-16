c4d521f52076585537c8f47c0de1550ab83194f6
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-01-16 22:41:34 +0700

    feature: create the AxisButton component

---
+++ examples/ssr/src/pages/scroll/_components/AxisButton.ts
@@ -0,0 +1,22 @@
+import { fics } from 'ficsjs'
+import Icon from '@/components/Icon'
+import { dark } from '@/utils'
+import { MoveHorizontal, MoveVertical, X } from 'lucide-static'
+
+export default fics<{}, { isHorizontal: boolean; click: () => void }>({
+  name: 'axis-button',
+  className: 'fixed top-4 right-4',
+  children: [Icon()],
+  props: {
+    descendant: ({ children: { icon } }) => icon,
+    values: ({ props: { isHorizontal, click } }) => ({
+      svg: isHorizontal ? MoveVertical : MoveHorizontal,
+      areaLabel: `Switch axis direction to ${isHorizontal ? 'vertical' : 'horizontal'}`,
+      isLarge: true,
+      isPressed: isHorizontal,
+      click
+    })
+  },
+  html: ({ children: { icon }, template }) => template`${icon}`,
+  css: { ':host': { background: dark() } }
+})
