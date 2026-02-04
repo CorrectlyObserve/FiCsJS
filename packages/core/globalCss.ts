8e4b9efe11f3a3ec633801ec41e9a14237e0267f
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-02-04 23:54:57 +0700

    refactor: apply modification of types

--- packages/core/globalCss.ts
+++ packages/core/globalCss.ts
@@ -1,7 +1,7 @@
 import FiCsElement from './class'
 import { toArray } from './helpers'
-import type { GlobalCss, SingleOrArray } from './types'
+import type { Css, SingleOrArray } from './types'
 
-export default (css: SingleOrArray<GlobalCss>): void => {
+export default (css: SingleOrArray<Css.Global>): void => {
   FiCsElement.globalCss = toArray(css)
 }
