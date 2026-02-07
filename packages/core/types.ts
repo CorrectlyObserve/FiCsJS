0298b60827e0261f81fb702863e9a00304c5818a
MasamiOgasawara <m-ogasawara@wely.tech>
2026-02-07 15:27:06 +0700

    refactor: abolish the DebounceThrottle type and define the RateLimitFn type

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -84,17 +84,6 @@ export type DataProps<D extends object, P, B extends boolean = false> = {
   props: P
 } & (B extends true ? { crud: Crud.Fn } : {})
 
-export interface DebounceThrottle {
-  debounce: <T extends (...args: Parameters<T>) => void>(
-    func: T,
-    time: number
-  ) => (...args: Parameters<T>) => void
-  throttle: <T extends (...args: Parameters<T>) => void>(
-    func: T,
-    time: number
-  ) => (...args: Parameters<T>) => void
-}
-
 export type Descendant = FiCsElement<any, any>
 
 export interface FiCs<D extends object, P extends object> {
