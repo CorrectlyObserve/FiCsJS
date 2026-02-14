ffeda113311d052ccb50dfd66d9d0eb948fb8124
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-02-14 12:44:00 +0700

    fix: resolve the error

--- packages/core/scroll/cache.ts
+++ packages/core/scroll/cache.ts
@@ -10,7 +10,7 @@ export const evictCache = <D extends object, P>(
     cache
 
   numberError({ maxLength })
-  numberError({ newStartIndex })
+  numberError({ newStartIndex }, false)
 
   /**
    * @remarks
