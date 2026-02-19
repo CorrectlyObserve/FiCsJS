c9f7eb958008e2d1fb5d6415a092e337ab98b7b6
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-02-19 10:43:11 +0700

    refactor: modify comment style

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -59,8 +59,8 @@ export declare namespace Crud {
 
   interface StreamOptions extends Options {
     /**
-      @remarks The chunk is NOT sanitized. Be cautious of XSS vulnerabilities.
-    */
+     * @remarks The chunk is NOT sanitized. Be cautious of XSS vulnerabilities.
+     */
     onChunk: (chunk: string, index: number) => void
   }
 }
