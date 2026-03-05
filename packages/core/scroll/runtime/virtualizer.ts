53c5cb3a0b4a9cbe0e31beab714ffd1efbe98759
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-03-05 12:24:10 +0700

    refactor: apply modification of the the numberError function

--- packages/core/scroll/runtime/virtualizer.ts
+++ packages/core/scroll/runtime/virtualizer.ts
@@ -61,7 +61,7 @@ export const updateRange = <D extends object, P>({
     totalCount,
     flags: { isRangeLocked }
   }: Scroll.Resolved<D, P> = scrollOptions
-  numberError({ totalCount }, 'non-negative')
+  numberError({ totalCount }, 'non-negative-int')
   if (totalCount === 0) return
 
   if (isRangeLocked) {
