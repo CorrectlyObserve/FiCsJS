76500c0465c0a9ae2ef1ea35391fc0b74f20e66d
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-03-05 11:50:01 +0700

    feat: define the Menu component

---
+++ examples/ssr/src/pages/_components/Menu.ts
@@ -0,0 +1,81 @@
+import { fics } from 'ficsjs'
+import { flexCenter } from 'ficsjs/style'
+import Icon from '@/components/Icon'
+import { Direction } from '@/types'
+import { ArrowDownFromLine, ArrowUpFromLine, CopyPlus } from 'lucide-static'
+
+interface Data {
+  isCopy: boolean
+  buttons: (isCopy: boolean) => {
+    id: 'action' | Direction
+    svg: string
+    ariaLabel: string
+    isPressed?: boolean
+  }[]
+}
+
+interface Props {
+  isDisabled: boolean
+  moveItem: (direction: Direction, isCopy: boolean) => void | Promise<void>
+}
+
+export default fics<Data, Props>({
+  name: 'menu',
+  children: [Icon()],
+  data: () => ({
+    isCopy: false,
+    buttons: (isCopy: boolean) => [
+      {
+        id: 'action',
+        svg: CopyPlus,
+        ariaLabel: `Switch to ${isCopy ? 'move' : 'copy'} mode`,
+        isPressed: isCopy
+      },
+      {
+        id: 'up',
+        svg: ArrowUpFromLine,
+        ariaLabel: `${isCopy ? 'Copy' : 'Move'} the selected item upward`
+      },
+      {
+        id: 'down',
+        svg: ArrowDownFromLine,
+        ariaLabel: `${isCopy ? 'Copy' : 'Move'} the selected item downward`
+      }
+    ]
+  }),
+  className: 'fixed bottom-8 left-4 z-1',
+  html: ({
+    children: { icon },
+    data,
+    props: { isDisabled, moveItem },
+    template,
+    attributes: { statusLiveRegion }
+  }) => {
+    const { isCopy, buttons } = data
+
+    return template`
+      <p class="sr-only" ${statusLiveRegion}>The current mode is ${isCopy ? 'copy' : 'move'}.</p>
+      ${buttons(isCopy).map(
+        ({ id, svg, ariaLabel, isPressed }) => template`
+          ${icon.setIndividualProps(id, {
+            svg,
+            ariaLabel,
+            isLarge: true,
+            isDisabled,
+            isActive: id === 'action' && isCopy,
+            isPressed,
+            click: async () => {
+              if (id === 'action') {
+                data.isCopy = !data.isCopy
+                return
+              }
+
+              await moveItem(id, isCopy)
+            }
+          })}
+        `
+      )}
+  `
+  },
+  css: { ':host': flexCenter('y', 'column') }
+})
