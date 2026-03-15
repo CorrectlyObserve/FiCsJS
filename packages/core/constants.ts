2e223dddf89e77e49ca3810c5b0576b714187ccd
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-03-15 12:10:10 +0700

    feat: define the new constant

--- packages/core/constants.ts
+++ packages/core/constants.ts
@@ -1,9 +1,37 @@
 const HOST_SELECTOR = ':host' as const
 
 export default {
-  a11y: {
-    STATUS_LIVE_REGION: 'role="status" aria-live="polite" aria-atomic="true"'
-  },
+  a11y: { STATUS_LIVE_REGION: 'role="status" aria-live="polite" aria-atomic="true"' },
+  BOOLEAN_ATTRS: new Set([
+    'allowfullscreen',
+    'async',
+    'autofocus',
+    'autoplay',
+    'checked',
+    'controls',
+    'default',
+    'defer',
+    'disabled',
+    'formnovalidate',
+    'hidden',
+    'inert',
+    'ismap',
+    'itemscope',
+    'loop',
+    'multiple',
+    'muted',
+    'nomodule',
+    'novalidate',
+    'open',
+    'playsinline',
+    'readonly',
+    'required',
+    'reversed',
+    'shadowrootclonable',
+    'shadowrootdelegatesfocus',
+    'shadowrootserializable',
+    'selected'
+  ]),
   /** @remarks The maximum capacity of 1 byte (8 bits), which is a power of 2 */
   CLONED_SELVES_LENGTH: 256,
   FICS_ID_ATTR: 'fics-id',
