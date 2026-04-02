87a9cb7d0fdfbfb2da29ae454c4eac807ab9e196
CorrectlyObserve <-g>
2026-04-02 18:45:18 +0700

    refactor: abolish functions regarding space

--- packages/core/template/sanitize/space.ts
+++
@@ -1,7 +0,0 @@
-export const isSpace = (char: string): boolean =>
-  char === ' ' || char === '\t' || char === '\n' || char === '\f' || char === '\r'
-
-export const skipSpace = (fragment: string, index: number): number => {
-  while (index < fragment.length && isSpace(fragment[index])) index++
-  return index
-}
