09c297fd288e1423588d78390d52b8c9cec9063e
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-02 19:30:41 +0700

    feat: define the error function

--- packages/core/template/sanitize/errors.ts
+++ packages/core/template/sanitize/errors.ts
@@ -1,6 +1,24 @@
-export default {
-  attr: (name: string, type: 'name-error' | 'unquoted-error') =>
-    new Error(
-      `The attribute fragment in ${name} is ${type === 'name-error' ? 'invalid' : 'not properly closed'}...`
-    )
-} as const
+import { Template } from '../../types'
+import consts from '../constants'
+
+export default (name: string, type: 'name' | 'unquoted' | Template.Context | unknown) => {
+  switch (type) {
+    case 'name':
+    case 'unquoted':
+      return new Error(
+        `The attribute fragment in ${name} is ${type === 'name' ? 'invalid' : 'not properly closed'}...`
+      )
+
+    case 'text':
+    case 'tag':
+    case consts.char.DOUBLE_QUOTE:
+    case consts.char.SINGLE_QUOTE:
+      return new Error(
+        `HTML content cannot be interpolated into an ${type === 'tag' ? 'element tag' : 'attribute value'} in the ${name}...`
+      )
+
+    default:
+      const _type: string = type === null ? 'null' : Array.isArray(type) ? 'array' : typeof type
+      return new Error(`The "${_type}" interpolation is not supported in the ${name}...`)
+  }
+}
