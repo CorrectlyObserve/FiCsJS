c8134285677173512520e92ee41543b12c7c4ee6
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-02 22:13:08 +0700

    refactor: make code robust

--- packages/core/template/sanitize/error.ts
+++ packages/core/template/sanitize/error.ts
@@ -1,4 +1,4 @@
-import { Template } from '../../types'
+import type { Template } from '../../types'
 import consts from '../constants'
 
 export default (name: string, type: 'name' | 'unquoted' | Template.Context | unknown) => {
@@ -17,8 +17,9 @@ export default (name: string, type: 'name' | 'unquoted' | Template.Context | unk
         `HTML content cannot be interpolated into an ${type === 'tag' ? 'element tag' : 'attribute value'} in the ${name}...`
       )
 
-    default:
+    default: {
       const _type: string = type === null ? 'null' : Array.isArray(type) ? 'array' : typeof type
       return new Error(`The "${_type}" interpolation is not supported in the ${name}...`)
+    }
   }
 }
