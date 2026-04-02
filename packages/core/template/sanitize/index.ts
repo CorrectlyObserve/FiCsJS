e93be9fa56cc626cd3efe0a56ed038d2c8357f9d
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-02 19:37:08 +0700

    feat: define the sanitize function

---
+++ packages/core/template/sanitize/index.ts
@@ -0,0 +1,89 @@
+import consts from '../../constants'
+import { isBlankString } from '../../helpers'
+import type { Template } from '../../types'
+import escape from '../escape'
+import error from './errors'
+import { normalizeAttrFragment } from './normalizer'
+import { getTemplateContexts } from './parser'
+import { hasSymbol } from './validator'
+
+const {
+  symbols: { SANITIZED, UNSAFE_HTML }
+} = consts
+
+export default <T>({
+  strings,
+  variables,
+  name,
+  isFiCsElement
+}: Template.Sanitized<T>): (T | string)[] => {
+  const converted: (T | string)[] = [],
+    processValue = (variable: unknown, context: Template.Context): void => {
+      if (variable === null || variable === undefined) return
+
+      if (hasSymbol(variable, SANITIZED)) {
+        if (context !== 'text') throw error(name, context)
+
+        const sanitized: Template.Variable<T> = variable[SANITIZED] as Template.Variable<T>
+
+        if (Array.isArray(sanitized) && sanitized.length > 0) converted.push(...sanitized)
+        else if (typeof sanitized === 'string' && !isBlankString(sanitized))
+          converted.push(sanitized)
+
+        return
+      }
+
+      if (hasSymbol(variable, UNSAFE_HTML)) {
+        if (context !== 'text') throw error(name, context)
+
+        const unsafe: string = variable[UNSAFE_HTML] as string
+        if (!isBlankString(unsafe)) converted.push(unsafe)
+
+        return
+      }
+
+      if (isFiCsElement(variable)) {
+        if (context !== 'text') throw error(name, context)
+        converted.push(variable)
+        return
+      }
+
+      if (Array.isArray(variable)) {
+        for (const child of variable) processValue(child, context)
+        return
+      }
+
+      /* @remarks Prohibits other invalid types */
+      if (
+        typeof variable === 'object' ||
+        typeof variable === 'function' ||
+        typeof variable === 'symbol'
+      )
+        throw error(name, variable)
+
+      if (typeof variable === 'boolean' && context === 'tag') {
+        if (!variable) return
+        throw error(name, variable)
+      }
+
+      const stringified: string = variable.toString(),
+        fragment: string =
+          context === 'tag'
+            ? normalizeAttrFragment(stringified, name)
+            : escape(stringified, context === 'text' ? 'text-content' : 'attr')
+
+      if (!isBlankString(fragment)) converted.push(fragment)
+    }
+
+  for (let index = 0; index < variables.length; index++) {
+    const template: string = strings[index]
+    if (!isBlankString(template)) converted.push(template)
+
+    processValue(variables[index], getTemplateContexts(strings)[index])
+  }
+
+  const trailing: string = strings[strings.length - 1]
+  if (!isBlankString(trailing)) converted.push(trailing)
+
+  return converted
+}
