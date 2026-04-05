5175b705fa3ee1e5f81d51477a64a56fb936788e
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-05 11:52:33 +0700

    refactor: rearrange the code

--- packages/core/template/sanitize/validator.ts
+++ packages/core/template/sanitize/validator.ts
@@ -7,6 +7,12 @@ const {
   regExp: { CONTROL_CHAR, INVALID_ATTR_FRAGMENT }
 } = consts
 
+export const hasSymbol = <T, S extends symbol>(
+  variable: unknown,
+  symbol: S
+): variable is Record<S, Template.Variable<T>> =>
+  !!(variable && isPlainObject(variable) && symbol in variable)
+
 export const isQuote = (char: string): char is Template.Quote =>
   char === DOUBLE_QUOTE || char === SINGLE_QUOTE
 
@@ -15,9 +21,3 @@ export const isSpace = (char: string): boolean =>
 
 export const isValidAttrName = (attr: string): boolean =>
   attr.length > 0 && !CONTROL_CHAR.test(attr) && !INVALID_ATTR_FRAGMENT.test(attr)
-
-export const hasSymbol = <T, S extends symbol>(
-  variable: unknown,
-  symbol: S
-): variable is Record<S, Template.Variable<T>> =>
-  !!(variable && isPlainObject(variable) && symbol in variable)
