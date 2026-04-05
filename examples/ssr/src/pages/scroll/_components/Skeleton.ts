100a1a2ffa3e2a073215412db5c65ba38005ca8a
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-05 11:53:17 +0700

    refactor: remove the unnecessary class name

--- examples/ssr/src/pages/scroll/_components/Skeleton.ts
+++ examples/ssr/src/pages/scroll/_components/Skeleton.ts
@@ -4,6 +4,6 @@ import { white } from '@/utils'
 export default fics({
   name: 'skeleton',
   html: ({ template }) =>
-    template`<div class="skeleton size-50 block mx-auto animate-pulse"></div>`,
+    template`<div class="skeleton size-50 mx-auto animate-pulse"></div>`,
   css: { 'div.skeleton': { background: white(0.05) } }
 })
