ecdcbc11317b8e2ef01cc0001ca7b8f8bb727e7d
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-11 23:38:43 +0700

    feat: enable fade-in and fade-out

--- packages/animation/fadeInOut.ts
+++
@@ -1,7 +0,0 @@
-export default (transition: string) =>
-  ({
-    opacity: 1,
-    transition: `${transition.trim()} allow-discrete`,
-    '@starting-style': { opacity: 0 },
-    '&[style*="display: none"]': { opacity: 0 }
-  }) as const
