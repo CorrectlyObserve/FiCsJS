d9c04e0564bb1ab98120349c3bdd18b9f02b0cca
CorrectlyObserve <-g>
2026-04-11 12:13:04 +0700

    refactor: import the consolidated global CSS

--- examples/ssr/src/pages/scroll/index.ts
+++ examples/ssr/src/pages/scroll/index.ts
@@ -1,10 +1,8 @@
-import { configGlobalCss } from 'ficsjs'
+import '@/globalCss'
 import Link from '@/components/Link'
 import Photos from '@/pages/scroll/_components/Photos'
 import ChatButton from '@/components/ChatButton'
-import css from '@/.tailwindcss.txt'
 
-configGlobalCss(css)
 Link().describe()
 Photos.describe()
 ChatButton().describe()
