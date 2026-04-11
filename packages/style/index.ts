9b5698c026068c1d3395e399f652b855a3819715
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-11 18:43:46 +0700

    feat: export the truncate function

--- packages/style/index.ts
+++ packages/style/index.ts
@@ -3,5 +3,16 @@ import flexCenter from './flexCenter'
 import forScreenReaders from './forScreenReaders'
 import oklch from './oklch'
 import { calc, cssVar, hideScrollbar, remToPx } from './others'
+import truncate from './truncate'
 
-export { absoluteCenter, calc, cssVar, flexCenter, forScreenReaders, hideScrollbar, oklch, remToPx }
+export {
+  absoluteCenter,
+  calc,
+  cssVar,
+  flexCenter,
+  forScreenReaders,
+  hideScrollbar,
+  oklch,
+  remToPx,
+  truncate
+}
