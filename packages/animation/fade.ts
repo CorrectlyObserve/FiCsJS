69dd2a9dad88c6bc6ab05d81c4bf7397b673ca50
CorrectlyObserve <-g>
2026-04-11 23:51:16 +0700

    refactor: rename the param

--- packages/animation/fade.ts
+++ packages/animation/fade.ts
@@ -1,9 +1,9 @@
 import type { TransitionMode } from './types'
 
-export default (transition: string, type: TransitionMode = 'in-out') =>
+export default (transition: string, mode: TransitionMode = 'in-out') =>
   ({
     opacity: 1,
     transition: `${transition.trim()} allow-discrete`,
-    '@starting-style': type.startsWith('in') ? { opacity: 0 } : {},
-    '&[style*="display: none"]': type.endsWith('out') ? { opacity: 0 } : {}
+    '@starting-style': mode.startsWith('in') ? { opacity: 0 } : {},
+    '&[style*="display: none"]': mode.endsWith('out') ? { opacity: 0 } : {}
   }) as const
