3ff5bea7b25cdcd703186c9251b262f26f8f0a7d
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-12 02:03:16 +0700

    feat: export the circle function and the size function

--- packages/style/index.ts
+++ packages/style/index.ts
@@ -3,17 +3,20 @@ import flexCenter from './flexCenter'
 import forScreenReaders from './forScreenReaders'
 import oklch from './oklch'
 import { calc, cssVar, hideScrollbar, remToPx, space } from './others'
+import { circle, size } from './size'
 import truncate from './truncate'
 
 export {
   absoluteCenter,
   calc,
+  circle,
   cssVar,
   flexCenter,
   forScreenReaders,
   hideScrollbar,
   oklch,
   remToPx,
+  size,
   space,
   truncate
 }
