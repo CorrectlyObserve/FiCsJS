8ade397ad0ad6b7912787e0cbc95cf1a4b1131b7
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-12 01:36:44 +0700

    feat: export the vibrate function

--- packages/animation/index.ts
+++ packages/animation/index.ts
@@ -1,4 +1,5 @@
 import { pulse, spin } from './loading'
 import { fade, slide, zoom } from './transition'
+import vibrate from './vibrate'
 
-export { fade, pulse, slide, spin, zoom }
+export { fade, pulse, slide, spin, vibrate, zoom }
