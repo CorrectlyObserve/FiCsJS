fa2440b6785651ac8fc037503bd61497ec2f368b
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-12 00:40:56 +0700

    refactor: delete a unnecessary file

--- packages/animation/fade.ts
+++
@@ -1,9 +0,0 @@
-import type { TransitionMode } from './types'
-
-export default (transition: string, mode: TransitionMode = 'in-out') =>
-  ({
-    opacity: 1,
-    transition: `${transition.trim()} allow-discrete`,
-    '@starting-style': mode.startsWith('in') ? { opacity: 0 } : {},
-    '&[style*="display: none"]': mode.endsWith('out') ? { opacity: 0 } : {}
-  }) as const
