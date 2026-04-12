a900ae5fdd34edf0f53385ca7a10a2804542cdef
CorrectlyObserve <-g>
2026-04-12 23:04:23 +0700

    refactor: make the codes easier to read

--- packages/style/truncate.ts
+++ packages/style/truncate.ts
@@ -4,14 +4,13 @@ import { numberError } from './../core/helpers'
 export default (maxLines: number = 1) => {
   numberError({ maxLines }, 'positive-int')
 
-  if (maxLines === 1) {
+  if (maxLines === 1)
     return { overflow: 'hidden', 'white-space': 'nowrap', 'text-overflow': 'ellipsis' } as const
-  }
 
   return {
     display: '-webkit-box',
     overflow: 'hidden',
     '-webkit-box-orient': 'vertical',
-    '-webkit-line-clamp': maxLines.toString()
+    '-webkit-line-clamp': maxLines
   } as const
 }
