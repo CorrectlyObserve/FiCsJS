e34f6192bbda3d0188933a567dc30bfc2e2fe2ed
CorrectlyObserve <-g>
2026-04-13 17:01:06 +0700

    refactor: make the code easier to read

--- packages/persistent-state/index.ts
+++ packages/persistent-state/index.ts
@@ -1,4 +1,12 @@
 import PersistentState from './state'
+import type { Options } from './type'
 
-export default <S>(value: S, options?: { readonly: boolean }): PersistentState<S> =>
+/**
+ * @param options.backoff.maxRetry Must be a non-negative integer. Default is `10`.
+ * @param options.backoff.interval Must be a non-negative integer. Default is `100`.
+ * @param options.backoff.multiplier Must be a non-negative number. Default is `1.5`.
+ * @param options.backoff.maxDelay Must be a non-negative integer. Default is `30_000`.
+ * @param options.backoff.jitter Must be a positive integer. Default is `100`.
+ */
+export default <S>(value: S, options?: Options): PersistentState<S> =>
   new PersistentState(value, options)
