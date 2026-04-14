678192a990f4bd84b1b371cc5944ef6a418f7aa1
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-14 11:09:59 +0700

    feat: apply modification of the StringOrFn type

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -64,7 +64,7 @@ export declare namespace Crud {
 }
 
 export declare namespace Css {
-  type Ctx<D extends object, P> = SingleOrArray<StringOrFn | Css.Rules<D, P>>
+  type Ctx<D extends object, P> = SingleOrArray<StringOrFn<D, P> | Css.Rules<D, P>>
 
   interface Declarations {
     [key: string]: string | number | undefined | Declarations
