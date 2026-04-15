e2366f08fc7700b25af20d4bfb02352955048a44
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-15 11:49:08 +0700

    refactor: use template literal in CSS

--- examples/ssr/src/pages/scroll/_components/Skeleton.ts
+++ examples/ssr/src/pages/scroll/_components/Skeleton.ts
@@ -4,7 +4,6 @@ import { white } from '@/utils'
 
 export default fics({
   name: 'skeleton',
-  html: ({ template }) =>
-    template`<div class="skeleton size-50 mx-auto"></div>`,
-  css: { 'div.skeleton': { background: white(0.05), ...pulse() } }
+  html: ({ template }) => template`<div class="size-50 mx-auto"></div>`,
+  css: ({ cssToString }) => `div { background: ${white(0.05)}; ${cssToString(pulse())} }`
 })
