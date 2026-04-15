64b80ffb601f10151633c9099989fd012112a237
CorrectlyObserve <-g>
2026-04-15 15:37:15 +0700

    refactor: edit the comment

--- packages/style/size.ts
+++ packages/style/size.ts
@@ -7,7 +7,7 @@ export const circle = (diameter: number) =>
 
 /**
  * @param width Must be a positive integer.
- * @param height Must be a positive integer. Default is same as `width`.
+ * @param height Must be a positive integer. Default is same as `auto`.
  */
 export const rect = (width: Rect, height: Rect = 'auto') => {
   const isNumber = (size: Rect): size is number => typeof size === 'number'
