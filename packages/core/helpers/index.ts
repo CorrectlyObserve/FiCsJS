23e6b89106cf3f4a8876b42f421d512e1781fed0
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-04-25 13:09:50 +0700

    refactor: split helper functions

---
+++ packages/core/helpers/index.ts
@@ -0,0 +1,31 @@
+import { browserError, isBrowser } from './browser'
+import deepEqual from './deepEqual'
+import numberError from './numberError'
+import {
+  convertStr,
+  joinArray,
+  normalizePath,
+  normalizeRootMargin,
+  toArray,
+  typedEntries,
+  uid
+} from './others'
+import { isBlankString, isEmptyObject, isObject, isPlainObject } from './typeCheck'
+
+export {
+  browserError,
+  convertStr,
+  deepEqual,
+  isBlankString,
+  isBrowser,
+  isEmptyObject,
+  isObject,
+  isPlainObject,
+  joinArray,
+  normalizePath,
+  normalizeRootMargin,
+  numberError,
+  toArray,
+  typedEntries,
+  uid
+}
