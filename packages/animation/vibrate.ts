1ec4192193f6bea4a4a546772b0d372b76c71c71
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-04 01:19:25 +0700

    refactor: make the param easier to understand

--- packages/animation/vibrate.ts
+++ packages/animation/vibrate.ts
@@ -3,15 +3,15 @@ import { numberError } from '../core/helpers'
 const translate3d = (x: number | string) => ({ transform: `translate3d(${x}, 0, 0)` }) as const
 
 /**
- * @param duration Must be greater than 0. Default is `0.4`.
+ * @param durationSec Must be greater than 0. Default is `0.4`.
  * @param unit Must be a positive integer. Default is `2`.
  */
-export default (duration = 0.4, unit = 2) => {
-  numberError({ duration }, 'positive')
+export default (durationSec = 0.4, unit = 2) => {
+  numberError({ durationSec }, 'positive')
   numberError({ unit }, 'positive-int')
 
   return {
-    animation: `vibrate ${duration}s cubic-bezier(0.36, 0.07, 0.19, 0.97) both`,
+    animation: `vibrate ${durationSec}s cubic-bezier(0.36, 0.07, 0.19, 0.97) both`,
     '@keyframes vibrate': {
       '10%, 90%': translate3d(`-${unit}px`),
       '20%, 80%': translate3d(`${unit}px`),
