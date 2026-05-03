f605040f3d203afbcf3261818b0539850ee5ec32
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-04 02:10:37 +0700

    feat: define constants

---
+++ packages/core/query/constants.ts
@@ -0,0 +1,14 @@
+export default {
+  GC_LIMIT_MS: 300_000,
+  hash: {
+    CIRCULAR: '~',
+    DATE: 'D',
+    FALSE: 'F',
+    MAP: 'M',
+    NULL: 'N',
+    SET: 'S',
+    TRUE: 'T',
+    UNDEFINED: 'U'
+  },
+  STALE_MS: 0
+} as const
