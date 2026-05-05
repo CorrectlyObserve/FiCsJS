a4b60b48dd16bda50aac8dc5182dda77db5609fb
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-06 01:01:17 +0700

    refactor: apply the the RuntimeCtx type in the Query namespace

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -205,7 +205,7 @@ export declare namespace Options {
     websocket?: WebSocket.Options<D, P>
     sse?: SSE.Options<D, P>
     scroll?: Scroll.Resolved<D, P>
-    query?: Query.Runtime<D, P, unknown>['options']
+    query?: Query.RuntimeCtx<D, P, unknown>['options']
   }
 }
 
