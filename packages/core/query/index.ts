9e3436cdfef3ed6dc9c8bd0fd451512b6d1897c1
CorrectlyObserve <-g>
2026-05-06 00:34:49 +0700

    feat: define the configQueryCache function and the getQueryCache function

--- packages/core/query/index.ts
+++ packages/core/query/index.ts
@@ -1,8 +1,23 @@
+import { isBrowser } from '../helpers'
 import type { Query } from '../types'
 import QueryCache from './cache'
 import syncQueryCache from './runtime'
 
-const createQueryCache = <T>(config?: Query.Config.Global): QueryCache<T> => new QueryCache(config)
+let currentCache: QueryCache<unknown> | null = null,
+  isLocked: boolean = false
+
+const configQueryCache = (config?: Query.Config.Global): void => {
+    if (isLocked)
+      throw new Error(
+        'The configQueryCache function must be called before any FiCsElement is described in the browser...'
+      )
+    currentCache?.destroy()
+    currentCache = new QueryCache<unknown>(config)
+  },
+  getQueryCache = (): QueryCache<unknown> => {
+    if (isBrowser()) isLocked = true
+    return (currentCache ??= new QueryCache<unknown>())
+  }
 
 export { createQueryCache, syncQueryCache }
 export type { QueryCache }
