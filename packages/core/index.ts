f6dacb202507ccdbc7f007da168c70ebe9f74825
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-06 00:37:12 +0700

    feat: export the configQueryCache function for developers

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,6 +2,7 @@ import fics from './fics'
 import configGlobalCss from './globalCss'
 import { configI18n, i18n } from './i18n'
 import type { FiCs } from './namespaces'
+import { configQueryCache } from './query'
 
-export { configGlobalCss, configI18n, fics, i18n }
+export { configGlobalCss, configI18n, configQueryCache, fics, i18n }
 export type { FiCs }
