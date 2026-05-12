a9f7fff5b12a7838c4501804e0154c35d40d4d09
CorrectlyObserve <-g>
2026-05-13 01:20:40 +0700

    refactor: replace default export with named export

--- packages/style/positionCenter.ts
+++ packages/style/positionCenter.ts
@@ -3,13 +3,13 @@ import type { Axis, Center, Position } from './types'
 const horizontal = { left: '50%' } as const,
   vertical = { top: '50%' } as const
 
-function positionCenter(axis: 'x', position?: Position): Readonly<Center & typeof horizontal>
-function positionCenter(axis: 'y', position?: Position): Readonly<Center & typeof vertical>
-function positionCenter(
+export function positionCenter(axis: 'x', position?: Position): Readonly<Center & typeof horizontal>
+export function positionCenter(axis: 'y', position?: Position): Readonly<Center & typeof vertical>
+export function positionCenter(
   axis: 'xy',
   position?: Position
 ): Readonly<Center & typeof horizontal & typeof vertical>
-function positionCenter(axis: Axis, position: Position = 'absolute'): Readonly<Center> {
+export function positionCenter(axis: Axis, position: Position = 'absolute'): Readonly<Center> {
   switch (axis) {
     case 'x':
       return { position, ...horizontal, transform: 'translateX(-50%)' }
@@ -21,5 +21,3 @@ function positionCenter(axis: Axis, position: Position = 'absolute'): Readonly<C
       return { position, ...horizontal, ...vertical, transform: 'translate(-50%, -50%)' }
   }
 }
-
-export default positionCenter
