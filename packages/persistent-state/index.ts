3eae96d294705cac8a8c93ad6ef7f3d6a6af8f8b
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-13 01:43:39 +0700

    refactor: replace default export with named export

--- packages/persistent-state/index.ts
+++ packages/persistent-state/index.ts
@@ -5,5 +5,5 @@ import type { Ctx } from './types'
  * @param options.intervalMs Must be a non-negative integer if it is a number.
  * @param options.maxRetries Must be a non-negative integer if it is a number.
  */
-export default <S>({ stateId, state, options }: Ctx<S>): PersistentState<S> =>
+export const createPersistentState = <S>({ stateId, state, options }: Ctx<S>): PersistentState<S> =>
   new PersistentState({ stateId, state, options })
