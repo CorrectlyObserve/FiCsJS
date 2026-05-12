40285e99df4ed8a226296da5dbf9d602e553321c
CorrectlyObserve <-g>
2026-05-13 01:25:39 +0700

    refactor: apply named export

--- packages/core/template/sanitize/error.ts
+++ packages/core/template/sanitize/error.ts
@@ -1,5 +1,5 @@
 import type { Template } from '../../types'
-import consts from '../constants'
+import { constants } from '../constants'
 
 export const error = (name: string, type: 'name' | 'unquoted' | Template.Context | unknown) => {
   switch (type) {
@@ -11,8 +11,8 @@ export const error = (name: string, type: 'name' | 'unquoted' | Template.Context
 
     case 'text':
     case 'tag':
-    case consts.char.DOUBLE_QUOTE:
-    case consts.char.SINGLE_QUOTE:
+    case constants.char.DOUBLE_QUOTE:
+    case constants.char.SINGLE_QUOTE:
       return new Error(
         `HTML content cannot be interpolated into an ${type === 'tag' ? 'element tag' : 'attribute value'} in the ${name}...`
       )
