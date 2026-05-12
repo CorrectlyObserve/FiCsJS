bdc0c2b0ef0c0bfee01ac46c62169f4ee2879c24
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-13 01:18:16 +0700

    refactor: replace default export with named export

--- packages/core/globalCss.ts
+++ packages/core/globalCss.ts
@@ -2,6 +2,6 @@ import FiCsElement from './class'
 import { toArray } from './helpers'
 import type { Css, SingleOrArray } from './types'
 
-export default (css: SingleOrArray<Css.Global>): void => {
+export const configGlobalCss = (css: SingleOrArray<Css.Global>): void => {
   FiCsElement.globalCss = toArray(css)
 }
