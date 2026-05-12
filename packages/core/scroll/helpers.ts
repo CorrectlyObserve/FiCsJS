1282163d155c9d6f337bced0920515091fdb8bad
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-13 01:25:25 +0700

    refactor: apply named export

--- packages/core/scroll/helpers.ts
+++ packages/core/scroll/helpers.ts
@@ -1,4 +1,4 @@
-import { numberError, joinArray } from '../helpers'
+import { numberError } from '../helpers'
 import type { Scroll, SetTimeout } from '../types'
 
 export const clearTimers = <D extends object, P>(scrollOptions: Scroll.Resolved<D, P>): void => {
