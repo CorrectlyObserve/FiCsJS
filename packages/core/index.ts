b2ebdd4123363544f554d1a293549f56fdb34ea5
CorrectlyObserve <-g>
2026-05-13 14:31:10 +0700

    refactor: apply named exports

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,8 +1,5 @@
-import fics from './fics'
-import configGlobalCss from './globalCss'
-import { configI18n, i18n } from './i18n'
-import type { FiCs } from './namespaces'
-import { configQueryCache } from './query'
-
-export { configGlobalCss, configI18n, configQueryCache, fics, i18n }
-export type { FiCs }
+export { fics } from './fics'
+export { configGlobalCss } from './globalCss'
+export { configI18n, i18n } from './i18n'
+export type { FiCs } from './namespaces'
+export { configQueryCache, createQueryCache, getQueryCache } from './query'
