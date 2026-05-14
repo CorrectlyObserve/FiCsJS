35674519d0f3b7cbefa1efac4c25baebcaa0d24d
CorrectlyObserve <-g>
2026-05-14 14:22:06 +0700

    feat: define the constant

--- examples/ssr/src/data/users.ts
+++ examples/ssr/src/data/users.ts
@@ -4,3 +4,5 @@ export const BASE_URL = 'https://jsonplaceholder.typicode.com/users' as const
 
 export const fetchUsers = async ({ signal }: { signal: AbortSignal }): Promise<User[]> =>
   await fetch(BASE_URL, { signal }).then(res => res.json())
+
+export const USERS_KEY = ['users'] as const
