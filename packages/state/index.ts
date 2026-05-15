3c255cc203fbd5355d2789c52cbf3ce1cd5b575b
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-15 15:02:34 +0700

    refactor: apply the Options type

--- packages/state/index.ts
+++ packages/state/index.ts
@@ -2,5 +2,5 @@ import { State } from './state'
 import type { Options } from './types'
 
 /** @param options.version must be a positive integer if it is a number. */
-export const createState = <S>(value: S, options?: Options.Global<S>): State<S> =>
+export const createState = <S>(value: S, options?: Options<S>): State<S> =>
   new State(value, options)
