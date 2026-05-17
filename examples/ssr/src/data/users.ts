93d13b8435772813a953685958d9bc90202f442e
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-17 12:54:37 +0700

    feat: define the getExpectedUser function

--- examples/ssr/src/data/users.ts
+++ examples/ssr/src/data/users.ts
@@ -5,4 +5,18 @@ export const BASE_URL = 'https://jsonplaceholder.typicode.com/users' as const
 export const fetchUsers = async ({ signal }: { signal: AbortSignal }): Promise<User[]> =>
   await fetch(BASE_URL, { signal }).then(res => res.json())
 
+export const getExpectedUser = (users: User[], userId: User['id']): User => {
+  let targetUser: User | undefined
+  let maxId = 0
+
+  for (const user of users) {
+    if (user.id === userId) targetUser = user
+    if (user.id > maxId) maxId = user.id
+  }
+
+  if (!targetUser) throw new Error(`The user with ID ${userId} does not exist.`)
+
+  return { ...targetUser, id: maxId + 1 }
+}
+
 export const USERS_KEY = ['users'] as const
