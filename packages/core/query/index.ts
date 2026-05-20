6656d6167423f67110fa2ad6c110bf6644b63d1d
CorrectlyObserve <46045163+CorrectlyObserve@users.noreply.github.com>
2026-05-20 08:45:44 +0700

    refactor: use closure to make the code robust and easier to understand

--- packages/core/query/index.ts
+++ packages/core/query/index.ts
@@ -1,28 +1,38 @@
 import type { Query } from '../types'
 import { QueryCache } from './cache'
 
-let currentCache: QueryCache | null = null,
-  isLocked: boolean = false
+const queryCacheClosure = (() => {
+  let currentCache: QueryCache | null = null,
+    isLocked: boolean = false
 
-export const configQueryCache = (config?: Partial<Query.Config.Global>): void => {
-  if (isLocked)
-    throw new Error(
-      'The configQueryCache function must be called before any FiCsElement is described in the browser...'
-    )
+  const createQueryCache = (config?: Partial<Query.Config.Global>): QueryCache =>
+    new QueryCache(config)
 
-  currentCache?.destroy()
-  currentCache = createQueryCache(config)
-}
+  return {
+    configQueryCache: (config?: Partial<Query.Config.Global>): void => {
+      if (isLocked)
+        throw new Error(
+          'The configQueryCache function must be called before any FiCsElement is described in the browser...'
+        )
 
-export const createQueryCache = (config?: Partial<Query.Config.Global>): QueryCache =>
-  new QueryCache(config)
+      currentCache?.destroy()
+      currentCache = createQueryCache(config)
+    },
+    createQueryCache,
+    getQueryCache: (): QueryCache => (currentCache ??= createQueryCache()),
+    isQueryCacheLocked: (): boolean => isLocked,
+    lockQueryCache: (): void => {
+      isLocked = true
+    }
+  }
+})()
 
-export const getQueryCache = (): QueryCache => (currentCache ??= createQueryCache())
-
-export const isQueryCacheLocked = (): boolean => isLocked
-
-export const lockQueryCache = (): void => {
-  isLocked = true
-}
+export const {
+  configQueryCache,
+  createQueryCache,
+  getQueryCache,
+  isQueryCacheLocked,
+  lockQueryCache
+} = queryCacheClosure
 
 export type { QueryCache }
