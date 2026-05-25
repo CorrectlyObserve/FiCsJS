8408999fab6627d6fc8f994404b8403bd3d42ba3
CorrectlyObserve <-g>
2026-05-25 23:40:12 +0700

    feat: define helper functions for optimistic update

---
+++ packages/core/optimisticUpdate/helpers.ts
@@ -0,0 +1,56 @@
+import type { Optimistic, ProxyMutable } from '../types'
+
+export const createBackup = <D extends object>(
+  data: D,
+  guardKey?: (key: keyof D) => void
+): { trackedData: ProxyMutable<D>; rollback: () => void; modifiedKeys: Set<keyof D> } => {
+  const backupData: Partial<D> = {},
+    modifiedKeys: Set<keyof D> = new Set(),
+    originalKeys: ReadonlySet<keyof D> = new Set(getDataKeys(data))
+
+  const backup = (target: D, prop: keyof D): void => {
+    if (modifiedKeys.has(prop)) return
+
+    guardKey?.(prop)
+
+    if (typeof target[prop] === 'function') backupData[prop] = target[prop]
+    else
+      try {
+        backupData[prop] = structuredClone(target[prop])
+      } catch (error) {
+        throw new Error(`The value "${String(prop)}" cannot be deep-cloned...`, { cause: error })
+      }
+
+    modifiedKeys.add(prop)
+  }
+
+  const trackedData: ProxyMutable<D> = new Proxy(data, {
+      set(target, prop, value, receiver): boolean {
+        backup(target, prop as keyof D)
+        return Reflect.set(target, prop, value, receiver)
+      },
+      deleteProperty(target, prop): boolean {
+        backup(target, prop as keyof D)
+        return Reflect.deleteProperty(target, prop)
+      }
+    }) as ProxyMutable<D>,
+    rollback = (): void => {
+      for (const currentKey of getDataKeys(data))
+        if (!originalKeys.has(currentKey)) delete data[currentKey]
+
+      for (const modifiedKey of modifiedKeys) {
+        const backedUp: Partial<D>[keyof D] = backupData[modifiedKey]
+        if (originalKeys.has(modifiedKey) && backedUp !== undefined) data[modifiedKey] = backedUp
+      }
+    }
+
+  return { trackedData, rollback, modifiedKeys }
+}
+
+export const getDataKeys = <D extends object>(
+  data: D,
+  dataKeys?: Optimistic.DataKeys<D>
+): (keyof D)[] => (hasDataKeys(dataKeys) ? [...dataKeys!] : (Object.keys(data) as (keyof D)[]))
+
+export const hasDataKeys = <D extends object>(dataKeys?: Optimistic.DataKeys<D>): boolean =>
+  !!dataKeys?.length
