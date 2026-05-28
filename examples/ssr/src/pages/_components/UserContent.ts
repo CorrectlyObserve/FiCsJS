e4bb5b2557e807fb20f3053208f7a0526730f6b2
CorrectlyObserve <-g>
2026-05-28 22:55:13 +0700

    refactor: make the component easier to read

--- examples/ssr/src/pages/_components/UserContent.ts
+++ examples/ssr/src/pages/_components/UserContent.ts
@@ -1,35 +1,40 @@
-import { fics } from 'ficsjs'
+import { fics, type FiCs } from 'ficsjs'
 import { flexCenter } from 'ficsjs/style'
 import type { User } from '@/types'
 
-export default fics<{}, { user: User; userId: number }>({
-  name: 'user-content',
-  html: ({ props: { user, userId }, template }) => {
-    const { id } = user,
-      isSelected = userId === id
+interface Props {
+  user: User
+  userId: number
+}
 
-    return template`
-      <div class="space-y-2 px-3" key="${id}">
-        ${(['id', 'name', 'email'] as const).map(key => {
-          const line = `${key.charAt(0).toUpperCase() + key.slice(1)}: ${user[key]}`
+const html: FiCs.Html<{}, Props> = ({ props: { user, userId }, template }) => {
+  const { id } = user,
+    isSelected = userId === id
 
-          return template`
-            <p class="grid text-base ${isSelected ? 'text-pink' : 'text-white'}" key="${id}-${key}">
-              <span class="invisible select-none pointer-events-none font-semibold" aria-hidden="true">
-                ${line}
-              </span>
-              <span class="${isSelected ? 'font-semibold' : 'font-normal'}">${line}</span>
-            </p>
-          `
-        })}
-      </div>
-    `
-  },
-  css: ({ cssToString }) => `
-    :host {
-      ${cssToString(flexCenter('y'))}
+  return template`
+    <div class="space-y-2 px-3" key="${id}">
+      ${(['id', 'name', 'email'] as const).map(key => {
+        const line = `${key.charAt(0).toUpperCase() + key.slice(1)}: ${user[key]}`
 
-      p > span { grid-area: 1/1; }
-    }
+        return template`
+          <p class="grid text-base ${isSelected ? 'text-pink' : 'text-white'}" key="${id}-${key}">
+            <span class="invisible select-none pointer-events-none font-semibold" aria-hidden="true">
+              ${line}
+            </span>
+            <span class="${isSelected ? 'font-semibold' : 'font-normal'}">${line}</span>
+          </p>
+        `
+      })}
+    </div>
   `
-})
+}
+
+const css: FiCs.Css<{}, Props> = ({ cssToString }) => `
+  :host {
+    ${cssToString(flexCenter('y'))}
+
+    p > span { grid-area: 1/1; }
+  }
+`
+
+export default fics<{}, Props>({ name: 'user-content', html, css })
