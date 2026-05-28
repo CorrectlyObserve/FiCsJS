3444593c1df81790156a96f7dd24e67bb0f1d379
CorrectlyObserve <-g>
2026-05-28 22:56:43 +0700

    refactor: make the component easier to read

--- examples/todo/src/components/materials/Loading.ts
+++ examples/todo/src/components/materials/Loading.ts
@@ -1,36 +1,48 @@
-import { fics } from 'ficsjs'
+import { fics, type FiCs } from 'ficsjs'
 import { spin } from 'ficsjs/animation'
 import { forScreenReaders, rect, size } from 'ficsjs/style'
 import type { Lang } from '@/utils/lang'
 import { white } from '@/utils/others'
 import { Loader } from 'lucide-static'
 
+interface Data {
+  texts: Record<Lang, string>
+}
+
+interface Props {
+  lang: Lang
+}
+
+const html: FiCs.Html<Data, Props> = ({
+  data: { texts },
+  props: { lang },
+  template,
+  unsafeHtml,
+  attributes: { statusLiveRegion }
+}) => template`
+  <p ${statusLiveRegion}>${texts[lang]}</p>
+  <div aria-hidden="true">${unsafeHtml(Loader)}</div>
+`
+
+const css: FiCs.Css<Data, Props> = ({ cssToString }) => `
+  p {${cssToString(forScreenReaders)}}
+
+  div {
+    padding: ${size(2)};
+    margin-inline: auto;
+
+    svg {
+      ${cssToString({ ...rect(16), ...spin() })}
+      display: flex;
+      stroke: ${white()};
+    }
+  }
+`
+
 export default () =>
-  fics<{ texts: Record<Lang, string> }, { lang: Lang }>({
+  fics<Data, Props>({
     name: 'loading',
     data: () => ({ texts: { en: 'Loading...', ja: '読み込み中' } }),
-    html: ({
-      data: { texts },
-      props: { lang },
-      template,
-      unsafeHtml,
-      attributes: { statusLiveRegion }
-    }) => template`
-      <p ${statusLiveRegion}>${texts[lang]}</p>
-      <div aria-hidden="true">${unsafeHtml(Loader)}</div>
-    `,
-    css: ({ cssToString }) => `
-      p {${cssToString(forScreenReaders)}}
-
-      div {
-        padding: ${size(2)};
-        margin-inline: auto;
-
-        svg {
-          ${cssToString({ ...rect(16), ...spin() })}
-          display: flex;
-          stroke: ${white()};
-        }
-      }
-    `
+    html,
+    css
   })
