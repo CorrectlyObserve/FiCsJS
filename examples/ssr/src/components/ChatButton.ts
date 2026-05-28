d8f55a765f035e6bf45b83c145b809d291959923
CorrectlyObserve <-g>
2026-05-28 22:54:40 +0700

    refactor: make the component easier to read

--- examples/ssr/src/components/ChatButton.ts
+++ examples/ssr/src/components/ChatButton.ts
@@ -1,22 +1,26 @@
-import { fics } from 'ficsjs'
+import { fics, type FiCs } from 'ficsjs'
 import Icon from '@/components/Icon'
 import { CHAT_PAGE, dark } from '@/utils'
 import { MessageCircleMore } from 'lucide-static'
+
+const props: FiCs.Props<{}, {}> = {
+  descendant: ({ children: { icon } }) => icon,
+  values: () => ({
+    svg: MessageCircleMore,
+    ariaLabel: 'Go to the chat page',
+    isLarge: true,
+    click: () => (window.location.href = CHAT_PAGE)
+  })
+}
+const html: FiCs.Html<{}, {}> = ({ children: { icon }, template }) => template`${icon}`
+const css: FiCs.Css<{}, {}> = `:host { background: ${dark()}; }`
 
 export default () =>
   fics({
     name: 'chat-button',
     className: 'fixed bottom-8 right-4',
     children: [Icon()],
-    props: {
-      descendant: ({ children: { icon } }) => icon,
-      values: () => ({
-        svg: MessageCircleMore,
-        ariaLabel: 'Go to the chat page',
-        isLarge: true,
-        click: () => (window.location.href = CHAT_PAGE)
-      })
-    },
-    html: ({ children: { icon }, template }) => template`${icon}`,
-    css: `:host { background: ${dark()}; }`
+    props,
+    html,
+    css
   })
