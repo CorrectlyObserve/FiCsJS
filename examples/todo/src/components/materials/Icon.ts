620037fd5ec5e26d5203cb2058a802f97b29abdd
CorrectlyObserve <-g>
2026-05-28 22:56:32 +0700

    refactor: make the component easier to read

--- examples/todo/src/components/materials/Icon.ts
+++ examples/todo/src/components/materials/Icon.ts
@@ -1,4 +1,4 @@
-import { fics } from 'ficsjs'
+import { fics, type FiCs } from 'ficsjs'
 import { rect, size } from 'ficsjs/style'
 import { white } from '@/utils/others'
 
@@ -10,38 +10,38 @@ interface Props {
   click?: () => void
 }
 
-export default () =>
-  fics<{}, Props>({
-    name: 'icon',
-    html: ({
-      props: { ariaLabel, svg, isPressed },
-      template,
-      unsafeHtml,
-      attributes: { boolean }
-    }) => template`
-      <button
-        aria-label="${ariaLabel}"
-        ${isPressed === undefined ? '' : `aria-pressed="${boolean(isPressed)}"`}
-        type="button"
-      >${unsafeHtml(svg)}</button>
-    `,
-    css: ({ props: { color }, cssToString }) => `
-      button[type="button"] {
-        background: none;
-        color: ${color ?? white()};
-        padding: ${size(2)};
+const html: FiCs.Html<{}, Props> = ({
+  props: { ariaLabel, svg, isPressed },
+  template,
+  unsafeHtml,
+  attributes: { boolean }
+}) => template`
+  <button
+    aria-label="${ariaLabel}"
+    ${isPressed === undefined ? '' : `aria-pressed="${boolean(isPressed)}"`}
+    type="button"
+  >${unsafeHtml(svg)}</button>
+`
 
-        &:hover { background: ${white(0.1)}; }
-        &:focus, &:focus-visible { outline-color: ${color ?? white()}; }
+const css: FiCs.Css<{}, Props> = ({ props: { color }, cssToString }) => `
+  button[type="button"] {
+    background: none;
+    color: ${color ?? white()};
+    padding: ${size(2)};
 
-        svg {
-          ${cssToString(rect(8))}
-          display: flex;
-          stroke: currentColor;
-        }
-      }
-    `,
-    actions: {
-      button: { click: [({ props: { click } }) => click?.(), { throttleMs: 500, blur: true }] }
+    &:hover { background: ${white(0.1)}; }
+    &:focus, &:focus-visible { outline-color: ${color ?? white()}; }
+
+    svg {
+      ${cssToString(rect(8))}
+      display: flex;
+      stroke: currentColor;
     }
-  })
+  }
+`
+
+const actions: FiCs.Actions<{}, Props> = {
+  button: { click: [({ props: { click } }) => click?.(), { throttleMs: 500, blur: true }] }
+}
+
+export default () => fics<{}, Props>({ name: 'icon', html, css, actions })
