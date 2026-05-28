ae25132df6f3fefd6126bdc6667043a2e9a1d45c
CorrectlyObserve <-g>
2026-05-28 22:55:36 +0700

    refactor: make the component easier to read

--- examples/ssr/src/pages/scroll/_components/Skeleton.ts
+++ examples/ssr/src/pages/scroll/_components/Skeleton.ts
@@ -1,9 +1,9 @@
-import { fics } from 'ficsjs'
+import { fics, type FiCs } from 'ficsjs'
 import { pulse } from 'ficsjs/animation'
 import { white } from '@/utils'
 
-export default fics({
-  name: 'skeleton',
-  html: ({ template }) => template`<div class="size-50 mx-auto"></div>`,
-  css: ({ cssToString }) => `div { background: ${white(0.05)}; ${cssToString(pulse())} }`
-})
+const html: FiCs.Html<{}, {}> = ({ template }) => template`<div class="size-50 mx-auto"></div>`
+const css: FiCs.Css<{}, {}> = ({ cssToString }) =>
+  `div { background: ${white(0.05)}; ${cssToString(pulse())} }`
+
+export default fics({ name: 'skeleton', html, css })
