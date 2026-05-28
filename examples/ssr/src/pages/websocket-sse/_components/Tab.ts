a99b869448cffeaca451669454dfb85dd2217d82
CorrectlyObserve <-g>
2026-05-28 22:55:55 +0700

    refactor: make the component easier to read

--- examples/ssr/src/pages/websocket-sse/_components/Tab.ts
+++ examples/ssr/src/pages/websocket-sse/_components/Tab.ts
@@ -1,8 +1,38 @@
-import { fics } from 'ficsjs'
+import { fics, type FiCs } from 'ficsjs'
 import { goto } from 'ficsjs/router'
 import { flexCenter } from 'ficsjs/style'
 import Button from '@/components/Button'
 import { CHAT_PAGE } from '@/utils'
+
+interface Data {
+  tabs: { href: string; text: string }[]
+  current: string
+}
+
+const html: FiCs.Html<Data, {}> = ({ children: { button }, data, template }) => template`
+  <div class="container mb-6 mx-auto gap-4">
+    ${data.tabs.map(
+      ({ href, text }, index) => template`
+        ${button.setIndividualProps(index, {
+          isDisabled: data.current === '' || data.current === href,
+          isCurrent: data.current === href,
+          buttonText: text,
+          click: () => {
+            goto(href)
+            data.current = href
+          }
+        })}
+      `
+    )}
+  </div>
+`
+
+const css: FiCs.Css<Data, {}> = ({ cssToString }) =>
+  `:host div.container {${cssToString(flexCenter('x'))}}`
+
+const hooks: FiCs.Hooks<Data, {}> = {
+  mounted: ({ data }) => (data.current = window.location.pathname)
+}
 
 export default fics({
   name: 'tab',
@@ -15,23 +45,7 @@ export default fics({
     ],
     current: ''
   }),
-  html: ({ children: { button }, data, template }) => template`
-    <div class="container mb-6 mx-auto gap-4">
-      ${data.tabs.map(
-        ({ href, text }, index) => template`
-          ${button.setIndividualProps(index, {
-            isDisabled: data.current === '' || data.current === href,
-            isCurrent: data.current === href,
-            buttonText: text,
-            click: () => {
-              goto(href)
-              data.current = href
-            }
-          })}
-        `
-      )}
-    </div>
-  `,
-  css: ({ cssToString }) => `:host div.container {${cssToString(flexCenter('x'))}}`,
-  hooks: { mounted: ({ data }) => (data.current = window.location.pathname) }
+  html,
+  css,
+  hooks
 })
