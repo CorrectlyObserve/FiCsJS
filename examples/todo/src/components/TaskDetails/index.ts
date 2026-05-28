3865d1d02dd8c7b4f5b54305bc11a9d11a25d9b3
CorrectlyObserve <-g>
2026-05-28 22:57:00 +0700

    refactor: make the component easier to read

--- examples/todo/src/components/TaskDetails/index.ts
+++ examples/todo/src/components/TaskDetails/index.ts
@@ -1,4 +1,4 @@
-import { fics } from 'ficsjs'
+import { fics, type FiCs } from 'ficsjs'
 import { dynamicPaths, goto } from 'ficsjs/router'
 import { cssVar, flexCenter, size } from 'ficsjs/style'
 import Loading from '@/components/materials/Loading'
@@ -38,6 +38,151 @@ interface Props {
 
 const { SM } = breakpoints
 
+const props: FiCs.Props<Data, Props> = [
+  {
+    descendant: ({ children: { icon } }) => icon,
+    values: ({ props: { draft, editTask } }) => ({
+      click: () => {
+        if ('completedAt' in draft)
+          editTask({ completedAt: draft?.completedAt ? undefined : getTimestamp() })
+      }
+    })
+  },
+  {
+    descendant: ({ children: { input } }) => input,
+    values: ({
+      data: { labels, isError, error, descriptions, placeholders },
+      props: { draft, editTask }
+    }) => ({
+      id: 'title',
+      label: labels[0],
+      isError: isError(draft),
+      error,
+      description: descriptions[0],
+      placeholder: placeholders[0],
+      value: draft?.title,
+      input: (title: string) => editTask({ title })
+    })
+  },
+  {
+    descendant: ({ children: { textarea } }) => textarea,
+    values: ({ data: { labels, descriptions, placeholders }, props: { draft, editTask } }) => ({
+      id: 'description',
+      label: labels[1],
+      description: descriptions[1],
+      placeholder: placeholders[1],
+      value: draft?.description,
+      input: (description: string) => editTask({ description })
+    })
+  }
+]
+
+const html: FiCs.Html<Data, Props> = ({
+  children: { loading, icon, input, textarea, button },
+  data: {
+    heading,
+    status,
+    texts: [complete, revert, _delete, back, close],
+    datetimes,
+    buttonText,
+    confirmation
+  },
+  props: { draft, editTask, updateTasks },
+  template,
+  isDeferred
+}) => {
+  if (!isDeferred) return template`${loading}`
+
+  const label = draft?.completedAt ? revert : complete
+
+  return template`
+    <h2>${heading}</h2>
+    <div class="container">
+      <fieldset>
+        <legend>${status}</legend>
+        <div>
+          ${icon.setIndividualProps('icon', {
+            svg: draft?.completedAt ? CircleCheckBig : Circle,
+            ariaLabel: label,
+            isPressed: !!draft?.completedAt
+          })}
+          ${button.setIndividualProps('status', {
+            type: 'label',
+            buttonText: label,
+            isPressed: !!draft?.completedAt,
+            click: () => editTask({ completedAt: draft?.completedAt ? undefined : getTimestamp() })
+          })}
+        </div>
+      </fieldset>
+      <fieldset>${input}</fieldset>
+      <fieldset>${textarea}</fieldset>
+      ${Object.entries(datetimes).map(
+        ([key, value]) => template`<p>${value}${convertTimestamp(draft?.[key as Datetime])}</p>`
+      )}
+      <div>
+        ${button.setIndividualProps('save', {
+          isDisabled: draft?.title === '',
+          type: 'gradation',
+          fixedUnit: 32,
+          buttonText,
+          click: async () => {
+            const { id, title, description, completedAt }: Task = draft
+            const tasks: Task[] = await updateTask({ id, title, description, completedAt })
+
+            const task: Task | undefined = getTask(tasks, id)
+            if (!task) return
+
+            editTask(task)
+            updateTasks(tasks)
+            goto('/')
+          }
+        })}
+        ${[_delete, !Number.isInteger(parseInt(dynamicPaths().taskId)) ? close : back].map(
+          (buttonText, index) =>
+            template`${button.setIndividualProps(index, {
+              type: index === 0 ? 'delete' : 'normal',
+              fixedUnit: 32,
+              buttonText,
+              click: async () => {
+                if (index === 0) {
+                  if (window.confirm(confirmation)) {
+                    await deleteTask(draft?.id)
+                    updateTasks(await getAllTasks())
+                    goto('/')
+                  }
+                } else goto('/')
+              }
+            })}`
+        )}
+      </div>
+    </div>
+  `
+}
+
+const css: FiCs.Css<Data, Props> = {
+  'div.container': {
+    width: SM,
+    maxWidth: size(120 - 16),
+    marginInline: 'auto',
+    [`@media (max-width: ${SM})`]: { width: '100%' },
+    fieldset: {
+      display: 'flex',
+      flexDirection: 'column',
+      marginBlockEnd: size(4),
+      border: 0,
+      legend: { paddingBlockEnd: size(2) },
+      button: { paddingInline: size(4), '&:hover': { background: white(0.1) } },
+      div: { ...flexCenter('y'), button: { paddingInline: cssVar('outline') } }
+    },
+    p: {
+      marginBlockEnd: size(2),
+      textAlign: 'left',
+      '&:last-of-type': { marginBlockEnd: size(8) }
+    },
+    '> div': { display: 'flex', flexDirection: 'column', gap: size(2) }
+  }
+}
+
 export default fics<Data, Props>({
   name: 'task-details',
   children: [Loading(), Icon(), Input(), Textarea, Button()],
@@ -51,151 +196,11 @@ export default fics<Data, Props>({
   }),
   i18nData: async ({ props: { lang }, i18n }) => ({
     ...(await i18n<Data>({ lang, key: 'task' })),
-    confirmation: await i18n({ lang, key: ['tasks', 'confirmation'] })
+    confirmation: (await i18n({ lang, key: ['tasks', 'confirmation'] })) as string
   }),
-  props: [
-    {
-      descendant: ({ children: { icon } }) => icon,
-      values: ({ props: { draft, editTask } }) => ({
-        click: () => {
-          if ('completedAt' in draft)
-            editTask({ completedAt: draft?.completedAt ? undefined : getTimestamp() })
-        }
-      })
-    },
-    {
-      descendant: ({ children: { input } }) => input,
-      values: ({
-        data: { labels, isError, error, descriptions, placeholders },
-        props: { draft, editTask }
-      }) => ({
-        id: 'title',
-        label: labels[0],
-        isError: isError(draft),
-        error,
-        description: descriptions[0],
-        placeholder: placeholders[0],
-        value: draft?.title,
-        input: (title: string) => editTask({ title })
-      })
-    },
-    {
-      descendant: ({ children: { textarea } }) => textarea,
-      values: ({ data: { labels, descriptions, placeholders }, props: { draft, editTask } }) => ({
-        id: 'description',
-        label: labels[1],
-        description: descriptions[1],
-        placeholder: placeholders[1],
-        value: draft?.description,
-        input: (description: string) => editTask({ description })
-      })
-    }
-  ],
+  props,
   className: 'task-details',
-  html: ({
-    children: { loading, icon, input, textarea, button },
-    data: {
-      heading,
-      status,
-      texts: [complete, revert, _delete, back, close],
-      datetimes,
-      buttonText,
-      confirmation
-    },
-    props: { draft, editTask, updateTasks },
-    template,
-    isDeferred
-  }) => {
-    if (!isDeferred) return template`${loading}`
-
-    const label = draft?.completedAt ? revert : complete
-
-    return template`
-      <h2>${heading}</h2>
-      <div class="container">
-        <fieldset>
-          <legend>${status}</legend>
-          <div>
-            ${icon.setIndividualProps('icon', {
-              svg: draft?.completedAt ? CircleCheckBig : Circle,
-              ariaLabel: label,
-              isPressed: !!draft?.completedAt
-            })}
-            ${button.setIndividualProps('status', {
-              type: 'label',
-              buttonText: label,
-              isPressed: !!draft?.completedAt,
-              click: () =>
-                editTask({ completedAt: draft?.completedAt ? undefined : getTimestamp() })
-            })}
-          </div>
-        </fieldset>
-        <fieldset>${input}</fieldset>
-        <fieldset>${textarea}</fieldset>
-        ${Object.entries(datetimes).map(
-          ([key, value]) => template`<p>${value}${convertTimestamp(draft?.[key as Datetime])}</p>`
-        )}
-        <div>
-          ${button.setIndividualProps('save', {
-            isDisabled: draft?.title === '',
-            type: 'gradation',
-            fixedUnit: 32,
-            buttonText,
-            click: async () => {
-              const { id, title, description, completedAt }: Task = draft
-              const tasks: Task[] = await updateTask({ id, title, description, completedAt })
-
-              const task: Task | undefined = getTask(tasks, id)
-              if (!task) return
-
-              editTask(task)
-              updateTasks(tasks)
-              goto('/')
-            }
-          })}
-          ${[_delete, !Number.isInteger(parseInt(dynamicPaths().taskId)) ? close : back].map(
-            (buttonText, index) =>
-              template`${button.setIndividualProps(index, {
-                type: index === 0 ? 'delete' : 'normal',
-                fixedUnit: 32,
-                buttonText,
-                click: async () => {
-                  if (index === 0) {
-                    if (window.confirm(confirmation)) {
-                      await deleteTask(draft?.id)
-                      updateTasks(await getAllTasks())
-                      goto('/')
-                    }
-                  } else goto('/')
-                }
-              })}`
-          )}
-        </div>
-      </div>
-    `
-  },
-  css: {
-    'div.container': {
-      width: SM,
-      maxWidth: size(120 - 16),
-      marginInline: 'auto',
-      [`@media (max-width: ${SM})`]: { width: '100%' },
-      fieldset: {
-        display: 'flex',
-        flexDirection: 'column',
-        marginBlockEnd: size(4),
-        border: 0,
-        legend: { paddingBlockEnd: size(2) },
-        button: { paddingInline: size(4), '&:hover': { background: white(0.1) } },
-        div: { ...flexCenter('y'), button: { paddingInline: cssVar('outline') } }
-      },
-      p: {
-        marginBlockEnd: size(2),
-        textAlign: 'left',
-        '&:last-of-type': { marginBlockEnd: size(8) }
-      },
-      '> div': { display: 'flex', flexDirection: 'column', gap: size(2) }
-    }
-  },
+  html,
+  css,
   options: { lazyLoad: true }
 })
