61c81015fd291aef02e3fcd0cd6f30746aa00b9a
CorrectlyObserve <-g>
2026-06-07 00:42:37 +0700

    feat: create the top page

---
+++ examples/todo/src/pages/+page.ts
@@ -0,0 +1,16 @@
+import type { FiCsRouter } from 'ficsjs/router'
+import type { Data } from '@/components/Router'
+import { measureOffsetWidth } from '@/utils/others'
+
+const page: FiCsRouter.Page<Data> = ({
+  children: { tasks, taskDetails },
+  data: {
+    queries: { taskId }
+  },
+  template
+}) => {
+  if (taskId) return measureOffsetWidth() ? template`${tasks}${taskDetails}` : taskDetails
+  return tasks
+}
+
+export default page
