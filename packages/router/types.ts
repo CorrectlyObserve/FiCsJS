57f1a14c0b46e9c5a9783e5bf6efc781b955cd44
CorrectlyObserve <-g>
2026-06-07 17:27:27 +0700

    feat: modify the Page type

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -41,6 +41,7 @@ export interface FiCsRouter<D extends object> {
 
 export interface Page<D extends object> extends PageContent<D> {
   path: string
+  meta?: Record<string, string>
 }
 
 export interface PageContent<D extends object> {
