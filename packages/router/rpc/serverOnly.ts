729a80f10b19641296c81d0be1c56d2ff3778530
CorrectlyObserve <-g>
2026-06-13 17:19:30 +0700

    feat: define the serverOnly guard in implementing rpc

---
+++ packages/router/rpc/serverOnly.ts
@@ -0,0 +1,6 @@
+import { isBrowser } from './../../core/helpers/browser'
+
+if (isBrowser()) throw new Error('Please import only its types on the client...')
+
+/** @remarks ensures as an ES module */
+export {}
