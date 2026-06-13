f9e7d56f24e72f2f0a1c33d0e484e96d1b159fa4
CorrectlyObserve <-g>
2026-06-13 16:01:35 +0700

    feat: add the layout

---
+++ examples/ssr/src/pages/websocket-sse/+layout.ts
@@ -0,0 +1,11 @@
+import type { FiCsRouter } from 'ficsjs/router'
+import Link from '@/components/Link'
+import Tab from '@/pages/websocket-sse/Tab'
+
+const layout: FiCsRouter.SSRLayout = ({ slot }) => `
+  ${Link.toString({ data: { href: '/', text: 'Back to the top page' } })}
+  ${Tab.toString()}
+  ${slot}
+`
+
+export default layout
