cc0a41281f2c2815cbeb16fdae4070da1b4e5977
CorrectlyObserve <-g>
2026-06-13 16:04:39 +0700

    refactor: rename the file

--- examples/ssr/src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-import '@/globalCss'
-import Link from '@/components/Link'
-import Users from '@/pages/_components/Users'
-import ChatButton from '@/components/ChatButton'
-
-Link().describe()
-Users.describe()
-ChatButton().describe()
---
+++ examples/ssr/src/pages/index/script.ts
@@ -0,0 +1,8 @@
+import '@/globalCss'
+import ChatButton from '@/components/ChatButton'
+import Link from '@/components/Link'
+import Users from '@/pages/Users'
+
+Link.describe()
+Users.describe()
+ChatButton.describe()
