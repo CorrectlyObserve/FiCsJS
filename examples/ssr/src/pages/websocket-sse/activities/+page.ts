b2b9c55d8e5f09b3a08366c85dace7c1c2fcbd96
CorrectlyObserve <-g>
2026-06-13 16:01:06 +0700

    feat: add the page

---
+++ examples/ssr/src/pages/websocket-sse/activities/+page.ts
@@ -0,0 +1,11 @@
+import type { FiCsRouter } from 'ficsjs/router'
+import type { Data } from '@/pages/websocket-sse/+spa'
+
+const page: FiCsRouter.Page<Data> = ({ data: { activities }, template }) => template`
+  <h2 class="text-lg text-white text-center mb-6">Activities</h2>
+  <div class="w-fit mx-auto">
+    ${activities.map(activity => template`<p class="text-white mb-4">${activity}</p>`)}
+  </div>
+`
+
+export default page
