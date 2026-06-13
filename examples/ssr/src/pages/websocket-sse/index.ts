275a63dc7e992bfe4c81599e6b47ccefaa254765
CorrectlyObserve <-g>
2026-06-13 16:04:16 +0700

    refactor: rename the file

--- examples/ssr/src/pages/websocket-sse/index.ts
+++
@@ -1,8 +0,0 @@
-import '@/globalCss'
-import Link from '@/components/Link'
-import Tab from '@/pages/websocket-sse/_components/Tab'
-import Router from '@/pages/websocket-sse/_components/Router'
-
-Link().describe()
-Tab.describe()
-Router.describe()
---
+++ examples/ssr/src/pages/websocket-sse/script.ts
@@ -0,0 +1,8 @@
+import '@/globalCss'
+import Link from '@/components/Link'
+import Tab from '@/pages/websocket-sse/Tab'
+import { WebsocketSse } from '@/routes.gen'
+
+Link.describe()
+Tab.describe()
+WebsocketSse.describe()
