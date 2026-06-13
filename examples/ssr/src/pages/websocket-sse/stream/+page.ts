ecb270478342c72466d3cf8e4f324bf2f56731b8
CorrectlyObserve <-g>
2026-06-13 16:00:54 +0700

    feat: add the page

---
+++ examples/ssr/src/pages/websocket-sse/stream/+page.ts
@@ -0,0 +1,5 @@
+import type { FiCsRouter } from 'ficsjs/router'
+import type { Data } from '@/pages/websocket-sse/+spa'
+
+const page: FiCsRouter.Page<Data> = ({ children: { stream } }) => stream
+export default page
