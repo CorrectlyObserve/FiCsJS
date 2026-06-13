a043d5931321ae9fdd965a247f55fe04b1d72880
CorrectlyObserve <-g>
2026-06-13 17:15:19 +0700

    refactor: apply rename of the type

--- examples/ssr/src/pages/websocket-sse/+layout.ts
+++ examples/ssr/src/pages/websocket-sse/+layout.ts
@@ -2,7 +2,7 @@ import type { FiCsRouter } from 'ficsjs/router'
 import Link from '@/components/Link'
 import Tab from '@/pages/websocket-sse/Tab'
 
-const layout: FiCsRouter.SSRLayout = ({ slot }) => `
+const layout: FiCsRouter.SsrLayout<{}> = ({ slot }) => `
   ${Link.toString({ data: { href: '/', text: 'Back to the top page' } })}
   ${Tab.toString()}
   ${slot}
