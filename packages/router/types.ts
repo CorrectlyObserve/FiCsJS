e2f828ec50f36085575140232a7ecf97d10bf789
CorrectlyObserve <-g>
2026-06-20 02:23:03 +0700

    feat: define Ctx type in Routing namespace and integrate Redirect type

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -51,12 +51,6 @@ export interface PageContent<D extends object = Record<string, unknown>> {
 
 export type ParamType = 'dynamicPaths' | 'queries'
 
-export interface Redirect {
-  pathname: string
-  redirectMap: ReadonlyMap<string, string>
-  redirectFn?: Routing.RedirectFn
-}
-
 export type Returned<D extends object, P extends object> = Descendant | Html.Sanitized<D, P>
 
 export type RouterData<D extends object> = D & {
