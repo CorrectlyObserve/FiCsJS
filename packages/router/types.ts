2e9a07030e89c93fcb53a763fbddcc4390ecb490
CorrectlyObserve <-g>
2026-06-23 13:21:27 +0700

    feat: apply Transformed type in Routing namespace

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -94,7 +94,7 @@ export declare namespace Routing {
     buildStart?: () => void
     configureServer?: (server: { watcher: { add: (path: string) => void } }) => void
     handleHotUpdate?: ({ file }: { file: string }) => void
-    transform?: ({ id, code }: { id: string; code: string }) => { code: string; map: null } | null
+    transform?: ({ id, code }: { id: string; code: string }) => Transformed | null
   }
 
   interface Redirect {
