12cc775258b2d4971e691d365d72e209e949cf2f
CorrectlyObserve <-g>
2026-06-23 17:04:33 +0700

    refactor: modify applyLayout function

--- packages/router/layout.ts
+++ packages/router/layout.ts
@@ -1,14 +1,14 @@
 import { hasMethod } from './helpers'
 import type { Routing } from './types'
 
-export const applyLayout = ({
-  layout: { default: layoutDef },
+export const applyLayout = <T extends Routing.Module>({
+  layout,
   page
-}: {
-  layout: Routing.Module
-  page: Routing.Module
-}): Routing.Module => {
-  const { default: pageDef, redirect }: Routing.Module = page
+}: Omit<Routing.Route<T>, 'path'>): T => {
+  if (!layout) return page
+
+  const { default: layoutDef }: T = layout,
+    { default: pageDef, redirect }: T = page
 
   if (typeof layoutDef !== 'function' || typeof redirect === 'string') return page
 
