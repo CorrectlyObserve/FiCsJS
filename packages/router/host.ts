711a827e586f5a602c3c53bb0c56628dbb250360
CorrectlyObserve <-g>
2026-06-25 00:34:11 +0700

    feat: define registerPages function

---
+++ packages/router/host.ts
@@ -0,0 +1,57 @@
+import { statusCodes } from './constants'
+import { applyLayout } from './layout'
+import type { Routing } from './types'
+
+export const registerPages = <C extends Record<string, unknown>>(
+  app: {
+    get: (path: string, handler: (ctx: Routing.Host) => unknown) => unknown
+    use: (middleware: (ctx: Routing.Host, next: () => Promise<void>) => unknown) => unknown
+  },
+  routes: Routing.ServerRoute<C>[],
+  { render, createContext, toHostRoutePath, serverError, redirects }: Routing.Options.Register<C>
+): void => {
+  if (typeof redirects === 'function')
+    app.use(
+      async ({ req, redirect }: Routing.Host, next: () => Promise<void>): Promise<unknown> => {
+        const { path }: { path?: unknown } = req as { path?: unknown }
+
+        if (typeof path === 'string') {
+          const redirected: string | null = redirects(path)
+          if (typeof redirected === 'string') return redirect(redirected, statusCodes.REDIRECT)
+        }
+
+        await next()
+        return undefined
+      }
+    )
+  else if (redirects)
+    for (const [from, to] of Object.entries(redirects))
+      app.get(from, ({ redirect }: Routing.Host) => redirect(to, statusCodes.REDIRECT))
+
+  for (const { path, page, layout } of routes) {
+    const { meta = {}, default: def }: Routing.ServerModule<C> = applyLayout({
+      layout: layout as Routing.ServerModule<C>,
+      page
+    })
+
+    app.get(toHostRoutePath(path), async (ctx: Routing.Host) => {
+      const _ctx: C & { req: unknown } = { ...(createContext?.(ctx) ?? ({} as C)), req: ctx.req }
+
+      try {
+        return ctx.html(render({ meta, content: def ? await def(_ctx) : '', path }))
+      } catch (error) {
+        if (!serverError) throw error
+
+        const { meta: errorMeta = {}, default: errorDef } = serverError
+        return ctx.html(
+          render({
+            meta: errorMeta,
+            content: errorDef ? await errorDef({ ..._ctx, error }) : '',
+            path
+          }),
+          statusCodes.INTERNAL_SERVER_ERROR
+        )
+      }
+    })
+  }
+}
