89b9955a6a41bac513ad1517503b9ae0a9d3c7ee
CorrectlyObserve <-g>
2026-06-28 14:32:39 +0700

    feat: define RPC_MODULE_TYPE

--- packages/router/constants.ts
+++ packages/router/constants.ts
@@ -1,3 +1,5 @@
 export const FICS_NAVIGATE = 'fics:navigate' as const
 
+export const RPC_MODULE_TYPE = { module: 'rpc' } as const
+
 export const statusCodes = { REDIRECT: 302, INTERNAL_SERVER_ERROR: 500 } as const
