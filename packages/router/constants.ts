fb816302583195f20c36816bb96835ca3f655746
CorrectlyObserve <-g>
2026-06-28 16:54:58 +0700

    feat: add a new status code

--- packages/router/constants.ts
+++ packages/router/constants.ts
@@ -2,4 +2,8 @@ export const FICS_NAVIGATE = 'fics:navigate' as const
 
 export const RPC_MODULE_TYPE = { module: 'rpc' } as const
 
-export const statusCodes = { REDIRECT: 302, INTERNAL_SERVER_ERROR: 500 } as const
+export const statusCodes = {
+  INTERNAL_SERVER_ERROR: 500,
+  METHOD_NOT_ALLOWED: 405,
+  REDIRECT: 302
+} as const
