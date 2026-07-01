cca04e6c75562597ead3e44f023b7136795c25bc
CorrectlyObserve <-g>
2026-07-01 18:50:15 +0700

    feat: define and apply isHeadMethod function

--- packages/router/rpc/helpers.ts
+++ packages/router/rpc/helpers.ts
@@ -1,4 +1,6 @@
 import type { Rpc } from './../types'
 
 export const isBodiless = (method: Rpc.Method | string): method is 'GET' | 'HEAD' =>
-  method === 'GET' || method === 'HEAD'
+  method === 'GET' || isHeadMethod(method)
+
+export const isHeadMethod = (method: Rpc.Method | string): method is 'HEAD' => method === 'HEAD'
