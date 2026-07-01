5df8a259cb07d640b9b02ea201957f03870cd02f
CorrectlyObserve <-g>
2026-07-01 18:50:39 +0700

    feat: define getByteLength function

--- packages/router/rpc/helpers.ts
+++ packages/router/rpc/helpers.ts
@@ -1,5 +1,8 @@
 import type { Rpc } from './../types'
 
+const textEncoder: TextEncoder = new TextEncoder()
+export const getByteLength = (str: string): number => textEncoder.encode(str).byteLength
+
 export const isBodiless = (method: Rpc.Method | string): method is 'GET' | 'HEAD' =>
   method === 'GET' || isHeadMethod(method)
 
