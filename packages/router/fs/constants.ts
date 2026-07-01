3f0b8c16aa47aa68af291d5b1d990e33c7b23852
CorrectlyObserve <-g>
2026-07-01 18:47:30 +0700

    refactor: modify config

--- packages/router/fs/constants.ts
+++ packages/router/fs/constants.ts
@@ -3,7 +3,8 @@ export const BASE_DIR = './pages' as const
 export const config = {
   DIR: 'src/pages',
   OUTPUT: 'src/routes.gen.ts',
-  rpc: { BASE_PATH: '/_rpc', CLIENT: 'rpc.client.gen.ts', SERVER: 'rpc.server.gen.ts' }
+  RPC_CLIENT: 'rpc.client.gen.ts',
+  RPC_SERVER: 'rpc.server.gen.ts'
 } as const
 
 export const exitCodes = { SUCCESS: 0, FAILURE: 1 } as const
