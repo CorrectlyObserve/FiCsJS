b87eddebf3e690d8026a37915984efae81338fc2
CorrectlyObserve <-g>
2026-07-01 18:51:03 +0700

    feat: define RPC_BASE_PATH

--- packages/router/constants.ts
+++ packages/router/constants.ts
@@ -1,5 +1,6 @@
 export const FICS_NAVIGATE = 'fics:navigate' as const
 
+export const RPC_BASE_PATH = '/_rpc' as const
 export const RPC_MODULE_TYPE = { module: 'rpc' } as const
 
 export const statusCodes = {
