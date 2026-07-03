bea02fc3e5f84639987ec8249fc8745c00622f79
CorrectlyObserve <-g>
2026-07-03 16:40:15 +0700

    feat: define FiCsRpcServer as FiCsRpc

--- packages/router/serverOnly.ts
+++ packages/router/serverOnly.ts
@@ -3,5 +3,5 @@ import { isBrowser } from './../core/helpers/browser'
 if (isBrowser()) throw new Error('Please import only its types on the client...')
 
 export { registerPages } from './host'
+export type { FiCsRouting, FiCsRpcServer as FiCsRpc } from './namespaces'
 export { initRpc, createRpcHandler, mutationOnly, RpcError } from './rpc'
-export type { Routing, Rpc } from './types'
