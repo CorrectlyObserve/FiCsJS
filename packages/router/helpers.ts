18efa3e31883bbe78464b3b1582b9d8486bce535
CorrectlyObserve <-g>
2026-07-03 16:36:46 +0700

    feat: define isDynamicPath function

--- packages/router/helpers.ts
+++ packages/router/helpers.ts
@@ -2,3 +2,5 @@ import { isObject } from './../core/helpers'
 
 export const hasMethod = <T>(value: unknown, key: string): value is T =>
   isObject(value) && typeof (value as { [key]?: unknown })[key] === 'function'
+
+export const isDynamicPath = (path: string): boolean => path.includes(':')
