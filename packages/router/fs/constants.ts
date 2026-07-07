3a79fe5a2e44498e8d2a4a83dbf0045b6f9d8f06
CorrectlyObserve <-g>
2026-07-07 09:05:18 +0700

    feat: define REDIRECT_PATH

--- packages/router/fs/constants.ts
+++ packages/router/fs/constants.ts
@@ -22,7 +22,10 @@ export const fileNames = {
 
 export const MODULE_EXT_REGEX: RegExp = /\.(?:tsx?|jsx?|mts|mjs|cts|cjs)$/
 
-export const routerImport = (path: string = '') => `'ficsjs/router${path ? `/${path}` : ''}'` as const
+export const REDIRECT_PATH = '__redirect' as const
+
+export const routerImport = (path: string = '') =>
+  `'ficsjs/router${path ? `/${path}` : ''}'` as const
 
 export const segments = {
   CATCH_ALL: /^\[\.\.\.([^[\].]+)\]$/,
