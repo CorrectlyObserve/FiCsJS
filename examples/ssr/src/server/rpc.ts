e994508b9701d08bb2e2a84a37735df4e4ae559d
CorrectlyObserve <-g>
2026-07-14 10:46:19 +0700

    refactor: remove type annotation

--- examples/ssr/src/server/rpc.ts
+++ examples/ssr/src/server/rpc.ts
@@ -1,4 +1,4 @@
 import { initRpc, RpcError, type FiCsRpc } from 'ficsjs/router/server-only'
 
-export const { defineProcedure } = initRpc<{ actor: string; requestId: string }>()
+export const { defineProcedure } = initRpc()
 export const rpcError = (init: FiCsRpc.ErrorInit): RpcError => new RpcError(init)
