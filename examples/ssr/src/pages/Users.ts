1e5a634782a97527aa79817c1134f5068a328694
CorrectlyObserve <-g>
2026-07-14 18:47:38 +0700

    refactor: prevent unnecessary props delivery

--- examples/ssr/src/pages/Users.ts
+++ examples/ssr/src/pages/Users.ts
@@ -35,7 +35,7 @@ export const USERS_KEY = ['users'] as const
 
 const props: FiCs.Props<Data, {}> = [
   {
-    descendants: ({ children: { button, draggable } }) => [button, draggable.getChildren().menu],
+    descendants: ({ children: { draggable } }) => draggable.getChildren().menu,
     values: ({ data: { userId } }) => ({ isDisabled: !Number.isInteger(userId) })
   },
   {
