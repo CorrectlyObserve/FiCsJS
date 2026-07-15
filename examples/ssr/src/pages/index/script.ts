1c289f59eb48d1499de300399ba5b6e6e895a7cb
CorrectlyObserve <-g>
2026-07-15 13:05:38 +0700

    refactor: apply moving a directory

--- examples/ssr/src/pages/index/script.ts
+++ examples/ssr/src/pages/index/script.ts
@@ -1,7 +1,7 @@
 import '@/globalCss'
 import ChatButton from '@/components/ChatButton'
 import Link from '@/components/Link'
-import Users from '@/pages/Users'
+import Users from '@/pages/index/Users'
 
 Link.describe()
 Users.describe()
