b21259be71765f2f00e0a157ecb1b7faecc99157
CorrectlyObserve <-g>
2026-07-20 11:19:05 +0700

    feat: import Router from routes.gen

--- examples/todo/main.ts
+++ examples/todo/main.ts
@@ -1,8 +1,8 @@
 import { configGlobalCss, configI18n, i18n } from 'ficsjs'
 import Header from '@/components/Header'
-import Router from '@/components/Router'
 import Footer from '@/components/Footer'
 import globalCss from '@/globalCss'
+import { Router } from '@/routes.gen'
 import { $lang } from '@/stores'
 import type { Lang } from '@/utils/lang'
 
