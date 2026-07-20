6541d0dfb75625f10844a00ad43c91978d122af8
CorrectlyObserve <-g>
2026-07-20 14:38:14 +0700

    refactor: standardize relative path

--- packages/router/rpc/index.ts
+++ packages/router/rpc/index.ts
@@ -1,4 +1,4 @@
-export { createRpcClient } from './client'
+export { configRpcClient, createRpcClient } from './client'
 export { RpcError } from './error'
 export { initRpc, mutationOnly } from './procedure'
 export { createRpcHandler } from './server'
