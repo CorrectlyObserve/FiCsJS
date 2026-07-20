a4d38f2473d78e1ba39f077ce506c61272a7b7d6
CorrectlyObserve <-g>
2026-07-20 14:43:09 +0700

    refactor: standardize relative path

--- packages/router/helpers.ts
+++ packages/router/helpers.ts
@@ -1,4 +1,4 @@
-import { isObject } from './../core/helpers'
+import { isObject } from '../core/helpers'
 
 export const hasMethod = <T>(value: unknown, key: string): value is T =>
   isObject(value) && typeof (value as { [key]?: unknown })[key] === 'function'
