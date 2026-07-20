de96b5205eb307ff20e85b647e44cf5748625859
CorrectlyObserve <-g>
2026-07-20 11:17:46 +0700

    feat: configure 404 redirect

--- examples/todo/src/pages/+page.ts
+++ examples/todo/src/pages/+page.ts
@@ -1,14 +1,16 @@
 import type { FiCsRouter } from 'ficsjs/router'
-import type { Data } from '@/components/Router'
+import type { Data } from '@/pages/+spa.config'
 import { measureOffsetWidth } from '@/utils/others'
 
 const page: FiCsRouter.Page<Data> = ({
-  children: { tasks, taskDetails },
+  children: { tasks, taskDetails, notFound },
   data: {
+    isNotFound,
     queries: { taskId }
   },
   template
 }) => {
+  if (isNotFound) return notFound
   if (taskId) return measureOffsetWidth() ? template`${tasks}${taskDetails}` : taskDetails
   return tasks
 }
