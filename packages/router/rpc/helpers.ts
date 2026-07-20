e591e0b0f61ff5c4ca56847df13da07871067b25
CorrectlyObserve <-g>
2026-07-20 14:37:46 +0700

    refactor: standardize relative path

--- packages/router/rpc/helpers.ts
+++ packages/router/rpc/helpers.ts
@@ -1,4 +1,4 @@
-import type { Rpc } from './../types'
+import type { Rpc } from '../types'
 
 const textEncoder: TextEncoder = new TextEncoder()
 export const getByteLength = (str: string): number => textEncoder.encode(str).byteLength
