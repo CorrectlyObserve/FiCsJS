29554f928117b6eb1f5a841d71a971968c355422
CorrectlyObserve <-g>
2026-07-20 11:16:33 +0700

    feat: configure 404 redirect

--- examples/todo/src/pages/[taskId]/+page.ts
+++ examples/todo/src/pages/[taskId]/+page.ts
@@ -1,6 +1,7 @@
 import type { FiCsRouter } from 'ficsjs/router'
-import type { Data } from '@/components/Router'
+import type { Data } from '@/pages/+spa.config'
 
-const page: FiCsRouter.Page<Data> = ({ children: { taskDetails } }) => taskDetails
+const page: FiCsRouter.Page<Data> = ({ children: { taskDetails, notFound }, data: { isNotFound } }) =>
+  isNotFound ? notFound : taskDetails
 
 export default page
