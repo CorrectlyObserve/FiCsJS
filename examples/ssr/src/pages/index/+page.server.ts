ebcb9985f4753ceb8fac8091f68d17477f062212
CorrectlyObserve <-g>
2026-07-20 11:28:47 +0700

    feat: define +page.server for server-side rendering

---
+++ examples/ssr/src/pages/index/+page.server.ts
@@ -0,0 +1,23 @@
+import { type QueryCache } from 'ficsjs'
+import ChatButton from '@/components/ChatButton'
+import Link from '@/components/Link'
+import Users, { USERS_KEY } from '@/pages/index/Users'
+import { getUsers } from '@/server/users'
+import type { User } from '@/types'
+
+export const meta = {
+  title: 'FiCsJS with Hono',
+  description: 'This is a simple example of FiCsJS with Hono in SSR.'
+}
+
+export default async ({ queryCache }: { queryCache: QueryCache }): Promise<string> => {
+  await queryCache.prefetch(USERS_KEY, async () => getUsers())
+  const users: User[] = queryCache.get<User[]>(USERS_KEY) ?? []
+
+  return `
+    ${Link.toString({ data: { href: '/scroll', text: 'Go to the scroll page' } })}
+    ${Link.toString({ data: { href: '/admin', text: 'Go to the admin area (sign in required)' } })}
+    ${Users.toString({ data: { users } })}
+    ${ChatButton.toString()}
+  `
+}
