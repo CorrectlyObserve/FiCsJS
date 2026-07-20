6f37991bbf1551d2db929a7341efb5b3c34dcfad
CorrectlyObserve <-g>
2026-07-20 11:26:20 +0700

    feat: abolish script.ts

--- examples/ssr/src/pages/index/+page.ts
+++ examples/ssr/src/pages/index/+page.ts
@@ -1,22 +1,8 @@
-import { type QueryCache } from 'ficsjs'
+import '@/globalCss'
 import ChatButton from '@/components/ChatButton'
 import Link from '@/components/Link'
-import Users, { USERS_KEY } from '@/pages/index/Users'
-import { getUsers } from '@/server/users'
-import type { User } from '@/types'
+import Users from '@/pages/index/Users'
 
-export const meta = {
-  title: 'FiCsJS with Hono',
-  description: 'This is a simple example of FiCsJS with Hono in SSR.'
-}
-
-export default async ({ queryCache }: { queryCache: QueryCache }): Promise<string> => {
-  await queryCache.prefetch(USERS_KEY, async () => getUsers())
-  const users: User[] = queryCache.get<User[]>(USERS_KEY) ?? []
-
-  return `
-    ${Link.toString({ data: { href: '/scroll', text: 'Go to the scroll page' } })}
-    ${Users.toString({ data: { users } })}
-    ${ChatButton.toString()}
-  `
-}
+Link.describe()
+Users.describe()
+ChatButton.describe()
--- examples/ssr/src/pages/index/script.ts
+++
@@ -1,8 +0,0 @@
-import '@/globalCss'
-import ChatButton from '@/components/ChatButton'
-import Link from '@/components/Link'
-import Users from '@/pages/index/Users'
-
-Link.describe()
-Users.describe()
-ChatButton.describe()
