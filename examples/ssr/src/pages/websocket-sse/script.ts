04f5119f4e93d716d81841380a5526d83608279d
CorrectlyObserve <-g>
2026-07-20 11:30:48 +0700

    feat: abolish script.ts

--- examples/ssr/src/pages/websocket-sse/script.ts
+++
@@ -1,8 +0,0 @@
-import '@/globalCss'
-import Link from '@/components/Link'
-import Tab from '@/pages/websocket-sse/Tab'
-import { WebsocketSse } from '@/routes.gen'
-
-Link.describe()
-Tab.describe()
-WebsocketSse.describe()
