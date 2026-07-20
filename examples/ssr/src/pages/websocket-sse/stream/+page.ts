85d988926376b5216b769c8420bce0ede1967471
CorrectlyObserve <-g>
2026-07-20 11:33:00 +0700

    feat: import Data type from +spa.config

--- examples/ssr/src/pages/websocket-sse/stream/+page.ts
+++ examples/ssr/src/pages/websocket-sse/stream/+page.ts
@@ -1,5 +1,5 @@
 import type { FiCsRouter } from 'ficsjs/router'
-import type { Data } from '@/pages/websocket-sse/+spa'
+import type { Data } from '@/pages/websocket-sse/+spa.config'
 
 const page: FiCsRouter.Page<Data> = ({ children: { stream } }) => stream
 export default page
