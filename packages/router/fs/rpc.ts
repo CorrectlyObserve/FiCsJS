97f07b7e9fadcb35b7ad85645fc1646dbf3263d8
CorrectlyObserve <-g>
2026-07-21 13:28:47 +0700

    feat: import joinArray function

--- packages/router/fs/rpc.ts
+++ packages/router/fs/rpc.ts
@@ -1,3 +1,4 @@
+import { joinArray } from '../../core/helpers/others'
 import { RPC_BASE_PATH } from '../constants'
 import type { Routing, Rpc, TypeNode } from '../types'
 import { COMMENT, fileNames, routerImport, segments } from './constants'
