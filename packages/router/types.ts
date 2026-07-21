a4844d189c7cc7c7310639aabd4ef7ad053aef12
CorrectlyObserve <-g>
2026-07-21 15:19:07 +0700

    refactor: abolish Options.Deny type in Routing namespace

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -142,11 +142,6 @@ export declare namespace Routing {
   }
 
   namespace Options {
-    interface Deny {
-      code?: number
-      redirect?: string
-    }
-
     interface Generate {
       baseDir?: string
       pageFile?: string
