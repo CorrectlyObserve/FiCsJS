0ee5b7c563f3ab4ca987569329d81f32595fc729
CorrectlyObserve <-g>
2026-07-21 11:35:34 +0700

    refactor: make RpcError class robust

--- packages/router/rpc/error.ts
+++ packages/router/rpc/error.ts
@@ -1,17 +1,21 @@
 import { statusCodes } from '../constants'
-import { Rpc } from '../types'
+import type { Rpc } from '../types'
 
 export class RpcError extends Error {
   readonly code: string
   readonly status?: number
   readonly expose: boolean
+  readonly redirect?: string
 
-  constructor({ code, message, expose }: Rpc.ErrorInit) {
+  constructor({ code, message, expose, redirect }: Rpc.ErrorInit) {
     super(message)
     this.name = 'RpcError'
-    this.code = code.toLocaleLowerCase()
+    this.code = code.toUpperCase()
+
     this.status =
       this.code in statusCodes ? statusCodes[this.code as keyof typeof statusCodes] : undefined
+
     this.expose = expose ?? true
+    this.redirect = redirect
   }
 }
