1648238c4b70ba355c2964ff872a263aee22c16c
CorrectlyObserve <-g>
2026-07-22 11:00:52 +0700

    feat: apply modification of Rpc.ErrorInit type

--- packages/router/rpc/error.ts
+++ packages/router/rpc/error.ts
@@ -7,13 +7,14 @@ export class RpcError extends Error {
   readonly expose: boolean
   readonly redirect?: string
 
-  constructor({ code, message, expose, redirect }: Rpc.ErrorInit) {
+  constructor({ code, message, status, expose, redirect }: Rpc.ErrorInit) {
     super(message)
     this.name = 'RpcError'
     this.code = code.toUpperCase()
 
     this.status =
-      this.code in statusCodes ? statusCodes[this.code as keyof typeof statusCodes] : undefined
+      status ??
+      (this.code in statusCodes ? statusCodes[this.code as keyof typeof statusCodes] : undefined)
 
     this.expose = expose ?? true
     this.redirect = redirect
