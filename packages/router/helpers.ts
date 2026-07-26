5d1c27585b3797f1bc95496019ec5379271a4fad
CorrectlyObserve <-g>
2026-07-26 14:21:14 +0700

    refactor: move isBodiless and isHeadMethod function

--- packages/router/helpers.ts
+++ packages/router/helpers.ts
@@ -1,8 +1,14 @@
 import { isObject } from '../core/helpers'
+import { Rpc } from './types'
 
 export const hasMethod = <T>(value: unknown, key: string): value is T =>
   isObject(value) && typeof (value as { [key]?: unknown })[key] === 'function'
 
+export const isBodiless = (method: Rpc.Method | string): method is 'GET' | 'HEAD' =>
+  method === 'GET' || isHeadMethod(method)
+
 export const isDynamicPath = (path: string): boolean => path.includes(':')
+
+export const isHeadMethod = (method: Rpc.Method | string): method is 'HEAD' => method === 'HEAD'
 
 export const prependSlash = (path: string): string => (path.startsWith('/') ? path : `/${path}`)
--- packages/router/rpc/helpers.ts
+++ packages/router/rpc/helpers.ts
@@ -1,9 +1,3 @@
-import type { Rpc } from '../types'
-
 const textEncoder: TextEncoder = new TextEncoder()
+
 export const getByteLength = (str: string): number => textEncoder.encode(str).byteLength
-
-export const isBodiless = (method: Rpc.Method | string): method is 'GET' | 'HEAD' =>
-  method === 'GET' || isHeadMethod(method)
-
-export const isHeadMethod = (method: Rpc.Method | string): method is 'HEAD' => method === 'HEAD'
