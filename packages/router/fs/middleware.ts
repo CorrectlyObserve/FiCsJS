c492b8b717acec5f364c973264dadc99f7d2a377
CorrectlyObserve <-g>
2026-07-26 20:49:25 +0700

    refactor: abolish reverse option

--- packages/router/fs/middleware.ts
+++ packages/router/fs/middleware.ts
@@ -1,20 +1,15 @@
-export const getAllMiddlewares = ({
-  dirs,
-  mws,
-  reverse
-}: {
-  dirs: string[]
-  mws: Map<string, string>
-  reverse?: boolean
-}): string[] => {
+export const getAllMiddlewares = (
+  dirs: string[],
+  middlewareFiles: Map<string, string>
+): string[] => {
   const chain: string[] = []
 
   for (let i = 0; i <= dirs.length; i++) {
     const current: string = dirs.slice(0, i).join('/'),
-      mw: string | undefined = mws.get(current)
+      mw: string | undefined = middlewareFiles.get(current)
 
     if (mw) chain.push(mw)
   }
 
-  return reverse ? chain.reverse() : chain
+  return chain
 }
