1757b016c9ef8fb7a39a524d3f28dc537270d4fd
CorrectlyObserve <-g>
2026-07-27 11:48:31 +0700

    feat: export FiCsPage type instead of FiCsHost type and createPageHandler function

--- packages/router/serverOnly.ts
+++ packages/router/serverOnly.ts
@@ -2,6 +2,6 @@ import { isBrowser } from '../core/helpers/browser'
 
 if (isBrowser()) throw new Error('Please import only its types on the client...')
 
-export { registerPages } from './host'
-export type { FiCsHost, FiCsRpcServer as FiCsRpc } from './namespaces'
+export type { FiCsPage, FiCsRpcServer as FiCsRpc } from './namespaces'
+export { createPageHandler } from './pages'
 export { initRpc, createRpcHandler, mutationOnly, RpcError } from './rpc'
