c5bafdc4e4bfca1d7fabc1d6c74aeaf97ad11fad
CorrectlyObserve <-g>
2026-07-27 10:22:59 +0700

    feat: delete status property from RpcError class

--- packages/router/rpc/error.ts
+++ packages/router/rpc/error.ts
@@ -1,21 +1,14 @@
-import { statusCodes } from '../constants'
 import type { Rpc } from '../types'
 
 export class RpcError extends Error {
-  readonly code: string
-  readonly status?: number
+  readonly code: string | number
   readonly expose: boolean
   readonly redirect?: string
 
-  constructor({ code, message, status, expose, redirect }: Rpc.ErrorInit) {
+  constructor({ code, message, expose, redirect }: Rpc.ErrorInit) {
     super(message)
     this.name = 'RpcError'
-    this.code = code.toUpperCase()
-
-    this.status =
-      status ??
-      (this.code in statusCodes ? statusCodes[this.code as keyof typeof statusCodes] : undefined)
-
+    this.code = typeof code === 'number' ? code : code.trim().toLowerCase()
     this.expose = expose ?? true
     this.redirect = redirect
   }
