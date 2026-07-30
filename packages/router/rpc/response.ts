39dfae3500c5fa6cf0476c1f741d3add196d8f6d
CorrectlyObserve <-g>
2026-07-30 19:44:02 +0700

    feat: apply modificaion of RpcError class

--- packages/router/rpc/response.ts
+++ packages/router/rpc/response.ts
@@ -60,11 +60,10 @@ export const errorResponse = <C = unknown>({
     stage
   })
 
+  /** @remarks Always a status error, as denials are handled in middleware. */
   if (error instanceof RpcError) {
-    const { code, message, expose, redirect }: RpcError = error
-    return typeof code === 'number'
-      ? denialResponse({ code, method, redirect })
-      : response({ code, error: expose ? message : true })
+    const { code, message, expose }: RpcError = error
+    return response({ code, error: expose ? message : true })
   }
 
   onError?.(error, { path, req })
