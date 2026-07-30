270c947022ac071052e9a72c7d5792a916e111d8
CorrectlyObserve <-g>
2026-07-30 14:12:09 +0700

    feat: define DENIED_HEADER

--- packages/router/rpc/constants.ts
+++ packages/router/rpc/constants.ts
@@ -3,17 +3,19 @@ import { Rpc } from '../types'
 
 export const CONTENT_LENGTH = 'content-length' as const
 
-export const metricReasons = {
-  NOT_FOUND: 'not-found',
-  BAD_REQUEST: 'bad-request',
-  PAYLOAD_TOO_LARGE: 'payload-too-large'
-} as const satisfies Record<string, Extract<Rpc.Metric.Payload, { type: 'reject' }>['reason']>
-
 export const DEFAULT_ERRORS: Partial<Record<keyof typeof statusCodes, string>> = {
   BAD_REQUEST: 'The request is invalid or malformed...',
   PAYLOAD_TOO_LARGE: 'The request body is too large...',
   INTERNAL_SERVER_ERROR: 'The server encountered an unexpected error...'
 } as const
+
+export const DENIED_HEADER = 'fics-denied' as const
+
+export const metricReasons = {
+  NOT_FOUND: 'not-found',
+  BAD_REQUEST: 'bad-request',
+  PAYLOAD_TOO_LARGE: 'payload-too-large'
+} as const satisfies Record<string, Extract<Rpc.Metric.Payload, { type: 'reject' }>['reason']>
 
 export const RESERVED_KEYS: ReadonlySet<string> = new Set(['__proto__', 'prototype', 'constructor'])
 
