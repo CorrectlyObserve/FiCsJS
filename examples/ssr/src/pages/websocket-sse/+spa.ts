8c069ace0365774b68258b2e63bf809ce778821c
CorrectlyObserve <-g>
2026-07-30 20:18:14 +0700

    feat: modify import path of WebsocketSseRouter

--- examples/ssr/src/pages/websocket-sse/+spa.ts
+++ examples/ssr/src/pages/websocket-sse/+spa.ts
@@ -1,7 +1,7 @@
 import '@/globalCss'
+import { WebsocketSseRouter } from '@fics/routing/client'
 import Link from '@/components/Link'
 import Tab from '@/pages/websocket-sse/Tab'
-import { WebsocketSseRouter } from '@/routes.gen'
 
 Link.describe()
 Tab.describe()
