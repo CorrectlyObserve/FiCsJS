9e510d7b121dc3d60c315705a0200fe68077487b
CorrectlyObserve <-g>
2026-08-03 15:56:36 +0700

    feat: define SCRIPT_BASE

--- packages/router/constants.ts
+++ packages/router/constants.ts
@@ -11,6 +11,7 @@ export const FICS_NAVIGATE = 'fics:navigate' as const
 
 export const RPC_BASE_PATH = '/_rpc' as const
 export const RPC_MODULE_TYPE = { module: 'rpc' } as const
+export const SCRIPT_BASE = '/dist' as const
 
 export const statusCodes = {
   BAD_REQUEST,
