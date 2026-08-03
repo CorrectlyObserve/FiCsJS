95f1603746faecc3beb1318ce01109f4de4835fe
CorrectlyObserve <-g>
2026-08-03 13:19:07 +0700

    refactor: destructure constants

--- packages/persistent-state/constants.ts
+++ packages/persistent-state/constants.ts
@@ -1,8 +1,6 @@
-export const constants = {
-  COMPOSITE_ID_INDEX: 'compositeId',
-  SNAPSHOT_ID_INDEX: 'snapshotId',
-  SNAPSHOT_STORE: 'snapshots',
-  STATE_ID_INDEX: 'stateId',
-  STATE_STORE: 'states',
-  VERSION: 1
-} as const
+export const COMPOSITE_ID_INDEX = 'compositeId' as const
+export const SNAPSHOT_ID_INDEX = 'snapshotId' as const
+export const SNAPSHOT_STORE = 'snapshots' as const
+export const STATE_ID_INDEX = 'stateId' as const
+export const STATE_STORE = 'states' as const
+export const VERSION = 1 as const
