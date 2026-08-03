d11436174b3bed572fb27cfb3090e6a0eac80b80
CorrectlyObserve <-g>
2026-08-03 13:35:03 +0700

    refactor: destructure constants

--- packages/core/scroll/constants.ts
+++ packages/core/scroll/constants.ts
@@ -1,7 +1,5 @@
-export const constants = {
-  CACHE_LENGTH: 200,
-  FRAME_INTERVAL_MS: 16,
-  SIZE_DELTA_TOLERANCE_PX: 1,
-  SMOOTHING_FACTOR: 0.2,
-  THRESHOLD_RATIO: 0
-} as const
+export const CACHE_LENGTH = 200 as const
+export const FRAME_INTERVAL_MS = 16 as const
+export const SIZE_DELTA_TOLERANCE_PX = 1 as const
+export const SMOOTHING_FACTOR = 0.2 as const
+export const THRESHOLD_RATIO = 0 as const
