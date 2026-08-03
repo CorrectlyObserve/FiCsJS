29d519d0f1cd6e7d40a8349c88ef46aa45448f09
CorrectlyObserve <-g>
2026-08-03 13:26:47 +0700

    refactor: apply destructured constants

--- packages/core/helpers/index.ts
+++ packages/core/helpers/index.ts
@@ -1,10 +1,20 @@
-import { APPLICATION_JSON, CONTENT_TYPE, constants, NOOP } from './constants'
+import { statusCodes } from './constants'
 export const {
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR,
+  NO_CONTENT,
+  REQUEST_TIMEOUT,
+  TOO_MANY_REQUESTS
+} = statusCodes
+
+export {
+  APPLICATION_JSON,
+  CONTENT_TYPE,
+  EVENT_STREAM,
   MAX_DELAY_MS,
   MAX_RETRIES,
-  statusCode: { BAD_REQUEST, INTERNAL_SERVER_ERROR, NO_CONTENT, REQUEST_TIMEOUT, TOO_MANY_REQUESTS }
-} = constants
-export { APPLICATION_JSON, CONTENT_TYPE, NOOP }
+  NOOP
+} from './constants'
 
 export { forwardAbort, isClientTermination, onAbort, scheduleAbort } from './abort'
 export { browserError, isBrowser } from './browser'
