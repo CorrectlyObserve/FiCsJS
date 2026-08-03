05349aebc6521f932978f1086db2e32c004f528b
CorrectlyObserve <-g>
2026-08-04 00:22:21 +0700

    feat: define ResolvedPages type in Routing namespace

--- packages/router/types.ts
+++ packages/router/types.ts
@@ -196,6 +196,12 @@ export declare namespace Routing {
     script: string
   }
 
+  interface ResolvedPages<C = Record<string, unknown>> {
+    statics: Map<string, ResolvedRoute<C>>
+    dynamics: ({ regex: RegExp } & ResolvedRoute<C>)[]
+    redirects: RedirectFn | Map<string, string> | undefined
+  }
+
   interface ResolvedRoute<C = Record<string, unknown>> extends ServerStatus<C> {
     path: string
     middlewares: readonly Middleware<C>[]
