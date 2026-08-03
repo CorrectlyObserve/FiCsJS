14c23c12adebe28e1a8fbedcb87272de947947ae
CorrectlyObserve <-g>
2026-08-03 13:38:18 +0700

    refactor: destructure constants

--- packages/core/template/constants.ts
+++ packages/core/template/constants.ts
@@ -1,22 +1,22 @@
-export const constants = {
-  char: {
-    COMMENT_OPEN_TAG: '<!--',
-    COMMENT_CLOSE_TAG: '-->',
-    DOUBLE_QUOTE: '"',
-    EQUAL_SIGN: '=',
-    LEFT_ANGLE_BRACKET: '<',
-    RIGHT_ANGLE_BRACKET: '>',
-    SINGLE_QUOTE: "'"
-  },
-  DISPLAY_NONE: 'display:none;',
-  regExp: {
-    ATTR: (attr: string) => new RegExp(`(?:\\s+)${attr}(?=(\\s|>|/))`, 'g'),
-    CONTROL_CHAR: /[\u0000-\u001F\u007F-\u009F]/u,
-    DISPLAY: /(^|;)(\s*)display\s*:[^;]*(?=;|$)/i,
+export const char = {
+  COMMENT_OPEN_TAG: '<!--',
+  COMMENT_CLOSE_TAG: '-->',
+  DOUBLE_QUOTE: '"',
+  EQUAL_SIGN: '=',
+  LEFT_ANGLE_BRACKET: '<',
+  RIGHT_ANGLE_BRACKET: '>',
+  SINGLE_QUOTE: "'"
+} as const
 
-    INVALID_ATTR_FRAGMENT: /["'<>\/=`]/,
-    INVALID_UNQUOTED_ATTR_VALUE: /["'<>`]/,
-    STYLE: /\sstyle\s*=\s*(["'])([\s\S]*?)\1/i,
-    TAG_END: /(\s*\/?>)$/
-  }
+export const DISPLAY_NONE = 'display:none;' as const
+
+export const regExp = {
+  ATTR: (attr: string) => new RegExp(`(?:\\s+)${attr}(?=(\\s|>|/))`, 'g'),
+  CONTROL_CHAR: /[\u0000-\u001F\u007F-\u009F]/u,
+  DISPLAY: /(^|;)(\s*)display\s*:[^;]*(?=;|$)/i,
+
+  INVALID_ATTR_FRAGMENT: /["'<>\/=`]/,
+  INVALID_UNQUOTED_ATTR_VALUE: /["'<>`]/,
+  STYLE: /\sstyle\s*=\s*(["'])([\s\S]*?)\1/i,
+  TAG_END: /(\s*\/?>)$/
 } as const
