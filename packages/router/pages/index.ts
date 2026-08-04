da34a4c78179c18dd9dd61fe7551f3d899760eff
CorrectlyObserve <-g>
2026-08-04 10:52:08 +0700

    refactor: integrate resolvePages function to createPageHandler function

--- packages/router/pages/index.ts
+++ packages/router/pages/index.ts
@@ -1,22 +1,49 @@
-import { SCRIPT_BASE } from '../constants'
-import { isHeadMethod } from '../helpers'
+import { removeTrailingSlash, typedEntries } from '../../core/helpers'
+import { dynamicPathToRegex } from '../dynamicPaths'
+import { isDynamicPath, isHeadMethod } from '../helpers'
+import { applyLayout } from '../layout'
 import type { Routing } from '../types'
-import { dispatch, resolvePages } from './dispatch'
+import { dispatch } from './dispatch'
 
 export const createPageHandler = <C extends Record<string, unknown>>(
   { routes, middlewares = {}, statusPages = {}, redirects }: Routing.Options.PageManifest<C>,
-  { render, createContext, scriptBase = SCRIPT_BASE }: Routing.Options.PageHost<C>
+  { render, createContext, scriptBase }: Routing.Options.PageHost<C>
 ): ((req: Request) => Promise<Response>) => {
-  const pages: Routing.ResolvedPages<C> = resolvePages({ routes, middlewares, redirects })
+  const statics: Map<string, Routing.ResolvedRoute<C>> = new Map(),
+    dynamics: ({ regex: RegExp } & Routing.ResolvedRoute<C>)[] = []
 
+  for (const { path, page, entry, layout } of routes) {
+    const resolved: Routing.ResolvedRoute<C> = {
+      path,
+      module: applyLayout({ layout: layout as Routing.ServerModule<C>, page }),
+      entry,
+      middlewares: middlewares[path] ?? []
+    }
+
+    if (isDynamicPath(path)) dynamics.push({ regex: dynamicPathToRegex(path), ...resolved })
+    else statics.set(path, resolved)
+  }
+
+  let _redirects: Routing.Redirects | undefined
+  if (redirects)
+    _redirects =
+      typeof redirects === 'function'
+        ? redirects
+        : new Map(
+            typedEntries(redirects).map(([from, to]) => [removeTrailingSlash(from) || '/', to])
+          )
+
+  const sb: string = removeTrailingSlash(scriptBase ?? '/dist')
   return async (req: Request): Promise<Response> => {
     const res: Response = await dispatch({
       req,
+      statics,
+      dynamics,
+      redirects: _redirects,
       statusPages,
       render,
       createContext,
-      scriptBase,
-      ...pages
+      scriptBase: sb
     })
 
     if (isHeadMethod(req.method))
