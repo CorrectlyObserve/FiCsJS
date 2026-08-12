9e0b50368bf4004d1854895a5b17a4718ad952f9
CorrectlyObserve <-g>
2026-08-13 00:09:13 +0700

    feat: define 400 error page for SSR

---
+++ examples/ssr/src/pages/+404.server.ts
@@ -0,0 +1,8 @@
+import StatusError from '@/pages/StatusError'
+
+export const meta = {
+  title: '404 Not Found',
+  description: 'The page you requested could not be found.'
+}
+
+export default (): string => StatusError.toString({ data: { description: meta.description } })
