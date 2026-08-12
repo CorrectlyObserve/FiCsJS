ddea239f1510ff2e840826a15f94b22f9a8e1a44
CorrectlyObserve <-g>
2026-08-13 00:16:45 +0700

    fix: resolve Biome error

--- examples/todo/src/pages/[taskId]/+page.ts
+++ examples/todo/src/pages/[taskId]/+page.ts
@@ -1,7 +1,9 @@
 import type { FiCsRouter } from 'ficsjs/router'
 import type { Data } from '@/pages/+spa.config'
 
-const page: FiCsRouter.Page<Data> = ({ children: { taskDetails, notFound }, data: { isNotFound } }) =>
-  isNotFound ? notFound : taskDetails
+const page: FiCsRouter.Page<Data> = ({
+  children: { taskDetails, notFound },
+  data: { isNotFound }
+}) => (isNotFound ? notFound : taskDetails)
 
 export default page
