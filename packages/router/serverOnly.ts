86aa56c5c6c5a3fa6c13ad53e439125f3e84e25b
CorrectlyObserve <-g>
2026-08-13 10:38:46 +0700

    refactor: export FiCsServerRouter namespace and FiCsServerRpc namespace

--- packages/router/serverOnly.ts
+++ packages/router/serverOnly.ts
@@ -2,6 +2,6 @@ import { isBrowser } from '../core/helpers/browser'
 
 if (isBrowser()) throw new Error('Please import only its types on the client...')
 
-export type { FiCsPage, FiCsRpcServer as FiCsRpc } from './namespaces'
+export type { FiCsServerRouter as FiCsRouter, FiCsServerRpc as FiCsRpc } from './namespaces'
 export { createPageHandler } from './pages'
 export { initRpc, createRpcHandler, mutationOnly, RpcError } from './rpc'
