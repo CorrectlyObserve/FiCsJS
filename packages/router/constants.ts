65f6243c0124471f64667796ac102baa64b88bf9
CorrectlyObserve <-g>
2026-08-13 11:43:20 +0700

    feat: define ROUTER_COMPONENT_NAME

--- packages/router/constants.ts
+++ packages/router/constants.ts
@@ -8,6 +8,7 @@ import {
 
 export const FICS_META = 'fics:meta' as const
 export const FICS_NAVIGATE = 'fics:navigate' as const
+export const ROUTER_COMPONENT_NAME = '_router' as const
 export const RPC_BASE = '/_rpc' as const
 export const RPC_MODULE_TYPE = { module: 'rpc' } as const
 
