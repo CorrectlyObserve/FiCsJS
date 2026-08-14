7b71b6b7c0cddf1abd3abcf0538cbd0b4edce1f6
CorrectlyObserve <-g>
2026-08-14 10:30:43 +0700

    refactor: import Updated type

--- examples/ssr/src/pages/index/Users.ts
+++ examples/ssr/src/pages/index/Users.ts
@@ -2,7 +2,7 @@ import { fics, type FiCs } from 'ficsjs'
 import { flexCenter } from 'ficsjs/style'
 import { api } from '@fics/routing/client'
 import Button from '@/components/Button'
-import Draggable from '@/pages/index/Draggable'
+import Draggable, { type Updated } from '@/pages/index/Draggable'
 import UserContent from '@/pages/index/UserContent'
 import type { User } from '@/server/users'
 
