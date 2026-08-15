1ea681e33a1237d2a2340e39324ffd178b3fbefc
CorrectlyObserve <-g>
2026-08-15 18:51:42 +0700

    feat: add marge option to applyMeta function

--- packages/router/meta.ts
+++ packages/router/meta.ts
@@ -1,46 +1,53 @@
 import { escape, isBrowser, typedEntries } from '../core/helpers'
-import { FICS_META } from './constants'
 
-export const applyMeta = (meta?: Record<string, string>): void => {
+const defaultMeta: Map<string, string> = new Map(),
+  activeMetaKeys: Set<string> = new Set(),
+  getMetaConfig = (key: string): { tagName: string; nameAttr: string; valAttr: string } => {
+    if (key === 'canonical') return { tagName: 'link', nameAttr: 'rel', valAttr: 'href' }
+
+    return {
+      tagName: 'meta',
+      nameAttr: key.startsWith('og:') ? 'property' : 'name',
+      valAttr: 'content'
+    }
+  }
+
+export const applyMeta = (
+  meta?: Record<string, string>,
+  { merge }: { merge?: boolean } = {}
+): void => {
   if (!isBrowser()) return
 
-  const applied: Set<Element> = new Set()
+  for (const key of new Set([...Object.keys(meta ?? {}), ...(merge ? [] : activeMetaKeys)])) {
+    const value: string | undefined = meta?.[key]
 
-  if (meta)
-    for (const [key, value] of typedEntries(meta)) {
-      if (key === 'title') {
-        document.title = value
+    if (value === undefined) activeMetaKeys.delete(key)
+    else if (merge) defaultMeta.set(key, value)
+    else activeMetaKeys.add(key)
+
+    const target: string | undefined = value ?? defaultMeta.get(key)
+
+    if (key === 'title') {
+      if (target !== undefined) document.title = target
+    } else {
+      const { tagName, nameAttr, valAttr }: ReturnType<typeof getMetaConfig> = getMetaConfig(key)
+      let tag: Element | null = document.head.querySelector(
+        `${tagName}[${nameAttr}="${CSS.escape(key)}"]`
+      )
+
+      if (target === undefined) {
+        tag?.remove()
         continue
       }
 
-      const { tagName, nameAttr, valAttr } = getMetaConfig(key),
-        selector: string = `${tagName}[${nameAttr}="${key.replace(/["\\]/g, '\\$&')}"]`
-
-      let tag: Element | null = document.head.querySelector(selector)
-      if (!tag) {
+      if (tag === null) {
         tag = document.createElement(tagName)
         tag.setAttribute(nameAttr, key)
         document.head.append(tag)
       }
 
-      tag.setAttribute(valAttr, value)
-      tag.setAttribute(FICS_META, '')
-      applied.add(tag)
+      tag.setAttribute(valAttr, target)
     }
-
-  for (const tag of document.head.querySelectorAll(`[${CSS.escape(FICS_META)}]`))
-    if (!applied.has(tag)) tag.remove()
-}
-
-export const getMetaConfig = (
-  key: string
-): { tagName: string; nameAttr: string; valAttr: string } => {
-  if (key === 'canonical') return { tagName: 'link', nameAttr: 'rel', valAttr: 'href' }
-
-  return {
-    tagName: 'meta',
-    nameAttr: key.startsWith('og:') ? 'property' : 'name',
-    valAttr: 'content'
   }
 }
 
