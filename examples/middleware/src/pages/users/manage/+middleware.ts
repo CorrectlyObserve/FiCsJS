f0c8bcee1d83d6cf412dfe44ef5d9ea5479b186a
CorrectlyObserve <-g>
2026-08-17 10:34:12 +0700

    feat: define middleware for manage page

---
+++ examples/middleware/src/pages/users/manage/+middleware.ts
@@ -0,0 +1,10 @@
+import type { FiCsRouter } from 'ficsjs/router/server-only'
+import { requireUser } from '@/server/auth'
+import type { User } from '@/server/users'
+
+const middleware: FiCsRouter.Middleware = async ctx => {
+  const account: User | null = await requireUser(ctx)
+  if (account?.role !== 'admin') return ctx.deny()
+}
+
+export default middleware
