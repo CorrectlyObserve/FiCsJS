536479aec8399716e9d069459381e598d8f9a4a5
CorrectlyObserve <-g>
2026-08-19 13:47:19 +0700

    refactor: make RpcError class robust

--- packages/router/rpc/error.ts
+++ packages/router/rpc/error.ts
@@ -1,15 +1,15 @@
-import type { Rpc } from '../types'
+import type { Rpc, StatusCodes, TransportCodes } from '../types'
 
-export class RpcError extends Error {
-  readonly code: string
+export class RpcError<T extends StatusCodes | TransportCodes = StatusCodes> extends Error {
+  readonly code: StatusCodes | TransportCodes
   readonly denied: boolean
   readonly expose: boolean
   readonly redirect?: string
 
-  constructor({ code, message, denied, expose, redirect }: Rpc.ErrorInit) {
+  constructor({ code, message, denied, expose, redirect }: Rpc.ErrorInit<NoInfer<T>>) {
     super(message)
     this.name = 'RpcError'
-    this.code = code.trim().toLowerCase()
+    this.code = code
     this.denied = denied ?? false
     this.expose = expose ?? true
     this.redirect = redirect
