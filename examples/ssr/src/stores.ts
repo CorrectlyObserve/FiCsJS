09932385cd0845293d447a9207530e71a7289924
CorrectlyObserve <-g>
2026-08-19 10:22:44 +0700

    feat: define new state

--- examples/ssr/src/stores.ts
+++ examples/ssr/src/stores.ts
@@ -1,3 +1,10 @@
 import { createState } from 'ficsjs/state'
 
+export const $isHorizontal = createState<boolean>(false, {
+  sessionStorage: {
+    key: 'isHorizontal',
+    validate: (value): value is boolean => typeof value === 'boolean'
+  }
+})
+
 export const $userName = createState<string>('', { sessionStorage: { key: 'userName' } })
