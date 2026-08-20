ac2c29f45e5e7a9f2c6dc0279bf2fd1e37a272fe
CorrectlyObserve <-g>
2026-08-20 12:50:10 +0700

    refactor: make resolveModule function robust

--- packages/router/routeModule.ts
+++ packages/router/routeModule.ts
@@ -1,22 +1,33 @@
 import type { Page, PageContent, Routing } from './types'
 
-export function resolveModule(module: Routing.Module): PageContent
-export function resolveModule(module: Routing.Module, path: string): Page
-export function resolveModule(module: Routing.Module, path?: string): Page | PageContent {
-  const { default: def, redirect, meta }: Routing.Module = module,
-    pageContent: PageContent = {}
+export function resolveModule(module: Routing.Module, options: { path: string }): Page
+
+export function resolveModule(
+  module: Routing.Module,
+  options: { key: string; isOptional: boolean }
+): PageContent | undefined
+
+export function resolveModule(
+  { default: def, redirect, meta }: Routing.Module,
+  options: { path: string } | { key: string; isOptional: boolean }
+): Page | PageContent | undefined {
+  const pageContent: PageContent = {}
 
   if (typeof redirect === 'string') pageContent.redirect = redirect
   else {
-    if (def === undefined)
-      throw new Error(
-        `The ${path === undefined ? 'status module' : `module with path "${path}"`} must export default content or a "redirect" string...`
-      )
+    if (def === undefined) {
+      if ('isOptional' in options && options.isOptional) return undefined
+
+      const subject: string =
+        'path' in options ? `module with path "${options.path}"` : `status module "${options.key}"`
+
+      throw new Error(`The ${subject} must export default content or a "redirect" string...`)
+    }
 
     pageContent.content = (typeof def === 'function' ? def : () => def) as PageContent['content']
   }
 
   if (!pageContent.redirect && meta) pageContent.meta = meta
 
-  return path === undefined ? pageContent : { path, ...pageContent }
+  return 'path' in options ? { path: options.path, ...pageContent } : pageContent
 }
