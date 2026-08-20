d028a16d677b9a9d3858c7d31176b1b8a625276e
CorrectlyObserve <-g>
2026-08-20 11:07:37 +0700

    refactor: apply Routing.Status.DenialCode type

--- packages/router/middleware.ts
+++ packages/router/middleware.ts
@@ -2,8 +2,8 @@ import { denialCodes } from './constants'
 import type { Routing } from './types'
 
 export const deny = ({ code, redirect }: Partial<Routing.Denial> = {}): Routing.Denial => {
-  const codes: Routing.DenialCode[] = Object.values(denialCodes),
-    _code: Routing.DenialCode = code ?? denialCodes[redirect ? 'UNAUTHORIZED' : 'FORBIDDEN']
+  const codes: Routing.Status.DenialCode[] = Object.values(denialCodes),
+    _code: Routing.Status.DenialCode = code ?? denialCodes[redirect ? 'UNAUTHORIZED' : 'FORBIDDEN']
 
   if (!codes.includes(_code))
     throw new Error(`The middleware denial code ${_code} must be one of ${codes.join(', ')}...`)
