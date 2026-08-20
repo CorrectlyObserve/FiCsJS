aaa1b62097a4f619a8b85ec9428b133b32e69cd7
CorrectlyObserve <-g>
2026-08-20 17:22:28 +0700

    refactor: make buildSpa function robust

--- packages/router/fs/route/builder.ts
+++ packages/router/fs/route/builder.ts
@@ -176,6 +176,12 @@ export const buildSpa = ({
     areSpaEntry.push(isSpa && spaOwner === getDirName(src))
   }
 
+  for (const dir of dirs)
+    if (!spaOwners.includes(dir))
+      throw new Error(
+        `Please add at least a "${fileNames.PAGE}" file as the SPA "${prependSlash(dir)}" currently has no page...`
+      )
+
   return { dirs, files, spaAlias, configAlias, spaOwners, areSpaEntry }
 }
 
