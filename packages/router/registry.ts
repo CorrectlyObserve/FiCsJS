02a1c79a772f32f9a954e43005b68c937ec53dca
CorrectlyObserve <-g>
2026-08-20 17:14:15 +0700

    feat: define resolveStatus function instead of abolished resolveModule function

--- packages/router/registry.ts
+++ packages/router/registry.ts
@@ -32,11 +32,27 @@ export const resolveSpec = (spec?: Routing.Spec): Readonly<Routing.ResolvedSpec>
 
   const { routes, statusModules, statusFallback, inheritedStatusKeys = [] }: Routing.Spec = spec,
     pages: Page[] = [],
-    modules: Routing.ResolvedSpec['statusModules'] = {},
-    inherited: ReadonlySet<string> = new Set(inheritedStatuses ?? [])
+    modules: Routing.ResolvedSpec['statusModules'] = {}
 
-  for (const { path, page, layout } of routes)
-    pages.push(resolveModule(applyLayout({ layout, page }), { path }))
+  for (const { path, page, layout } of routes) {
+    const content: PageContent | undefined = resolveModule(applyLayout({ layout, page }))
+
+    if (!content)
+      throw new Error(
+        `The module with path "${path}" must export default content or a "redirect"...`
+      )
+
+    pages.push({ path, ...content })
+  }
+
+  const resolveStatus = (module: Routing.Module, key: string): PageContent | undefined => {
+    const content: PageContent | undefined = resolveModule(module)
+
+    if (!content && !inheritedStatusKeys.includes(key))
+      throw new Error(`The status module "${key}" must export default content or a "redirect"...`)
+
+    return content
+  }
 
   if (statusModules)
     for (const [key, module] of typedEntries(statusModules))
