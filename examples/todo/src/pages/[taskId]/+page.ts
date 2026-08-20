ee5eb57ded069138bbb0388695b259999a859906
CorrectlyObserve <-g>
2026-08-20 14:41:54 +0700

    feat: apply modification of RouterData type

--- examples/todo/src/pages/[taskId]/+page.ts
+++ examples/todo/src/pages/[taskId]/+page.ts
@@ -1,9 +1,6 @@
 import type { FiCsRouter } from 'ficsjs/router'
 import type { Data } from '@/pages/+spa.config'
 
-const page: FiCsRouter.Page<Data> = ({
-  children: { taskDetails, notFound },
-  data: { isNotFound }
-}) => (isNotFound ? notFound : taskDetails)
+const page: FiCsRouter.Page<Data> = ({ children: { taskDetails } }) => taskDetails
 
 export default page
