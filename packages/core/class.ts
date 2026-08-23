b4d1582d70e0d6c461e1c09f416dcc1fc506215d
CorrectlyObserve <-g>
2026-08-23 15:22:02 +0700

    feat: name an anonymous class

--- packages/core/class.ts
+++ packages/core/class.ts
@@ -1491,11 +1491,8 @@ export class FiCsElement<D extends object, P extends object> {
     browserError()
 
     const that: FiCsElement<D, P> = this,
-      { lazyLoad, rootMargin }: Options.Resolved<D, P> = that.#options
-
-    window.customElements.define(
-      that.#name,
-      class extends HTMLElement {
+      { lazyLoad, rootMargin }: Options.Resolved<D, P> = that.#options,
+      FiCsCustomElement = class extends HTMLElement {
         readonly #shadowRoot: ShadowRoot
         #isRendered: boolean = false
         #websocket?: WebSocketNS.Runtime
