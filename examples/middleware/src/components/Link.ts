45f78ee581703d8f86f1375f759c662aab622d81
CorrectlyObserve <-g>
2026-08-26 16:35:49 +0700

    feat: define Link component

---
+++ examples/middleware/src/components/Link.ts
@@ -0,0 +1,32 @@
+import { fics, type FiCs } from 'ficsjs'
+import { flexCenter, size } from 'ficsjs/style'
+
+interface Props {
+  href: string
+  text: string
+}
+
+const html: FiCs.Html<{}, Props> = ({ props: { href, text }, template }) => {
+  href = href.trim()
+
+  if (href === '') throw new Error('The "href" must be a non-empty string...')
+  return template`<p><a href="${href}">${text}</a></p>`
+}
+
+const css: FiCs.Css<{}, Props> = ({ cssToString }) => `
+  p {
+    text-align: center;
+
+    a {
+      ${cssToString(flexCenter('y', { inline: true }))}
+      text-decoration: underline;
+      text-decoration-thickness: 1px;
+      text-underline-offset: ${size(1)};
+      line-height: inherit;
+      &:visited { color: inherit; }
+      &:hover, &:focus, &:focus-visible { color: #8ac6ff; }
+    }
+  }
+`
+
+export default () => fics({ name: 'link', html, css })
