6f49b6442f692a4f9b919c363fc570267a7f6330
CorrectlyObserve <-g>
2026-08-26 11:20:30 +0700

    feat: apply cssToString function

--- packages/core/globalCss.ts
+++ packages/core/globalCss.ts
@@ -1,7 +1,9 @@
 import { FiCsElement } from './class'
+import { cssToString } from './css'
 import { toArray } from './helpers'
 import type { Css, SingleOrArray } from './types'
 
-export const configGlobalCss = (css: SingleOrArray<Css.Global>): void => {
+export const configGlobalCss = (css: SingleOrArray<Css.Global>): string => {
   FiCsElement.globalCss = toArray(css)
+  return cssToString(FiCsElement.globalCss)
 }
