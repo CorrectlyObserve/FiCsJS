eec4c7aad77142579b853c37cbd43332af1f524f
CorrectlyObserve <-g>
2026-08-28 09:35:57 +0700

    refactor: make codes short

--- examples/ssr/src/components/Button.ts
+++ examples/ssr/src/components/Button.ts
@@ -15,8 +15,8 @@ const html: FiCs.Html<{}, Props> = ({
 }) => template`
   <button
     class="clickable w-24 text-white border border-white p-3 rounded-lg"
-    ${!isBrowser || isDisabled ? 'disabled' : ''}
-    ${isCurrent ? 'aria-current="page"' : ''}
+    ${(!isBrowser || isDisabled) && 'disabled'}
+    ${isCurrent && 'aria-current="page"'}
     type="button"
   >${buttonText}</button>
 `
