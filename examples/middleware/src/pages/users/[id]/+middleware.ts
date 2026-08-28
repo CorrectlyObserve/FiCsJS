b8487d2345a4c7da6c21f5b656414f8987372618
CorrectlyObserve <-g>
2026-08-28 14:47:10 +0700

    refactor: abolish unncessary middleware

--- examples/middleware/src/pages/users/[id]/+middleware.ts
+++
@@ -1,8 +0,0 @@
-import type { FiCsRouter } from 'ficsjs/router/server-only'
-import { findUser } from '@/server/users'
-
-const middleware: FiCsRouter.Middleware = async ({ dynamicParams: { id }, deny }) => {
-  if (!findUser(Number(id))) return deny({ code: 404 })
-}
-
-export default middleware
