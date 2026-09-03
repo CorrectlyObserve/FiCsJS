892ab51c15faeaee4ceea17c00a28623041f620c
CorrectlyObserve <-g>
2026-09-03 16:57:30 +0700

    refactor: make codes robust

--- packages/core/helpers/numberError.ts
+++ packages/core/helpers/numberError.ts
@@ -15,22 +15,22 @@ export const numberError = (
   for (const [key, value] of typedEntries(numbers)) {
     if (value === undefined) continue
 
-    if (!Number.isFinite(value)) throw new Error(`The ${key} must be a number...`)
+    if (!Number.isFinite(value)) throw new RangeError(`The ${key} must be a number...`)
     if (condition === 'finite') continue
 
     if (condition === 'ratio' && (value < 0 || value > 1))
-      throw new Error(`The ${key} must be a number between 0 and 1...`)
+      throw new RangeError(`The ${key} must be a number between 0 and 1...`)
 
     if (condition === 'int' && !Number.isInteger(value))
-      throw new Error(`The ${key} must be an integer...`)
+      throw new RangeError(`The ${key} must be an integer...`)
 
     for (const remaining of ['positive', 'non-negative'] as const)
       if (condition.startsWith(remaining)) {
         if (value < 0 || (remaining === 'positive' && value === 0))
-          throw new Error(`The ${key} must be a ${remaining} number...`)
+          throw new RangeError(`The ${key} must be a ${remaining} number...`)
 
         if (condition === `${remaining}-int` && !Number.isInteger(value))
-          throw new Error(`The ${key} must be a ${remaining} integer...`)
+          throw new RangeError(`The ${key} must be a ${remaining} integer...`)
 
         continue
       }
