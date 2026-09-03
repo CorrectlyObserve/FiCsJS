68a12ea204a04f19ea2b4e87f00757143b5670b5
CorrectlyObserve <-g>
2026-09-03 10:28:24 +0700

    feat: apply cssDeclarations function

--- packages/animation/vibrate.ts
+++ packages/animation/vibrate.ts
@@ -1,4 +1,4 @@
-import { numberError } from '../core/helpers'
+import { cssDeclarations, numberError } from '../core/helpers'
 
 const translate3d = (x: number | string) => ({ transform: `translate3d(${x}, 0, 0)` }) as const
 
@@ -10,7 +10,7 @@ export const vibrate = (durationSec = 0.4, unit = 2) => {
   numberError({ durationSec }, 'positive')
   numberError({ unit }, 'positive-int')
 
-  return {
+  return cssDeclarations({
     animation: `vibrate ${durationSec}s cubic-bezier(0.36, 0.07, 0.19, 0.97) both`,
     '@keyframes vibrate': {
       '10%, 90%': translate3d(`-${unit}px`),
@@ -18,5 +18,5 @@ export const vibrate = (durationSec = 0.4, unit = 2) => {
       '30%, 50%, 70%': translate3d(`-${unit * 2}px`),
       '40%, 60%': translate3d(`${unit * 2}px`)
     }
-  } as const
+  } as const)
 }
