538fcd2f804e73b6f880ac0fb993c43ddd5b1fab
CorrectlyObserve <-g>
2026-09-03 10:28:14 +0700

    feat: apply cssDeclarations function

--- packages/animation/loading.ts
+++ packages/animation/loading.ts
@@ -1,4 +1,4 @@
-import { numberError } from '../core/helpers'
+import { cssDeclarations, numberError } from '../core/helpers'
 
 /**
  * @param durationSec Must be greater than 0. Defaults to `2`.
@@ -8,18 +8,18 @@ export const pulse = (durationSec = 2, opacity = 0.4) => {
   numberError({ durationSec }, 'positive')
   numberError({ opacity }, 'ratio')
 
-  return {
+  return cssDeclarations({
     animation: `pulse ${durationSec}s cubic-bezier(0.4, 0, 0.6, 1) infinite`,
     '@keyframes pulse': { '0%, 100%': { opacity: 1 }, '50%': { opacity } }
-  } as const
+  } as const)
 }
 
 /** @param durationSec Must be greater than 0. Defaults to `1.5`. */
 export const spin = (durationSec: number = 1.5) => {
   numberError({ durationSec }, 'positive')
 
-  return {
+  return cssDeclarations({
     animation: `spin ${durationSec}s infinite linear`,
     '@keyframes spin': { from: { transform: 'rotate(0deg)' }, to: { transform: 'rotate(360deg)' } }
-  } as const
+  } as const)
 }
