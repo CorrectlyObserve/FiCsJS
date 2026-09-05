f9ddd6eb216865b64dd2a834c48a87fb6ac308e9
CorrectlyObserve <-g>
2026-09-05 15:43:49 +0700

    design: delete unnecessary css

--- examples/middleware/src/components/Link.ts
+++ examples/middleware/src/components/Link.ts
@@ -1,6 +1,4 @@
 import { fics, type FiCs } from 'ficsjs'
-import { flexCenter, oklch, size } from 'ficsjs/style'
-import { black } from '@/utils/color'
 
 interface Props {
   href: string
@@ -14,17 +12,4 @@ const html: FiCs.Html<{}, Props> = ({ props: { href, text }, template }) => {
   return template`<p align="center"><a href="${href}">${text}</a></p>`
 }
 
-const css: FiCs.Css<{}, Props> = `
-  a {
-    ${flexCenter('y', { inline: true })}
-    text-decoration: underline;
-    text-decoration-thickness: 1px;
-    text-underline-offset: ${size(1)};
-    line-height: inherit;
-    &:visited { color: inherit; }
-    &:hover { background: ${oklch(black, { darker: 0.05 })}; cursor: pointer; }
-    &:hover, &:focus-visible { color: #8ac6ff; }
-  }
-`
-
-export default () => fics({ name: 'link', html, css })
+export default () => fics({ name: 'link', html })
