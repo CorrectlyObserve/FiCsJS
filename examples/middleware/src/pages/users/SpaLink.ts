83315b7c6640599285e9be41b2eb347fc6094881
CorrectlyObserve <-g>
2026-09-05 15:37:34 +0700

    feat: define Link component for SPA

---
+++ examples/middleware/src/pages/users/SpaLink.ts
@@ -0,0 +1,20 @@
+import { ficsLink, type FiCsLink } from 'ficsjs/router'
+import { size, textSize } from 'ficsjs/style'
+
+interface Props {
+  href: string
+  text: string
+}
+
+const href: FiCsLink.Href<Props> = ({ props: { href } }) => href
+const content: FiCsLink.Content<Props> = ({ props: { text }, template }) => template`${text}`
+
+const css: FiCsLink.Css<Props> = `
+  :host {
+    ${textSize('base')}
+    display: inline-block;
+    width: auto;
+  }
+`
+
+export default () => ficsLink<Props>({ href, content, css })
