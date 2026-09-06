c7b261dfba992aa518aebb4ded57f40aa57979b5
CorrectlyObserve <-g>
2026-09-06 16:08:48 +0700

    feat: apply abolishment of +spa.config

--- examples/todo/src/pages/[taskId]/+page.ts
+++ examples/todo/src/pages/[taskId]/+page.ts
@@ -1,5 +1,5 @@
 import type { FiCsRouter } from 'ficsjs/router'
-import type { Data } from '@/pages/+spa.config'
+import type { Data } from '@/pages/+spa'
 
 const page: FiCsRouter.Page<Data> = ({ children: { taskDetails } }) => taskDetails
 
