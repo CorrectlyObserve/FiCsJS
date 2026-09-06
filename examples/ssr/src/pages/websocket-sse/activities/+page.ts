6ecfefd7f2973789e535ff79b9c409bb81210f73
CorrectlyObserve <-g>
2026-09-06 16:10:36 +0700

    feat: apply abolishment of +spa.config

--- examples/ssr/src/pages/websocket-sse/activities/+page.ts
+++ examples/ssr/src/pages/websocket-sse/activities/+page.ts
@@ -1,5 +1,5 @@
 import type { FiCsRouter } from 'ficsjs/router'
-import type { Data } from '@/pages/websocket-sse/+spa.config'
+import type { Data } from '@/pages/websocket-sse/+spa'
 
 const page: FiCsRouter.Page<Data> = ({ data: { activities }, template }) => template`
   <h2 class="text-lg text-white text-center mb-6">Activities</h2>
