d9844532e263c05e1c649d8edba8b05edc47a93d
CorrectlyObserve <-g>
2026-09-06 16:10:17 +0700

    feat: apply abolishment of +spa.config

--- examples/ssr/src/pages/websocket-sse/+page.ts
+++ examples/ssr/src/pages/websocket-sse/+page.ts
@@ -1,5 +1,5 @@
 import type { FiCsRouter } from 'ficsjs/router'
-import type { Data } from '@/pages/websocket-sse/+spa.config'
+import type { Data } from '@/pages/websocket-sse/+spa'
 
 export const meta = {
   title: 'WebSocket and SSE',
