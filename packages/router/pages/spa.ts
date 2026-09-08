4752ec0882ed84804c7b34965bf0a7fd88dfd6b9
CorrectlyObserve <-g>
2026-09-08 15:38:02 +0700

    feat: abolish renderSpa function and define createSpaPage function

--- packages/router/pages/spa.ts
+++ packages/router/pages/spa.ts
@@ -1,18 +1,18 @@
-import type { FiCsElement } from '../core/class'
-import { normalizePath } from '../core/helpers'
-import type { RouterData, Routing } from './types'
+import type { FiCsElement } from '../../core/class'
+import { normalizePath } from '../../core/helpers'
+import type { RouterData, Routing } from '../types'
 
-export const renderSpa =
+export const createSpaPage =
   <D extends object, C extends Record<string, unknown>>(
     router: FiCsElement<RouterData<D>, {}>,
-    load?: Routing.SpaServerLoad<D, C>
+    spaServer?: Routing.SpaServer<D, C>
   ) =>
   async (ctx: Routing.MiddlewareCtx<C> & { status: Routing.Status.Resolved }): Promise<string> => {
     const { pathname, searchParams }: URL = new URL(ctx.req.url)
 
     return router.toString({
       data: {
-        ...(load ? await load(ctx) : {}),
+        ...(spaServer && (await spaServer(ctx))),
         pathname: normalizePath(pathname),
         queries: Object.fromEntries(searchParams)
       } as Partial<RouterData<D>>
