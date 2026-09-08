b5586eddbea9a2a3aa9963500a7322cc01cc4ca3
CorrectlyObserve <-g>
2026-09-08 08:53:23 +0700

    feat: apply modification of FiCs.GlobalCss type

--- packages/core/globalCss.ts
+++ packages/core/globalCss.ts
@@ -1,10 +1,8 @@
 import { FiCsElement } from './class'
 import { cssToString } from './css'
-import { toArray } from './helpers'
-import type { SingleOrArray } from './types'
 
-export const configGlobalCss = (css: SingleOrArray<string>): string => {
-  FiCsElement.globalCss = toArray(css)
+export const configGlobalCss = (css: string): string => {
+  FiCsElement.globalCss = css
   return getGlobalCss()
 }
 
