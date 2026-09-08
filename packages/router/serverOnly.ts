a2ed05233f6e3e65e924834236105953dc1fe3e7
CorrectlyObserve <-g>
2026-09-08 16:30:37 +0700

    feat: export createSpaPage function instead of renderSpa function

--- packages/router/serverOnly.ts
+++ packages/router/serverOnly.ts
@@ -3,6 +3,5 @@ import { isBrowser } from '../core/helpers/browser'
 if (isBrowser()) throw new Error('Please import only its types on the client...')
 
 export type { FiCsServerRouter as FiCsRouter, FiCsServerRpc as FiCsRpc } from './namespaces'
-export { createPageHandler } from './pages'
+export { createPageHandler, createSpaPage } from './pages'
 export { initRpc, createRpcHandler, mutationOnly, RpcError } from './rpc'
-export { renderSpa } from './spa'
