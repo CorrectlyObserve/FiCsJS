f88fa97971d91104c3ddf9b9b1402c793062640e
CorrectlyObserve <-g>
2026-09-08 09:29:11 +0700

    feat: apply modification of FiCs.GlobalCss type

--- examples/todo/src/globalCss.ts
+++ examples/todo/src/globalCss.ts
@@ -1,43 +1,54 @@
+import type { FiCs } from 'ficsjs'
 import { calc, cssVar, size, textSize } from 'ficsjs/style'
 import { breakpoints, white } from '@/utils/others'
 
 const outline = `${cssVar('outline')} solid ${white()}` as const
+const globalCss: FiCs.GlobalCss = `
+  * { padding: 0; margin: 0; box-sizing: border-box; }
 
-export default {
-  '*': { padding: 0, margin: 0, boxSizing: 'border-box' },
-  'h2, p, button, label, legend, input, textarea, span': { color: white() },
-  'h2, p, button': { textAlign: 'center' },
-  'p, label, legend, input, textarea': textSize('base'),
-  'button, input, textarea': {
-    transition: cssVar('transition'),
-    background: 'none',
-    border: 'none',
-    outline: 'none',
-    borderRadius: size(2),
-    '&:not([disabled])': {
-      '&:hover': { background: white(0.1), cursor: 'pointer' },
-      '&:focus-visible': { outline }
+  h2, p, button, label, legend, input, textarea, span { color: ${white()}; }
+  h2, p, button { text-align: center; }
+  p, label, legend, input, textarea { ${textSize('base')} }
+
+  button, input, textarea {
+    transition: ${cssVar('transition')};
+    background: none;
+    border: none;
+    outline: none;
+    border-radius: ${size(2)};
+
+    &:not([disabled]) {
+      &:hover { background: ${white(0.1)}; cursor: pointer; }
+      &:focus-visible { outline: ${outline}; }
     }
-  },
-  h2: {
-    ...textSize('xl'),
-    marginBlockEnd: size(8),
-    [`@media (max-width: ${breakpoints.SM})`]: { marginBlockEnd: size(6) }
-  },
-  'label:hover': { cursor: 'pointer' },
-  'input, textarea': {
-    minWidth: '20rem',
-    maxWidth: size(120 - 16),
-    paddingBlock: size(3),
-    paddingInline: size(4),
-    border: `${calc(`${size(1)} / 4`)} solid ${white()}`
-  },
-  a: {
-    transition: cssVar('transition'),
-    borderRadius: size(2),
-    '&:hover': { background: white(0.1), cursor: 'pointer' },
-    '&:focus-visible': { color: 'inherit', outline, outlineOffset: 0 },
-    '&:active': { scale: 0.98 }
-  },
-  'span[role="button"]': { padding: size(4) }
-}
+  }
+
+  h2 {
+    ${textSize('xl')}
+    margin-block-end: ${size(8)};
+
+    @media (max-width: ${breakpoints.SM}) { margin-block-end: ${size(6)}; }
+  }
+
+  label:hover { cursor: pointer; }
+
+  input, textarea {
+    max-width: ${size(120 - 16)};
+    padding-block: ${size(3)};
+    padding-inline: ${size(4)};
+    border: ${calc(`${size(1)} / 4`)} solid ${white()};
+  }
+
+  a {
+    transition: ${cssVar('transition')};
+    border-radius: ${size(2)};
+
+    &:hover { background: ${white(0.1)}; cursor: pointer; }
+    &:focus-visible { color: inherit; outline: ${outline}; outline-offset: 0; }
+    &:active { scale: 0.98; }
+  }
+
+  span[role="button"] { padding: ${size(4)}; }
+`
+
+export default globalCss
