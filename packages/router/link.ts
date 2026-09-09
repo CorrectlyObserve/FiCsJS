7a78310d2ae72168a755d3d53b3fb88e47a8a7ed
CorrectlyObserve <-g>
2026-09-09 16:12:55 +0700

    feat: apply CSS_LAYER

--- packages/router/link.ts
+++ packages/router/link.ts
@@ -1,5 +1,5 @@
 import { FiCsElement } from '../core/class'
-import { escape, isBlankString, typedEntries } from '../core/helpers'
+import { CSS_LAYER, escape, isBlankString, typedEntries } from '../core/helpers'
 import { LINK_COMPONENT_NAME } from './constants'
 import { goto } from './goto'
 import type { FiCsLink, Returned } from './types'
@@ -56,7 +56,7 @@ export const ficsLink = <P extends object>({
     },
     css: [
       `
-        @layer default {
+        ${CSS_LAYER} {
           :host {
             display: block;
             width: 100%;
