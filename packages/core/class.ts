6bb84a81181bf75a2c0e8a9d29bae61b161bf87f
CorrectlyObserve <-g>
2026-09-10 11:26:54 +0700

    feat: define childOwners as static property

--- packages/core/class.ts
+++ packages/core/class.ts
@@ -69,6 +69,7 @@ export class FiCsElement<D extends object, P extends object> {
   static #generator: Generator<number> = uid()
   static #nameGenerators: Map<string, Generator<number>> = new Map()
   static #activeEffect: { instance: Descendant; run: () => void } | null = null
+  static #childOwners: WeakMap<Descendant, string> = new WeakMap()
   static globalCss: string
   readonly #nameKey: string
   readonly #instanceId: string
