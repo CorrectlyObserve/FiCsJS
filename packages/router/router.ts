db30306113516cd78e73d030aed596afbf99641e
CorrectlyObserve <-g>
2026-09-11 14:26:47 +0700

    feat: apply HOST_SELECTOR

--- packages/router/router.ts
+++ packages/router/router.ts
@@ -1,5 +1,5 @@
 import { FiCsElement } from '../core/class'
-import { CSS_LAYER, NOOP, normalizePath, toArray } from '../core/helpers'
+import { CSS_LAYER, HOST_SELECTOR, NOOP, normalizePath, toArray } from '../core/helpers'
 import type { DeepReadonly, Html } from '../core/types'
 import {
   FICS_NAVIGATE,
@@ -220,7 +220,7 @@ export const ficsRouter = <D extends object>(
 
       return setContent()
     },
-    css: [`${CSS_LAYER}{:host{display:contents;}}`, ...toArray(css ?? [])],
+    css: [`${CSS_LAYER}{${HOST_SELECTOR}{display:contents;}}`, ...toArray(css ?? [])],
     hooks: {
       created: ({ data, ...args }) => {
         hooks?.created?.({ data, ...args })
