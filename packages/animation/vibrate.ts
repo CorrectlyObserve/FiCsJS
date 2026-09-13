99586368212b4a567c0e43a8cfbd2ac9dfc9a875
CorrectlyObserve <-g>
2026-09-13 18:11:17 +0700

    feat: apply VIBRATE_UNIT

--- packages/animation/vibrate.ts
+++ packages/animation/vibrate.ts
@@ -1,6 +1,5 @@
 import { cssObject, numberError } from '../core/helpers'
-
-const translate3d = (x: number | string) => ({ transform: `translate3d(${x}, 0, 0)` }) as const
+import { VIBRATE_UNIT } from './constants'
 
 /**
  * @param durationSec Must be greater than 0. Defaults to `0.4`.
@@ -12,11 +11,6 @@ export const vibrate = (durationSec = 0.4, unit = 2) => {
 
   return cssObject({
     animation: `vibrate ${durationSec}s cubic-bezier(0.36, 0.07, 0.19, 0.97) both`,
-    '@keyframes vibrate': {
-      '10%, 90%': translate3d(`-${unit}px`),
-      '20%, 80%': translate3d(`${unit}px`),
-      '30%, 50%, 70%': translate3d(`-${unit * 2}px`),
-      '40%, 60%': translate3d(`${unit * 2}px`)
-    }
+    [VIBRATE_UNIT]: `${unit}px`
   } as const)
 }
