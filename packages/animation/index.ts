ff714794cc99dfa326365858db60f2350d141ea7
CorrectlyObserve <-g>
2026-09-13 18:12:24 +0700

    feat: apply PULSE_OPACITY, VIBRATE_UNIT and addGlobalCss function

--- packages/animation/index.ts
+++ packages/animation/index.ts
@@ -1,3 +1,20 @@
+import { addGlobalCss } from '../core/css'
+import { PULSE_OPACITY, VIBRATE_UNIT } from './constants'
+
+const translate3d = (multiplier: number): string =>
+  `translate3d(calc(var(${VIBRATE_UNIT}) * ${multiplier}), 0, 0)`
+
+addGlobalCss(`
+  @keyframes pulse{0%,100%{opacity:1}50%{opacity:var(${PULSE_OPACITY})}}
+  @keyframes spin{from{transform:rotate(0deg)}to{transform:rotate(360deg)}}
+  @keyframes vibrate{
+    10%,90%{transform:${translate3d(-1)}}
+    20%,80%{transform:${translate3d(1)}}
+    30%,50%,70%{transform:${translate3d(-2)}}
+    40%,60%{transform:${translate3d(2)}}
+  }
+`)
+
 export { pulse, spin } from './loading'
 export { fade, float, slide, zoom } from './transition'
 export { vibrate } from './vibrate'
