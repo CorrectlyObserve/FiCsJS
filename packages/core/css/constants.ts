c63f470458540585e0784d860e59737f86f15138
CorrectlyObserve <-g>
2026-09-14 11:00:08 +0700

    feat: define COMMENT_TOKEN

--- packages/core/css/constants.ts
+++ packages/core/css/constants.ts
@@ -1,6 +1,8 @@
 export const AT_RULE = '@' as const
 export const BACKSLASH = '\\' as const
+
 export const BRACKET_PAIRS = { '(': ')', '{': '}' } as const
+export const COMMENT_TOKEN = { open: '/*', close: '*/' } as const
 
 export const GROUPING_AT_RULES = [
   '@container',
