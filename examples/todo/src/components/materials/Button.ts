aee01b42775e1f378c4c83852fd12181de26194d
CorrectlyObserve <-g>
2026-09-15 23:01:35 +0700

    refactor: apply renaming others.ts to style.ts

--- examples/todo/src/components/materials/Button.ts
+++ examples/todo/src/components/materials/Button.ts
@@ -1,6 +1,6 @@
 import { fics, type FiCs } from 'ficsjs'
 import { cssVar, size, textSize } from 'ficsjs/style'
-import { white } from '@/utils/others'
+import { white } from '@/utils/style'
 
 interface Props {
   isDisabled?: boolean
