9a2f444cb381e9c1d8fd5a45733bab81d27a5300
CorrectlyObserve <-g>
2026-09-15 22:58:27 +0700

    refactor: apply renaming others.ts to style.ts

--- examples/todo/src/components/Loading.ts
+++ examples/todo/src/components/Loading.ts
@@ -2,7 +2,7 @@ import { fics, type FiCs } from 'ficsjs'
 import { spin } from 'ficsjs/animation'
 import { forScreenReaders, rect, size } from 'ficsjs/style'
 import type { Lang } from '@/utils/lang'
-import { white } from '@/utils/others'
+import { white } from '@/utils/style'
 import { Loader } from 'lucide-static'
 
 interface Data {
