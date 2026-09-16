64d2781c6e28a62bd3382f9aad87f56d8f6576e5
CorrectlyObserve <-g>
2026-09-16 08:50:24 +0700

    refactor: apply modification of FiCs.Css type

--- examples/todo/src/components/materials/Button.ts
+++ examples/todo/src/components/materials/Button.ts
@@ -1,5 +1,5 @@
 import { fics, type FiCs } from 'ficsjs'
-import { cssVar, size, textSize } from 'ficsjs/style'
+import { cssVar, oklch, size, textSize } from 'ficsjs/style'
 import { white } from '@/utils/style'
 
 interface Props {
@@ -26,48 +26,63 @@ const html: FiCs.Html<{}, Props> = ({
   >${buttonText}</button>
 `
 
-const css: FiCs.Css<{}, Props> = {
-  ':host': ({ props: { fixedUnit } }) => ({
-    textAlign: 'center',
-    button: {
-      ...textSize('base', true),
-      ...(fixedUnit ? { width: size(fixedUnit) } : {}),
-      minWidth: size(16),
-      padding: size(4),
-      '&[disabled]': {
-        background: 'none !important',
-        color: white(0.2),
-        cursor: 'not-allowed'
-      },
-      '&:not([disabled]):active': { scale: 0.95 },
-      '&[data-type="gradation"]': {
-        position: 'relative',
-        background: cssVar('gradation'),
-        paddingInline: size(4),
-        overflow: 'hidden',
-        zIndex: 0,
-        '&::before': {
-          position: 'absolute',
-          content: "''",
-          inset: 0,
-          background: cssVar('black'),
-          opacity: 0,
-          transition: cssVar('transition'),
-          zIndex: -1
-        },
-        '&:not([disabled]):hover::before': { opacity: 0.5 }
-      },
-      '&[data-type="selected"]': {
-        color: cssVar('red'),
-        fontWeight: 'bold',
-        textDecoration: 'underline',
-        textUnderlineOffset: cssVar('outline')
-      },
-      '&[data-type="label"]': { paddingInline: cssVar('outline') },
-      '&[data-type="delete"]': { color: cssVar('red') }
+const css: FiCs.Css<{}, Props> = ({ props: { fixedUnit } }) => `
+  :host {
+    text-align: center;
+
+    button {
+      ${textSize('base', true)}
+      ${fixedUnit ? `width: ${size(fixedUnit)};` : ''}
+      min-width: ${size(16)};
+      padding: ${size(4)};
+
+      &[disabled] {
+        background: none !important;
+        color: ${white(0.2)};
+        cursor: not-allowed;
+      }
+
+      &:not([disabled]):active { scale: 0.95; }
+
+      &[data-type="gradation"] {
+        position: relative;
+        background: ${cssVar('gradation')};
+        padding-inline: ${size(4)};
+        overflow: hidden;
+        z-index: 0;
+
+        &::before {
+          position: absolute;
+          content: '';
+          inset: 0;
+          background: ${cssVar('black')};
+          opacity: 0;
+          transition: ${cssVar('transition')};
+          z-index: -1;
+        }
+
+        &:not([disabled]):hover::before { opacity: 0.5; }
+      }
+
+      &[data-type="selected"], &[data-type="delete"] {
+        color: ${cssVar('red')};
+        text-underline-offset: ${cssVar('outline')};
+
+        &:not([disabled]):hover {
+          background: ${oklch(cssVar('red'), { opacity: 0.3 })};
+          color: ${white()};
+        }
+      }
+
+      &[data-type="selected"] {
+        font-weight: bold;
+        text-decoration: underline;
+      }
+
+      &[data-type="label"] { padding-inline: ${cssVar('outline')}; }
     }
-  })
-}
+  }
+`
 
 const actions: FiCs.Actions<{}, Props> = {
   button: {
